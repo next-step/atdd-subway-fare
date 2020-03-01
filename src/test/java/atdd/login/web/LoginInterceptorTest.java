6457b97da644a76d6efe4bb2d02ae631445e1313
yhs <mvmaniaz@gmail.com>
2020-03-01 18:37:37 +0900

    feat(favorite): update loginInterceptorTest

--- src/test/java/atdd/login/web/LoginInterceptorTest.java
+++ src/test/java/atdd/login/web/LoginInterceptorTest.java
@@ -1,11 +1,41 @@
 package atdd.login.web;
 
+import atdd.member.web.LoginInterceptor;
+import atdd.security.BearerTokenExtractor;
+import atdd.security.JwtTokenProvider;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
+import static atdd.TestConstant.TEST_MEMBER_EMAIL;
+import static atdd.member.web.MemberController.MEMBER_URL;
+import static atdd.security.JwtTokenProvider.TOKEN_TYPE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = {LoginInterceptor.class, JwtTokenProvider.class, BearerTokenExtractor.class})
 public class LoginInterceptorTest {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
 
     @Test
     public void preHandle() {
-        // TODO: MockHttpServletRequest와 MockHttpServletResponse을 이용한 테스트 구현
+        String token = jwtTokenProvider.createToken(TEST_MEMBER_EMAIL);
+
+        MockHttpServletRequest request = new MockHttpServletRequest(HttpMethod.GET.name(), MEMBER_URL + "/me");
+        request.addHeader(HttpHeaders.AUTHORIZATION, TOKEN_TYPE + " " + token);
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        loginInterceptor.preHandle(request, response, null);
+
+        assertThat(request.getAttribute("loginUserEmail")).isEqualTo(TEST_MEMBER_EMAIL);
     }
+
 }
--- src/test/java/atdd/security/JwtTokenProviderTest.java
+++ src/test/java/atdd/security/JwtTokenProviderTest.java
@@ -1,6 +1,5 @@
 package atdd.security;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
