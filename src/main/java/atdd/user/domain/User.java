925aea79180b24400f8217743766b23c1d3c42bd
parkeeseul <eeseul.p@gmail.com>
2020-03-03 23:54:21 +0900

    feat : User 관련 기능 추가

--- src/main/java/atdd/user/domain/User.java
+++ src/main/java/atdd/user/domain/User.java
@@ -1,8 +1,14 @@
 package atdd.user.domain;
 
+import atdd.exception.ErrorType;
+import atdd.exception.SubwayException;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.mindrot.jbcrypt.BCrypt;
+
 public class User {
     private Long id;
     private String email;
+    @JsonIgnore
     private String password;
     private String name;
 
@@ -30,5 +36,18 @@ public class User {
 
     public String getName() {
         return name;
+    }
+
+    public void encryptPassword() {
+        this.password = encrypt(this.password);
+    }
+
+    public void checkPassword(String password) {
+        if (BCrypt.checkpw(password, encrypt(this.password)))
+            throw new SubwayException(ErrorType.INVALID_PASSWORD);
+    }
+
+    private String encrypt(final String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
     }
 }
--- src/main/java/atdd/user/web/UserController.java
+++ src/main/java/atdd/user/web/UserController.java
@@ -2,6 +2,7 @@ package atdd.user.web;
 
 import atdd.user.application.UserService;
 import atdd.user.application.dto.CreateUserRequestView;
+import atdd.user.application.dto.LoginRequestView;
 import atdd.user.application.dto.UserResponseView;
 import atdd.user.domain.User;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,8 @@ import java.net.URI;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    final String ACCESS_TOKEN_HEADER = "Authorization";
+
     private UserService userService;
 
     public UserController(UserService userService) {
@@ -29,5 +32,13 @@ public class UserController {
     public ResponseEntity retrieveUser(@LoginUser User user) {
         User persistUser = userService.findUserByEmail(user.getEmail());
         return ResponseEntity.ok().body(new UserResponseView(persistUser));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody final LoginRequestView view) {
+        String token = userService.login(view.getEmail(), view.getPassword());
+
+        return ResponseEntity.noContent()
+                .header(ACCESS_TOKEN_HEADER, token).build();
     }
 }
