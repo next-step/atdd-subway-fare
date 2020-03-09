ffc82cdbd14c14761a2b2405778330b665e8139d
parkeeseul <eeseul.p@gmail.com>
2020-03-10 00:37:53 +0900

    test: Line 단위 테스트 추가

--- src/test/java/atdd/path/domain/LineTest.java
+++ src/test/java/atdd/path/domain/LineTest.java
@@ -18,7 +18,18 @@ public class LineTest {
         List<String> upTime = line.getUpTimetablesOf(STATION_ID_7);
 
         assertThat(upTime.size()).isEqualTo(113);
-        assertThat(upTime.get(0)).isEqualTo(LocalTime.of(5, 20).toString());
-        assertThat(upTime.get(upTime.size() - 1)).isEqualTo(LocalTime.of(00, 00).toString());
+        assertThat(upTime.get(0)).isEqualTo(LocalTime.of(5, 25).toString());
+        assertThat(upTime.get(upTime.size() - 1)).isEqualTo(LocalTime.of(00, 5).toString());
+    }
+
+    @Test
+    public void getDownTimetablesOf() {
+        Line line = TEST_LINE_2;
+
+        List<String> downTime = line.getDownTimetablesOf(STATION_ID_7);
+
+        assertThat(downTime.size()).isEqualTo(113);
+        assertThat(downTime.get(0)).isEqualTo(LocalTime.of(5, 20).toString());
+        assertThat(downTime.get(downTime.size() - 1)).isEqualTo(LocalTime.of(00, 0).toString());
     }
 }
