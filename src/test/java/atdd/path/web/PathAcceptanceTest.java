fa4526ba6f7fa525daf4d3f897c0a6a48f48c5f1
parkeeseul <eeseul.p@gmail.com>
2020-03-16 13:42:47 +0900

    test: findMinTimePath 수정

--- src/test/java/atdd/path/web/PathAcceptanceTest.java
+++ src/test/java/atdd/path/web/PathAcceptanceTest.java
@@ -14,17 +14,15 @@ public class PathAcceptanceTest extends AbstractAcceptanceTest {
     private StationHttpTest stationHttpTest;
     private PathHttpTest pathHttpTest;
 
+    private long startStationId;
+    private long endStationId;
+
     @BeforeEach
     void setUp() {
         this.lineHttpTest = new LineHttpTest(webTestClient);
         this.stationHttpTest = new StationHttpTest(webTestClient);
         this.pathHttpTest = new PathHttpTest(webTestClient);
-    }
 
-    @DisplayName("최단 시간 경로 조회")
-    @Test
-    public void findMinTimePath() {
-        //given
         Long stationId = stationHttpTest.createStation(STATION_NAME); // 강남역
         Long stationId2 = stationHttpTest.createStation(STATION_NAME_2);
         Long stationId3 = stationHttpTest.createStation(STATION_NAME_3);
@@ -49,12 +47,19 @@ public class PathAcceptanceTest extends AbstractAcceptanceTest {
         lineHttpTest.createEdgeRequest(lineId3, stationId12, stationId13, 5, 10);
         lineHttpTest.createEdgeRequest(lineId3, stationId13, stationId14, 5, 10);
 
+        startStationId = stationId11;
+        endStationId = stationId4;
+    }
+
+    @DisplayName("최단 시간 경로 조회")
+    @Test
+    public void findMinTimePath() {
         //when
-        MinTimePathResponseView minTimePathResponseView = pathHttpTest.findMinTimePathRequest(stationId11, stationId4).getResponseBody();
+        MinTimePathResponseView minTimePathResponseView = pathHttpTest.findMinTimePathRequest(startStationId, endStationId).getResponseBody();
 
         //then
-        assertThat(minTimePathResponseView.getStartStationId()).isEqualTo(stationId11);
-        assertThat(minTimePathResponseView.getEndStationId()).isEqualTo(stationId4);
+        assertThat(minTimePathResponseView.getStartStationId()).isEqualTo(startStationId);
+        assertThat(minTimePathResponseView.getEndStationId()).isEqualTo(endStationId);
         assertThat(minTimePathResponseView.getStations().size()).isEqualTo(6);
         assertThat(minTimePathResponseView.getLines().size()).isEqualTo(2);
     }
