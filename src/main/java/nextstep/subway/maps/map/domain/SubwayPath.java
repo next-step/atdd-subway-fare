798687ea0def4a36113011c4f06e2c83cfc4b76b
showpointer <wanho92@gmail.com>
2020-07-26 21:13:17 +0900

    refactor(fare): 요금 계산 로직 리펙토링

--- src/main/java/nextstep/subway/maps/map/domain/SubwayPath.java
+++ src/main/java/nextstep/subway/maps/map/domain/SubwayPath.java
@@ -6,9 +6,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class SubwayPath {
+    // 요금 기준
     public static int BASE_FARE = 1250;
+    public static int ADDITIONAL_FARE = 100;
+    public static int FIRST_LIMIT_MAX_FARE = 800;
+
+    // 구간 거리 기준
     public static int FIRST_LIMIT = 10;
     public static int SECOND_LIMIT = 50;
+    public static int FIRST_SECTION_RANGE = SECOND_LIMIT - FIRST_LIMIT;
+
+    // 구간 별 km 기준
+    public static int FIRST_SECTION_PER_KILO = 5;
+    public static int SECOND_SECTION_PER_KILO = 8;
 
     private List<LineStationEdge> lineStationEdges;
 
@@ -46,14 +56,26 @@ public class SubwayPath {
 
     public int calculateOverFare(int distance) {
         int fare = 0;
-        if (distance <= 40) {
-            fare += (int) ((Math.ceil((distance - 1) / 5) + 1) * 100);
-            return fare;
+        if (distance <= FIRST_SECTION_RANGE) {
+            return calculateFareInFirstRange(distance, fare);
         }
 
-        int leftDistance = distance - 40;
-        fare += 800;
-        fare += (int) ((Math.ceil((leftDistance - 1) / 8) + 1) * 100);
+        return calcauateFareInSecondRange(distance, fare);
+    }
+
+    private int calculateFareInFirstRange(int distance, int fare) {
+        fare += getAdditionalFarePer(distance, FIRST_SECTION_PER_KILO);
         return fare;
+    }
+
+    private int calcauateFareInSecondRange(int distance, int fare) {
+        int leftDistance = distance - FIRST_SECTION_RANGE;
+        fare += FIRST_LIMIT_MAX_FARE;
+        fare += getAdditionalFarePer(leftDistance, SECOND_SECTION_PER_KILO);
+        return fare;
+    }
+
+    private int getAdditionalFarePer(int distance, int perKilometers) {
+        return (int) ((Math.ceil((distance - 1) / perKilometers) + 1) * ADDITIONAL_FARE);
     }
 }
