a821d267e8300e1c0d0e742f90c613badf11c431
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-08-01 17:50:14 +0900

    feat: FareCalculator to calculate overfare according to the policy

--- src/main/java/nextstep/subway/maps/map/application/FareCalculator.java
+++ src/main/java/nextstep/subway/maps/map/application/FareCalculator.java
@@ -6,8 +6,16 @@ import org.springframework.stereotype.Component;
 public class FareCalculator {
 
     public static final int BASIC = 1250;
+    public static final int OVERFARE_DISTANCE = 10;
 
     public int calculate(int distance) {
+        if (distance > OVERFARE_DISTANCE) {
+            return BASIC + calculateOverFare(distance - OVERFARE_DISTANCE);
+        }
         return BASIC;
+    }
+
+    private int calculateOverFare(int distance) {
+        return (int)((Math.ceil(distance - 1) / 5) + 1) * 100;
     }
 }
