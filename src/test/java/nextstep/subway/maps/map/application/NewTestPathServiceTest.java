8198f350b53b45d1fdc77620d578da8077b7b5e5
junwoochoi <cjw4690@gmail.com>
2020-08-01 21:36:54 +0900

    test : create new PathService and Its test

---
+++ src/test/java/nextstep/subway/maps/map/application/NewTestPathServiceTest.java
@@ -0,0 +1,63 @@
+package nextstep.subway.maps.map.application;
+
+import com.google.common.collect.Lists;
+import nextstep.subway.maps.line.domain.Line;
+import nextstep.subway.maps.line.domain.LineStation;
+import nextstep.subway.maps.map.domain.PathType;
+import nextstep.subway.maps.map.domain.SubwayPath;
+import nextstep.subway.maps.station.domain.Station;
+import nextstep.subway.utils.TestObjectUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NewTestPathServiceTest {
+    private List<Line> lines;
+    private PathService pathService;
+
+    @BeforeEach
+    void setUp() {
+        Map<Long, Station> stations = new HashMap<>();
+        stations.put(1L, TestObjectUtils.createStation(1L, "교대역"));
+        stations.put(2L, TestObjectUtils.createStation(2L, "강남역"));
+        stations.put(3L, TestObjectUtils.createStation(3L, "양재역"));
+        stations.put(4L, TestObjectUtils.createStation(4L, "남부터미널역"));
+
+        Line line1 = TestObjectUtils.createLine(1L, "2호선", "GREEN", 0);
+        line1.addLineStation(new LineStation(1L, null, 0, 0));
+        line1.addLineStation(new LineStation(2L, 1L, 2, 2));
+
+        Line line2 = TestObjectUtils.createLine(2L, "신분당선", "RED", 0);
+        line2.addLineStation(new LineStation(2L, null, 0, 0));
+        line2.addLineStation(new LineStation(3L, 2L, 2, 1));
+
+        Line line3 = TestObjectUtils.createLine(3L, "3호선", "ORANGE", 0);
+        line3.addLineStation(new LineStation(1L, null, 0, 0));
+        line3.addLineStation(new LineStation(4L, 1L, 1, 2));
+        line3.addLineStation(new LineStation(3L, 4L, 2, 2));
+
+        lines = Lists.newArrayList(line1, line2, line3);
+
+        pathService = new PathService();
+    }
+
+    @Test
+    void findPathByArrivalTime() {
+        //given
+        LocalDateTime departTime = LocalDateTime.of(2020, 7, 1, 6, 15);
+        // when
+        SubwayPath subwayPath = pathService.findPathByArrivalTime(lines, 1L, 3L, departTime);
+
+        // then
+        assertThat(subwayPath.extractStationId().size()).isEqualTo(3);
+        assertThat(subwayPath.extractStationId().get(0)).isEqualTo(1L);
+        assertThat(subwayPath.extractStationId().get(1)).isEqualTo(4L);
+        assertThat(subwayPath.extractStationId().get(2)).isEqualTo(3L);
+    }
+}
