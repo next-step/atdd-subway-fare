f71742b4d57b1bc1e4ff26cbb52bddcb89dd19db
koamania <koamania2@gmail.com>
2020-08-02 00:03:14 +0900

    test(path): step2 요금에 대한 wrapping 객체 생성 및 테스트 작성

---
+++ src/main/java/nextstep/subway/maps/fare/domain/Fare.java
@@ -0,0 +1,23 @@
+package nextstep.subway.maps.fare.domain;
+
+public class Fare {
+    public Fare(int fare) {
+
+    }
+
+    public void plusFare(int fare) {
+
+    }
+
+    public int getFare() {
+        return 0;
+    }
+
+    public void discountFare(int fare) {
+
+    }
+
+    public void discountPercent(int percent) {
+
+    }
+}
---
+++ src/test/java/nextstep/subway/maps/fare/domain/FareTest.java
@@ -0,0 +1,56 @@
+package nextstep.subway.maps.fare.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FareTest {
+
+    private Fare fare;
+
+    @BeforeEach
+    void setUp() {
+        fare = new Fare(0);
+    }
+
+    @DisplayName("요금을 추가한다")
+    @Test
+    void plusFare() {
+        // when
+        fare.plusFare(100);
+        fare.plusFare(200);
+        fare.plusFare(300);
+        fare.plusFare(400);
+
+        // then
+        assertThat(fare.getFare()).isEqualTo(FareContext.DEFAULT_FARE + 1000);
+    }
+
+    @DisplayName("요금을 할인한다")
+    @Test
+    void discountFare() {
+        // when
+        fare.discountFare(100);
+        fare.discountFare(200);
+        fare.discountFare(300);
+        fare.discountFare(400);
+
+        // then
+        assertThat(fare.getFare()).isEqualTo(FareContext.DEFAULT_FARE - 1000);
+    }
+
+    @DisplayName("요금을 퍼센트 할인한다")
+    @ParameterizedTest
+    @CsvSource({"55, 0.45", "30, 0.7", "10, 0.9"})
+    void discountPercent(int percent, float expected) {
+        // when
+        fare.discountPercent(percent);
+
+        // then
+        assertThat(fare.getFare()).isEqualTo((int) Math.floor(FareContext.DEFAULT_FARE * expected));
+    }
+}
