3aa0deaffa7e7ab4af343342af064f4090452f5e
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-08-06 01:09:00 +0900

    feat: MapController to have another business logic when loginMember is EmptyMember

--- src/main/java/nextstep/subway/maps/map/ui/MapController.java
+++ src/main/java/nextstep/subway/maps/map/ui/MapController.java
@@ -1,16 +1,17 @@
 package nextstep.subway.maps.map.ui;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import nextstep.subway.auth.domain.AuthenticationPrincipal;
+import nextstep.subway.auth.domain.EmptyMember;
 import nextstep.subway.maps.map.application.FareMapService;
 import nextstep.subway.maps.map.domain.PathType;
 import nextstep.subway.maps.map.dto.FarePathResponse;
 import nextstep.subway.maps.map.dto.MapResponse;
 import nextstep.subway.members.member.domain.LoginMember;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MapController {
@@ -23,6 +24,9 @@ public class MapController {
     @GetMapping("/paths")
     public ResponseEntity<FarePathResponse> findPath(@AuthenticationPrincipal LoginMember loginMember,
         @RequestParam Long source, @RequestParam Long target, @RequestParam PathType type) {
+        if (loginMember instanceof EmptyMember) {
+            return ResponseEntity.ok(mapService.findPathWithFare(source, target, type));
+        }
         return ResponseEntity.ok(mapService.findPathWithFare(loginMember, source, target, type));
     }
 
