4d0e047fbd27496d3d560380a69dd00adc0ddd5b
koamania <koamania2@gmail.com>
2020-08-06 00:54:04 +0900

    feature(path): step3 노선의 방향을 가져오는 메소드 추가

--- src/main/java/nextstep/subway/maps/map/domain/PathDirection.java
+++ src/main/java/nextstep/subway/maps/map/domain/PathDirection.java
@@ -1,5 +1,7 @@
 package nextstep.subway.maps.map.domain;
 
+import nextstep.subway.maps.line.domain.LineStation;
+
 import java.util.List;
 
 public enum PathDirection {
@@ -7,7 +9,29 @@ public enum PathDirection {
     ;
 
     public static PathDirection getDirection(List<LineStationEdge> lineStationEdges) {
+        if (lineStationEdges.isEmpty() || lineStationEdges.size() == 1) {
+            throw new RuntimeException("invalid line stations");
+        }
 
-        return null;
+        LineStation prevStation = lineStationEdges.get(0).getLineStation();
+        LineStation nextstation = lineStationEdges.get(1).getLineStation();
+
+        if (isForwardDirection(prevStation, nextstation)) {
+            return FORWARD;
+        }
+
+        if (isReverseDirection(prevStation, nextstation)) {
+            return REVERS;
+        }
+
+        throw new RuntimeException("invalid line stations");
+    }
+
+    private static boolean isForwardDirection(LineStation prevStation, LineStation nextstation) {
+        return nextstation.getPreStationId().equals(prevStation.getStationId());
+    }
+
+    private static boolean isReverseDirection(LineStation prevStation, LineStation nextstation) {
+        return prevStation.getPreStationId().equals(nextstation.getStationId());
     }
 }
