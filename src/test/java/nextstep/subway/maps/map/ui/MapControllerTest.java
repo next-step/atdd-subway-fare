81e52105188335dd4693063b8852a1ce25687d1f
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-08-09 16:49:54 +0900

    test: add FindPath method that has non-member parameter - clarifying mocking type

--- src/test/java/nextstep/subway/maps/map/ui/MapControllerTest.java
+++ src/test/java/nextstep/subway/maps/map/ui/MapControllerTest.java
@@ -4,10 +4,13 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import nextstep.subway.auth.domain.EmptyMember;
 import nextstep.subway.maps.map.application.FareMapService;
 import nextstep.subway.maps.map.domain.PathType;
 import nextstep.subway.maps.map.dto.FarePathResponse;
@@ -28,18 +31,32 @@ public class MapControllerTest {
     }
 
     @Test
-    void findPathWithUser() {
-        // given
-        LoginMember loginMember = new LoginMember(1L, "email@email.com", "password", 15);
+    void findPath() {
         FareMapService mapService = mock(FareMapService.class);
         MapController controller = new MapController(mapService);
-        when(mapService.findPathWithFare(any(), anyLong(), anyLong(), any())).thenReturn(new FarePathResponse());
+        when(mapService.findPathWithFare(anyLong(), anyLong(), any(PathType.class), nullable(LocalDateTime.class)))
+            .thenReturn(new FarePathResponse());
+        ResponseEntity<FarePathResponse> entity = controller.findPath(new EmptyMember(), 1L, 2L,
+            PathType.DISTANCE, null);
+        verify(mapService).findPathWithFare(anyLong(), anyLong(), any(PathType.class), nullable(LocalDateTime.class));
+        assertThat(entity.getBody()).isNotNull();
+    }
+
+    @Test
+    void findPathWithUser() {
+        // given
+        LoginMember loginMember = new LoginMember(1L, "javajigi@slipp.net", "pobiconan", 15);
+        FareMapService mapService = mock(FareMapService.class);
+        MapController controller = new MapController(mapService);
+        when(mapService.findPathWithFare(any(), anyLong(), anyLong(), any(PathType.class),
+            nullable(LocalDateTime.class))).thenReturn(new FarePathResponse());
 
         // when
         ResponseEntity<FarePathResponse> entity = controller.findPath(loginMember, 1L, 2L, PathType.DISTANCE, null);
 
         // then
-        verify(mapService).findPathWithFare(any(LoginMember.class), anyLong(), anyLong(), any(PathType.class));
+        verify(mapService).findPathWithFare(any(LoginMember.class), anyLong(), anyLong(), any(PathType.class),
+            nullable(LocalDateTime.class));
         assertThat(entity.getBody()).isNotNull();
     }
 }
