c4a45ebeaed0abd928322761d78dbd0d1d1a7c21
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-08-09 12:16:26 +0900

    test: LineTest to have ```@DataJpaTest``` annotation

--- src/test/java/nextstep/subway/maps/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/maps/line/domain/LineTest.java
@@ -6,16 +6,23 @@ import java.time.LocalTime;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+@DataJpaTest
 public class LineTest {
+
+    @Autowired
+    private LineRepository lineRepository;
 
     @DisplayName("지하철 노선에는 추가 요금 필드가 존재한다.")
     @Test
     void 지하철_노선에_추가요금이_존재한다() {
         // given
         Line line = new Line("2호선", "GREEN", LocalTime.now(), LocalTime.now(), 3, 400);
+        Line savedLine = lineRepository.save(line);
 
         // when
-        assertThat(line.getExtraFare()).isNotNull();
+        assertThat(savedLine.getExtraFare()).isNotNull();
     }
 }
