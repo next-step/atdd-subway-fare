3ba36f267b5253663d92433abea30b95b03873b0
etff <wish2rich@gmail.com>
2020-08-13 22:30:30 +0900

    test(fare): add extra fare test

--- src/test/java/nextstep/subway/maps/map/application/FareCalculatorTest.java
+++ src/test/java/nextstep/subway/maps/map/application/FareCalculatorTest.java
@@ -28,6 +28,15 @@ class FareCalculatorTest {
         assertThat(fare).isEqualTo(result);
     }
 
+    @DisplayName("10Km 초과 ~ 50Km 추가 요금 노선의 요금을 계산한다.")
+    @ParameterizedTest
+    @CsvSource({"15, 1550, 200", "50, 2250, 200"})
+    void calculateExtraFareUnderFiftyKM(int distance, int result, int extraFare) {
+        FareCalculator fareCalculator = new FareCalculator();
+        int fare = fareCalculator.calculate(distance, extraFare);
+        assertThat(fare).isEqualTo(result);
+    }
+
     @DisplayName("50Km 이상 요금을 계산한다.")
     @ParameterizedTest
     @CsvSource({"51, 2150, 0"})
