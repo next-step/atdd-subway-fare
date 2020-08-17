a0070955f6111823f821df2d4c72261e4a9a2a9d
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-08-17 11:38:46 +0900

    refactor: migrate findPathByArrivalTime method from test service to real service

--- src/main/java/nextstep/subway/maps/map/application/PathService.java
+++ src/main/java/nextstep/subway/maps/map/application/PathService.java
@@ -1,9 +1,12 @@
 package nextstep.subway.maps.map.application;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.alg.shortestpath.KShortestPaths;
 import org.springframework.stereotype.Service;
 
 import com.google.common.collect.Lists;
@@ -12,9 +15,43 @@ import nextstep.subway.maps.map.domain.LineStationEdge;
 import nextstep.subway.maps.map.domain.PathType;
 import nextstep.subway.maps.map.domain.SubwayGraph;
 import nextstep.subway.maps.map.domain.SubwayPath;
+import nextstep.subway.maps.map.domain.path.TimePath;
+import nextstep.subway.maps.map.domain.path.TimePaths;
 
 @Service
 public class PathService {
+
+    public static final int MAX_PATH_COUNT = 10000;
+
+    public SubwayPath findPath(List<Line> lines, Long source, Long target, PathType type, LocalDateTime time) {
+        if (type == PathType.ARRIVAL) {
+            return findPathByArrivalTime(lines, source, target, time);
+        }
+
+        SubwayGraph graph = createSubwayGraph(lines, type);
+        DijkstraShortestPath<Long, LineStationEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
+        GraphPath<Long, LineStationEdge> path = dijkstraShortestPath.getPath(source, target);
+
+        return convertSubwayPath(path);
+    }
+
+    protected SubwayPath findPathByArrivalTime(List<Line> lines, Long source, Long target,
+        LocalDateTime departureTime) {
+        SubwayGraph graph = createSubwayGraph(lines, PathType.ARRIVAL);
+        List<GraphPath<Long, LineStationEdge>> paths = getAllPaths(source, target, graph);
+        List<SubwayPath> subwayPaths = paths.stream()
+            .map(this::convertSubwayPath)
+            .collect(Collectors.toList());
+        TimePaths timePaths = TimePaths.of(subwayPaths);
+        TimePath fastestArrivalPath = timePaths.findFastestArrivalPath(departureTime);
+        return fastestArrivalPath.getPath();
+    }
+
+    private List<GraphPath<Long, LineStationEdge>> getAllPaths(Long source, Long target, SubwayGraph graph) {
+        KShortestPaths<Long, LineStationEdge> kShortestPaths = new KShortestPaths<>(graph, MAX_PATH_COUNT);
+        return kShortestPaths.getPaths(source, target);
+    }
+
     public SubwayPath findPath(List<Line> lines, Long source, Long target, PathType type) {
         SubwayGraph graph = createSubwayGraph(lines, type);
         graph.addVertexWith(lines);
