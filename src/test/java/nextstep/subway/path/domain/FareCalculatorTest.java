02a41beb0b577ba3f8f6fed939a60deb75e716c3
junilHwang <junil.h@kakao.com>
2021-03-26 00:33:33 +0900

    feat: 운임 요금에 대한 단위 테스트 작성 완료

---
+++ src/test/java/nextstep/subway/path/domain/FareCalculatorTest.java
@@ -0,0 +1,48 @@
+package nextstep.subway.path.domain;
+
+import nextstep.subway.path.application.AdultFareCalculator;
+import nextstep.subway.path.application.FareCalculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static nextstep.subway.path.application.AdultFareCalculator.ADULT_DEFAULT_FARE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("운임 요금 계산기 테스트")
+public class FareCalculatorTest {
+    private final FareCalculator fareCalculator = new AdultFareCalculator();
+
+    @DisplayName("기본운임(10㎞ 이내) : 기본운임 1,250원")
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    @ParameterizedTest
+    void calculateDefaultFareTest(int distance) {
+        assertThat(ADULT_DEFAULT_FARE)
+            .isEqualTo(fareCalculator.calculateFare(distance));
+    }
+
+    @Nested
+    @DisplayName("이용 거리초과 시 추가운임 부과")
+    class 초과_요금_계산 {
+
+        @DisplayName("10km초과 ∼ 50km까지(5km마다 100원)")
+        @CsvSource({"15,1350", "20,1450", "25,1550", "30,1650", "35,1750", "40,1850", "45,1950", "50,2050" })
+        @ParameterizedTest
+        void calculateOver10FareTest(int distance, int expected) {
+            assertThat(expected)
+                .isEqualTo(fareCalculator.calculateFare(distance));
+        }
+
+        @DisplayName("50km초과 시 (8km마다 100원)")
+        @CsvSource({"58,2150", "66,2250", "72,2350", "80,2450" })
+        @ParameterizedTest
+        void calculateOver50FareTest(int distance, int expected) {
+            assertThat(expected)
+                .isEqualTo(fareCalculator.calculateFare(distance));
+        }
+
+    }
+}
