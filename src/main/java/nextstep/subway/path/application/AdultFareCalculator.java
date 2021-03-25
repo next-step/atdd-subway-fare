52d69c43b80f945119514fd7466f7ca822ecf155
junilHwang <junil.h@kakao.com>
2021-03-26 00:33:52 +0900

    refactor: 운임 요금 계산기 코드 리팩토링

--- src/main/java/nextstep/subway/path/application/AdultFareCalculator.java
+++ src/main/java/nextstep/subway/path/application/AdultFareCalculator.java
@@ -12,15 +12,37 @@ public class AdultFareCalculator implements FareCalculator {
 
     public int calculateFare(int distance) {
         int fare = ADULT_DEFAULT_FARE;
-        if (distance > 10) {
-            fare += calculateOverFare(distance);
+        if (distance > OverFareCalculator.FIRST.getSection()) {
+            fare += OverFareCalculator.FIRST.calculate(Math.min(distance, 50));
+        }
+        if (distance > OverFareCalculator.SECOND.getSection()) {
+            fare += OverFareCalculator.SECOND.calculate(distance);
         }
         return fare;
     }
 
-    private int calculateOverFare(int distance) {
-        int unit = distance < 50 ? 5 : 8;
-        return (int)((Math.ceil((distance - 1) / unit) + 1) * 100);
-    }
+    private enum OverFareCalculator {
+        FIRST(10, 5),
+        SECOND(50, 8);
 
+        private final int section;
+        private final int unit;
+
+        OverFareCalculator(int section, int unit) {
+            this.section = section;
+            this.unit = unit;
+        }
+
+        public int getSection() {
+            return section;
+        }
+
+        public int getUnit() {
+            return unit;
+        }
+
+        public int calculate(int distance) {
+            return (int)((Math.ceil((distance - section - 1) / unit) + 1) * 100);
+        }
+    }
 }
