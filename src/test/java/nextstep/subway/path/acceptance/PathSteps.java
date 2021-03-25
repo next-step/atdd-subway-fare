d069f3f7b190924acb837995b74c43d808a9d02c
junilHwang <junil.h@kakao.com>
2021-03-26 00:38:02 +0900

    test(fix): PathAcceptanceTest 관련 오류 수정

--- src/test/java/nextstep/subway/path/acceptance/PathSteps.java
+++ src/test/java/nextstep/subway/path/acceptance/PathSteps.java
@@ -65,7 +65,7 @@ public class PathSteps {
         PathResponse pathResponse = response.as(PathResponse.class);
         assertThat(pathResponse.getDistance()).isEqualTo(distance);
         assertThat(pathResponse.getDuration()).isEqualTo(duration);
-        assertThat(pathResponse.getDuration()).isEqualTo(duration);
+        assertThat(pathResponse.getFare()).isEqualTo(fare);
 
         List<Long> stationIds = pathResponse.getStations().stream()
             .map(StationResponse::getId)
