015e010a8660bcb8bfd59c53790fde5b1d4f59db
HyunSung-Na <oceana57@naver.com>
2021-03-26 20:21:45 +0900

    PathDocumentation 리펙토링

--- src/test/java/nextstep/subway/path/acceptance/PathSteps.java
+++ src/test/java/nextstep/subway/path/acceptance/PathSteps.java
@@ -3,26 +3,19 @@ package nextstep.subway.path.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.dto.StationResponse;
 import nextstep.subway.utils.Documentation;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.payload.JsonFieldType;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static nextstep.subway.line.acceptance.LineSteps.지하철_노선_생성_요청;
-import static nextstep.subway.utils.ApiDocumentUtils.getDocumentRequest;
-import static nextstep.subway.utils.ApiDocumentUtils.getDocumentResponse;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathSteps extends Documentation {
 
@@ -34,22 +27,7 @@ public class PathSteps extends Documentation {
     public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target) {
 
         return RestAssured
-                .given(spec).log().all()
-                .filter(document("path",
-                        getDocumentRequest(),
-                        getDocumentResponse(),
-                        requestParameters(
-                                parameterWithName("source").description("출발역 아이디"),
-                                parameterWithName("target").description("도착역 아이디"),
-                                parameterWithName("type").description("조회 기준")),
-                        responseFields(
-                                fieldWithPath("stations").type(JsonFieldType.ARRAY).description("경로 지하철역 목록"),
-                                fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("지하철역 아이디"),
-                                fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("지하철역 이름"),
-                                fieldWithPath("stations[].createdDate").type(JsonFieldType.STRING).description("지하철역 생성날짜"),
-                                fieldWithPath("stations[].modifiedDate").type(JsonFieldType.STRING).description("지하철역 수정날짜"),
-                                fieldWithPath("duration").type(JsonFieldType.NUMBER).description("소요시간(분)"),
-                                fieldWithPath("distance").type(JsonFieldType.NUMBER).description("거리(km)"))))
+                .given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", source)
                 .queryParam("target", target)
@@ -60,26 +38,21 @@ public class PathSteps extends Documentation {
 
     public static ExtractableResponse<Response> 두_역의_최소_소요_시간_경로_조회를_요청(Long source, Long target) {
         return RestAssured
-                .given(spec).log().all()
-                .filter(document("path",
-                        getDocumentRequest(),
-                        getDocumentResponse(),
-                        requestParameters(
-                                parameterWithName("source").description("출발역 아이디"),
-                                parameterWithName("target").description("도착역 아이디"),
-                                parameterWithName("type").description("조회 기준")),
-                        responseFields(
-                                fieldWithPath("stations").type(JsonFieldType.ARRAY).description("경로 지하철역 목록"),
-                                fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("지하철역 아이디"),
-                                fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("지하철역 이름"),
-                                fieldWithPath("stations[].createdDate").type(JsonFieldType.STRING).description("지하철역 생성날짜"),
-                                fieldWithPath("stations[].modifiedDate").type(JsonFieldType.STRING).description("지하철역 수정날짜"),
-                                fieldWithPath("duration").type(JsonFieldType.NUMBER).description("소요시간(분)"),
-                                fieldWithPath("distance").type(JsonFieldType.NUMBER).description("거리(km)"))))
+                .given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", source)
                 .queryParam("target", target)
                 .queryParam("type", "DURATION")
+                .when().get("/paths")
+                .then().log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(RequestSpecification given, Long source, Long target) {
+        return given
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("source", source)
+                .queryParam("target", target)
+                .queryParam("type", "DISTANCE")
                 .when().get("/paths")
                 .then().log().all().extract();
     }
