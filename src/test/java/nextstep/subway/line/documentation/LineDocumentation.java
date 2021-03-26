91f753a134691ef34c1791e6e7aef4ed36b2e845
junilHwang <junil.h@kakao.com>
2021-03-26 15:31:36 +0900

    test: Line 문서에 Request, Response Fields 작성 완료

--- src/test/java/nextstep/subway/line/documentation/LineDocumentation.java
+++ src/test/java/nextstep/subway/line/documentation/LineDocumentation.java
@@ -24,10 +24,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class LineDocumentation extends Documentation {
@@ -57,9 +54,32 @@ public class LineDocumentation extends Documentation {
         지하철_노선_생성_요청(
             RestAssured.given(spec).log().all().filter(
                 document(
-                    "lines",
+                    "line",
                     preprocessRequest(prettyPrint()),
-                    preprocessResponse(prettyPrint())
+                    preprocessResponse(prettyPrint()),
+                    requestFields(
+                        fieldWithPath("name").type(JsonFieldType.STRING).description("노선 이름"),
+                        fieldWithPath("color").type(JsonFieldType.STRING).description("노선 색상"),
+                        fieldWithPath("upStationId").type(JsonFieldType.NUMBER).description("기점역 ID"),
+                        fieldWithPath("downStationId").type(JsonFieldType.NUMBER).description("종점역 ID"),
+                        fieldWithPath("distance").type(JsonFieldType.NUMBER).description("거리"),
+                        fieldWithPath("duration").type(JsonFieldType.NUMBER).description("소요 시간"),
+                        fieldWithPath("additionalFare").type(JsonFieldType.NUMBER).description("추가 요금")
+                    ),
+                    responseFields(
+                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("노선 ID"),
+                        fieldWithPath("color").type(JsonFieldType.STRING).description("노선 색상"),
+                        fieldWithPath("name").type(JsonFieldType.STRING).description("노선 이름"),
+                        fieldWithPath("stations[]").type(JsonFieldType.ARRAY).description("노선에 포함된 역들"),
+                        fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("역 아이디"),
+                        fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("역 이름"),
+                        fieldWithPath("stations[].createdDate").type(JsonFieldType.STRING).description("생성일"),
+                        fieldWithPath("stations[].modifiedDate").type(JsonFieldType.STRING).description("수정일"),
+                        fieldWithPath("additionalFare").type(JsonFieldType.NUMBER).description("추가 요금"),
+                        fieldWithPath("createdDate").type(JsonFieldType.STRING).description("생성일"),
+                        fieldWithPath("modifiedDate").type(JsonFieldType.STRING).description("수정일")
+                    )
+
                 )
             ),
             new LineRequest(
