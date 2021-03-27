51d0b2487210f3a4d753a0b96e6458fa2d6d6d26
임한결 <dlagksruf19@naver.com>
2021-03-27 17:39:24 +0900

    docs : path API Document에 ResponseFields 추가

--- src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
@@ -9,6 +9,7 @@ import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -17,6 +18,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
@@ -47,6 +50,15 @@ public class PathDocumentation extends Documentation {
                                 parameterWithName("source").description("출발역 ID"),
                                 parameterWithName("target").description("도착역 ID"),
                                 parameterWithName("type").description("조회 방법")
+                        ),
+                        responseFields(
+                                fieldWithPath("stations").type(JsonFieldType.ARRAY).description("경로 지하철역 목록"),
+                                fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("지하철 역 ID"),
+                                fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("지하철 역 이름"),
+                                fieldWithPath("stations[].createdDate").type(JsonFieldType.STRING).description("지하철 역 등록 날짜"),
+                                fieldWithPath("stations[].modifiedDate").type(JsonFieldType.STRING).description("지하철 역 최종 수정 날짜"),
+                                fieldWithPath("distance").type(JsonFieldType.NUMBER).description("거리 (km)"),
+                                fieldWithPath("duration").type(JsonFieldType.NUMBER).description("소요시간 (분)")
                         )))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
