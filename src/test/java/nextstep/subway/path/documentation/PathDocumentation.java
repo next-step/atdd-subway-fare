2392c0e44375c5487f834e4a237b8089c7896661
임한결 <dlagksruf19@naver.com>
2021-03-27 16:26:02 +0900

    test : PathDocumentation.path Test 작성

--- src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
@@ -1,22 +1,52 @@
 package nextstep.subway.path.documentation;
 
+import com.google.common.collect.Lists;
 import io.restassured.RestAssured;
 import nextstep.subway.Documentation;
+import nextstep.subway.path.application.PathService;
+import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
     @Test
     void path() {
+        // given
+        ArrayList<StationResponse> stationResponses = Lists.newArrayList(
+                new StationResponse(1L, "강남역", LocalDateTime.now(), LocalDateTime.now()),
+                new StationResponse(2L, "역삼역", LocalDateTime.now(), LocalDateTime.now())
+        );
+
+        PathResponse pathResponse = new PathResponse(stationResponses, 10, 10);
+        given(pathService.findPath(anyLong(), anyLong(), any())).willReturn(pathResponse);
+
+        // when & then
         RestAssured
-                .given().log().all()
+                .given(spec).log().all()
+                .filter(document("path",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
                 .queryParam("type", "DISTANCE")
                 .when().get("/paths")
-                .then().log().all().extract();
+                .then().log().all();
     }
 }
 
