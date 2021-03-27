8e20549cea13272d25b8ea16bdf97bd32f8be2f5
bbbnam <bbbnam@naver.com>
2021-03-28 00:13:30 +0900

    feat : 경로찾기 기능 문서화 테스트 작성

--- src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
@@ -2,21 +2,50 @@ package nextstep.subway.path.documentation;
 
 import io.restassured.RestAssured;
 import nextstep.subway.Documentation;
+import nextstep.subway.path.application.PathService;
+import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.dto.StationResponse;
+import org.assertj.core.util.Lists;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
+    private PathResponse 강남_역삼_경로;
+
     @Test
     void path() {
+        강남_역삼_경로 = new PathResponse(
+                Lists.newArrayList(new StationResponse(1L, "강남역", LocalDateTime.now(), LocalDateTime.now()),
+                        new StationResponse(2L, "역삼역", LocalDateTime.now(), LocalDateTime.now())
+                ), 10, 10
+        );
+
+        when(pathService.findPath(anyLong(), anyLong(), any())).thenReturn(강남_역삼_경로);
+
         RestAssured
-                .given().log().all()
+                .given(spec).log().all()
+                .filter(document("path",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
                 .queryParam("type", "DISTANCE")
                 .when().get("/paths")
-                .then().log().all().extract();
+                .then().log().all();
     }
 }
-
