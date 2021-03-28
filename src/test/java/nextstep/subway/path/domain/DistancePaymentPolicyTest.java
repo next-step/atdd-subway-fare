c9f20d8d15db9dfa5c59bd882e371d97afda06b1
catsbi <killfine1988@gmail.com>
2021-03-28 13:23:17 +0900

    [refactor] 거리별 요금정책 테스트픽스쳐 고립형태의 픽스쳐 구현

--- src/test/java/nextstep/subway/path/domain/DistancePaymentPolicyTest.java
+++ src/test/java/nextstep/subway/path/domain/DistancePaymentPolicyTest.java
@@ -1,9 +1,5 @@
 package nextstep.subway.path.domain;
 
-import nextstep.subway.line.domain.PathType;
-import nextstep.subway.path.application.PathService;
-import nextstep.subway.station.domain.Station;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,16 +7,8 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("DistancePaymentPolicy 클래스")
-public class DistancePaymentPolicyTest extends PathTest {
+public class DistancePaymentPolicyTest extends IsolatePathTest {
     private PaymentPolicy paymentPolicy = new DistancePaymentPolicy();
-    private SubwayGraph subwayGraph;
-    @BeforeEach
-    void setup() {
-        super.setup();
-        pathService = new PathService(graphService, stationService, paymentPolicy);
-
-        subwayGraph = graphService.findGraph(PathType.DISTANCE);
-    }
 
     @DisplayName("cost 메서드는")
     @Nested
@@ -31,13 +19,8 @@ public class DistancePaymentPolicyTest extends PathTest {
             @DisplayName("기본요금이 부과된다.")
             @Test
             void it_is_default_cost() {
-                //given
-                Station sourceStation = stationService.findStationById(양재역.getId());
-                Station targetStation = stationService.findStationById(강남역.getId());
-                PathResult pathResult = subwayGraph.findPath(sourceStation, targetStation);
-
                 //when
-                Cost cost = paymentPolicy.cost(pathResult);
+                Cost cost = paymentPolicy.cost(강남역_양재역_경로);
 
                 //then
                 assertThat(cost.getCost()).isEqualTo(PaymentPolicy.DEFAULT_COST + 900);
@@ -50,13 +33,8 @@ public class DistancePaymentPolicyTest extends PathTest {
             @DisplayName("5키로당 100원의 요금이 부과된다.")
             @Test
             void it_is_additional_cost_100_by_5km() {
-                //given
-                Station sourceStation = stationService.findStationById(교대역.getId());
-                Station targetStation = stationService.findStationById(남부터미널역.getId());
-                PathResult pathResult = subwayGraph.findPath(sourceStation, targetStation);
-
                 //when
-                Cost cost = paymentPolicy.cost(pathResult);
+                Cost cost = paymentPolicy.cost(교대역_남부터미널역_경로);
 
                 //then
                 assertThat(cost.getCost()).isEqualTo(1950);
@@ -69,13 +47,8 @@ public class DistancePaymentPolicyTest extends PathTest {
             @DisplayName("8키로당 100원씩 요금이 부과된다.")
             @Test
             void it_is_additional_cost_100_by_8km() {
-                //given
-                Station sourceStation = stationService.findStationById(교대역.getId());
-                Station targetStation = stationService.findStationById(양재역.getId());
-                PathResult pathResult = subwayGraph.findPath(sourceStation, targetStation);
-
                 //when
-                Cost cost = paymentPolicy.cost(pathResult);
+                Cost cost = paymentPolicy.cost(양재역_교대역_경로);
 
                 //then 59km 1250 + 800 + 200
                 assertThat(cost.getCost()).isEqualTo(2750);
