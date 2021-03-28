bb301adbb694f2895e3d71a13c441e34f036fe8c
SC <pch838811@gmail.com>
2021-03-28 14:39:34 +0900

    refactor : 요청 파라미터, 응답 필드가 테이블 형태로 정리되도록 문서 수정

--- src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
@@ -3,6 +3,8 @@ package nextstep.subway.path.documentation;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.*;
 
 import java.time.LocalDateTime;
@@ -11,6 +13,7 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 
 import io.restassured.RestAssured;
 import nextstep.subway.Documentation;
@@ -36,7 +39,20 @@ public class PathDocumentation extends Documentation {
         RestAssured
                 .given(spec).log().all()
                 .filter(document("path",
-                    preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())))
+                    preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                    requestParameters(
+                        parameterWithName("source").description("출발역 식별자"),
+                        parameterWithName("target").description("도착역 식별자"),
+                        parameterWithName("type").description("경로 찾기 타입")),
+                    responseFields(
+                        fieldWithPath("stations[]").type(JsonFieldType.ARRAY).description("최단 경로 역 정보"),
+                        fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("최단 경로 역 식별자"),
+                        fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("최단 경로 역 이름"),
+                        fieldWithPath("stations[].createdDate").type(JsonFieldType.STRING).description("최단 경로 역 생성시간"),
+                        fieldWithPath("stations[].modifiedDate").type(JsonFieldType.STRING).description("최단 경로 역 수정시간"),
+                        fieldWithPath("distance").type(JsonFieldType.NUMBER).description("최단 경로 거리"),
+                        fieldWithPath("duration").type(JsonFieldType.NUMBER).description("최단 경로 시간")
+                    )))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
