925218355334520b5db541df7c889c92201d01dc
SC <pch838811@gmail.com>
2021-03-28 14:17:11 +0900

    feat : 경로찾기 문서화 구현

--- src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
@@ -1,22 +1,48 @@
 package nextstep.subway.path.documentation;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.*;
+
+import java.time.LocalDateTime;
+
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+
 import io.restassured.RestAssured;
 import nextstep.subway.Documentation;
-import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import nextstep.subway.path.application.PathService;
+import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.dto.StationResponse;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
     @Test
     void path() {
+        PathResponse pathResponse = new PathResponse(
+            Lists.newArrayList(
+                new StationResponse(1L, "강남역", LocalDateTime.now(), LocalDateTime.now()),
+                new StationResponse(1L, "역삼역", LocalDateTime.now(), LocalDateTime.now())
+            ), 10, 10
+        );
+        when(pathService.findPath(anyLong(), anyLong(), any())).thenReturn(pathResponse);
+
         RestAssured
-                .given().log().all()
+                .given(spec).log().all()
+                .filter(document("path",
+                    preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
                 .queryParam("type", "DISTANCE")
                 .when().get("/paths")
-                .then().log().all().extract();
+                .then().log();
     }
 }
 
