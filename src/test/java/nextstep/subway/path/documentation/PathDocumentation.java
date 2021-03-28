e6e3c3ae35c8d45328ed5b375d6da349eec7f273
SC <pch838811@gmail.com>
2021-03-28 15:08:43 +0900

    refactor : 공통으로 사용할 수 있는 부분 추출

--- src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
@@ -1,22 +1,21 @@
 package nextstep.subway.path.documentation;
 
 import static nextstep.subway.path.acceptance.PathSteps.*;
+import static nextstep.subway.utils.DocumentationUtil.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.*;
 
 import java.time.LocalDateTime;
 
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
+import org.springframework.restdocs.request.RequestParametersSnippet;
 
-import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import nextstep.subway.Documentation;
 import nextstep.subway.path.application.PathService;
@@ -38,23 +37,23 @@ public class PathDocumentation extends Documentation {
         );
         when(pathService.findPath(anyLong(), anyLong(), any())).thenReturn(pathResponse);
 
-        final RequestSpecification requestSpecification = RestAssured
-            .given(spec).log().all()
-            .filter(document("path",
-                preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
-                requestParameters(
-                    parameterWithName("source").description("출발역 식별자"),
-                    parameterWithName("target").description("도착역 식별자"),
-                    parameterWithName("type").description("경로 찾기 타입")),
-                responseFields(
-                    fieldWithPath("stations[]").type(JsonFieldType.ARRAY).description("최단 경로 역 정보"),
-                    fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("최단 경로 역 식별자"),
-                    fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("최단 경로 역 이름"),
-                    fieldWithPath("stations[].createdDate").type(JsonFieldType.STRING).description("최단 경로 역 생성시간"),
-                    fieldWithPath("stations[].modifiedDate").type(JsonFieldType.STRING).description("최단 경로 역 수정시간"),
-                    fieldWithPath("distance").type(JsonFieldType.NUMBER).description("최단 경로 거리"),
-                    fieldWithPath("duration").type(JsonFieldType.NUMBER).description("최단 경로 시간")
-                )));
+        final RequestParametersSnippet requestParametersSnippet = requestParameters(
+            parameterWithName("source").description("출발역 식별자"),
+            parameterWithName("target").description("도착역 식별자"),
+            parameterWithName("type").description("경로 찾기 타입"));
+
+        final ResponseFieldsSnippet responseFieldsSnippet = responseFields(
+            fieldWithPath("stations[]").type(JsonFieldType.ARRAY).description("최단 경로 역 정보"),
+            fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("최단 경로 역 식별자"),
+            fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("최단 경로 역 이름"),
+            fieldWithPath("stations[].createdDate").type(JsonFieldType.STRING).description("최단 경로 역 생성시간"),
+            fieldWithPath("stations[].modifiedDate").type(JsonFieldType.STRING).description("최단 경로 역 수정시간"),
+            fieldWithPath("distance").type(JsonFieldType.NUMBER).description("최단 경로 거리"),
+            fieldWithPath("duration").type(JsonFieldType.NUMBER).description("최단 경로 시간")
+        );
+
+        RequestSpecification requestSpecification =
+            given(spec, "path", requestParametersSnippet, responseFieldsSnippet);
 
         두_역의_최단_거리_경로_조회를_요청(requestSpecification, 1L, 2L);
     }
