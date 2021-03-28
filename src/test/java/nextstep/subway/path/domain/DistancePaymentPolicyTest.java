84380f0b7941dd575a30cac551f056478b838f80
catsbi <killfine1988@gmail.com>
2021-03-28 16:10:07 +0900

    [feat] DistancePaymentPolicyTest 에서는 노선별 추가요금에 대한부분을 제외하여 수정

--- src/test/java/nextstep/subway/path/domain/DistancePaymentPolicyTest.java
+++ src/test/java/nextstep/subway/path/domain/DistancePaymentPolicyTest.java
@@ -1,5 +1,8 @@
 package nextstep.subway.path.domain;
 
+import nextstep.subway.path.domain.policy.DistancePaymentPolicy;
+import nextstep.subway.path.domain.policy.PaymentPolicy;
+import nextstep.subway.path.dto.CostRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -8,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("DistancePaymentPolicy 클래스")
 public class DistancePaymentPolicyTest extends IsolatePathTest {
-    private PaymentPolicy paymentPolicy = new DistancePaymentPolicy();
+    private final PaymentPolicy paymentPolicy = new DistancePaymentPolicy();
 
     @DisplayName("cost 메서드는")
     @Nested
@@ -20,10 +23,11 @@ public class DistancePaymentPolicyTest extends IsolatePathTest {
             @Test
             void it_is_default_cost() {
                 //when
-                Cost cost = paymentPolicy.cost(강남역_양재역_경로);
+                CostRequest costRequest = paymentPolicy.cost(CostRequest.of(강남역_양재역_경로));
 
                 //then
-                assertThat(cost.getCost()).isEqualTo(PaymentPolicy.DEFAULT_COST + 900);
+                Cost cost = costRequest.getCost();
+                assertThat(cost.getCost()).isEqualTo(PaymentPolicy.DEFAULT_COST);
             }
         }
 
@@ -34,10 +38,11 @@ public class DistancePaymentPolicyTest extends IsolatePathTest {
             @Test
             void it_is_additional_cost_100_by_5km() {
                 //when
-                Cost cost = paymentPolicy.cost(교대역_남부터미널역_경로);
+                CostRequest costRequest = paymentPolicy.cost(CostRequest.of(교대역_남부터미널역_경로));
 
                 //then
-                assertThat(cost.getCost()).isEqualTo(1950);
+                Cost cost = costRequest.getCost();
+                assertThat(cost.getCost()).isEqualTo(1450);
             }
         }
 
@@ -48,10 +53,11 @@ public class DistancePaymentPolicyTest extends IsolatePathTest {
             @Test
             void it_is_additional_cost_100_by_8km() {
                 //when
-                Cost cost = paymentPolicy.cost(양재역_교대역_경로);
+                CostRequest costRequest = paymentPolicy.cost(CostRequest.of(양재역_교대역_경로));
 
                 //then 59km 1250 + 800 + 200
-                assertThat(cost.getCost()).isEqualTo(2750);
+                Cost cost = costRequest.getCost();
+                assertThat(cost.getCost()).isEqualTo(2250);
             }
         }
 
