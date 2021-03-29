7c9c5f406e701e5a2d1bcfc77296b5909e59a993
eunsolJo <eunsol704@gmail.com>
2021-03-30 00:27:16 +0900

    refactor : 경로조회 문서화 - 공통로직 분리

--- src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
@@ -2,6 +2,7 @@ package nextstep.subway.path.documentation;
 
 import com.google.common.collect.Lists;
 import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
 import nextstep.subway.Documentation;
 import nextstep.subway.line.domain.PathType;
 import nextstep.subway.path.application.PathService;
@@ -9,15 +10,19 @@ import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
+import org.springframework.restdocs.request.RequestParametersSnippet;
+import org.springframework.restdocs.restassured3.RestDocumentationFilter;
 
 import java.time.LocalDateTime;
 
+import static nextstep.subway.path.acceptance.PathSteps.두_역의_최단_거리_경로_조회를_요청;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
@@ -39,31 +44,34 @@ public class PathDocumentation extends Documentation {
 
         when(pathService.findPath(anyLong(), anyLong(), any(PathType.class))).thenReturn(pathResponse);
 
-        RestAssured
-                .given(spec).log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .filter(document("path",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        requestParameters(
-                                parameterWithName("source").description("출발역 아이디"),
-                                parameterWithName("target").description("도착역 아이디"),
-                                parameterWithName("type").description("DISTANCE : 최소거리 / DURATION : 최소시간")
-                                ),
-                        responseFields(
-                                fieldWithPath("stations[]").description("역 정보"),
-                                fieldWithPath("stations[].id").description("역 아이디"),
-                                fieldWithPath("stations[].name").description("역 이름"),
-                                fieldWithPath("stations[].createdDate").description("생성일시"),
-                                fieldWithPath("stations[].modifiedDate").description("수정일시"),
-                                fieldWithPath("distance").description("구간의 거리"),
-                                fieldWithPath("duration").description("구간의 소요시간")
-                )))
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .queryParam("type", "DISTANCE")
-                .when().get("/paths")
-                .then().log().all().extract();
+        // 요청
+        RequestParametersSnippet requestParametersSnippet = requestParameters(
+                parameterWithName("source").description("출발역 아이디"),
+                parameterWithName("target").description("도착역 아이디"),
+                parameterWithName("type").description("DISTANCE : 최소거리 / DURATION : 최소시간")
+        );
+
+        // 응답
+        ResponseFieldsSnippet responseFieldsSnippet = responseFields(
+                fieldWithPath("stations[]").description("역 정보"),
+                fieldWithPath("stations[].id").description("역 아이디"),
+                fieldWithPath("stations[].name").description("역 이름"),
+                fieldWithPath("stations[].createdDate").description("생성일시"),
+                fieldWithPath("stations[].modifiedDate").description("수정일시"),
+                fieldWithPath("distance").description("구간의 거리"),
+                fieldWithPath("duration").description("구간의 소요시간")
+        );
+
+        // 필터
+        RestDocumentationFilter documentationFilter = document("path",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestParametersSnippet,
+                responseFieldsSnippet);
+
+        // RequestSpecification 정보 세팅
+        RequestSpecification requestSpecification = RestAssured.given(spec).log().all().filter(documentationFilter);
+        두_역의_최단_거리_경로_조회를_요청(requestSpecification, 1L, 2L);
     }
 }
 
