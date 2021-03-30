76ac0c3a00ff003cf4562d6390eed9326d4c0eff
임한결 <dlagksruf19@naver.com>
2021-03-31 01:33:53 +0900

    test : 요금 계산 기능 ParameterizedTest로 변경

--- src/test/java/nextstep/subway/path/domain/FareTest.java
+++ src/test/java/nextstep/subway/path/domain/FareTest.java
@@ -1,44 +1,19 @@
 package nextstep.subway.path.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FareTest {
 
-    @Test
-    @DisplayName("거리가 10km 이내일 경우 기본 요금")
-    void getFare10KmUnder() {
-        Fare fare = new Fare(10);
-        assertThat(fare.getFare()).isEqualTo(1250);
-    }
-
-    @Test
-    @DisplayName("거리가 11km ~ 50km 이내일 경우 5Km 당 100원 추가요금")
-    void getFare10KmOverAnd50KmUnder1() {
-        Fare fare = new Fare(20);
-        assertThat(fare.getFare()).isEqualTo(1450);
-    }
-
-    @Test
-    @DisplayName("거리가 11km ~ 50km 이내일 경우 5Km 당 100원 추가요금")
-    void getFare10KmOverAnd50KmUnder2() {
-        Fare fare = new Fare(50);
-        assertThat(fare.getFare()).isEqualTo(2050);
-    }
-
-    @Test
-    @DisplayName("거리가 50km 초과될 경우 8Km 당 100원 추가요금")
-    void getFare50KmOver1() {
-        Fare fare = new Fare(51);
-        assertThat(fare.getFare()).isEqualTo(2150);
-    }
-
-    @Test
-    @DisplayName("거리가 50km 초과될 경우 8Km 당 100원 추가요금")
-    void getFare50KmOver2() {
-        Fare fare = new Fare(66);
-        assertThat(fare.getFare()).isEqualTo(2250);
+    @ParameterizedTest
+    @CsvSource(value = {"10:1250", "20:1450", "50:2050", "51:2150", "66:2250"}, delimiter = ':')
+    @DisplayName("요금 계산")
+    void getFare(int distance, int expected) {
+        FareRuleStrategy strategy = FareStrategyFactory.from(distance);
+        Fare fare = new Fare(strategy, distance);
+        assertThat(fare.getFare()).isEqualTo(expected);
     }
 }
