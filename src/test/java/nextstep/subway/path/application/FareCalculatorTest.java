0db39f21ad3b5743c969dbc9fa1dc48e2f811a0f
bbbnam <bbbnam@naver.com>
2021-03-30 21:44:07 +0900

    fix : 테스트 MockitoExtension.class 누락 추가

--- src/test/java/nextstep/subway/path/application/FareCalculatorTest.java
+++ src/test/java/nextstep/subway/path/application/FareCalculatorTest.java
@@ -2,13 +2,16 @@ package nextstep.subway.path.application;
 
 import nextstep.subway.path.domain.Fare;
 import nextstep.subway.path.domain.PathResult;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class FareCalculatorTest {
 
     @Mock
