51ad68c50d890e04930ba9f2509290b13aef3e94
eunsol <30275690+eunsolJo@users.noreply.github.com>
2021-03-30 13:17:30 +0900

    1단계 - 문서화 실습 (#53)
    * docs : 경로조회 문서화
    
    * refactor : 경로조회 문서화 - 공통로직 분리

--- src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
@@ -1,22 +1,77 @@
 package nextstep.subway.path.documentation;
 
+import com.google.common.collect.Lists;
 import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
 import nextstep.subway.Documentation;
+import nextstep.subway.line.domain.PathType;
+import nextstep.subway.path.application.PathService;
+import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
+import org.springframework.restdocs.request.RequestParametersSnippet;
+import org.springframework.restdocs.restassured3.RestDocumentationFilter;
+
+import java.time.LocalDateTime;
+
+import static nextstep.subway.path.acceptance.PathSteps.두_역의_최단_거리_경로_조회를_요청;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    PathService pathService;
+
     @Test
     void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .queryParam("type", "DISTANCE")
-                .when().get("/paths")
-                .then().log().all().extract();
+
+        PathResponse pathResponse = new PathResponse(
+                Lists.newArrayList(
+                        new StationResponse(1L, "강남역", LocalDateTime.now(), LocalDateTime.now())
+                        , new StationResponse(2L, "역삼역", LocalDateTime.now(), LocalDateTime.now())
+                ), 10, 10
+        );
+
+        when(pathService.findPath(anyLong(), anyLong(), any(PathType.class))).thenReturn(pathResponse);
+
+        // 요청
+        RequestParametersSnippet requestParametersSnippet = requestParameters(
+                parameterWithName("source").description("출발역 아이디"),
+                parameterWithName("target").description("도착역 아이디"),
+                parameterWithName("type").description("DISTANCE : 최소거리 / DURATION : 최소시간")
+        );
+
+        // 응답
+        ResponseFieldsSnippet responseFieldsSnippet = responseFields(
+                fieldWithPath("stations[]").description("역 정보"),
+                fieldWithPath("stations[].id").description("역 아이디"),
+                fieldWithPath("stations[].name").description("역 이름"),
+                fieldWithPath("stations[].createdDate").description("생성일시"),
+                fieldWithPath("stations[].modifiedDate").description("수정일시"),
+                fieldWithPath("distance").description("구간의 거리"),
+                fieldWithPath("duration").description("구간의 소요시간")
+        );
+
+        // 필터
+        RestDocumentationFilter documentationFilter = document("path",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestParametersSnippet,
+                responseFieldsSnippet);
+
+        // RequestSpecification 정보 세팅
+        RequestSpecification requestSpecification = RestAssured.given(spec).log().all().filter(documentationFilter);
+        두_역의_최단_거리_경로_조회를_요청(requestSpecification, 1L, 2L);
     }
 }
 
