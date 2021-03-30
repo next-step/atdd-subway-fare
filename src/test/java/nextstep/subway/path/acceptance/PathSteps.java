3717f20ee5893795a33d82c2d204783f9d519bdb
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-03-30 11:50:27 +0900

    step1 경로 api 문서화 (#50)

--- src/test/java/nextstep/subway/path/acceptance/PathSteps.java
+++ src/test/java/nextstep/subway/path/acceptance/PathSteps.java
@@ -3,17 +3,28 @@ package nextstep.subway.path.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import nextstep.subway.line.domain.PathType;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.path.application.PathService;
 import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.dto.StationResponse;
+import org.assertj.core.util.Lists;
 import org.springframework.http.MediaType;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static nextstep.subway.line.acceptance.LineSteps.지하철_노선_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathSteps {
     public static LineResponse 지하철_노선_등록되어_있음(String name, String color, StationResponse upStation, StationResponse downStation, int distance, int duration) {
@@ -53,5 +64,32 @@ public class PathSteps {
                 .collect(Collectors.toList());
 
         assertThat(stationIds).containsExactlyElementsOf(expectedStationIds);
+    }
+
+    public static ExtractableResponse<Response> 문서를_위한_두_역의_최단_거리_경로_조회를_요청(Long source, Long target,  RequestSpecification spec) {
+        return RestAssured
+                .given(spec).log().all()
+                .filter(document("path",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())))
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("source", source)
+                .queryParam("target", target)
+                .queryParam("type", "DISTANCE")
+                .when().get("/paths")
+                .then().log().all().extract();
+    }
+
+
+    public static PathResponse 응답_만들기(int distance, int duration, StationResponse... stationResponses){
+        return new PathResponse(Lists.newArrayList(stationResponses), distance, duration);
+    }
+
+    public static StationResponse 역_만들기(Long id, String name){
+        return new StationResponse(id, name, LocalDateTime.now(), LocalDateTime.now());
+    }
+
+    public static void 경로_탐색할_때(PathService pathService, PathResponse pathResponse){
+        when(pathService.findPath(anyLong(), anyLong(), any(PathType.class))).thenReturn(pathResponse);
     }
 }
--- src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
@@ -2,21 +2,45 @@ package nextstep.subway.path.documentation;
 
 import io.restassured.RestAssured;
 import nextstep.subway.Documentation;
+import nextstep.subway.line.domain.PathType;
+import nextstep.subway.path.application.PathService;
+import nextstep.subway.path.dto.PathResponse;
+import nextstep.subway.station.dto.StationResponse;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static nextstep.subway.path.acceptance.PathSteps.*;
+import static nextstep.subway.station.StationSteps.지하철역_등록되어_있음;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
     @Test
     void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .queryParam("type", "DISTANCE")
-                .when().get("/paths")
-                .then().log().all().extract();
+        //given
+        PathResponse pathResponse = 응답_만들기(10, 10,
+                역_만들기(1L, "강남역"),
+                역_만들기(2L, "역삼역"),
+                역_만들기(3L, "선릉역")
+        );
+
+        //when
+        경로_탐색할_때(pathService, pathResponse);
+
+        //then
+        문서를_위한_두_역의_최단_거리_경로_조회를_요청(1L, 3L, spec);
     }
 }
 
