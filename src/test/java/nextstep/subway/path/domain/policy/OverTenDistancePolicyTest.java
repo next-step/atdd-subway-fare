5331406428708801d127abd2a4bd15930c871aee
bbbnam <bbbnam@naver.com>
2021-03-31 22:56:45 +0900

    test : 10킬로 초과시 요금 정책 테스트

--- src/test/java/nextstep/subway/path/domain/policy/OverTenDistancePolicyTest.java
+++ src/test/java/nextstep/subway/path/domain/policy/OverTenDistancePolicyTest.java
@@ -1,5 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package nextstep.subway.path.domain.policy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class OverTenDistancePolicyTest {
 
+    @DisplayName("거리 10킬로 넘을 때 요금 계산")
+    @ParameterizedTest
+    @CsvSource(value = {"11:1350", "16:1450", "26:1850"},  delimiter = ':')
+    void calculate(int distance, int expectedFare) {
+        // given
+        OverTenDistancePolicy policy = new OverTenDistancePolicy(distance);
+
+        // when
+        int fare = policy.calculate();
+
+        // then
+        assertThat(fare).isEqualTo(expectedFare);
+    }
 }
