50a9c1e2b6e97f93cab8fd712abdd4ff85c39ca5
bbbnam <bbbnam@naver.com>
2021-03-31 22:41:46 +0900

    refactor : 50킬로 초과시 요금 계산정책 테스트 parameterizedTest 활용으로 수정

--- src/test/java/nextstep/subway/path/domain/policy/OverFiftyDistancePolicyTest.java
+++ src/test/java/nextstep/subway/path/domain/policy/OverFiftyDistancePolicyTest.java
@@ -1,22 +1,24 @@
 package nextstep.subway.path.domain.policy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class OverFiftyDistancePolicyTest {
 
     @DisplayName("거리 50킬로 넘을 때 요금 계산")
-    @Test
-    void calculate() {
+    @ParameterizedTest
+    @CsvSource(value = {"53:2150", "59:2250", "67:2350"},  delimiter = ':')
+    void calculate(int distance, int expectedFare) {
         // given
-        OverFiftyDistancePolicy policy = new OverFiftyDistancePolicy(53);
+        OverFiftyDistancePolicy policy = new OverFiftyDistancePolicy(distance);
 
         // when
         int fare = policy.calculate();
 
         // then
-        assertThat(fare).isEqualTo(2150);
+        assertThat(fare).isEqualTo(expectedFare);
     }
 }
