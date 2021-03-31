ec8a4d99d723051cdc50866e988a13685e889eef
bbbnam <bbbnam@naver.com>
2021-04-01 00:50:06 +0900

    test : 할인 관련 테스트 코드 작성

---
+++ src/test/java/nextstep/subway/path/domain/policy/discount/ChildrenDiscountPolicyTest.java
@@ -0,0 +1,20 @@
+package nextstep.subway.path.domain.policy.discount;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ChildrenDiscountPolicyTest {
+
+    @Test
+    void discount() {
+        // given
+        ChildrenDiscountPolicy policy = new ChildrenDiscountPolicy();
+
+        // when
+        int discounted = policy.discount(1250);
+
+        // then  1250 - ((1250 - 350) * 0.5)
+        assertThat(discounted).isEqualTo(800);
+    }
+}
