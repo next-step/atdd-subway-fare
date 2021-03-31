ca5b58a630fb6a22d82dce7ba033ae3d2e2fa2b2
bbbnam <bbbnam@naver.com>
2021-04-01 00:37:54 +0900

    feat : 로그인 사용자 할인 정책 ui 로직 작성

--- src/main/java/nextstep/subway/path/application/PathService.java
+++ src/main/java/nextstep/subway/path/application/PathService.java
@@ -1,9 +1,13 @@
 package nextstep.subway.path.application;
 
 import nextstep.subway.line.domain.PathType;
+import nextstep.subway.member.domain.LoginMember;
+import nextstep.subway.path.domain.Discount;
 import nextstep.subway.path.domain.Fare;
 import nextstep.subway.path.domain.PathResult;
 import nextstep.subway.path.domain.SubwayGraph;
+import nextstep.subway.path.domain.policy.DiscountPolicy;
+import nextstep.subway.path.domain.policy.discount.DiscountPolicyFactory;
 import nextstep.subway.path.domain.policy.distance.DistancePolicyFactory;
 import nextstep.subway.path.domain.policy.FarePolicy;
 import nextstep.subway.path.domain.policy.line.LinePolicyFactory;
@@ -11,6 +15,8 @@ import nextstep.subway.path.dto.PathResponse;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class PathService {
@@ -22,19 +28,28 @@ public class PathService {
         this.stationService = stationService;
     }
 
-    public PathResponse findPath(Long source, Long target, PathType type) {
+    public PathResponse findPath(Long source, Long target, PathType type, LoginMember loginMember) {
         SubwayGraph subwayGraph = graphService.findGraph(type);
         Station sourceStation = stationService.findStationById(source);
         Station targetStation = stationService.findStationById(target);
         PathResult pathResult = subwayGraph.findPath(sourceStation, targetStation);
 
-        Fare fare = getFare(pathResult);
-        return PathResponse.of(pathResult, fare.getFareValue());
+        int fare = getFare(pathResult);
+        fare = excuteDiscount(loginMember, fare);
+        return PathResponse.of(pathResult, fare);
     }
 
-    private Fare getFare(PathResult pathResult) {
+    private int getFare(PathResult pathResult) {
         FarePolicy distancePolicy = DistancePolicyFactory.findPolicy(pathResult.getTotalDistance());
         FarePolicy linePolicy = LinePolicyFactory.findPolicy(pathResult.getMaxLineFare());
-        return Fare.calculate(distancePolicy, linePolicy);
+        return Fare.calculate(distancePolicy, linePolicy).getFareValue();
+    }
+
+    private int excuteDiscount(LoginMember loginMember, int fare) {
+        if (!Objects.isNull(loginMember)) {
+            DiscountPolicy discountPolicy = DiscountPolicyFactory.findPolicy(loginMember.getAge());
+            fare = Discount.excute(fare, discountPolicy);
+        }
+        return fare;
     }
 }
