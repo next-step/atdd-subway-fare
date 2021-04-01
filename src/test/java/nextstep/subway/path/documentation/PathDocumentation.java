55bdec8bf047734cff7289d60e2403119df872b6
perelandrax <luvyum4e@gmail.com>
2021-04-02 00:09:15 +0900

    test: 문서 테스트 성공되도록 변경

--- src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
@@ -1,19 +1,47 @@
 package nextstep.subway.path.documentation;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import nextstep.subway.Documentation;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.dto.StationResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+
+import static nextstep.subway.line.acceptance.LineSteps.지하철_노선에_지하철역_등록_요청;
+import static nextstep.subway.path.acceptance.PathSteps.지하철_노선_등록되어_있음;
+import static nextstep.subway.station.StationSteps.지하철역_등록되어_있음;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
 
 public class PathDocumentation extends Documentation {
+
+    private StationResponse 교대역;
+    private StationResponse 강남역;
+
+    @BeforeEach
+    public void setUp(RestDocumentationContextProvider restDocumentation) {
+        super.setUp(restDocumentation);
+
+        교대역 = 지하철역_등록되어_있음("교대역").as(StationResponse.class);
+        강남역 = 지하철역_등록되어_있음("강남역").as(StationResponse.class);
+
+        지하철_노선_등록되어_있음("2호선", "green", 교대역, 강남역, 10, 10);
+    }
 
     @Test
     void path() {
         RestAssured
-                .given().log().all()
+                .given(spec).log().all()
+                .filter(document("path",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
+                .queryParam("source", 교대역.getId())
+                .queryParam("target", 강남역.getId())
                 .queryParam("type", "DISTANCE")
                 .when().get("/paths")
                 .then().log().all().extract();
