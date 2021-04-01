1ab358342a8e13a01b0f18c60ee1c1bd193b78d9
임한결 <dlagksruf19@naver.com>
2021-04-01 14:57:15 +0900

    test : 노선별 추가 요금을 확인하는 인수테스트 추가

--- src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
@@ -31,8 +31,11 @@ public class PathAcceptanceTest extends AcceptanceTest {
 
     private StationResponse 교대역;
     private StationResponse 강남역;
+    private StationResponse 역삼역;
     private StationResponse 삼성역;
     private StationResponse 양재역;
+    private StationResponse 양재시민의숲역;
+    private StationResponse 청계산입구역;
     private StationResponse 남부터미널역;
 
     private LineResponse 이호선;
@@ -54,16 +57,23 @@ public class PathAcceptanceTest extends AcceptanceTest {
 
         교대역 = 지하철_역_등록_됨("교대역").as(StationResponse.class);
         강남역 = 지하철_역_등록_됨("강남역").as(StationResponse.class);
-        삼성역 = 지하철_역_등록_됨("역삼역").as(StationResponse.class);
+        역삼역 = 지하철_역_등록_됨("역삼역").as(StationResponse.class);
+        삼성역 = 지하철_역_등록_됨("삼성역").as(StationResponse.class);
         양재역 = 지하철_역_등록_됨("양재역").as(StationResponse.class);
+        양재시민의숲역 = 지하철_역_등록_됨("양재시민의숲역").as(StationResponse.class);
+        청계산입구역 = 지하철_역_등록_됨("청계산입구역").as(StationResponse.class);
         남부터미널역 = 지하철_역_등록_됨("남부터미널역").as(StationResponse.class);
 
         신분당선 = 지하철_노선_생성_요청(givenDefault(), 노선_요청("신분당선", "green", 강남역.getId(), 양재역.getId(), 5, 5)).as(LineResponse.class);
-        이호선 = 지하철_노선_생성_요청(givenDefault(), 노선_요청("2호선", "green", 교대역.getId(), 강남역.getId(), 7, 7)).as(LineResponse.class);
-        삼호선 = 지하철_노선_생성_요청(givenDefault(), 노선_요청("3호선", "green", 교대역.getId(), 남부터미널역.getId(), 3, 3)).as(LineResponse.class);
+        지하철_노선에_구간_등록_요청(givenDefault(), 신분당선.getId(), 양재역.getId(), 양재시민의숲역.getId(), 3, 3);
+        지하철_노선에_구간_등록_요청(givenDefault(), 신분당선.getId(), 양재시민의숲역.getId(), 청계산입구역.getId(), 4, 7);
 
+        이호선 = 지하철_노선_생성_요청(givenDefault(), 노선_요청("2호선", "green", 교대역.getId(), 강남역.getId(), 7, 7)).as(LineResponse.class);
+        지하철_노선에_구간_등록_요청(givenDefault(), 이호선.getId(), 강남역.getId(), 역삼역.getId(), 5, 5);
+        지하철_노선에_구간_등록_요청(givenDefault(), 이호선.getId(), 역삼역.getId(), 삼성역.getId(), 8, 8);
+
+        삼호선 = 지하철_노선_생성_요청(givenDefault(), 노선_요청("3호선", "green", 교대역.getId(), 남부터미널역.getId(), 3, 3)).as(LineResponse.class);
         지하철_노선에_구간_등록_요청(givenDefault(), 삼호선.getId(), 남부터미널역.getId(), 양재역.getId(), 3, 3);
-        지하철_노선에_구간_등록_요청(givenDefault(), 이호선.getId(), 강남역.getId(), 삼성역.getId(), 13, 13);
     }
 
     @Test
@@ -89,9 +99,33 @@ public class PathAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 지하철_최단_거리_및_최소_시간_경로_조회_요청(givenDefault(), 삼성역.getId(), 남부터미널역.getId(), PathType.DURATION);
 
         // then
-        지하철_최단_경로_조회_됨(response, Arrays.asList(삼성역.getId(), 강남역.getId(), 양재역.getId(), 남부터미널역.getId()));
+        지하철_최단_경로_조회_됨(response, Arrays.asList(삼성역.getId(), 역삼역.getId(), 강남역.getId(), 양재역.getId(), 남부터미널역.getId()));
         경로_조회_거리와_소요_시간_응답_됨(response, 21, 21);
         지하철_역_경로_조회_요금_확인(response, 1550);
+    }
+
+    @Test
+    @DisplayName("지하철 최단 거리 경로 조회 - 추가요금이 있는 신분당선을 경유할 경우 (10Km 이하)")
+    void findPathByDistanceToNewBunDang1() {
+        // when
+        ExtractableResponse<Response> response = 지하철_최단_거리_및_최소_시간_경로_조회_요청(givenDefault(), 양재역.getId(), 청계산입구역.getId(), PathType.DISTANCE);
+
+        // then
+        지하철_최단_경로_조회_됨(response, Arrays.asList(역삼역.getId(), 강남역.getId(), 양재역.getId(), 양재시민의숲역.getId()));
+        경로_조회_거리와_소요_시간_응답_됨(response, 7, 10);
+        지하철_역_경로_조회_요금_확인(response, 2150);
+    }
+
+    @Test
+    @DisplayName("지하철 최단 거리 경로 조회 - 추가요금이 있는 신분당선을 경유할 경우 (10Km 초과)")
+    void findPathByDistanceToNewBunDang2() {
+        // when
+        ExtractableResponse<Response> response = 지하철_최단_거리_및_최소_시간_경로_조회_요청(givenDefault(), 역삼역.getId(), 양재시민의숲역.getId(), PathType.DISTANCE);
+
+        // then
+        지하철_최단_경로_조회_됨(response, Arrays.asList(역삼역.getId(), 강남역.getId(), 양재역.getId(), 양재시민의숲역.getId()));
+        경로_조회_거리와_소요_시간_응답_됨(response, 13, 13);
+        지하철_역_경로_조회_요금_확인(response, 2250);
     }
 
     @Test
