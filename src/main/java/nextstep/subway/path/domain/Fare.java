6fb9246c4c5725005a264f2d2f818b0f1eb13712
dbeod2 <darung.0211@gmail.com>
2021-04-03 17:57:08 +0900

    refactor : fare 계산로직 분리

--- src/main/java/nextstep/subway/path/domain/Fare.java
+++ src/main/java/nextstep/subway/path/domain/Fare.java
@@ -1,18 +1,10 @@
 package nextstep.subway.path.domain;
 
-import nextstep.subway.path.domain.policy.FarePolicy;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Fare {
 
     private static final int BASIC_FARE = 1250;
 
     private int fare;
-
-    private List<FarePolicy> farePolicies = new ArrayList<>();
 
     public Fare() {
         this.fare = BASIC_FARE;
@@ -24,18 +16,5 @@ public class Fare {
 
     public int getFare() {
         return fare;
-    }
-
-    public Fare addAllFarePolicy(FarePolicy... policies) {
-        farePolicies.addAll(Arrays.asList(policies));
-        return this;
-    }
-
-    public int calculate(int fare) {
-        int result = fare;
-        for (FarePolicy policy : farePolicies) {
-            result = policy.fareCalculate(result);
-        }
-        return result;
     }
 }
