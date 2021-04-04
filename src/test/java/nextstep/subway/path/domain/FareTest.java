063b8f794af3cc31972f6d4373b85fab3d8205ec
bgpark <bgpark82@gmail.com>
2021-04-04 17:09:43 +0900

    refactoring: ParameterizedTest로 변경

--- src/test/java/nextstep/subway/path/domain/FareTest.java
+++ src/test/java/nextstep/subway/path/domain/FareTest.java
@@ -2,80 +2,35 @@ package nextstep.subway.path.domain;
 
 import nextstep.subway.path.domain.fare.Fare;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("요금 관련 테스트")
 class FareTest {
 
-    @DisplayName("총 거리가 10km 미만일 경우, 1250의 요금이 발생한다")
-    @Test
-    void getFareAt10() {
+    @ParameterizedTest(name = "총 거리가 {0}km일 경우, {1}의 요금이 발생한다")
+    @MethodSource("farePerDistance")
+    void getFareAt10(int distance, int expected) {
         // when
-        Fare fare = new Fare(10);
+        Fare fare = new Fare(distance);
 
         // then
-        assertThat(fare.getFare()).isEqualTo(1250);
+        assertThat(fare.getFare()).isEqualTo(expected);
     }
 
-    @DisplayName("총 거리가 11km인 경우, 1350의 요금이 발생한다")
-    @Test
-    void getFareAt11() {
-        // when
-        Fare fare = new Fare(11);
-
-        // then
-        assertThat(fare.getFare()).isEqualTo(1350);
-    }
-
-    @DisplayName("총 거리가 15km인 경우, 1350의 요금이 발생한다")
-    @Test
-    void getFareAt15() {
-        // when
-        Fare fare = new Fare(15);
-
-        // then
-        assertThat(fare.getFare()).isEqualTo(1350);
-    }
-
-    @DisplayName("총 거리가 16km인 경우, 1450의 요금이 발생한다")
-    @Test
-    void getFareAt16() {
-        // when
-        Fare fare = new Fare(16);
-
-        // then
-        assertThat(fare.getFare()).isEqualTo(1450);
-    }
-
-    @DisplayName("총 거리가 50km인 경우, 2050의 요금이 발생한다")
-    @Test
-    void getFareAt50() {
-        // when
-        Fare fare = new Fare(50);
-
-        // then
-        assertThat(fare.getFare()).isEqualTo(2050);
-    }
-
-    @DisplayName("총 거리가 57km인 경우, 2150의 요금이 발생한다")
-    @Test
-    void getFareAt57() {
-        // when
-        Fare fare = new Fare(57);
-
-        // then
-        assertThat(fare.getFare()).isEqualTo(2150);
-    }
-
-    @DisplayName("총 거리가 58km인 경우, 2150의 요금이 발생한다")
-    @Test
-    void getFareAt58() {
-        // when
-        Fare fare = new Fare(58);
-
-        // then
-        assertThat(fare.getFare()).isEqualTo(2150);
+    private static Stream<Arguments> farePerDistance() {
+        return Stream.of(
+                Arguments.of(10, 1250),
+                Arguments.of(11, 1350),
+                Arguments.of(15, 1350),
+                Arguments.of(16, 1450),
+                Arguments.of(50, 2050),
+                Arguments.of(57, 2150),
+                Arguments.of(58, 2150));
     }
 }
