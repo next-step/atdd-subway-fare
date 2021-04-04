37adaeb68b0d9b80b34a670f10479290006c38f5
bgpark <bgpark82@gmail.com>
2021-04-04 22:32:09 +0900

    test: 나이별 요금 계산 테스트

--- src/test/java/nextstep/subway/path/domain/FareTest.java
+++ src/test/java/nextstep/subway/path/domain/FareTest.java
@@ -2,6 +2,7 @@ package nextstep.subway.path.domain;
 
 import nextstep.subway.path.domain.fare.Fare;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -23,6 +24,16 @@ class FareTest {
         assertThat(fare.getFare()).isEqualTo(expected);
     }
 
+    @ParameterizedTest(name = "{0}세가 총 거리 {1}km를 탈 경우, {2}의 요금이 발생한다")
+    @MethodSource("farePerAge")
+    void getFareForChild(int age, int distance, int expected) {
+        // when
+        Fare fare = new Fare(distance, age);
+
+        // then
+        assertThat(fare.getFare()).isEqualTo(expected);
+    }
+
     private static Stream<Arguments> farePerDistance() {
         return Stream.of(
                 Arguments.of(10, 1250),
@@ -31,6 +42,19 @@ class FareTest {
                 Arguments.of(16, 1450),
                 Arguments.of(50, 2050),
                 Arguments.of(57, 2150),
-                Arguments.of(58, 2150));
+                Arguments.of(58, 2150)
+        );
     }
+
+    private static Stream<Arguments> farePerAge() {
+        return Stream.of(
+                Arguments.of(6, 10, 450),
+                Arguments.of(12, 10, 450),
+                Arguments.of(13, 10, 720),
+                Arguments.of(18, 10, 720),
+                Arguments.of(20, 10, 1250)
+        );
+    }
+
+
 }
