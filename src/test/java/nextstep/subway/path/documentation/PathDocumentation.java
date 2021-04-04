5c973ca3a69beeeb4e92cf684ef8266721f9743b
bgpark <bgpark82@gmail.com>
2021-04-04 16:59:52 +0900

    refactoring: Response Field 적용

--- src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 import org.springframework.restdocs.request.RequestParametersSnippet;
 
 import java.time.LocalDateTime;
@@ -23,6 +24,8 @@ import static nextstep.subway.path.acceptance.PathSteps.두_역의_최단_거리
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
@@ -48,11 +51,22 @@ public class PathDocumentation extends Documentation {
                 parameterWithName("source").description("출발역 아이디"),
                 parameterWithName("target").description("도착역 아이디"),
                 parameterWithName("type").description("검색 기준"));
+
+        ResponseFieldsSnippet responseFieldsSnippet = responseFields(
+                fieldWithPath("stations.[].id").description("지하철역 아이디"),
+                fieldWithPath("stations.[].name").description("지하철역 이름"),
+                fieldWithPath("stations.[].createdDate").description("생성된 날짜"),
+                fieldWithPath("stations.[].modifiedDate").description("마지막 수정된 날짜"),
+                fieldWithPath("distance").description("총 이동 거리"),
+                fieldWithPath("duration").description("총 이동 시간"),
+                fieldWithPath("fare").description("요금"));
+
         RequestSpecification requestSpecification = RestAssured
                 .given(spec).log().all()
                 .filter(document("path",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
+                        responseFieldsSnippet,
                         requestParametersSnippet));
 
         // when
