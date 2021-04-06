baea916167ce142a49df008bf6680caea808587a
boraming9 <79071041+boraming9@users.noreply.github.com>
2021-04-06 20:21:44 +0900

    Step2 : 요금조회 (#76)
    * Step2:요금조회-인수테스트(수정)
    
    * Step2:기능구현 및 인수테스트성공
    
    Co-authored-by: Admin <admin@BR-MacBook.local>

--- src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/path/documentation/PathDocumentation.java
@@ -1,31 +1,12 @@
 package nextstep.subway.path.documentation;
 
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import nextstep.subway.Documentation;
-import nextstep.subway.line.domain.PathType;
 import nextstep.subway.path.application.PathService;
 import nextstep.subway.path.dto.PathResponse;
-import nextstep.subway.station.dto.StationResponse;
-import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 
-import java.time.LocalDateTime;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
+import static nextstep.subway.path.acceptance.PathSteps.*;
 
 public class PathDocumentation extends Documentation {
 
@@ -34,40 +15,18 @@ public class PathDocumentation extends Documentation {
 
     @Test
     void path() {
-        PathResponse pathResponse = new PathResponse(
-                Lists.newArrayList(
-                        new StationResponse(1L, "강남역", LocalDateTime.now(), LocalDateTime.now())
-                        , new StationResponse(2L, "역삼역", LocalDateTime.now(), LocalDateTime.now())
-                ), 10, 9
+        //given
+        PathResponse pathResponse = 응답_만들기(30, 10, 1650,
+                역_만들기(1L, "강남역"),
+                역_만들기(2L, "역삼역"),
+                역_만들기(3L, "선릉역")
         );
 
-        when(pathService.findPath(anyLong(), anyLong(), any(PathType.class))).thenReturn(pathResponse);
+        //when
+        경로_탐색시(pathService, pathResponse);
 
-        RestAssured
-                .given(spec).log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .filter(document("path",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        requestParameters(
-                                parameterWithName("source").description("출발역ID"),
-                                parameterWithName("target").description("도착역ID"),
-                                parameterWithName("type").description("DISTANCE(최소거리)/DURATION(최소시간)")
-                        ),
-                        responseFields(
-                                fieldWithPath("stations[]").description("경로 내 역 정보"),
-                                fieldWithPath("stations[].id").description("역 아이디"),
-                                fieldWithPath("stations[].name").description("역 이름"),
-                                fieldWithPath("stations[].createdDate").description("생성일시"),
-                                fieldWithPath("stations[].modifiedDate").description("수정일시"),
-                                fieldWithPath("distance").description("거리"),
-                                fieldWithPath("duration").description("소요시간")
-                        )))
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .queryParam("type", "DISTANCE")
-                .when().get("/paths")
-                .then().log().all().extract();
+        //then
+        문서화_두_역의_최단_거리_경로_조회를_요청(spec,1L, 3L);
     }
 }
 
