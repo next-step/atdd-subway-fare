4941026d9c8708c0930b4890446c80132fdbbfea
00hongjun <00hongjun@gmail.com>
2022-02-13 01:21:31 +0900

    <test> 실습 단계 - 문서화

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,47 @@
 package nextstep.subway.documentation;
 
 import io.restassured.RestAssured;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
     @Test
     void path() {
+
+        PathResponse pathResponse = new PathResponse(
+                Arrays.asList(new StationResponse(1L, "강남역", null, null),
+                        new StationResponse(2L, "판교역", null, null)),
+                10
+        );
+
+        when(pathService.findPath(anyLong(), anyLong()))
+                .thenReturn(pathResponse);
+
         RestAssured
-                .given().log().all()
+                .given(spec).log().all()
+                .filter(document("path",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
                 .when().get("/paths")
                 .then().log().all().extract();
     }
+
 }
