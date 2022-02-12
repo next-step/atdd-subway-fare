1383fafaeea3e6895defd5ea59c061ee5be9eba3
Hero <icraft2170@gmail.com>
2022-02-13 02:09:17 +0900

    중복 제거

---
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -0,0 +1,44 @@
+package nextstep.subway.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.restassured3.RestDocumentationFilter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+public class PathSteps {
+
+    public static ExtractableResponse<Response> 경로조회_및_문서_생성(RequestSpecification spec, RestDocumentationFilter filter, Map<String, String> params) {
+        return RestAssured
+                .given(spec).log().all()
+                .filter(filter)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .params(params)
+                .when().get("/paths")
+                .then().log().all().extract();
+    }
+
+    public static Map<String, String> 경로_조회_파라미터_생성() {
+        Map<String, String> params = new HashMap<>();
+        params.put("source", "1");
+        params.put("target", "2");
+        return params;
+    }
+
+    public static RestDocumentationFilter 경로관련_문서_필터생성() {
+        RestDocumentationFilter filter = document("path",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()));
+        return filter;
+    }
+
+
+}
--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,21 +1,21 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.applicaion.PathService;
 import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
-import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.restdocs.restassured3.RestDocumentationFilter;
 import java.time.LocalDateTime;
 import java.util.Arrays;
-
+import java.util.Map;
+import static nextstep.subway.acceptance.PathSteps.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
     @MockBean
@@ -23,23 +23,35 @@ public class PathDocumentation extends Documentation {
 
     @Test
     void path() {
+        //given
+        PathResponse pathResponse = getPathResponse();
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+        RestDocumentationFilter filter = 경로관련_문서_필터생성();
+        Map<String, String> params = 경로_조회_파라미터_생성();
+
+        //when
+        ExtractableResponse<Response> response = 경로조회_및_문서_생성(spec, filter, params);
+
+        //then
+        경로조회_검증됨(response);
+    }
+
+    private PathResponse getPathResponse() {
         PathResponse pathResponse = new PathResponse(
                 Arrays.asList(
                         new StationResponse(1L, "강남역", LocalDateTime.now(), LocalDateTime.now()),
                         new StationResponse(2L, "역삼역", LocalDateTime.now(), LocalDateTime.now())
                 ), 10
         );
-        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
-
-        RestAssured
-                .given(spec).log().all()
-                .filter(document("path",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint())))
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+        return pathResponse;
     }
+
+    private void 경로조회_검증됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id")).containsExactly(1, 2);
+        assertThat(response.jsonPath().getList("stations.name")).containsExactly("강남역", "역삼역");
+    }
+
+
 }
