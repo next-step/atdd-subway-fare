7b4b535c6c4525dec8fa85056e299fde29ab27d5
00hongjun <00hongjun@gmail.com>
2022-02-13 17:30:36 +0900

    <test> 구간에 소요 시간을 추가하 테스트 추가

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -2,10 +2,9 @@ package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
 import nextstep.subway.applicaion.dto.SectionRequest;
-import nextstep.subway.domain.Line;
-import nextstep.subway.domain.LineRepository;
-import nextstep.subway.domain.Station;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.domain.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @Transactional
 class LineServiceTest {
+
     @Autowired
     private StationRepository stationRepository;
     @Autowired
@@ -24,11 +24,20 @@ class LineServiceTest {
     @Autowired
     private LineService lineService;
 
+    private Station 강남역;
+    private Station 역삼역;
+    private Station 삼성역;
+
+    @BeforeEach
+    void setUp() {
+        강남역 = stationRepository.save(new Station("강남역"));
+        역삼역 = stationRepository.save(new Station("역삼역"));
+        삼성역 = stationRepository.save(new Station("삼성역"));
+    }
+
+
     @Test
     void addSection() {
-        Station 강남역 = stationRepository.save(new Station("강남역"));
-        Station 역삼역 = stationRepository.save(new Station("역삼역"));
-        Station 삼성역 = stationRepository.save(new Station("삼성역"));
         Line 이호선 = lineRepository.save(createLine(강남역, 역삼역));
 
         lineService.addSection(이호선.getId(), new SectionRequest(역삼역.getId(), 삼성역.getId(), 10));
@@ -36,6 +45,31 @@ class LineServiceTest {
         Line line = lineService.findById(이호선.getId());
 
         assertThat(line.getSections().size()).isEqualTo(2);
+    }
+
+    @DisplayName("노선에 구간을 추가")
+    @Test
+    void addSection2() {
+        // given
+        int distance = 10;
+        int duration = 20;
+        Line 이호선 = lineRepository.save(createLine2(강남역, 역삼역, distance, duration));
+        lineService.addSection(이호선.getId(), new SectionRequest(역삼역.getId(), 삼성역.getId(), distance, duration));
+
+        // when
+        Line line = lineService.findById(이호선.getId());
+
+        // then
+        Section section = line.getSections().get(0);
+        assertThat(line.getSections().size()).isEqualTo(2);
+        assertThat(section.getDistance()).isEqualTo(distance);
+        assertThat(section.getDuration()).isEqualTo(duration);
+    }
+
+    private Line createLine2(Station 강남역, Station 역삼역, int distance, int duration) {
+        Line line = new Line("2호선", "green");
+        line.addSection2(강남역, 역삼역, distance, duration);
+        return line;
     }
 
     private Line createLine(Station 강남역, Station 역삼역) {
