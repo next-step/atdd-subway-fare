e4e3332e82e33d57dade7a153e5f784535b4a36e
itdar <thesockerr@gmail.com>
2022-02-13 15:29:00 +0900

    test: 최단거리 길찾기 기능 인수테스틑 추가

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -27,10 +27,18 @@ class PathAcceptanceTest extends AcceptanceTest {
 
     /**
      * 교대역    --- *2호선* ---   강남역
-     * |                        |
+     *  |      거리:10, 시간:3       |
+     *  |                          |
      * *3호선*                   *신분당선*
-     * |                        |
+      거리:2, 시간:5             거리:10, 시간:3
+        |                          |
+     *  |                          |
      * 남부터미널역  --- *3호선* ---   양재
+     *           거리:3, 시간:5
+     *
+     * **교대역에서~ 양재로 갈 경우**
+     * 최단거리: 교대역-남부터미널역-양재역 (거리 합: 5)
+     * 최단시간: 교대역-강남역-양재역 (시간 합: 6)
      */
     @BeforeEach
     public void setUp() {
@@ -48,6 +56,23 @@ class PathAcceptanceTest extends AcceptanceTest {
         지하철_노선에_지하철_구간_생성_요청(삼호선, createSectionCreateParams(남부터미널역, 양재역, 3));
     }
 
+    /**
+     When 출발역에서 도착역까지의 최소 시간 기준으로 경로 조회를 요청
+     Then 최소 시간 기준 경로를 응답
+     And 총 거리와 소요 시간을 함께 응답함
+     */
+    @Test
+    void findPathByDuration() {
+
+        // when
+        ExtractableResponse<Response> response = 두_역의_최단_시간_경로_조회를_요청(교대역, 양재역);
+        // then
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 강남역, 양재역);
+        assertThat(response.jsonPath().getInt("duration")).isEqualTo(6);
+        // and
+
+    }
+
     @DisplayName("두 역의 최단 거리 경로를 조회한다.")
     @Test
     void findPathByDistance() {
@@ -61,15 +86,26 @@ class PathAcceptanceTest extends AcceptanceTest {
 
     private ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target) {
         return RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", source)
-                .queryParam("target", target)
-                .when().get("/paths")
-                .then().log().all().extract();
+            .given().log().all()
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .queryParam("source", source)
+            .queryParam("target", target)
+            .when().get("/paths")
+            .then().log().all().extract();
     }
 
-    private Long 지하철_노선_생성_요청(String name, String color, Long upStation, Long downStation, int distance) {
+    private ExtractableResponse<Response> 두_역의_최단_시간_경로_조회를_요청(Long source, Long target) {
+        return RestAssured
+            .given().log().all()
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .queryParam("source", source)
+            .queryParam("target", target)
+            .when().get("/paths")
+            .then().log().all().extract();
+    }
+
+    private Long 지하철_노선_생성_요청(String name, String color, Long upStation, Long downStation
+        , int distance, int duration) {
         Map<String, String> lineCreateParams;
         lineCreateParams = new HashMap<>();
         lineCreateParams.put("name", name);
@@ -77,15 +113,18 @@ class PathAcceptanceTest extends AcceptanceTest {
         lineCreateParams.put("upStationId", upStation + "");
         lineCreateParams.put("downStationId", downStation + "");
         lineCreateParams.put("distance", distance + "");
+        lineCreateParams.put("duration", duration + "");
 
         return LineSteps.지하철_노선_생성_요청(lineCreateParams).jsonPath().getLong("id");
     }
 
-    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId, int distance) {
+    private Map<String, String> createSectionCreateParams(Long upStationId, Long downStationId
+        , int distance, int duration) {
         Map<String, String> params = new HashMap<>();
         params.put("upStationId", upStationId + "");
         params.put("downStationId", downStationId + "");
         params.put("distance", distance + "");
+        params.put("duration", duration + "");
         return params;
     }
 }
