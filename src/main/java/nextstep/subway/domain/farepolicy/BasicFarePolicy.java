42e2519dec6f2a0c7a1142fef85afea351a174c8
darkant99 <darkant99@naver.com>
2022-02-14 20:33:25 +0900

    [FEAT] 50km 까지의 요금 정책

--- src/main/java/nextstep/subway/domain/farepolicy/BasicFarePolicy.java
+++ src/main/java/nextstep/subway/domain/farepolicy/BasicFarePolicy.java
@@ -2,7 +2,7 @@ package nextstep.subway.domain.farepolicy;
 
 public class BasicFarePolicy implements FarePolicy {
     @Override
-    public int calculate() {
+    public int calculate(int distance) {
         return 1250;
     }
 }
