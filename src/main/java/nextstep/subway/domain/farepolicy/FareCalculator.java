1c69e3462d615e24f5a01b8943ceb741ccbf5c5b
darkant99 <darkant99@naver.com>
2022-02-15 21:25:08 +0900

    [FEAT] FareCalculator에 노선 추가 요금 정책 덧붙이기

--- src/main/java/nextstep/subway/domain/farepolicy/FareCalculator.java
+++ src/main/java/nextstep/subway/domain/farepolicy/FareCalculator.java
@@ -14,9 +14,10 @@ public class FareCalculator implements FarePolicy {
 
     public FareCalculator() {
         this(Arrays.asList(
-            new BasicFarePolicy(), // 기본 요금 거리 정책
+            new BasicFarePolicy(), // 기본 요금 정책
             new DistanceFarePolicy(new DistanceFareRange(10, 50), 5, 100), // 10km부터 50km 까지의 거리 정책
-            new DistanceFarePolicy(new DistanceFareRange(50, Integer.MAX_VALUE), 8, 100) // 50km 초과의 거리 정책
+            new DistanceFarePolicy(new DistanceFareRange(50, Integer.MAX_VALUE), 8, 100), // 50km 초과의 거리 정책
+            new LineAdditionalFarePolicy() // 노선별 추가 요금 정책
         ));
     }
 
--- src/test/java/nextstep/subway/unit/farepolicy/FareCalculatorTest.java
+++ src/test/java/nextstep/subway/unit/farepolicy/FareCalculatorTest.java
@@ -14,31 +14,43 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import nextstep.subway.domain.Path;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Sections;
 import nextstep.subway.domain.farepolicy.BasicFarePolicy;
 import nextstep.subway.domain.farepolicy.DistanceFarePolicy;
 import nextstep.subway.domain.farepolicy.DistanceFareRange;
 import nextstep.subway.domain.farepolicy.FareCalculator;
 import nextstep.subway.domain.farepolicy.FarePolicy;
 
-@DisplayName("경로 결과 테스트")
+@DisplayName("경로 요금 계산 결과 테스트")
 @ExtendWith(MockitoExtension.class)
 public class FareCalculatorTest {
-    @Mock
-    private Path path;
+    @Mock private Section section1;
+    @Mock private Section section2;
+    @Mock private Section section3;
+    @Mock private Sections sections;
 
     @CsvSource({
-        "9,1250",
-        "10,1250",
-        "11,1350",
-        "50,2150",
-        "51,2250",
-        "59,2350"
+        "9,2150",
+        "10,2150",
+        "11,2250",
+        "50,3050",
+        "51,3150",
+        "59,3250"
     })
     @DisplayName("요금 계산 테스트")
     @ParameterizedTest
     void calculate(int distance, int totalCost) {
         // Given
-        when(path.extractDistance()).thenReturn(distance);
+        when(section1.getAdditionalFare()).thenReturn(0);
+        when(section2.getAdditionalFare()).thenReturn(500);
+        when(section3.getAdditionalFare()).thenReturn(900);
+
+        List<Section> dummySections = Arrays.asList(section1, section2, section3);
+        when(sections.getSections()).thenReturn(dummySections);
+        when(sections.totalDistance()).thenReturn(distance);
+
+        Path path = new Path(sections);
 
         // When, Then
         FareCalculator calculator = new FareCalculator();
