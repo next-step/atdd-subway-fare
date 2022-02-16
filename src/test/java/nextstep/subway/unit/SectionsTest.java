31aa174131eef118fd40e219dadad68094654872
darkant99 <darkant99@naver.com>
2022-02-16 23:41:15 +0900

    [TEST] 같은 노선의 지하철역으로만 이루어진 Sections의 도착 시간 찾기 제약 사항

--- src/test/java/nextstep/subway/unit/SectionsTest.java
+++ src/test/java/nextstep/subway/unit/SectionsTest.java
@@ -8,7 +8,8 @@ import java.time.LocalTime;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Section;
@@ -16,6 +17,49 @@ import nextstep.subway.domain.Sections;
 
 @DisplayName("Sections 테스트")
 public class SectionsTest {
+    @CsvSource(value = {
+        "2022-02-16T10:00 -> 2022-02-16T10:10"
+    }, delimiterString = " -> ")
+    @DisplayName("같은 노선의 지하철역으로만 이루어진 Sections의 도착 시간 찾기 - 운행 시간 이내")
+    @ParameterizedTest
+    void arrivalTimeCase1(LocalDateTime startTime, LocalDateTime expectedTime) {
+        Sections sections = createSingleLineSections();
+        assertThat(sections.arrivalTime(startTime)).isEqualTo(expectedTime);
+    }
+
+    @CsvSource(value = {
+        "2022-02-16T02:00 -> 2022-02-16T05:10",
+    }, delimiterString = " -> ")
+    @DisplayName("같은 노선의 지하철역으로만 이루어진 Sections의 도착 시간 찾기 - 출발 시각이 첫차 시간 이전")
+    @ParameterizedTest
+    void arrivalTimeCase2(LocalDateTime startTime, LocalDateTime expectedTime) {
+        Sections sections = createSingleLineSections();
+        assertThat(sections.arrivalTime(startTime)).isEqualTo(expectedTime);
+    }
+
+    @CsvSource(value = {
+        "2022-02-16T11:30 -> 2022-02-17T05:13",
+    }, delimiterString = " -> ")
+    @DisplayName("같은 노선의 지하철역으로만 이루어진 Sections의 도착 시간 찾기 - 도착 시각이 막차 시간을 지남")
+    @ParameterizedTest
+    void arrivalTimeCase3(LocalDateTime startTime, LocalDateTime expectedTime) {
+        Sections sections = createSingleLineSections();
+        assertThat(sections.arrivalTime(startTime)).isEqualTo(expectedTime);
+    }
+
+    private Sections createSingleLineSections() {
+        Line lineStub = createLineStub(
+            LocalTime.of(5, 0),
+            LocalTime.of(23, 0),
+            LocalTime.of(0, 10)
+        );
+        return new Sections(Arrays.asList(
+            createSectionStub(lineStub, 3),
+            createSectionStub(lineStub, 4),
+            createSectionStub(lineStub, 3)
+        ));
+    }
+
     private Line createLineStub(LocalTime startTime, LocalTime endTime, LocalTime intervalTime) {
         Line lineStub = mock(Line.class);
         when(lineStub.getStartTime()).thenReturn(startTime);
@@ -29,33 +73,5 @@ public class SectionsTest {
         when(sectionStub.getLine()).thenReturn(line);
         when(sectionStub.getDuration()).thenReturn(duration);
         return sectionStub;
-    }
-
-    @DisplayName("같은 노선의 지하철역으로만 이루어진 Sections의 도착 시간 찾기")
-    @Test
-    void arrivalTime() {
-        // Given
-        Line lineStub = createLineStub(
-            LocalTime.of(5, 0),
-            LocalTime.of(23, 0),
-            LocalTime.of(0, 10)
-        );
-        Sections sections = new Sections(Arrays.asList(
-            createSectionStub(lineStub, 3),
-            createSectionStub(lineStub, 4),
-            createSectionStub(lineStub, 3)
-        ));
-
-        // When
-        final LocalTime START_TIME = LocalTime.of(10, 0);
-        LocalDateTime arrivalTime = sections.arrivalTime(
-            LocalDateTime.of(LocalDateTime.now().toLocalDate(), LocalTime.of(10, 0))
-        );
-
-        // Then
-        final LocalDateTime EXPECTED_TIME = LocalDateTime.of(
-            LocalDateTime.now().toLocalDate(), LocalTime.of(10, 10)
-        );
-        assertThat(arrivalTime).isEqualTo(EXPECTED_TIME);
     }
 }
