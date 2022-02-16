29c823d80a024d530b4074e48e5260e6b95d18ac
mandykr <hongseokii92@gmail.com>
2022-02-16 11:36:44 +0900

    test: 경로 조회 문서화 테스트 작성 - 경로 조회 요청 step 추가

---
+++ src/test/java/nextstep/subway/documentation/PathSteps.java
@@ -0,0 +1,17 @@
+package nextstep.subway.documentation;
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.http.MediaType;
+
+public class PathSteps {
+    public static void 경로_조회_요청(RequestSpecification spec) {
+        RestAssured
+                .given(spec).log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("source", 1L)
+                .queryParam("target", 2L)
+                .when().get("/paths")
+                .then().log().all().extract();
+    }
+}
