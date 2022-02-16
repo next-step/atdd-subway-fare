b9925025f34bb59b8689129f0d16aaba02af9b8f
mandykr <hongseokii92@gmail.com>
2022-02-16 13:34:56 +0900

    test: 최소 시간 경로 조회 인수 테스트 작성

---
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -0,0 +1,60 @@
+package nextstep.subway.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.domain.PathType;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PathSteps {
+
+    public static Long 지하철_노선_생성_요청(String name, String color, Long upStation, Long downStation, int distance, int duration) {
+        Map<String, String> lineCreateParams;
+        lineCreateParams = new HashMap<>();
+        lineCreateParams.put("name", name);
+        lineCreateParams.put("color", color);
+        lineCreateParams.put("upStationId", upStation + "");
+        lineCreateParams.put("downStationId", downStation + "");
+        lineCreateParams.put("distance", distance + "");
+        lineCreateParams.put("duration", duration + "");
+
+        return LineSteps.지하철_노선_생성_요청(lineCreateParams).jsonPath().getLong("id");
+    }
+
+    public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target) {
+        return RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("source", source)
+                .queryParam("target", target)
+                .queryParam("pathType", PathType.DISTANCE)
+                .when().get("/paths")
+                .then().log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 두_역의_최소_시간_경로_조회를_요청(Long source, Long target) {
+        return RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("source", source)
+                .queryParam("target", target)
+                .queryParam("pathType", PathType.DURATION)
+                .when().get("/paths")
+                .then().log().all().extract();
+    }
+
+    public static void 두_역의_최단_거리_경로_조회_완료(ExtractableResponse<Response> response, int distance, Long ...stations) {
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(stations);
+        assertThat(response.jsonPath().getInt("distance")).isEqualTo(distance);
+    }
+
+    public static void 두_역의_최소_시간_경로_조회_완료(ExtractableResponse<Response> response, int duration, Long ...stations) {
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(stations);
+        assertThat(response.jsonPath().getInt("duration")).isEqualTo(duration);
+    }
+}
