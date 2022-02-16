05226712c2a9f59f0466296b9b04f39d4b720b61
Hero <icraft2170@gmail.com>
2022-02-16 20:42:26 +0900

    두 역의 최단 거리 경로를 조회하는데 이때 소요 시간과 요금을 포함하는 API에 대한 인수테스트 작성

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -1,15 +1,18 @@
 package nextstep.subway.acceptance;
 
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 
 import static nextstep.subway.acceptance.LineSteps.지하철_노선에_지하철_구간_생성_요청;
 import static nextstep.subway.acceptance.PathSteps.*;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("지하철 경로 검색")
 class PathAcceptanceTest extends AcceptanceTest {
@@ -51,20 +54,12 @@ class PathAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(교대역, 양재역);
 
         // then
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
-        assertThat(response.jsonPath().getInt("distance")).isEqualTo(5);
+        assertAll(
+                () -> assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역),
+                () -> assertThat(response.jsonPath().getInt("distance")).isEqualTo(5)
+        );
     }
 
-    /**
-     * Feature: 지하철 경로 검색
-     *   Scenario: 두 역의 최소 시간 경로를 조회
-     *     Given 지하철역이 등록되어있음 O
-     *     And 지하철 노선이 등록되어있음 O
-     *     And 지하철 노선에 지하철역이 등록되어있음 O
-     *     When 출발역에서 도착역까지의 최소 시간 기준으로 경로 조회를 요청
-     *     Then 최소 시간 기준 경로를 응답
-     *     And 총 거리와 소요 시간을 함께 응답함
-     */
     @DisplayName("두 역의 최소 시간 경로를 조회")
     @Test
     void findPathByDuration() {
@@ -72,7 +67,44 @@ class PathAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 두_역의_최소_시간_경로_조회를_요청(교대역, 양재역);
 
         //then
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 강남역, 양재역);
-        assertThat(response.jsonPath().getLong("duration")).isEqualTo(5);
+        assertAll(
+                () -> assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 강남역, 양재역),
+                () -> assertThat(response.jsonPath().getLong("duration")).isEqualTo(5)
+        );
     }
+
+    /**
+     * Feature: 지하철 경로 검색
+     *   Scenario: 두 역의 최소 시간 경로를 조회
+     *     Given 지하철역이 등록되어있음
+     *     And 지하철 노선이 등록되어있음
+     *     And 지하철 노선에 지하철역이 등록되어있음
+     *     When 출발역에서 도착역까지의 최소 시간 기준으로 경로 조회를 요청
+     *     Then 최소 시간 기준 경로를 응답
+     *     And 총 거리와 소요 시간을 함께 응답함
+     *     And 지하철 이용 요금도 함께 응답함
+     */
+    @DisplayName("두 역의 최단 거리 경로를 조회한다.(With 소요 시간, 요금)")
+    @Test
+    void findPathByDistanceWithDurationAndFee() {
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("source", 교대역)
+                .queryParam("target", 양재역)
+                .when().get("/pathsNew")
+                .then().log().all().extract();
+
+
+        // then
+        assertAll(
+                () -> assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역),
+                () -> assertThat(response.jsonPath().getInt("distance")).isEqualTo(5),
+                () -> assertThat(response.jsonPath().getInt("duration")).isEqualTo(6),
+                () -> assertThat(response.jsonPath().getInt("fee")).isEqualTo(1250)
+        );
+    }
+
+
 }
