0ced40c97635b69c18c0f3411b6b581f71ff35fd
soosue <yunsoo0510@daum.net>
2022-02-16 09:31:52 +0900

    [Refactor] LineServiceTest.addSection2() 테스트 추가 & 통과

--- src/main/java/nextstep/subway/applicaion/dto/SectionRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/SectionRequest.java
@@ -4,6 +4,7 @@ public class SectionRequest {
     private Long upStationId;
     private Long downStationId;
     private int distance;
+    private int duration;
 
     public SectionRequest() {
     }
@@ -12,6 +13,13 @@ public class SectionRequest {
         this.upStationId = upStationId;
         this.downStationId = downStationId;
         this.distance = distance;
+    }
+
+    public SectionRequest(Long upStationId, Long downStationId, int distance, int duration) {
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+        this.duration = duration;
     }
 
     public Long getUpStationId() {
--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -38,9 +38,25 @@ class LineServiceTest {
         assertThat(line.getSections().size()).isEqualTo(2);
     }
 
+    @Test
+    void addSection2() {
+        Station 강남역 = stationRepository.save(new Station("강남역"));
+        Station 역삼역 = stationRepository.save(new Station("역삼역"));
+        Station 삼성역 = stationRepository.save(new Station("삼성역"));
+        Line 이호선 = lineRepository.save(createLine(강남역, 역삼역));
+
+        lineService.addSection(이호선.getId(), new SectionRequest(역삼역.getId(), 삼성역.getId(), 10, 6));
+
+        Line line = lineService.findById(이호선.getId());
+
+        assertThat(line.getSections().size()).isEqualTo(2);
+        assertThat(line.getSections().get(0).getDistance()).isEqualTo(10);
+        assertThat(line.getSections().get(0).getDuration()).isEqualTo(6);
+    }
+
     private Line createLine(Station 강남역, Station 역삼역) {
         Line line = new Line("2호선", "green");
-        line.addSection(강남역, 역삼역, 10);
+        line.addSection(강남역, 역삼역, 10, 6);
         return line;
     }
 }
