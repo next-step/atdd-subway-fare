57a9b7d75a9efc8e8c46c496ca103008c406e6e5
soosue <yunsoo0510@daum.net>
2022-02-16 09:12:57 +0900

    [Refactor] LineTest.addSectionInMiddle12() 테스트 추가

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -44,6 +44,26 @@ class LineTest {
         assertThat(section.getDistance()).isEqualTo(5);
     }
 
+    @DisplayName("상행 기준으로 목록 중간에 추가할 경우")
+    @Test
+    void addSectionInMiddle12() {
+        Station 강남역 = new Station("강남역");
+        Station 역삼역 = new Station("역삼역");
+        Station 삼성역 = new Station("삼성역");
+        Line line = new Line("2호선", "green");
+
+        line.addSection(강남역, 역삼역, 10, 6);
+        line.addSection(강남역, 삼성역, 5, 2);
+
+        assertThat(line.getSections().size()).isEqualTo(2);
+        Section section = line.getSections().stream()
+                .filter(it -> it.getUpStation() == 강남역)
+                .findFirst().orElseThrow(RuntimeException::new);
+        assertThat(section.getDownStation()).isEqualTo(삼성역);
+        assertThat(section.getDistance()).isEqualTo(5);
+        assertThat(section.getDuration()).isEqualTo(2);
+    }
+
     @DisplayName("하행 기준으로 목록 중간에 추가할 경우")
     @Test
     void addSectionInMiddle2() {
