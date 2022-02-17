200a2b4c23171a6f2160daba216f1f72b7ef4295
MrPPak <matht1115@naver.com>
2022-02-17 11:23:45 +0900

    refactor: 1차 미션 피드백 반영

--- src/test/java/nextstep/subway/path/acceptance/PathUtils.java
+++ src/test/java/nextstep/subway/path/acceptance/PathUtils.java
@@ -21,10 +21,7 @@ public class PathUtils {
 
     public static ExtractableResponse<Response> 두_역의_경로_조회를_요청(Long source, Long target, PathType type) {
         RequestSpecification requestSpecification = RestAssured.given().log().all();
-        Map<String, Object> params = new HashMap<>();
-        params.put("source", source);
-        params.put("target", target);
-        params.put("type", type.name());
+        Map<String, Object> params = 경로_조회_요청_파라미터(source, target, type);
 
         return 두_역의_경로_조회를_요청(requestSpecification, params);
     }
@@ -34,6 +31,15 @@ public class PathUtils {
                 .queryParams(params)
                 .when().get("/paths")
                 .then().log().all().extract();
+    }
+
+    public static Map<String, Object> 경로_조회_요청_파라미터(Long source, Long target, PathType type) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("source", source);
+        params.put("target", target);
+        params.put("type", type.name());
+
+        return params;
     }
 
     public static void 경로_조회_성공(ExtractableResponse<Response> response, Iterable<Long> stations) {
