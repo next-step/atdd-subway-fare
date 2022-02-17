b7001db600435ad9ac03c5384865b22a9f7474fd
00hongjun <00hongjun@gmail.com>
2022-02-17 19:07:42 +0900

    <refactor> 요금 계산 관련 변수명 수정

--- src/main/java/nextstep/subway/domain/FareDistanceStrategy.java
+++ src/main/java/nextstep/subway/domain/FareDistanceStrategy.java
@@ -9,7 +9,7 @@ public class FareDistanceStrategy implements FareCalculator {
     @Override
     public Fare calculate(int distance) {
         BigDecimal fare = BASIC_FARE;
-        if (distance <= AdditionalFare.OVER_10KM.critical) {
+        if (distance <= AdditionalFare.OVER_10KM.startChargingDistance) {
             return Fare.of(fare);
         }
 
@@ -21,23 +21,23 @@ public class FareDistanceStrategy implements FareCalculator {
 
     private BigDecimal calculateUnder50Km(int distance) {
         int fareDistance = distance;
-        if (distance > AdditionalFare.OVER_50KM.critical) {
-            fareDistance = distance - (distance - AdditionalFare.OVER_50KM.critical);
+        if (distance > AdditionalFare.OVER_50KM.startChargingDistance) {
+            fareDistance = distance - (distance - AdditionalFare.OVER_50KM.startChargingDistance);
         }
-        fareDistance -= AdditionalFare.OVER_10KM.critical;
+        fareDistance -= AdditionalFare.OVER_10KM.startChargingDistance;
         return overFare(fareDistance, AdditionalFare.OVER_10KM);
     }
 
     private BigDecimal calculateOver50Km(int distance) {
-        if (distance <= AdditionalFare.OVER_50KM.critical) {
+        if (distance <= AdditionalFare.OVER_50KM.startChargingDistance) {
             return BigDecimal.ZERO;
         }
-        int fareDistance = distance - AdditionalFare.OVER_50KM.critical;
+        int fareDistance = distance - AdditionalFare.OVER_50KM.startChargingDistance;
         return overFare(fareDistance, AdditionalFare.OVER_50KM);
     }
 
     private BigDecimal overFare(int distance, AdditionalFare additionalFare) {
-        int addCount = (int) (Math.ceil((distance - 1) / additionalFare.cycleDistance) + 1);
+        int addCount = (int) (Math.ceil((distance - 1) / additionalFare.rechargeUnitDistance) + 1);
         return additionalFare.additionalFare.multiply(BigDecimal.valueOf(addCount));
     }
 
@@ -46,13 +46,13 @@ public class FareDistanceStrategy implements FareCalculator {
         OVER_10KM(10, 5, BigDecimal.valueOf(100)),
         OVER_50KM(50, 8, BigDecimal.valueOf(100));
 
-        private int critical;
-        private int cycleDistance;
+        private int startChargingDistance;
+        private int rechargeUnitDistance;
         private BigDecimal additionalFare;
 
-        AdditionalFare(int critical, int cycleDistance, BigDecimal additionalFare) {
-            this.critical = critical;
-            this.cycleDistance = cycleDistance;
+        AdditionalFare(int startChargingDistance, int rechargeUnitDistance, BigDecimal additionalFare) {
+            this.startChargingDistance = startChargingDistance;
+            this.rechargeUnitDistance = rechargeUnitDistance;
             this.additionalFare = additionalFare;
         }
 
