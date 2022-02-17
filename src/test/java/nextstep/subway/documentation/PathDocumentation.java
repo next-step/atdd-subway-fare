79db060bb70b97811e61cfa2e21f0c8627ee65b6
livehyun <livehyun@gmail.com>
2022-02-17 09:08:08 +0900

    docs : 문서화 실습

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,15 +1,56 @@
 package nextstep.subway.documentation;
 
+import com.google.common.collect.Lists;
 import io.restassured.RestAssured;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
     @Test
     void path() {
+        PathResponse pathResponse = new PathResponse(
+                Lists.newArrayList(
+                        new StationResponse(1L, "강남역", null, null),
+                        new StationResponse(2L, "역삼역", null, null)
+                ), 10
+        );
+
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
         RestAssured
-                .given().log().all()
+                .given(spec).log().all()
+                .filter(document("path",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestParameters(
+                                parameterWithName("source").description("경로 시작역 ID"),
+                                parameterWithName("target").description("경로 도착역 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("stations").description("역 리스트"),
+                                fieldWithPath("stations[].id").description("역 아이디"),
+                                fieldWithPath("stations[].name").description("역 이름"),
+                                fieldWithPath("stations[].createdDate").description("생성일"),
+                                fieldWithPath("stations[].modifiedDate").description("변경일"),
+                                fieldWithPath("distance").description("거리")
+                        )))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
