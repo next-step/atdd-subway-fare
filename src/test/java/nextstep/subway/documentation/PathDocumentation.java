7427c005bbab20e759630d44e10f5797233895a6
mandykr <hongseokii92@gmail.com>
2022-02-17 11:08:40 +0900

    test: PathDocumentationFixture 분리

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -3,18 +3,14 @@ package nextstep.subway.documentation;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.applicaion.PathService;
-import nextstep.subway.applicaion.dto.PathResponse;
-import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.PathType;
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.restdocs.restassured3.RestDocumentationFilter;
 
-import java.time.LocalDateTime;
-
 import static nextstep.subway.acceptance.PathSteps.두_역의_경로_조회를_요청;
+import static nextstep.subway.documentation.PathDocumentationFixture.*;
 import static nextstep.subway.documentation.PathDocumentationSteps.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -29,16 +25,7 @@ public class PathDocumentation extends Documentation {
     @Test
     void path() {
         // given
-        PathResponse pathResponse = new PathResponse(
-                Lists.newArrayList(
-                        new StationResponse(1L, "강남역", LocalDateTime.now(), LocalDateTime.now()),
-                        new StationResponse(2L, "역삼역", LocalDateTime.now(), LocalDateTime.now())
-                ),
-                10,
-                20
-        );
-
-        when(pathService.findPath(anyLong(), anyLong(), any(PathType.class))).thenReturn(pathResponse);
+        when(pathService.findPath(anyLong(), anyLong(), any(PathType.class))).thenReturn(PATH_RESPONSE);
 
         // when
         RestDocumentationFilter documentFilter = getDocumentFilter(getRequestParameters(), getResponseFields());
