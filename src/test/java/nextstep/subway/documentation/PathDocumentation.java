681f2c6548233d62567ebc9b8b242ce565f1fa06
apdkeb <apdkeb@naver.com>
2022-02-17 20:18:46 +0900

    feat : 문서화 실습
    2022. 02. 17.
    PathDocumentation
    - 문서화 실습
    
    PathSteps
    - 문서화 도우미 클래스
    
    Resolves : loop-study
    See also : junwoochoi

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,43 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
+
+import static nextstep.subway.documentation.PathSteps.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@DisplayName("경로 관리(문서화)")
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
+    @DisplayName("경로 찾기 요청")
     @Test
     void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+        PathResponse pathResponse = new PathResponse(
+                Lists.newArrayList(
+                        new StationResponse(1L, "이호선", LocalDateTime.now(), LocalDateTime.now()),
+                        new StationResponse(1L, "이호선", LocalDateTime.now(), LocalDateTime.now())
+                ), 10
+        );
+
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+        ExtractableResponse<Response> 최단_경로_요청 = 최단_경로_요청(spec, 1L, 2L);
+
+        assertThat(최단_경로_요청.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
---
+++ src/test/java/nextstep/subway/documentation/PathSteps.java
@@ -0,0 +1,28 @@
+package nextstep.subway.documentation;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+public class PathSteps {
+
+    public static ExtractableResponse<Response> 최단_경로_요청(RequestSpecification spec, Long source, Long target) {
+        return RestAssured
+                .given(spec).log().all()
+                .filter(document("path",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())))
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("source", source)
+                .queryParam("target", target)
+                .when().get("/paths")
+                .then().log().all().extract();
+    }
+}
