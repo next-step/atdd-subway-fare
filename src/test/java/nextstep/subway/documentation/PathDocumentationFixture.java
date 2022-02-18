559f50e78f3502a6705d52bc2592de96106e4699
mandykr <hongseokii92@gmail.com>
2022-02-18 14:16:47 +0900

    test: 경로 조회 문서화 테스트 요금 필드 추가

--- src/test/java/nextstep/subway/documentation/PathDocumentationFixture.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentationFixture.java
@@ -15,7 +15,7 @@ public class PathDocumentationFixture {
                         createStationResponse(1L, "강남역"),
                         createStationResponse(2L, "역삼역"),
                         createStationResponse(3L, "선릉역")
-                ), 10, 20);
+                ), 10, 20, 1250);
     }
 
     private static StationResponse createStationResponse(long id, String name) {
