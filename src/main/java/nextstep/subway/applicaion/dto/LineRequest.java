3d61fd2807cd4e536263f8fff82ca3bcf58c1460
00hongjun <00hongjun@gmail.com>
2022-02-18 22:04:52 +0900

    <feat> 노선 요청,응답에 요금 추가

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -1,5 +1,7 @@
 package nextstep.subway.applicaion.dto;
 
+import java.math.BigDecimal;
+
 public class LineRequest {
 
     private String name;
@@ -8,6 +10,7 @@ public class LineRequest {
     private Long downStationId;
     private int distance;
     private int duration;
+    private BigDecimal fare = BigDecimal.ZERO;
 
     public static LineRequest of(String name, String color,
                                  long upStationId, long downStationId,
@@ -19,6 +22,16 @@ public class LineRequest {
         lineRequest.downStationId = downStationId;
         lineRequest.distance = distance;
         lineRequest.duration = duration;
+
+        return lineRequest;
+    }
+
+    public static LineRequest of(String name, String color,
+                                 long upStationId, long downStationId,
+                                 int distance, int duration,
+                                 BigDecimal fare) {
+        LineRequest lineRequest = LineRequest.of(name, color, upStationId, downStationId, distance, duration);
+        lineRequest.fare = fare;
 
         return lineRequest;
     }
@@ -45,6 +58,10 @@ public class LineRequest {
 
     public int getDuration() {
         return duration;
+    }
+
+    public BigDecimal getFare() {
+        return fare;
     }
 
 }
--- src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineResponse.java
@@ -2,32 +2,40 @@ package nextstep.subway.applicaion.dto;
 
 import nextstep.subway.domain.Line;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LineResponse {
+
     private Long id;
     private String name;
     private String color;
+    private BigDecimal fare;
     private List<StationResponse> stations;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
 
-    public static LineResponse of(Line line) {
-        List<StationResponse> stations = line.getStations().stream()
-                .map(StationResponse::of)
-                .collect(Collectors.toList());
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations, line.getCreatedDate(), line.getModifiedDate());
-    }
-
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    public LineResponse(Long id, String name,
+                        String color, BigDecimal fare,
+                        List<StationResponse> stations, LocalDateTime createdDate,
+                        LocalDateTime modifiedDate) {
         this.id = id;
         this.name = name;
         this.color = color;
         this.stations = stations;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
+        this.fare = fare;
+    }
+
+    public static LineResponse of(Line line) {
+        List<StationResponse> stations = line.getStations().stream()
+                .map(StationResponse::of)
+                .collect(Collectors.toList());
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getFarePrimitive(), stations, line.getCreatedDate(), line.getModifiedDate());
+//        return LineResponse.of(line);
     }
 
     public Long getId() {
@@ -40,6 +48,10 @@ public class LineResponse {
 
     public String getColor() {
         return color;
+    }
+
+    public BigDecimal getFare() {
+        return fare;
     }
 
     public List<StationResponse> getStations() {
