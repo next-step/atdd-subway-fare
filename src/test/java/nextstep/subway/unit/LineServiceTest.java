ffa910f2af56e9e2f8de35e96533d5b8b2006a23
soo <yunsoo0510@daum.net>
2022-02-19 18:36:03 +0900

    [Refactor] 불필요한 테스트 제거

--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,8 +1,6 @@
 package nextstep.subway.unit;
 
 import nextstep.subway.applicaion.LineService;
-import nextstep.subway.applicaion.dto.LineRequest;
-import nextstep.subway.applicaion.dto.LineResponse;
 import nextstep.subway.applicaion.dto.SectionRequest;
 import nextstep.subway.domain.Line;
 import org.junit.jupiter.api.Test;
@@ -15,18 +13,6 @@ class LineServiceTest extends ServiceTestFixture {
     private LineService lineService;
 
     @Test
-    void saveLine() {
-        // when
-        LineResponse lineResponse = lineService.saveLine(
-                new LineRequest("신분당선2", "red", 양재역.getId(), 판교역.getId(), 10, 6));
-
-        // then
-        Line line = lineService.findById(lineResponse.getId());
-        assertThat(line.getSections().get(0).getDistance()).isEqualTo(10);
-        assertThat(line.getSections().get(0).getDuration()).isEqualTo(6);
-    }
-
-    @Test
     void addSection() {
         // when
         lineService.addSection(신분당선.getId(), new SectionRequest(양재역.getId(), 판교역.getId(), 10, 6));
@@ -35,7 +21,5 @@ class LineServiceTest extends ServiceTestFixture {
         Line line = lineService.findById(신분당선.getId());
 
         assertThat(line.getSections().size()).isEqualTo(2);
-        assertThat(line.getSections().get(1).getDistance()).isEqualTo(10);
-        assertThat(line.getSections().get(1).getDuration()).isEqualTo(6);
     }
 }
