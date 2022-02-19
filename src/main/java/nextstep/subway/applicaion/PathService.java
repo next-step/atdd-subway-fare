1fc7f3893723ff3ad30bc67ed13837cddab9d672
00hongjun <00hongjun@gmail.com>
2022-02-20 00:30:30 +0900

    <feat> 로그인 처리 경로 조회 메서드 추가

--- src/main/java/nextstep/subway/applicaion/PathService.java
+++ src/main/java/nextstep/subway/applicaion/PathService.java
@@ -1,5 +1,6 @@
 package nextstep.subway.applicaion;
 
+import nextstep.member.domain.LoginMember;
 import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.domain.*;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,27 @@ public class PathService {
         Fare fare = pathFare(path);
 
         return PathResponse.of(path, fare);
+    }
+
+    public PathResponse findPath2(LoginMember loginMember, Long source, Long target) {
+        Station upStation = stationService.findById(source);
+        Station downStation = stationService.findById(target);
+        List<Line> lines = lineService.findLines();
+        SubwayMap subwayMap = new SubwayDistanceMap(lines);
+        Path path = subwayMap.findPath(upStation, downStation);
+        Fare fare = pathFare2(path, loginMember);
+
+        return PathResponse.of(path, fare);
+    }
+
+    private Fare pathFare2(Path path, LoginMember loginMember) {
+        FarePolicyChain fareDistancePolicy = new FareDistancePolicy();
+        FarePolicyChain fareLinePolicy = new FareLinePolicy();
+        fareDistancePolicy.nextChain(fareLinePolicy);
+        Fare fare = fareDistancePolicy.calculate(path);
+
+        FareAgeStrategy fareAgeStrategy = new FareAgeStrategy();
+        return fareAgeStrategy.calculate(loginMember.getAge(), fare);
     }
 
     private Fare pathFare(Path path) {
