073ee904fe20c91d3ecb000a1f6f1a67929130fd
sombrero104 <sombrero.khy@gmail.com>
2022-02-19 10:58:55 +0900

    test: PathDocumentation 테스트 문서 생성

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,50 @@
 package nextstep.subway.documentation;
 
+import com.google.common.collect.Lists;
 import io.restassured.RestAssured;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+import static org.mockito.Mockito.when;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    PathService pathService;
+
     @Test
     void path() {
+        PathResponse pathResponse = new PathResponse(
+                Lists.newArrayList(
+                        new StationResponse(1L, "강남역", LocalDateTime.now(), LocalDateTime.now()),
+                        new StationResponse(2L, "역삼역", LocalDateTime.now(), LocalDateTime.now())
+                ), 10
+        );
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
         RestAssured
-                .given().log().all()
+                .given(spec).log().all()
+                .filter(document("path",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestParameters(
+                                parameterWithName("source").description("출발역"),
+                                parameterWithName("target").description("도착역"))))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
                 .when().get("/paths")
                 .then().log().all().extract();
     }
+
 }
