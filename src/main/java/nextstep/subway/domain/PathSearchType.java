bb644d25254eeb3095fcf522f2410c82bc10ba3e
misudev <jungmisu801@gmail.com>
2022-02-20 15:10:31 +0900

    test: 경로 조회 인수테스트 수정
    - 최소 시간 경로 타입 추가

---
+++ src/main/java/nextstep/subway/domain/PathSearchType.java
@@ -0,0 +1,6 @@
+package nextstep.subway.domain;
+
+public enum PathSearchType {
+    DURATION,
+    DISTANCE
+}
--- src/test/java/nextstep/subway/acceptance/PathSteps.java
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -4,15 +4,16 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import nextstep.subway.domain.PathSearchType;
 import org.springframework.http.MediaType;
 
 public class PathSteps {
 
-    public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target) {
-        return 두_역의_최단_거리_경로_조회를_요청(RestAssured.given(), source, target);
+    public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target, PathSearchType pathSearchType) {
+        return 두_역의_최단_거리_경로_조회를_요청(RestAssured.given(), source, target, pathSearchType);
     }
 
-    public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(RequestSpecification requestSpecification, Long source, Long target) {
+    public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(RequestSpecification requestSpecification, Long source, Long target, PathSearchType pathSearchType) {
         return requestSpecification.log().all()
             .accept(MediaType.APPLICATION_JSON_VALUE)
             .queryParam("source", source)
