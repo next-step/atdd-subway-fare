d4a180de326fd6fae9223b3365994f9662495e82
sombrero104 <sombrero.khy@gmail.com>
2022-02-20 23:30:35 +0900

    refactor: 경로 조회 테스트 문서 리팩터링

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,24 +1,13 @@
 package nextstep.subway.documentation;
 
-import com.google.common.collect.Lists;
-import io.restassured.RestAssured;
 import nextstep.subway.applicaion.PathService;
-import nextstep.subway.applicaion.dto.PathResponse;
-import nextstep.subway.applicaion.dto.StationResponse;
-import nextstep.subway.domain.PathType;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
+import static nextstep.subway.documentation.PathSteps.createPathResponse;
+import static nextstep.subway.documentation.PathSteps.경로_조회_요청;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
 
@@ -27,38 +16,8 @@ public class PathDocumentation extends Documentation {
 
     @Test
     void path() {
-        PathResponse pathResponse = new PathResponse(
-                Lists.newArrayList(
-                        new StationResponse(1L, "강남역", LocalDateTime.now(), LocalDateTime.now()),
-                        new StationResponse(2L, "역삼역", LocalDateTime.now(), LocalDateTime.now())
-                ), 10, 1, 1250
-        );
-        when(pathService.findPath(any())).thenReturn(pathResponse);
-
-        Map<String, Object> params = new HashMap<>();
-        params.put("source", 1L);
-        params.put("target", 2L);
-        params.put("pathType", PathType.DISTANCE);
-        RestAssured
-                .given(spec).log().all()
-                .filter(document("path",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        requestFields(fieldWithPath("source").optional().description("출발역"),
-                                        fieldWithPath("target").optional().description("도착역"),
-                                        fieldWithPath("pathType").optional().description("경로 조회 타입")),
-                        responseFields(
-                                fieldWithPath("stations[].id").description("지하철역 아이디"),
-                                fieldWithPath("stations[].name").description("지하철역 이름"),
-                                fieldWithPath("stations[].createdDate").description("생성일"),
-                                fieldWithPath("stations[].modifiedDate").description("수정일"),
-                                fieldWithPath("distance").description("거리"),
-                                fieldWithPath("duration").description("소요시간"),
-                                fieldWithPath("fare").description("요금"))))
-                .body(params)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/paths")
-                .then().log().all().extract();
+        when(pathService.findPath(any())).thenReturn(createPathResponse());
+        경로_조회_요청(spec);
     }
 
 }
