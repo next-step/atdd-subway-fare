6002efcd7d7547f3f0c90d4e0aff2b92c368ffd6
misu <jungmisu801@gmail.com>
2022-02-20 18:32:21 +0900

    [Step1] 경로 조회 타입 추가 리뷰 요청드립니다. (#123)
    * fix(adoc): template document
    
    * test: 경로 조회 문서화 테스트 작성
    
    * refactor: PathSteps로 중복 코드 분리
    
    * test: 경로 조회 인수테스트 수정
    
    - 최소 시간 경로 타입 추가
    
    * test: 경로 조회 문서화 테스트 수정
    
    * test: 조회 인수 테스트 수정
    
    * fix: 경로 조회 타입 추가
    
    * fix : 소요 시간 정보 추가
    
    * feat: 최소 시간 경로 조회 기능 구현
    
    * test: 기존 테스트 코드 수정
    
    * delete: index.html 제거
    
    Co-authored-by: boorownie <probitanima11@gmail.com>

--- src/main/java/nextstep/subway/domain/SubwayMap.java
+++ src/main/java/nextstep/subway/domain/SubwayMap.java
@@ -14,9 +14,10 @@ public class SubwayMap {
         this.lines = lines;
     }
 
-    public Path findPath(Station source, Station target) {
+    public Path findPath(Station source, Station target, PathSearchType pathSearchType) {
         // 그래프 만들기
-        SimpleDirectedWeightedGraph<Station, SectionEdge> graph = createGraph();
+        PathWeight pathWeight = PathSearchType.DISTANCE.equals(pathSearchType) ? Section::getDistance : Section::getDuration;
+        SimpleDirectedWeightedGraph<Station, SectionEdge> graph = createGraph(pathWeight);
 
         // 다익스트라 최단 경로 찾기
         DijkstraShortestPath<Station, SectionEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
@@ -29,12 +30,11 @@ public class SubwayMap {
         return new Path(new Sections(sections));
     }
 
-    private SimpleDirectedWeightedGraph<Station, SectionEdge> createGraph() {
+    private SimpleDirectedWeightedGraph<Station, SectionEdge> createGraph(PathWeight pathWeight) {
         SimpleDirectedWeightedGraph<Station, SectionEdge> graph = new SimpleDirectedWeightedGraph<>(SectionEdge.class);
-
         addVertex(graph);
-        addEdge(graph);
-        addOppositeEdge(graph);
+        addEdge(graph, pathWeight);
+        addOppositeEdge(graph, pathWeight);
 
         return graph;
     }
@@ -48,26 +48,26 @@ public class SubwayMap {
                 .forEach(it -> graph.addVertex(it));
     }
 
-    private void addEdge(SimpleDirectedWeightedGraph<Station, SectionEdge> graph) {
+    private void addEdge(SimpleDirectedWeightedGraph<Station, SectionEdge> graph, PathWeight pathWeight) {
         // 지하철 역의 연결 정보(간선)을 등록
         lines.stream()
                 .flatMap(it -> it.getSections().stream())
                 .forEach(it -> {
                     SectionEdge sectionEdge = SectionEdge.of(it);
                     graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
-                    graph.setEdgeWeight(sectionEdge, it.getDistance());
+                    graph.setEdgeWeight(sectionEdge, pathWeight.getWeight(it));
                 });
     }
 
-    private void addOppositeEdge(SimpleDirectedWeightedGraph<Station, SectionEdge> graph) {
+    private void addOppositeEdge(SimpleDirectedWeightedGraph<Station, SectionEdge> graph, PathWeight pathWeight) {
         // 지하철 역의 연결 정보(간선)을 등록
         lines.stream()
                 .flatMap(it -> it.getSections().stream())
-                .map(it -> new Section(it.getLine(), it.getDownStation(), it.getUpStation(), it.getDistance()))
+                .map(it -> new Section(it.getLine(), it.getDownStation(), it.getUpStation(), it.getDistance(), it.getDuration()))
                 .forEach(it -> {
                     SectionEdge sectionEdge = SectionEdge.of(it);
                     graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
-                    graph.setEdgeWeight(sectionEdge, it.getDistance());
+                    graph.setEdgeWeight(sectionEdge, pathWeight.getWeight(it));
                 });
     }
 }
--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,38 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import static nextstep.subway.acceptance.PathSteps.두_역의_경로_조회를_요청;
+import static nextstep.subway.documentation.PathDocumentationSteps.getFilteredRequestSpecification;
+import static nextstep.subway.documentation.PathDocumentationSteps.getSearchPathDocumentFilter;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+import io.restassured.specification.RequestSpecification;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import nextstep.subway.domain.PathSearchType;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 public class PathDocumentation extends Documentation {
+    @MockBean
+    private PathService pathService;
 
     @Test
     void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+        PathResponse pathResponse = new PathResponse(
+            Lists.newArrayList(
+                new StationResponse(1L, "강남역", null, null),
+                new StationResponse(2L, "역삼역", null, null)
+            ), 10, 10
+        );
+
+        when(pathService.findPath(anyLong(), anyLong(), any())).thenReturn(pathResponse);
+
+        RequestSpecification filteredRequestSpecification = getFilteredRequestSpecification(spec, getSearchPathDocumentFilter("path"));
+
+        두_역의_경로_조회를_요청(filteredRequestSpecification, 1L, 2L, PathSearchType.DISTANCE);
     }
 }
