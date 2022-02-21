5df8e75c6fe70c073c7dc7ebb8ae20d80ca327f4
Hero <icraft2170@gmail.com>
2022-02-21 21:55:14 +0900

    feature : 각 종 요금 정책 구현

--- src/main/java/nextstep/subway/domain/FeePolicy.java
+++ src/main/java/nextstep/subway/domain/FeePolicy.java
@@ -7,6 +7,7 @@ public class FeePolicy {
     public static final int BASIC_FEE = 1250;
     public static final int ADDITIONAL_FEE_STANDARD_DISTANCE_FIRST = 10;
     public static final int ADDITIONAL_FEE_STANDARD_DISTANCE_SECOND = 50;
+    public static final int DEDUCTION_FEE = 350;
 
     public int totalFee(int distance) {
         return totalFee(distance, 0, 19);
@@ -17,13 +18,35 @@ public class FeePolicy {
     }
 
     public int totalFee(int distance, int additionalFee ,int age) {
+        int fee = lineAdditionalFeePolicy(BASIC_FEE, additionalFee);
+        fee = distanceFeePolicy(fee, distance);
+        fee = ageAdditionalFeePolicy(fee, age);
+        return fee;
+    }
+
+    private int distanceFeePolicy(int fee, int distance) {
         if (distance > ADDITIONAL_FEE_STANDARD_DISTANCE_SECOND) {
-            return BASIC_FEE + 800 + getAdditionalFeePerEight(distance - ADDITIONAL_FEE_STANDARD_DISTANCE_SECOND);
+            return fee + 800 + getAdditionalFeePerEight(distance - ADDITIONAL_FEE_STANDARD_DISTANCE_SECOND);
         }
         if (distance > ADDITIONAL_FEE_STANDARD_DISTANCE_FIRST) {
-            return BASIC_FEE + getAdditionalFeePerFive(distance - ADDITIONAL_FEE_STANDARD_DISTANCE_FIRST);
+            return fee + getAdditionalFeePerFive(distance - ADDITIONAL_FEE_STANDARD_DISTANCE_FIRST);
         }
-        return BASIC_FEE;
+        return fee;
+    }
+
+    private int lineAdditionalFeePolicy(int fee, int additionalFee) {
+        return fee + additionalFee;
+    }
+
+    private int ageAdditionalFeePolicy(int fee, int age) {
+        if (age >= 6 && age < 13) {
+            return (int) ((fee - DEDUCTION_FEE) * 0.5);
+        }
+
+        if (age >= 13 && age < 19) {
+            return (int) ((fee - DEDUCTION_FEE) * 0.8);
+        }
+        return fee;
     }
 
     private int getAdditionalFeePerEight(int distance) {
