bfc958521ca458da0814ada552d99576f2d43d0e
soo <yunsoo0510@daum.net>
2022-02-21 19:32:18 +0900

    [Refactor] SubwayFare에 적용

--- src/main/java/nextstep/subway/domain/SubwayFare.java
+++ src/main/java/nextstep/subway/domain/SubwayFare.java
@@ -1,13 +1,24 @@
 package nextstep.subway.domain;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 public class SubwayFare {
     private final int distance;
+    private final List<SubwayFarePolicy> subwayFarePolicies ;
 
     public SubwayFare(int distance) {
         this.distance = distance;
+        this.subwayFarePolicies = Lists.newArrayList(new DefaultFarePolicy(), new From10To50Policy(), new Over50Policy());
     }
 
     public int calculate() {
-        return 0;
+        int currentFare = 0;
+        for (SubwayFarePolicy policy : subwayFarePolicies) {
+            currentFare = policy.apply(currentFare, distance);
+        }
+
+        return currentFare;
     }
 }
