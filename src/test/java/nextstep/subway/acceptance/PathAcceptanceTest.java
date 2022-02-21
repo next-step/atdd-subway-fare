3809c5f4ca9ace2d0436def97ded2d8dda2b700d
원스톤 <onestone@socar.kr>
2022-02-22 01:51:02 +0900

    test: 50KM 이상 구간 인수 테스트 추가

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -14,7 +14,6 @@ import static nextstep.subway.acceptance.PathSteps.두_역의_경로_조회;
 import static nextstep.subway.acceptance.PathSteps.지하철_노선_생성_요청;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.BIG_DECIMAL;
 
 @DisplayName("지하철 경로 검색")
 class PathAcceptanceTest extends AcceptanceTest {
@@ -22,9 +21,13 @@ class PathAcceptanceTest extends AcceptanceTest {
     private Long 강남역;
     private Long 양재역;
     private Long 남부터미널역;
+    private Long 연신내역;
+    private Long 서울역;
+
     private Long 이호선;
     private Long 신분당선;
     private Long 삼호선;
+    private Long GTX;
 
     /**
      *                         (10km, 2분)
@@ -33,7 +36,11 @@ class PathAcceptanceTest extends AcceptanceTest {
      * (2km, 9분)  *3호선*                     *신분당선* (10km, 3분)
      *               |                           |
      *             남부터미널역 --- *3호선* ---    양재
-     *                          (3km, 8분)
+     *                         (3km, 8분)
+     *
+     *
+     *             연신내역 --- *GTX* ---    서울역
+     *                         (50km, 39분)
      */
     @BeforeEach
     public void setUp() {
@@ -43,10 +50,13 @@ class PathAcceptanceTest extends AcceptanceTest {
         강남역 = 지하철역_생성_요청("강남역").jsonPath().getLong("id");
         양재역 = 지하철역_생성_요청("양재역").jsonPath().getLong("id");
         남부터미널역 = 지하철역_생성_요청("남부터미널역").jsonPath().getLong("id");
+        연신내역 = 지하철역_생성_요청("연신내역").jsonPath().getLong("id");
+        서울역 = 지하철역_생성_요청("서울역").jsonPath().getLong("id");
 
         이호선 = 지하철_노선_생성_요청("2호선", "green", 교대역, 강남역, 10, 2);
         신분당선 = 지하철_노선_생성_요청("신분당선", "red", 강남역, 양재역, 10, 3);
         삼호선 = 지하철_노선_생성_요청("3호선", "orange", 교대역, 남부터미널역, 2, 9);
+        GTX = 지하철_노선_생성_요청("GTX", "pupple", 연신내역, 서울역, 58, 30);
 
         지하철_노선에_지하철_구간_생성_요청(삼호선, createSectionCreateParams(남부터미널역, 양재역, 3, 8));
     }
@@ -64,7 +74,7 @@ class PathAcceptanceTest extends AcceptanceTest {
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
         assertThat(response.jsonPath().getInt("distance")).isEqualTo(5);
         assertThat(response.jsonPath().getInt("duration")).isEqualTo(17);
-        assertThat(response.jsonPath().getObject("fare", BigDecimal.class)).isEqualTo(BigDecimal.valueOf(1250));
+        assertThat(response.jsonPath().getObject("fare", BigDecimal.class)).isEqualTo(BigDecimal.valueOf(1250.0));
     }
 
     /**
@@ -80,6 +90,23 @@ class PathAcceptanceTest extends AcceptanceTest {
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 강남역, 양재역);
         assertThat(response.jsonPath().getInt("distance")).isEqualTo(20);
         assertThat(response.jsonPath().getInt("duration")).isEqualTo(5);
-        assertThat(response.jsonPath().getObject("fare", BigDecimal.class)).isEqualTo(BigDecimal.valueOf(1450));
+        assertThat(response.jsonPath().getObject("fare", BigDecimal.class)).isEqualTo(BigDecimal.valueOf(1450.0));
+    }
+
+    /**
+     * 1250(1~10) + 800 (11 ~ 50) + 200 (51 ~ 58)
+     * @see nextstep.subway.ui.PathController#findPath
+     */
+    @DisplayName("두_역의_최단_시간_경로_조회_50KM이상")
+    @Test
+    void 두_역의_최단_시간_경로_조회_50KM이상() {
+        // when
+        val response = 두_역의_경로_조회(연신내역, 서울역, PathType.DURATION);
+
+        // then
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(연신내역, 서울역);
+        assertThat(response.jsonPath().getInt("distance")).isEqualTo(58);
+        assertThat(response.jsonPath().getInt("duration")).isEqualTo(30);
+        assertThat(response.jsonPath().getObject("fare", BigDecimal.class)).isEqualTo(BigDecimal.valueOf(2250.0));
     }
 }
