f0c0a989120133dfd53113db67fe661af2b5c338
Hero <icraft2170@gmail.com>
2022-02-23 23:42:37 +0900

    chore : 코드 안쓰는 import제거

--- src/main/java/nextstep/subway/applicaion/dto/FavoriteRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/FavoriteRequest.java
@@ -7,11 +7,6 @@ public class FavoriteRequest {
     public FavoriteRequest() {
     }
 
-    public FavoriteRequest(Long source, Long target) {
-        this.source = source;
-        this.target = target;
-    }
-
     public Long getSource() {
         return source;
     }
--- src/test/java/nextstep/subway/domain/FeePolicyTest.java
+++ src/test/java/nextstep/subway/domain/FeePolicyTest.java
@@ -3,11 +3,7 @@ package nextstep.subway.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class FeePolicyTest {
     FeePolicy feePolicy;
