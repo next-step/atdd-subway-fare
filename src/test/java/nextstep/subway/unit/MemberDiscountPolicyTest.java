564dc341188ce0f21c79e80a5bc22a3b9752ecc2
mandykr <hongseokii92@gmail.com>
2022-02-23 14:41:13 +0900

    test: 할인 금액 조회 유아 테스트 케이스 추가

--- src/test/java/nextstep/subway/unit/MemberDiscountPolicyTest.java
+++ src/test/java/nextstep/subway/unit/MemberDiscountPolicyTest.java
@@ -1,6 +1,5 @@
 package nextstep.subway.unit;
 
-import nextstep.subway.domain.fare.Fare;
 import nextstep.subway.domain.fare.MemberDiscountPolicy;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -10,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MemberDiscountPolicyTest {
 
     @ParameterizedTest
-    @CsvSource({"6, 0.5", "13, 0.2", "20, 0"})
+    @CsvSource({"5, 1", "6, 0.5", "13, 0.2", "20, 0"})
     void decide(int age, double rate) {
         // when
         MemberDiscountPolicy policy = MemberDiscountPolicy.decide(age);
@@ -20,14 +19,14 @@ class MemberDiscountPolicyTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"6, 1100", "13, 440", "20, 0"})
-    void calculateOverFare(int age, int expectedOverFare) {
+    @CsvSource({"5, 2550, 6, 1100", "13, 440", "20, 0"})
+    void calculateDiscountFare(int age, int expectedOverFare) {
         // given
         int beforeDiscountFare = 2550;
         MemberDiscountPolicy policy = MemberDiscountPolicy.decide(age);
 
         // when
-        int overFare = policy.calculateDiscountFare(beforeDiscountFare, Fare.BASIC_DEDUCTION_FARE);
+        int overFare = policy.calculateDiscountFare(beforeDiscountFare);
 
         // then
         assertThat(overFare).isEqualTo(expectedOverFare);
