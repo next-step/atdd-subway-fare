d24ce7c0c7694feda722ffb9f8b368d39df59e21
mandykr <hongseokii92@gmail.com>
2022-02-23 14:53:54 +0900

    test: 요금에 대한 정책 적용 기능 단위 테스트 작성

--- src/test/java/nextstep/subway/unit/FareTest.java
+++ src/test/java/nextstep/subway/unit/FareTest.java
@@ -1,6 +1,6 @@
 package nextstep.subway.unit;
 
-import nextstep.subway.domain.Fare;
+import nextstep.subway.domain.fare.Fare;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,15 +9,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FareTest {
 
     @ParameterizedTest
-    @CsvSource({"10, 1250", "20, 1450", "66, 1950"})
-    void impose(int totalDistance, int expectedFare) {
+    @CsvSource({
+            "10, 5, 0",
+            "10, 6, 850",
+            "10, 13, 1150",
+            "10, 20, 1350",
+
+            "20, 5, 0",
+            "20, 6, 950",
+            "20, 13, 1310",
+            "20, 20, 1550",
+
+            "60, 5, 0",
+            "60, 6, 1200",
+            "60, 13, 1710",
+            "60, 20, 2050"
+    })
+    void impose(int totalDistance, int memberAge, int expectedFare) {
         // given
-        Fare fare = new Fare(totalDistance);
+        int maxLineFare = 100;
+        Fare fare = new Fare();
 
         // when
-        int actualFare = fare.impose();
+        fare.impose(totalDistance, memberAge, maxLineFare);
 
         // then
-        assertThat(actualFare).isEqualTo(expectedFare);
+        assertThat(fare.getTotalFare()).isEqualTo(expectedFare);
     }
 }
