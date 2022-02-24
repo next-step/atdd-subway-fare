7478f1419db3aadc1bf465a436fbff4491264e37
monkeyDugi <lbd4946@gmail.com>
2022-02-24 10:28:50 +0900

    refactor : entity 컨버팅 메서드 생성

--- src/main/java/nextstep/subway/applicaion/dto/StationRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/StationRequest.java
@@ -1,9 +1,15 @@
 package nextstep.subway.applicaion.dto;
 
+import nextstep.subway.domain.Station;
+
 public class StationRequest {
     private String name;
 
     public String getName() {
         return name;
+    }
+
+    public Station toEntity() {
+        return new Station(name);
     }
 }
