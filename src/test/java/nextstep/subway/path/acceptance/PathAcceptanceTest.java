9b5cfdd8aeda3bc23a654a65bb9f80855a5b5f82
MrPPak <matht1115@naver.com>
2022-02-24 16:37:38 +0900

    test: PathAcceptance Test 코드 완성

--- src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/path/acceptance/PathAcceptanceTest.java
@@ -3,14 +3,17 @@ package nextstep.subway.path.acceptance;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.common.AcceptanceTest;
+import nextstep.subway.member.MemberSteps;
 import nextstep.subway.path.domain.PathType;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 import static nextstep.subway.line.acceptance.LineSteps.지하철_노선에_지하철_구간_생성_요청;
+import static nextstep.subway.member.MemberSteps.*;
 import static nextstep.subway.path.acceptance.PathUtils.*;
 import static nextstep.subway.station.StationSteps.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,13 +21,17 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("지하철 경로 검색")
 class PathAcceptanceTest extends AcceptanceTest {
-    private Long 교대역;
-    private Long 강남역;
-    private Long 양재역;
-    private Long 남부터미널역;
-    private Long 이호선;
-    private Long 신분당선;
-    private Long 삼호선;
+    public static final String EMAIL = "email@email.com";
+    public static final String PASSWORD = "password";
+    public static final int AGE = 13;
+
+    Long 교대역;
+    Long 강남역;
+    Long 양재역;
+    Long 남부터미널역;
+    Long 이호선;
+    Long 신분당선;
+    Long 삼호선;
 
     /**          (40km, 40min)
      * 교대역    --- *2호선* ---   강남역
@@ -51,7 +58,7 @@ class PathAcceptanceTest extends AcceptanceTest {
         지하철_노선에_지하철_구간_생성_요청(삼호선, createSectionCreateParams(남부터미널역, 양재역, 33, 33));
     }
 
-    @ParameterizedTest(name = "두 역의 경로를 조회한다. [{arguments}]")
+    @ParameterizedTest(name = "두 역의 최소 경로를 조회한다. [{arguments}]")
     @EnumSource(PathType.class)
     void findPath(PathType type) {
         // when
@@ -59,5 +66,20 @@ class PathAcceptanceTest extends AcceptanceTest {
 
         // then
         경로_조회_성공(response, Lists.newArrayList(교대역, 남부터미널역, 양재역));
+    }
+
+    @ParameterizedTest(name = "두 역의 최소 경로를 조회한다 - 로그인(청소년) - [{arguments}]")
+    @EnumSource(PathType.class)
+    void findPathWithLogin(PathType type) {
+        // given
+        ExtractableResponse<Response> 회원_생성_응답 = 회원_생성_요청(EMAIL, PASSWORD, AGE);
+        회원_생성됨(회원_생성_응답);
+        String token = 로그인_되어_있음(EMAIL, PASSWORD);
+
+        // when
+        ExtractableResponse<Response> 경로_조회_응답 = 두_역의_경로_조회를_요청(교대역, 양재역, type, token);
+
+        // then
+        경로_조회_성공_로그인(경로_조회_응답, Lists.newArrayList(교대역, 남부터미널역, 양재역));
     }
 }
--- src/test/java/nextstep/subway/path/acceptance/PathUtils.java
+++ src/test/java/nextstep/subway/path/acceptance/PathUtils.java
@@ -19,8 +19,15 @@ public class PathUtils {
     private PathUtils() {
     }
 
-    public static ExtractableResponse<Response> 두_역의_경로_조회를_요청(Long source, Long target, PathType type) {
+    public static ExtractableResponse<Response> 두_역의_경로_조회를_요청(long source, long target, PathType type) {
         RequestSpecification requestSpecification = RestAssured.given().log().all();
+        Map<String, Object> params = 경로_조회_요청_파라미터(source, target, type);
+
+        return 두_역의_경로_조회를_요청(requestSpecification, params);
+    }
+
+    public static ExtractableResponse<Response> 두_역의_경로_조회를_요청(long source, long target, PathType type, String accessToken) {
+        RequestSpecification requestSpecification = RestAssured.given().log().all().auth().oauth2(accessToken);
         Map<String, Object> params = 경로_조회_요청_파라미터(source, target, type);
 
         return 두_역의_경로_조회를_요청(requestSpecification, params);
@@ -33,7 +40,7 @@ public class PathUtils {
                 .then().log().all().extract();
     }
 
-    public static Map<String, Object> 경로_조회_요청_파라미터(Long source, Long target, PathType type) {
+    public static Map<String, Object> 경로_조회_요청_파라미터(long source, long target, PathType type) {
         Map<String, Object> params = new HashMap<>();
         params.put("source", source);
         params.put("target", target);
@@ -49,11 +56,15 @@ public class PathUtils {
             assertThat(response.jsonPath().getInt("duration")).isEqualTo(66);
             assertThat(response.jsonPath().getInt("fare")).isEqualTo(2250);
         });
-         /*
-            어느 리뷰어님의 리뷰를 보니 하나의 테스트에는 하나의 검증이 있는 것을 추천한다면서
-            여러 검증이 필요한 경우 assertAll을 사용하라고 추천했습니다.
-            제가 생각하기에는 assertAll을 써도 검증하는 것이 3개인데 차이점이 궁금합니다 :)
-         */
+    }
+
+    public static void 경로_조회_성공_로그인(ExtractableResponse<Response> response, Iterable<Long> stations) {
+        assertAll(() -> {
+            assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactlyElementsOf(stations);
+            assertThat(response.jsonPath().getInt("distance")).isEqualTo(66);
+            assertThat(response.jsonPath().getInt("duration")).isEqualTo(66);
+            assertThat(response.jsonPath().getInt("fare")).isEqualTo(2400);
+        });
     }
 
     public static Long 지하철_노선_생성_요청(String name, String color, Long upStation, Long downStation, int distance, int duration) {
