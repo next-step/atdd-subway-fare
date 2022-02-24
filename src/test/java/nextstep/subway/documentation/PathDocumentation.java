bbd6313541afa71e99965560aef91bef9d0c03e1
songsimo <chy.song89@gmail.com>
2022-02-24 22:06:19 +0900

    Doc: Path 문서화

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -2,6 +2,8 @@ package nextstep.subway.documentation;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.*;
 
 import org.assertj.core.util.Lists;
@@ -13,6 +15,7 @@ import io.restassured.RestAssured;
 import nextstep.subway.applicaion.PathService;
 import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
+import nextstep.subway.domain.WeightType;
 
 public class PathDocumentation extends Documentation {
     @MockBean
@@ -33,10 +36,26 @@ public class PathDocumentation extends Documentation {
             .given(spec).log().all()
             .filter(document("path",
                 preprocessRequest(prettyPrint()),
-                preprocessResponse(prettyPrint())))
+                preprocessResponse(prettyPrint()),
+                requestParameters(
+                    parameterWithName("source").description("출발역 아이디"),
+                    parameterWithName("target").description("도착역 아이디"),
+                    parameterWithName("weightType").description("조회 조건")
+                ),
+                responseFields(
+                    fieldWithPath("stations").description("경로 지하철역 목록"),
+                    fieldWithPath("stations[].id").description("지하철역 아이디"),
+                    fieldWithPath("stations[].name").description("지하철역 이름"),
+                    fieldWithPath("stations[].createdDate").description("역 생성 일자"),
+                    fieldWithPath("stations[].modifiedDate").description("역 수정 일자"),
+                    fieldWithPath("distance").description("거리(km)"),
+                    fieldWithPath("duration").description("소요 시간(min)")
+                )
+            ))
             .accept(MediaType.APPLICATION_JSON_VALUE)
             .queryParam("source", 1L)
             .queryParam("target", 2L)
+            .queryParams("weightType", WeightType.DURATION)
             .when().get("/paths")
             .then().log().all().extract();
     }
