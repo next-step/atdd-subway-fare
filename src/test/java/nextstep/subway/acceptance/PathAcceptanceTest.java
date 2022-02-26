6b5f6d08111a5baef280ceb02ec79a4598e738a2
songsimo <chy.song89@gmail.com>
2022-02-27 01:52:43 +0900

    Test: 경로 조회시 요금 정보 포함 인수테스트 수정
    - 3가지 조건에 검증

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -37,7 +37,7 @@ class PathAcceptanceTest extends AcceptanceTest {
         신분당선 = 지하철_노선_생성_요청("신분당선", "red", 강남역, 양재역, 15, 2);
         삼호선 = 지하철_노선_생성_요청("3호선", "orange", 교대역, 남부터미널역, 7, 5);
 
-        지하철_노선에_지하철_구간_생성_요청(삼호선, 구간_파라메터(남부터미널역, 양재역, 2, 4));
+        지하철_노선에_지하철_구간_생성_요청(삼호선, 구간_파라메터(남부터미널역, 양재역, 5, 4));
     }
 
     @DisplayName("두 역의 최단 거리 경로를 조회한다.")
@@ -48,8 +48,7 @@ class PathAcceptanceTest extends AcceptanceTest {
 
         // then
         지하철_경로_역_확인(response, 교대역, 남부터미널역, 양재역);
-        경로_거리_확인(response, 9);
-        요금_확인(response, 1250);
+        경로_거리_확인(response, 12);
     }
 
     @DisplayName("두 역의 최단 시간 경로를 조회한다.")
@@ -61,5 +60,45 @@ class PathAcceptanceTest extends AcceptanceTest {
         // then
         지하철_경로_역_확인(response, 교대역, 남부터미널역, 양재역);
         소요_시간_확인(response, 4);
+    }
+
+    @DisplayName("거리 <= 10km 운임비용")
+    @Test
+    void 십키로_이하_거리_요금() {
+        // when
+        ExtractableResponse<Response> response = 두_역의_경로_조회를_요청(교대역, 남부터미널역, WeightType.DISTANCE);
+
+        // then
+        지하철_경로_역_확인(response, 교대역, 남부터미널역);
+        경로_거리_확인(response, 7);
+        요금_확인(response, 1250);
+    }
+
+    @DisplayName("10km < 거리 <= 50km 운임비용")
+    @Test
+    void 십키로_초과_오십키로_이하_거리_요금() {
+        // when
+        ExtractableResponse<Response> response = 두_역의_경로_조회를_요청(교대역, 양재역, WeightType.DISTANCE);
+
+        // then
+        지하철_경로_역_확인(response, 교대역, 남부터미널역, 양재역);
+        경로_거리_확인(response, 12);
+        요금_확인(response, 1350);
+    }
+
+    @DisplayName("50km < 거리 운임비용")
+    @Test
+    void 오십키로_초과_거리_요금() {
+        // given
+        Long 양재시민의숲역 = 지하철역_생성_요청("양재시민의숲역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(삼호선, 구간_파라메터(양재역, 양재시민의숲역, 59, 8));
+
+        // when
+        ExtractableResponse<Response> response = 두_역의_경로_조회를_요청(양재역, 양재시민의숲역, WeightType.DISTANCE);
+
+        // then
+        지하철_경로_역_확인(response, 양재역, 양재시민의숲역);
+        경로_거리_확인(response, 59);
+        요금_확인(response, 2250);
     }
 }
