cf8a49d70640db00e16b8c851f6d52b5c72eeb30
songsimo <chy.song89@gmail.com>
2022-02-27 13:03:41 +0900

    Refactor: 요금 계산 방식 enum 으로 관리

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -159,20 +159,6 @@ public class Sections {
     public int getFare() {
         int distance = sections.stream().mapToInt(Section::getDistance).sum();
 
-        if(distance <= 10) {
-            return 1250;
-        }
-
-        if(distance > 10 && distance <= 50) {
-            return 1250 + calculateOverFare(distance - 10);
-        }
-
-        int fare = (int) ((Math.floor((distance - 50 - 1) / 8) + 1) * 100);
-
-        return 1250 + calculateOverFare(40) + fare;
-    }
-
-    private int calculateOverFare(int distance) {
-        return (int) ((Math.floor((distance - 1) / 5) + 1) * 100);
+        return Fare.findFare(distance).calculate(distance);
     }
 }
