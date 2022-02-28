34d493580fd0b28511bd05ccfb3392af9916b551
songsimo <chy.song89@gmail.com>
2022-03-01 06:46:52 +0900

    Test: 요금 계산-노선별 추가 요금
    - 노선이 여러개 일경우 가장 높은 금액의 추가 요금만 적용

--- src/test/java/nextstep/subway/unit/FareCalculatorTest.java
+++ src/test/java/nextstep/subway/unit/FareCalculatorTest.java
@@ -2,10 +2,20 @@ package nextstep.subway.unit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import nextstep.subway.domain.FareCalculator;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Path;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.Sections;
+import nextstep.subway.domain.Station;
 
 public class FareCalculatorTest {
 	private final int 기본_운임_비용 = 1250;
@@ -13,6 +23,28 @@ public class FareCalculatorTest {
 	private final int 팔키로 = 8;
 	private final int 십이키로 = 12;
 	private final int 오십구키로 = 59;
+
+	private final int 추가요금_300 = 300;
+	private final int 추가요금_900 = 900;
+
+	private Station 교대역;
+	private Station 강남역;
+	private Station 양재역;
+	private Station 남부터미널역;
+	private Line 신분당선;
+	private Line 이호선;
+	private Line 삼호선;
+
+	@BeforeEach
+	void setUp() {
+		교대역 = createStation(1L, "교대역");
+		강남역 = createStation(2L, "강남역");
+		양재역 = createStation(3L, "양재역");
+		남부터미널역 = createStation(4L, "남부터미널역");
+
+		신분당선 = new Line("신분당선", "red", 추가요금_300);
+		이호선 = new Line("2호선", "red", 추가요금_900);
+	}
 
 	@DisplayName("거리 <= 10km 운임비용")
 	@Test
@@ -42,5 +74,27 @@ public class FareCalculatorTest {
 
 		// then
 		assertThat(운임비용).isEqualTo(기본_운임_비용 + 1000);
+	}
+
+	@DisplayName("노선별 추가 요금")
+	@Test
+	void 노선_추가_요금() {
+		// given
+		Sections sections = new Sections();
+		sections.add(new Section(신분당선, 강남역, 양재역, 3, 3));
+		sections.add(new Section(이호선, 교대역, 강남역, 3, 3));
+
+		// when
+		int 운임비용 = FareCalculator.calculate(new Path(sections));
+
+		// then
+		assertThat(운임비용).isEqualTo(기본_운임_비용 + 추가요금_900);
+	}
+
+	private Station createStation(long id, String name) {
+		Station station = new Station(name);
+		ReflectionTestUtils.setField(station, "id", id);
+
+		return station;
 	}
 }
