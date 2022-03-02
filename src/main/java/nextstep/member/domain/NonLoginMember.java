6875632b6a59f8b87192db88baac429894cc6a26
BAEKJungHo <designjava@naver.com>
2022-03-02 17:47:53 +0900

    [refactor] NonLoginMember

--- src/main/java/nextstep/member/domain/NonLoginMember.java
+++ src/main/java/nextstep/member/domain/NonLoginMember.java
@@ -2,7 +2,7 @@ package nextstep.member.domain;
 
 public class NonLoginMember extends LoginMember {
 
-    private final int DEFAULT_AGE_OF_ADULT = 20;
+    private static final int DEFAULT_AGE_OF_ADULT = 20;
 
     private static class LazyHolder {
         private static final NonLoginMember instance = new NonLoginMember();
