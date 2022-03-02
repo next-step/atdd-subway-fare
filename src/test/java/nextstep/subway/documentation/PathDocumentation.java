1c46fbcd69c298d77d1c995e888f05469d65d1b4
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2022-03-02 23:39:49 +0900

    feat : index 문서 추가

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -14,6 +14,10 @@ import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
@@ -36,11 +40,25 @@ public class PathDocumentation extends Documentation {
                 .given(spec).log().all()
                 .filter(document("path",
                         Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
-                        Preprocessors.preprocessResponse(Preprocessors.prettyPrint())))
+                        Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                        requestParameters(
+                                parameterWithName("source").description("경로 시작 ID"),
+                                parameterWithName("target").description("경로 종료 ID"),
+                                parameterWithName("type").description("경로 조회 타입")
+                        ),
+                        responseFields(
+                                fieldWithPath("stations").description("역 리스트"),
+                                fieldWithPath("stations[].id").description("역 아이디"),
+                                fieldWithPath("stations[].name").description("역 이름"),
+                                fieldWithPath("stations[].createdDate").description("생성일"),
+                                fieldWithPath("stations[].modifiedDate").description("변경일"),
+                                fieldWithPath("distance").description("거리"),
+                                fieldWithPath("duration").description("시간")
+                        )))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
-                .queryParam("type", SectionPathType.DURATION)
+                .queryParam("type", SectionPathType.DISTANCE)
                 .when().get("/paths")
                 .then().log().all().extract();
     }
