feeb346e93fcad9edde6af24427d31d1e73eb1ac
hiro <kiryuinh32@gmail.com>
2022-03-07 18:06:20 +0900

    test: 경로 조회 기능에 대한 문서작성을 위한 테스트 구현
    최단 거리 조회와 최단 시간 조회에 대한 테스트 구현 /path 하위에 snippets 을 생성 합니다.

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -4,40 +4,69 @@ import io.restassured.RestAssured;
 import nextstep.subway.applicaion.PathService;
 import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
+import nextstep.subway.domain.PathInquiryType;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 
+import java.time.LocalDateTime;
+
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
-public class PathDocumentation extends Documentation {
+class PathDocumentation extends Documentation {
 
     @MockBean
     private PathService pathService;
 
     @Test
-    void path() {
+    void 최단_경로_조회() {
         PathResponse pathResponse = new PathResponse(Lists.newArrayList(
                 new StationResponse(1L, "강남역", null, null),
                 new StationResponse(2L, "역삼역", null, null)
-        ), 10);
+        ), 10, 5);
 
-        when(pathService.findPath(anyLong(), anyLong()))
+        when(pathService.findPath(anyLong(), anyLong(), eq(PathInquiryType.DISTANCE)))
                 .thenReturn(pathResponse);
 
         RestAssured
                 .given(spec).log().all()
-                .filter(document("path",
+                .filter(document("path/distance",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint())))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
+                .queryParam("type", PathInquiryType.DISTANCE)
                 .when().get("/paths")
                 .then().log().all().extract();
     }
+
+    @Test
+    void 최단_시간_조회() {
+        PathResponse pathResponse = new PathResponse(Lists.newArrayList(
+                new StationResponse(1L, "강남역", LocalDateTime.now(), LocalDateTime.now()),
+                new StationResponse(2L, "역삼역", LocalDateTime.now(), LocalDateTime.now())
+        ), 10, 5);
+
+        when(pathService.findPath(anyLong(), anyLong(), eq(PathInquiryType.DURATION)))
+                .thenReturn(pathResponse);
+
+        RestAssured
+                .given(spec).log().all()
+                .filter(document("path/duration",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())))
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("source", 1L)
+                .queryParam("target", 2L)
+                .queryParam("type", PathInquiryType.DURATION)
+                .when().get("/paths")
+                .then().log().all().extract();
+    }
+
 }
