52d5bb709ebfe0550f2c86ac219ddcf1b8693d01
kwj1270 <kwj1270@naver.com>
2022-04-12 20:52:34 +0900

    test: 구간들 (Sections), 구간의 모든 소요시간의 합을 구한다.(중간에 추가된)

--- src/test/java/nextstep/subway/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/domain/SectionsTest.java
@@ -10,7 +10,7 @@ class SectionsTest {
 
     @DisplayName("구간의 모든 소요시간의 합을 구한다.(새로 연장된)")
     @Test
-    void totalDurationByNewLine() {
+    void totalDurationByNewSection() {
         // given
         final Station 강남역 = new Station("강남역");
         final Station 역삼역 = new Station("역삼역");
@@ -29,24 +29,24 @@ class SectionsTest {
         assertThat(actual).isEqualTo(15);
     }
 
-//    @DisplayName("중간에 들어온 구간의 모든 소요시간의 합을 구한다.")
-//    @Test
-//    void totalDuration() {
-//        // given
-//        final Station 강남역 = new Station("강남역");
-//        final Station 역삼역 = new Station("역삼역");
-//        final Station 삼성역 = new Station("삼성역");
-//
-//        final Line line = new Line("2호선", "green");
-//        line.addSection(강남역, 역삼역, 10, 10);
-//        line.addSection(강남역, 삼성역, 5, 5);
-//
-//        final Sections sections = new Sections(line.getSections());
-//
-//        // when
-//        final int actual = sections.totalDuration();
-//
-//        // then
-//        assertThat(actual).isEqualTo(10);
-//    }
+    @DisplayName("구간의 모든 소요시간의 합을 구한다.(중간에 추가된)")
+    @Test
+    void totalDurationByMiddleSection() {
+        // given
+        final Station 강남역 = new Station("강남역");
+        final Station 역삼역 = new Station("역삼역");
+        final Station 삼성역 = new Station("삼성역");
+
+        final Line line = new Line("2호선", "green");
+        line.addSection(강남역, 역삼역, 10, 10);
+        line.addSection(강남역, 삼성역, 5, 5);
+
+        final Sections sections = new Sections(line.getSections());
+
+        // when
+        final int actual = sections.totalDuration();
+
+        // then
+        assertThat(actual).isEqualTo(10);
+    }
 }
