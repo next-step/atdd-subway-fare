96e660ada0aa14794484906fa2096f9e59db8da0
임명섭 <lms@elonsoft.co.kr>
2022-08-05 23:20:56 +0900

    test 경로 조회 문서화 파라미터 및 응답값 정보 추가

--- src/test/java/nextstep/subway/acceptance/AcceptanceTestSteps.java
+++ src/test/java/nextstep/subway/acceptance/AcceptanceTestSteps.java
@@ -2,9 +2,15 @@ package nextstep.subway.acceptance;
 
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
+import org.springframework.restdocs.request.RequestParametersSnippet;
 
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class AcceptanceTestSteps {
@@ -25,6 +31,21 @@ public class AcceptanceTestSteps {
                 .given(spec).log().all()
                 .filter(document(identifier,
                         preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint())));
+                        preprocessResponse(prettyPrint())
+                ));
+    }
+
+    public static RequestSpecification given(RequestSpecification spec,
+                                             String identifier,
+                                             RequestParametersSnippet requestParametersSnippet,
+                                             ResponseFieldsSnippet responseFieldsSnippet) {
+        return RestAssured
+                .given(spec).log().all()
+                .filter(document(identifier,
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestParametersSnippet,
+                        responseFieldsSnippet
+                ));
     }
 }
--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -7,6 +7,9 @@ import nextstep.subway.applicaion.dto.StationResponse;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
+import org.springframework.restdocs.request.RequestParametersSnippet;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +18,10 @@ import static nextstep.subway.acceptance.path.PathSteps.두_역의_최단_거리
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 
 public class PathDocumentation extends Documentation {
 
@@ -33,6 +40,25 @@ public class PathDocumentation extends Documentation {
 
         when(pathService.findPath(anyLong(), anyLong(), any(PathType.class))).thenReturn(pathResponse);
 
-        두_역의_최단_거리_경로_조회를_요청(given(spec, "path"), 1L, 2L);
+        두_역의_최단_거리_경로_조회를_요청(given(spec, "path", getRequestParametersSnippet(), getResponseFieldsSnippet()), 1L, 2L);
+    }
+
+    private ResponseFieldsSnippet getResponseFieldsSnippet() {
+        return responseFields(
+                fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("지하철역 ID"),
+                fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("지하철역 이름"),
+                fieldWithPath("stations[].createdDate").type(JsonFieldType.STRING).description("생성일자"),
+                fieldWithPath("stations[].modifiedDate").type(JsonFieldType.STRING).description("수정일자"),
+                fieldWithPath("distance").type(JsonFieldType.NUMBER).description("거리"),
+                fieldWithPath("duration").type(JsonFieldType.NUMBER).description("경과 시간")
+        );
+    }
+
+    private RequestParametersSnippet getRequestParametersSnippet() {
+        return requestParameters(
+                parameterWithName("source").description("출발역 ID"),
+                parameterWithName("target").description("도착역 ID"),
+                parameterWithName("type").description("경로 조회 타입")
+        );
     }
 }
