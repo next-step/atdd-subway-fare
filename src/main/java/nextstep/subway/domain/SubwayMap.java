9bd7e9bf411406fceca93b1cc2b5865e2eedfa04
임명섭 <lms@elonsoft.co.kr>
2022-08-05 22:51:35 +0900

    refactor 메서드 분리 리팩터링

--- src/main/java/nextstep/subway/domain/SubwayMap.java
+++ src/main/java/nextstep/subway/domain/SubwayMap.java
@@ -1,5 +1,6 @@
 package nextstep.subway.domain;
 
+import nextstep.subway.exception.NotConnectSectionException;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
@@ -15,6 +16,30 @@ public class SubwayMap {
     }
 
     public Path findPath(Station source, Station target, PathType type) {
+        // 다익스트라 최단 경로 찾기
+        GraphPath<Station, SectionEdge> result = getGraphPath(source, target, type);
+
+        validateConnectSection(result);
+
+        List<Section> sections = result.getEdgeList().stream()
+                .map(it -> it.getSection())
+                .collect(Collectors.toList());
+
+        return new Path(new Sections(sections));
+    }
+
+    private void validateConnectSection(GraphPath<Station, SectionEdge> result) {
+        if (result == null) {
+            throw new NotConnectSectionException();
+        }
+    }
+
+    private GraphPath<Station, SectionEdge> getGraphPath(Station source, Station target, PathType type) {
+        DijkstraShortestPath<Station, SectionEdge> dijkstraShortestPath = new DijkstraShortestPath<>(getGraph(type));
+        return dijkstraShortestPath.getPath(source, target);
+    }
+
+    private SimpleDirectedWeightedGraph<Station, SectionEdge> getGraph(PathType type) {
         SimpleDirectedWeightedGraph<Station, SectionEdge> graph = new SimpleDirectedWeightedGraph<>(SectionEdge.class);
 
         // 지하철 역(정점)을 등록
@@ -37,11 +62,11 @@ public class SubwayMap {
         lines.stream()
                 .flatMap(it -> it.getSections().stream())
                 .map(it -> new Section(
-                        it.getLine(),
-                        it.getDownStation(),
-                        it.getUpStation(),
-                        it.getDistance(),
-                        it.getDuration()
+                                it.getLine(),
+                                it.getDownStation(),
+                                it.getUpStation(),
+                                it.getDistance(),
+                                it.getDuration()
                         )
                 )
                 .forEach(it -> {
@@ -49,15 +74,6 @@ public class SubwayMap {
                     graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
                     graph.setEdgeWeight(sectionEdge, type.getEdgeWeight(it));
                 });
-
-        // 다익스트라 최단 경로 찾기
-        DijkstraShortestPath<Station, SectionEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
-        GraphPath<Station, SectionEdge> result = dijkstraShortestPath.getPath(source, target);
-
-        List<Section> sections = result.getEdgeList().stream()
-                .map(it -> it.getSection())
-                .collect(Collectors.toList());
-
-        return new Path(new Sections(sections));
+        return graph;
     }
 }
