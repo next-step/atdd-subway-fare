a3138369a7daf9b5c0c8f2f8f3640c8a5b36ca6a
chosundeveloper <iddaumid@naver.com>
2022-08-06 19:59:41 +0900

    feat : 두 역의 최소 시간 경로를 조회 인수 조건 추가

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -3,6 +3,7 @@ package nextstep.subway.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.domain.PathType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -52,17 +53,33 @@ class PathAcceptanceTest extends AcceptanceTest {
     @Test
     void findPathByDistance() {
         // when
-        ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(교대역, 양재역);
-
+        ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(교대역, 양재역, PathType.DISTANCE);
         // then
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
     }
 
-    private ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target) {
+    /*
+        Feature: 지하철 경로 검색
+
+        Scenario: 두 역의 최소 시간 경로를 조회
+        Given 지하철역이 등록되어있음
+        And 지하철 노선이 등록되어있음
+        And 지하철 노선에 지하철역이 등록되어있음
+        When 출발역에서 도착역까지의 최소 시간 기준으로 경로 조회를 요청
+        Then 최소 시간 기준 경로를 응답
+        And 총 거리와 소요 시간을 함께 응답함
+    */
+    @DisplayName("두 역의 최소 시간 경로를 조회한다.")
+    @Test
+    void findPathByDuration() {
+
+    }
+
+    private ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target, PathType pathType) {
         return RestAssured
                 .given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/paths?source={sourceId}&target={targetId}", source, target)
+                .when().get("/paths?source={sourceId}&target={targetId}&pathType={pathType}", source, target, pathType)
                 .then().log().all().extract();
     }
 
