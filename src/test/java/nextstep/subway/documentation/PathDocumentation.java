df1602b21ba2b94472bb58408d1c1a6029ce2c03
tis <92219795+this-is-spear@users.noreply.github.com>
2022-08-06 19:25:14 +0900

    Test : PathDocumentation 수정

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -4,16 +4,24 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
 public class PathDocumentation extends Documentation {
 
     @Test
     void path() {
         RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+            .given(spec).log().all()
+            .filter(document("path",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint())))
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .queryParam("source", 1L)
+            .queryParam("target", 2L)
+            .when().get("/paths")
+            .then().log().all().extract();
     }
 }
