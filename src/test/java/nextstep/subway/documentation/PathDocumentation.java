d57ab81395544994a8f98677ec870d4a1b9b914d
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-08-07 17:55:29 +0900

    Step1 리뷰 요청드립니다 (#164)
    * feat : lombok 추가
    
    * feat : SubwayMap duration, distance로 추상화
    
    * refactor : duration 추가
    
    * refactor : duration 추가
    
    * feat : Path type 추가
    
    * feat : 두 역의 최소 시간 경로를 조회 인수 조건 추가
    
    * feat : 최소 시간 경로 조회 인수 테스트 추가
    
    * feat : Spring Docs 테스트 추가

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,71 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import nextstep.subway.acceptance.AcceptanceTestSteps;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import nextstep.subway.domain.PathType;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
+import org.springframework.restdocs.request.RequestParametersSnippet;
+
+import static nextstep.subway.acceptance.PathSteps.두_역의_최단_거리_경로_조회를_요청;
+import static nextstep.subway.acceptance.PathSteps.두_역의_최소_시간_경로_조회를_요청;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
     @Test
-    void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+    void pathByDistance() {
+
+        PathResponse pathResponse = new PathResponse(
+                Lists.newArrayList(new StationResponse(1L, "강남역"), new StationResponse(2L, "역삼역")), 10, 5);
+
+        when(pathService.findPath(any(), anyLong(), anyLong())).thenReturn(pathResponse);
+
+        AcceptanceTestSteps.given(spec, "path", getRequestParametersSnippet(), getResponseFieldsSnippet());
+
+        두_역의_최단_거리_경로_조회를_요청(AcceptanceTestSteps.given(spec, "path", getRequestParametersSnippet(), getResponseFieldsSnippet()), 1L, 2L);
     }
+
+    @Test
+    void pathByDuration() {
+
+        PathResponse pathResponse = new PathResponse(
+                Lists.newArrayList(new StationResponse(1L, "강남역"), new StationResponse(2L, "역삼역")), 10, 5);
+
+        when(pathService.findPath(any(PathType.class), anyLong(), anyLong())).thenReturn(pathResponse);
+
+        두_역의_최소_시간_경로_조회를_요청(AcceptanceTestSteps.given(spec, "path", getRequestParametersSnippet(), getResponseFieldsSnippet()), 1L, 2L);
+    }
+
+    private ResponseFieldsSnippet getResponseFieldsSnippet() {
+        return responseFields(
+                fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("지하철역 ID"),
+                fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("지하철역 이름"),
+                fieldWithPath("distance").type(JsonFieldType.NUMBER).description("거리"),
+                fieldWithPath("duration").type(JsonFieldType.NUMBER).description("경과 시간")
+        );
+    }
+
+    private RequestParametersSnippet getRequestParametersSnippet() {
+        return requestParameters(
+                parameterWithName("type").description("경로 조회 타입"),
+                parameterWithName("source").description("출발역 ID"),
+                parameterWithName("target").description("도착역 ID")
+        );
+    }
+
 }
--- src/test/java/nextstep/subway/unit/SubwayMapTest.java
+++ src/test/java/nextstep/subway/unit/SubwayMapTest.java
@@ -1,9 +1,6 @@
 package nextstep.subway.unit;
 
-import nextstep.subway.domain.Line;
-import nextstep.subway.domain.Path;
-import nextstep.subway.domain.Station;
-import nextstep.subway.domain.SubwayMap;
+import nextstep.subway.domain.*;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,17 +31,30 @@ public class SubwayMapTest {
         이호선 = new Line("2호선", "red");
         삼호선 = new Line("3호선", "red");
 
-        신분당선.addSection(강남역, 양재역, 3);
-        이호선.addSection(교대역, 강남역, 3);
-        삼호선.addSection(교대역, 남부터미널역, 5);
-        삼호선.addSection(남부터미널역, 양재역, 5);
+        신분당선.addSection(강남역, 양재역, 3, 5);
+        이호선.addSection(교대역, 강남역, 3, 5);
+        삼호선.addSection(교대역, 남부터미널역, 5, 5);
+        삼호선.addSection(남부터미널역, 양재역, 5, 5);
     }
 
     @Test
     void findPath() {
         // given
         List<Line> lines = Lists.newArrayList(신분당선, 이호선, 삼호선);
-        SubwayMap subwayMap = new SubwayMap(lines);
+        SubwayMap subwayMap = SubwayMap.create(lines, PathType.DISTANCE);
+
+        // when
+        Path path = subwayMap.findPath(교대역, 양재역);
+
+        // then
+        assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(교대역, 강남역, 양재역));
+    }
+
+    @Test
+    void 최단() {
+        // given
+        List<Line> lines = Lists.newArrayList(신분당선, 이호선, 삼호선);
+        SubwayMap subwayMap = SubwayMap.create(lines, PathType.DISTANCE);
 
         // when
         Path path = subwayMap.findPath(교대역, 양재역);
@@ -57,7 +67,7 @@ public class SubwayMapTest {
     void findPathOppositely() {
         // given
         List<Line> lines = Lists.newArrayList(신분당선, 이호선, 삼호선);
-        SubwayMap subwayMap = new SubwayMap(lines);
+        SubwayMap subwayMap = SubwayMap.create(lines, PathType.DISTANCE);
 
         // when
         Path path = subwayMap.findPath(양재역, 교대역);
