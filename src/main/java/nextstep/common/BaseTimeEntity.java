9560b255c2b5483458474e145a2bec8a39b4adb4
DahamLeee <dlek1011@gmail.com>
2022-08-08 16:32:52 +0900

    chore: add EntityListeners, EnableJpaAuditing

--- src/main/java/nextstep/common/BaseTimeEntity.java
+++ src/main/java/nextstep/common/BaseTimeEntity.java
@@ -2,11 +2,14 @@ package nextstep.common;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
     @CreationTimestamp
