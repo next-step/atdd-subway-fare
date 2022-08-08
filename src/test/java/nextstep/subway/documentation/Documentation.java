d4b6952beba8ac69ec3a4c8cd7cdcbe199ca238d
lsj8367 <lsj8367@naver.com>
2022-08-08 23:50:23 +0900

    :sparkles: 실 db 테스트로직 반영

--- src/test/java/nextstep/subway/documentation/Documentation.java
+++ src/test/java/nextstep/subway/documentation/Documentation.java
@@ -3,8 +3,11 @@ package nextstep.subway.documentation;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
+import nextstep.DataLoader;
+import nextstep.subway.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.restdocs.RestDocumentationContextProvider;
@@ -22,9 +25,18 @@ public class Documentation {
 
     protected RequestSpecification spec;
 
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @Autowired
+    private DataLoader dataLoader;
+
+
     @BeforeEach
     public void setUp(RestDocumentationContextProvider restDocumentation) {
         RestAssured.port = port;
+        databaseCleanup.execute();
+        dataLoader.loadData();
 
         this.spec = new RequestSpecBuilder()
                 .addFilter(documentationConfiguration(restDocumentation))
