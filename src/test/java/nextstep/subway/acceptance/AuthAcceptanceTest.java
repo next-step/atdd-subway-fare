8dd0d54c4df29bbcb63d58c095c641f8e9e8aeb6
Shin seongsu <tlstjdtn321@naver.com>
2022-08-10 03:37:09 +0900

    test: 로그인 실패 케이스 추가

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -4,6 +4,7 @@ import static nextstep.subway.acceptance.MemberSteps.PASSWORD_FIELD;
 import static nextstep.subway.acceptance.MemberSteps.USERNAME_FIELD;
 import static nextstep.subway.acceptance.MemberSteps.로그인_되어_있음;
 import static nextstep.subway.acceptance.MemberSteps.베이직_인증으로_내_회원_정보_조회_요청;
+import static nextstep.subway.acceptance.MemberSteps.회원_정보_조회_실패;
 import static nextstep.subway.acceptance.MemberSteps.회원_정보_조회됨;
 
 import io.restassured.RestAssured;
@@ -38,6 +39,14 @@ class AuthAcceptanceTest extends AcceptanceTest {
         회원_정보_조회됨(response, EMAIL, AGE);
     }
 
+    @DisplayName("Session 로그인 실패 테스트")
+    @Test
+    void myInfoWithSessionFail() {
+        ExtractableResponse<Response> response = 폼_로그인_후_내_회원_정보_조회_요청("NON_USER", "123");
+
+        회원_정보_조회_실패(response);
+    }
+
     @DisplayName("Bearer Auth")
     @Test
     void myInfoWithBearerAuth() {
@@ -56,8 +65,7 @@ class AuthAcceptanceTest extends AcceptanceTest {
                 new FormAuthConfig("/login/form", USERNAME_FIELD, PASSWORD_FIELD))
             .accept(MediaType.APPLICATION_JSON_VALUE)
             .when().get("/members/me")
-            .then().log().all()
-            .statusCode(HttpStatus.OK.value()).extract();
+            .then().log().all().extract();
     }
 
     public static ExtractableResponse<Response> 베어러_인증으로_내_회원_정보_조회_요청(String accessToken) {
