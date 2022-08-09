25a7b54dfb8203c4eba746cc5018897a4c540433
sangnyyy <dltkdals2202@gmail.com>
2022-08-09 17:53:48 +0900

    refactor : type 으로 최단거리를 찾을 수 있는 기능 정리

--- src/main/java/nextstep/subway/domain/SubwayMap.java
+++ src/main/java/nextstep/subway/domain/SubwayMap.java
@@ -43,28 +43,33 @@ public class SubwayMap {
     private void addEdge(SimpleDirectedWeightedGraph<Station, SectionEdge> graph, String type) {
         lines.stream()
                 .flatMap(it -> it.getSections().stream())
-                .forEach(it -> {
-                    SectionEdge sectionEdge = SectionEdge.of(it);
-                    graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
-                    if (type.equals("distance")) {
-                        graph.setEdgeWeight(sectionEdge, it.getDistance());
-                    } else if (type.equals("duration")) {
-                        graph.setEdgeWeight(sectionEdge, it.getDuration());
-                    }
-                });
+                .forEach(it -> setEdge(graph, type, it));
 
         lines.stream()
                 .flatMap(it -> it.getSections().stream())
                 .map(it -> new Section(it.getLine(), it.getDownStation(), it.getUpStation(), it.getDistance(), it.getDuration()))
-                .forEach(it -> {
-                    SectionEdge sectionEdge = SectionEdge.of(it);
-                    graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
-                    if (type.equals("distance")) {
-                        graph.setEdgeWeight(sectionEdge, it.getDistance());
-                    } else if (type.equals("duration")) {
-                        graph.setEdgeWeight(sectionEdge, it.getDuration());
-                    }
-                });
+                .forEach(it -> setEdge(graph, type, it));
+    }
+
+    private void setEdge(SimpleDirectedWeightedGraph<Station, SectionEdge> graph, String type, Section section) {
+        SectionEdge sectionEdge = SectionEdge.of(section);
+        graph.addEdge(section.getUpStation(), section.getDownStation(), sectionEdge);
+        setEdgeWeight(graph, type, section, sectionEdge);
+    }
+
+    private void setEdgeWeight(SimpleDirectedWeightedGraph<Station, SectionEdge> graph,
+                               String type, Section section, SectionEdge sectionEdge) {
+        if (type.equals("distance")) {
+            graph.setEdgeWeight(sectionEdge, section.getDistance());
+            return;
+        }
+
+        if (type.equals("duration")) {
+            graph.setEdgeWeight(sectionEdge, section.getDuration());
+            return;
+        }
+
+        throw new IllegalArgumentException("경로 조회의 타입이 잘못되었습니다.");
     }
 
     private List<Section> findShortesPathSections(Station source, Station target, SimpleDirectedWeightedGraph<Station, SectionEdge> graph) {
