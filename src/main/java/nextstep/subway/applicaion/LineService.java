12d3b4bbe571eff15e692404540e958818675bad
ryuhistory <ryuhistory@gmail.com>
2022-08-09 10:48:38 +0900

    feat :line, section api 파라미터추가(duration)

--- src/main/java/nextstep/subway/applicaion/LineService.java
+++ src/main/java/nextstep/subway/applicaion/LineService.java
@@ -1,5 +1,11 @@
 package nextstep.subway.applicaion;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import nextstep.subway.applicaion.dto.LineRequest;
 import nextstep.subway.applicaion.dto.LineResponse;
 import nextstep.subway.applicaion.dto.SectionRequest;
@@ -7,83 +13,78 @@ import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
 import nextstep.subway.domain.Station;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
 public class LineService {
-    private LineRepository lineRepository;
-    private StationService stationService;
+	private LineRepository lineRepository;
+	private StationService stationService;
 
-    public LineService(LineRepository lineRepository, StationService stationService) {
-        this.lineRepository = lineRepository;
-        this.stationService = stationService;
-    }
+	public LineService(LineRepository lineRepository, StationService stationService) {
+		this.lineRepository = lineRepository;
+		this.stationService = stationService;
+	}
 
-    @Transactional
-    public LineResponse saveLine(LineRequest request) {
-        Line line = lineRepository.save(new Line(request.getName(), request.getColor()));
-        if (request.getUpStationId() != null && request.getDownStationId() != null && request.getDistance() != 0) {
-            Station upStation = stationService.findById(request.getUpStationId());
-            Station downStation = stationService.findById(request.getDownStationId());
-            line.addSection(upStation, downStation, request.getDistance());
-        }
-        return LineResponse.of(line);
-    }
+	@Transactional
+	public LineResponse saveLine(LineRequest request) {
+		Line line = lineRepository.save(new Line(request.getName(), request.getColor()));
+		if (request.getUpStationId() != null && request.getDownStationId() != null && request.getDistance() != 0) {
+			Station upStation = stationService.findById(request.getUpStationId());
+			Station downStation = stationService.findById(request.getDownStationId());
+			line.addSection(upStation, downStation, request.getDistance(), request.getDuration());
+		}
+		return LineResponse.of(line);
+	}
 
-    public List<Line> findLines() {
-        return lineRepository.findAll();
-    }
+	public List<Line> findLines() {
+		return lineRepository.findAll();
+	}
 
-    public List<LineResponse> findLineResponses() {
-        return lineRepository.findAll().stream()
-                .map(LineResponse::of)
-                .collect(Collectors.toList());
-    }
+	public List<LineResponse> findLineResponses() {
+		return lineRepository.findAll().stream()
+			.map(LineResponse::of)
+			.collect(Collectors.toList());
+	}
 
-    public LineResponse findLineResponseById(Long id) {
-        return LineResponse.of(findById(id));
-    }
+	public LineResponse findLineResponseById(Long id) {
+		return LineResponse.of(findById(id));
+	}
 
-    public Line findById(Long id) {
-        return lineRepository.findById(id).orElseThrow(IllegalArgumentException::new);
-    }
+	public Line findById(Long id) {
+		return lineRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+	}
 
-    @Transactional
-    public void updateLine(Long id, LineRequest lineRequest) {
-        Line line = findById(id);
-        line.update(lineRequest.getName(), lineRequest.getColor());
-    }
+	@Transactional
+	public void updateLine(Long id, LineRequest lineRequest) {
+		Line line = findById(id);
+		line.update(lineRequest.getName(), lineRequest.getColor());
+	}
 
-    @Transactional
-    public void deleteLine(Long id) {
-        lineRepository.deleteById(id);
-    }
+	@Transactional
+	public void deleteLine(Long id) {
+		lineRepository.deleteById(id);
+	}
 
-    @Transactional
-    public void addSection(Long lineId, SectionRequest sectionRequest) {
-        Station upStation = stationService.findById(sectionRequest.getUpStationId());
-        Station downStation = stationService.findById(sectionRequest.getDownStationId());
-        Line line = findById(lineId);
+	@Transactional
+	public void addSection(Long lineId, SectionRequest sectionRequest) {
+		Station upStation = stationService.findById(sectionRequest.getUpStationId());
+		Station downStation = stationService.findById(sectionRequest.getDownStationId());
+		Line line = findById(lineId);
 
-        line.addSection(upStation, downStation, sectionRequest.getDistance());
-    }
+		line.addSection(upStation, downStation, sectionRequest.getDistance(), sectionRequest.getDuration());
+	}
 
-    private List<StationResponse> createStationResponses(Line line) {
-        return line.getStations().stream()
-                .map(it -> stationService.createStationResponse(it))
-                .collect(Collectors.toList());
-    }
+	private List<StationResponse> createStationResponses(Line line) {
+		return line.getStations().stream()
+			.map(it -> stationService.createStationResponse(it))
+			.collect(Collectors.toList());
+	}
 
-    @Transactional
-    public void deleteSection(Long lineId, Long stationId) {
-        Line line = findById(lineId);
-        Station station = stationService.findById(stationId);
+	@Transactional
+	public void deleteSection(Long lineId, Long stationId) {
+		Line line = findById(lineId);
+		Station station = stationService.findById(stationId);
 
-        line.deleteSection(station);
-    }
+		line.deleteSection(station);
+	}
 }
