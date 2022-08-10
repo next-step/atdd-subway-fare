b1824f950dfc00f01f7722684cfc5fa76b22b91c
kookiedncream <kim76744@naver.com>
2022-08-10 12:46:47 +0900

    test: PR반영 - SubwayMapTest 시간 검증 추가

--- src/test/java/nextstep/subway/unit/SubwayMapTest.java
+++ src/test/java/nextstep/subway/unit/SubwayMapTest.java
@@ -6,14 +6,18 @@ import nextstep.subway.domain.Station;
 import nextstep.subway.domain.SubwayMap;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
 
 import static nextstep.subway.domain.PathType.DISTANCE;
+import static nextstep.subway.domain.PathType.DURATION;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+@DisplayName("지하철 노선 검색 기능을 확인한다.")
 public class SubwayMapTest {
 
     private Station 교대역;
@@ -35,14 +39,15 @@ public class SubwayMapTest {
         이호선 = new Line("2호선", "red");
         삼호선 = new Line("3호선", "red");
 
-        신분당선.addSection(강남역, 양재역, 3, 2);
-        이호선.addSection(교대역, 강남역, 3, 2);
-        삼호선.addSection(교대역, 남부터미널역, 5, 3);
-        삼호선.addSection(남부터미널역, 양재역, 5, 3);
+        신분당선.addSection(강남역, 양재역, 3, 3);
+        이호선.addSection(교대역, 강남역, 3, 3);
+        삼호선.addSection(교대역, 남부터미널역, 5, 2);
+        삼호선.addSection(남부터미널역, 양재역, 5, 2);
     }
 
+    @DisplayName("최소 거리로 경로를 찾는다")
     @Test
-    void findPath() {
+    void findPathMinimumDistance() {
         // given
         List<Line> lines = Lists.newArrayList(신분당선, 이호선, 삼호선);
         SubwayMap subwayMap = new SubwayMap(lines);
@@ -51,11 +56,33 @@ public class SubwayMapTest {
         Path path = subwayMap.findPath(교대역, 양재역, DISTANCE);
 
         // then
-        assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(교대역, 강남역, 양재역));
+        assertAll(
+                () -> assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(교대역, 강남역, 양재역)),
+                () -> assertThat(path.extractDistance()).isEqualTo(6)
+        );
     }
 
+    @DisplayName("최소 시간으로 경로를 찾는다")
     @Test
-    void findPathOppositely() {
+    void findPathMinimumTime() {
+        // given
+        List<Line> lines = Lists.newArrayList(신분당선, 이호선, 삼호선);
+        SubwayMap subwayMap = new SubwayMap(lines);
+
+        // when
+        Path path = subwayMap.findPath(교대역, 양재역, DURATION);
+
+        // then
+        assertAll(
+                () -> assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(교대역, 남부터미널역, 양재역)),
+                () -> assertThat(path.extractDistance()).isEqualTo(10),
+                () -> assertThat(path.extractDurtaion()).isEqualTo(4)
+        );
+    }
+
+    @DisplayName("최소 거리로 경로를 반대로 찾는다")
+    @Test
+    void findPathOppositelyMinimumDistance() {
         // given
         List<Line> lines = Lists.newArrayList(신분당선, 이호선, 삼호선);
         SubwayMap subwayMap = new SubwayMap(lines);
@@ -64,7 +91,28 @@ public class SubwayMapTest {
         Path path = subwayMap.findPath(양재역, 교대역, DISTANCE);
 
         // then
-        assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(양재역, 강남역, 교대역));
+        assertAll(
+                () -> assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(양재역, 강남역, 교대역)),
+                () -> assertThat(path.extractDistance()).isEqualTo(6)
+        );
+    }
+
+    @DisplayName("최소 시간으로 경로를 반대로 찾는다")
+    @Test
+    void findPathOppositelyMinimumTime() {
+        // given
+        List<Line> lines = Lists.newArrayList(신분당선, 이호선, 삼호선);
+        SubwayMap subwayMap = new SubwayMap(lines);
+
+        // when
+        Path path = subwayMap.findPath(양재역, 교대역, DURATION);
+
+        // then
+        assertAll(
+                () -> assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(양재역, 남부터미널역, 교대역)),
+                () -> assertThat(path.extractDistance()).isEqualTo(10),
+                () -> assertThat(path.extractDurtaion()).isEqualTo(4)
+        );
     }
 
     private Station createStation(long id, String name) {
