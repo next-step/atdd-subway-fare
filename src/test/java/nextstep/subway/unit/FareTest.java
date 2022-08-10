2fc016ef39133d2185b030d638c756cdaa565a0d
tis <92219795+this-is-spear@users.noreply.github.com>
2022-08-11 01:40:24 +0900

    Refact : FareTest 코드 리팩토링

--- src/test/java/nextstep/subway/unit/FareTest.java
+++ src/test/java/nextstep/subway/unit/FareTest.java
@@ -3,6 +3,8 @@ package nextstep.subway.unit;
 import nextstep.subway.domain.Fare;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,42 +17,24 @@ class FareTest {
         assertThat(fare.getFare()).isEqualTo(1250);
     }
 
-    @Test
-    @DisplayName("거리가 11km 이면 요금은 1350원 이다.")
-    void getFare_distance_11() {
-        // given
-        int 거리 = 11;
-
+    @ParameterizedTest
+    @ValueSource(ints = {11, 51, 59})
+    @DisplayName("10km초과 ∼ 50km까지는 5km마다 100원의 추가 운임비를 받고, 50km 초과부터는 8km마다 100원의 추가 운임비를 받는다.")
+    void getFare_policy(int distance) {
         // when
-        Fare fare = Fare.of(거리);
+        Fare fare = Fare.of(distance);
 
         // then
-        assertThat(fare.getFare()).isEqualTo(1350);
-    }
+        if (distance == 11) {
+            assertThat(fare.getFare()).isEqualTo(1350);
+        }
 
-    @Test
-    @DisplayName("거리가 51km 이면 요금은 2150원 이다.")
-    void getFare_distance_51() {
-        // given
-        int 거리 = 51;
+        if (distance == 51) {
+            assertThat(fare.getFare()).isEqualTo(2150);
+        }
 
-        // when
-        Fare fare = Fare.of(거리);
-
-        // then
-        assertThat(fare.getFare()).isEqualTo(2150);
-    }
-
-    @Test
-    @DisplayName("거리가 59km 이면 요금은 2250원 이다.")
-    void getFare_distance_59() {
-        // given
-        int 거리 = 59;
-
-        // when
-        Fare fare = Fare.of(거리);
-
-        // then
-        assertThat(fare.getFare()).isEqualTo(2250);
+        if (distance == 59) {
+            assertThat(fare.getFare()).isEqualTo(2250);
+        }
     }
 }
