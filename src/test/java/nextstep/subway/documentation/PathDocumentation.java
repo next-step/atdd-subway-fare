4e2a1883f5a1f003f630dcfce75b54800e878960
realrains <real.longrain@gmail.com>
2022-08-11 22:53:26 +0900

    test: 경로 조회 문서화 테스트 수정

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -5,6 +5,7 @@ import nextstep.subway.applicaion.PathService;
 import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.PathType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -22,27 +23,50 @@ class PathDocumentation extends Documentation {
     @MockBean
     private PathService pathService;
 
+    @DisplayName("최단 거리 경로 조회 API")
     @Test
-    void path() {
+    void findPathByDistance() {
         PathResponse pathResponse = new PathResponse(
                 List.of(
                         new StationResponse(1L, "강남역"),
                         new StationResponse(2L, "역삼역")
                 ),
-                10
+                10,
+                6
         );
 
         when(pathService.findPath(anyLong(), anyLong(), any(PathType.class))).thenReturn(pathResponse);
 
+        경로_조회(1L, 2L, "DISTANCE");
+    }
+
+    @DisplayName("최소 시간 경로 조회 API")
+    @Test
+    void findPathByDuration() {
+        PathResponse pathResponse = new PathResponse(
+                List.of(
+                        new StationResponse(1L, "강남역"),
+                        new StationResponse(2L, "역삼역")
+                ),
+                10,
+                4
+        );
+
+        when(pathService.findPath(anyLong(), anyLong(), any(PathType.class))).thenReturn(pathResponse);
+
+        경로_조회(1L, 2L, "DURATION");
+    }
+
+    private void 경로_조회(Long source, Long target, String type) {
         RestAssured
                 .given(spec).log().all()
                 .filter(document("path",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint())))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .queryParam("path", "DISTANCE")
+                .queryParam("source", source)
+                .queryParam("target", target)
+                .queryParam("type", type)
                 .when().get("/paths")
                 .then().log().all().extract();
     }
