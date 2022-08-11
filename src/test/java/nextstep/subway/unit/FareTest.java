9ec392cee567348d4b74a87bfeb232c16cbd7944
kookiedncream <kim76744@naver.com>
2022-08-11 16:19:25 +0900

    test: 요금 관련 유닛 테스트 리팩토링

---
+++ src/test/java/nextstep/subway/unit/FareTest.java
@@ -0,0 +1,17 @@
+package nextstep.subway.unit;
+
+import nextstep.subway.domain.Fare;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FareTest {
+	@DisplayName("거리 별로 요금이 다르게 나오는 것을 확인한다.")
+	@CsvSource(value = {"10:1250", "50:2050", "51:2150", "100:2750"}, delimiter = ':')
+	@ParameterizedTest
+	void fareTest(int distance, int fare) {
+		assertThat(Fare.calculate(distance)).isEqualTo(fare);
+	}
+}
--- src/test/java/nextstep/subway/unit/PathTest.java
+++ src/test/java/nextstep/subway/unit/PathTest.java
@@ -1,5 +1,10 @@
-package nextstep.subway.domain;
+package nextstep.subway.unit;
 
+import nextstep.subway.domain.Fare;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Path;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.SubwayMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,7 +12,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
 
-import static nextstep.subway.domain.PathType.*;
+import static nextstep.subway.domain.PathType.DISTANCE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Path 관련 테스트를 진행한다.")
@@ -40,26 +45,6 @@ class PathTest {
 		이호선.addSection(교대역, 강남역, 3, 2);
 		삼호선.addSection(교대역, 남부터미널역, 5, 3);
 		삼호선.addSection(남부터미널역, 양재역, 5, 3);
-	}
-
-	@Test
-	void 총_거리가_10km_이하_일_때_요금을_반환한다() {
-		assertThat(Fare.calculate(10)).isEqualTo(1250);
-	}
-
-	@Test
-	void 총_거리가_50km_이하_일_때_요금을_반환한다() {
-		assertThat(Fare.calculate(50)).isEqualTo(2050);
-	}
-
-	@Test
-	void 총_거리가_51km_일_때_요금을_반환한다() {
-		assertThat(Fare.calculate(51)).isEqualTo(2150);
-	}
-
-	@Test
-	void 총_거리가_50km_초과_일_때_요금을_반환한다() {
-		assertThat(Fare.calculate(100)).isEqualTo(2750);
 	}
 
 	@Test
