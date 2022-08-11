b8ebc2d6c3aab64fc1bb8e9cab1047477720f98f
sangnyyy <dltkdals2202@gmail.com>
2022-08-11 17:42:09 +0900

    testRefactor : PathTest setup 생성

--- src/test/java/nextstep/subway/domain/PathTest.java
+++ src/test/java/nextstep/subway/domain/PathTest.java
@@ -1,5 +1,6 @@
 package nextstep.subway.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,19 +11,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SuppressWarnings("NonAsciiCharacters")
 class PathTest {
 
+    private Station 강남역;
+
+    private Station 역삼역;
+
+    private Line 이호선;
+
+    @BeforeEach
+    void setUp() {
+        강남역 = new Station("강남역");
+        역삼역 = new Station("역삼역");
+        이호선 = new Line("이호선", "초록");
+    }
+
     @DisplayName("경로의 요금을 계산할 수 있다. (10km 이내)")
     @Test
     void extractFare() {
         //given
-        Station 강남역 = new Station("강남역");
-        Station 역삼역 = new Station("역삼역");
-        Station 선릉역 = new Station("선릉역");
-
-        Line 이호선 = new Line("이호선", "초록");
         이호선.addSection(강남역, 역삼역, 5, 4);
-        이호선.addSection(역삼역, 선릉역, 4, 5);
-
-        Path 경로 = new Path(new Sections(이호선.getSections()));
+        Path 경로 = new Path(이호선의구간들());
 
         //when
         int 요금 = 경로.extractFare();
@@ -33,26 +40,22 @@ class PathTest {
 
     @DisplayName("경로의 요금을 계산할 수 있다. (10km 초과 ~ 50km 이내)")
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8})
+    @ValueSource(ints = {3, 4, 5, 6, 7, 8, 9, 10})
     void extractFareOver10KmUnder50Km(int multiply) {
         //given
-        Station 강남역 = new Station("강남역");
-        Station 역삼역 = new Station("역삼역");
-        Station 선릉역 = new Station("선릉역");
-        Station 삼성역 = new Station("삼성역");
-
-        Line 이호선 = new Line("이호선", "초록");
-        이호선.addSection(강남역, 역삼역, 5, 4);
-        이호선.addSection(역삼역, 선릉역, 4, 5);
-        이호선.addSection(선릉역, 삼성역, 5 * multiply, 5);
-
-        Path 경로 = new Path(new Sections(이호선.getSections()));
+        int 거리 = 5 * multiply;
+        이호선.addSection(강남역, 역삼역, 거리, 4);
+        Path 경로 = new Path(이호선의구간들());
 
         //when
         int 요금 = 경로.extractFare();
 
         //then
-        assertThat(요금).isEqualTo(1250 + 100 * multiply);
+        assertThat(요금).isEqualTo(1250 + (100 * (multiply - 2)));
+    }
+
+    private Sections 이호선의구간들() {
+        return new Sections(이호선.getSections());
     }
 
 }
