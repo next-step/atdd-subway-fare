35d5c6bbd1882eb6d010c28bd691fe15d9239754
YoungHo Choi <0505zxc@gmail.com>
2022-08-13 00:36:31 +0900

    test: 지하철 경로 조회 실패 테스트 케이스 추가

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -5,7 +5,9 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.util.HashMap;
@@ -49,25 +51,77 @@ class PathAcceptanceTest extends AcceptanceTest {
         지하철_노선에_지하철_구간_생성_요청(관리자, 삼호선, createSectionCreateParams(남부터미널역, 양재역, 3));
     }
 
-    @DisplayName("두 역의 최단 거리 경로를 조회한다.")
-    @Test
-    void findPathByDistance() {
-        // when
-        ExtractableResponse<Response> response = 두_역의_경로_조회를_요청(교대역, 양재역, "DISTANCE");
+    @Nested
+    @DisplayName("성공")
+    class success {
+        @DisplayName("두 역의 최단 거리 경로를 조회한다.")
+        @Test
+        void findPathByDistance() {
+            // when
+            ExtractableResponse<Response> response = 두_역의_경로_조회를_요청(교대역, 양재역, "DISTANCE");
 
-        // then
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
+            // then
+            assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
+        }
+
+        @DisplayName("두 역의 최소 시간 경로를 조회한다.")
+        @Test
+        void findPathByDuration() {
+            // when
+            ExtractableResponse<Response> response = 두_역의_경로_조회를_요청(교대역, 양재역, "DURATION");
+
+            // then
+            assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 강남역, 양재역);
+        }
     }
 
-    @DisplayName("두 역의 최소 시간 경로를 조회한다.")
-    @Test
-    void findPathByTime() {
-        // when
-        ExtractableResponse<Response> response = 두_역의_경로_조회를_요청(교대역, 양재역, "DURATION");
+    @Nested
+    @DisplayName("실패")
+    class fail {
+        @DisplayName("출발역이 노선에 없을 경우 조회 실패")
+        @Test
+        void findNotExistsStartStation() {
+            long 출발역 = 지하철역_생성_요청(관리자, "출발역").jsonPath().getLong("id");
 
-        // then
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 강남역, 양재역);
+            // when
+            ExtractableResponse<Response> responseByDistance = 두_역의_경로_조회를_요청(출발역, 강남역, "DISTANCE");
+            ExtractableResponse<Response> responseByDuration = 두_역의_경로_조회를_요청(출발역, 강남역, "DURATION");
+
+            // then
+            assertThat(responseByDistance.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+            assertThat(responseByDuration.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
+
+        @DisplayName("도착역이 노선에 없을 경우 조회 실패")
+        @Test
+        void findNotExistsEndStation() {
+            long 도착역 = 지하철역_생성_요청(관리자, "도착역").jsonPath().getLong("id");
+
+            // when
+            ExtractableResponse<Response> responseByDistance = 두_역의_경로_조회를_요청(강남역, 도착역, "DISTANCE");
+            ExtractableResponse<Response> responseByDuration = 두_역의_경로_조회를_요청(강남역, 도착역, "DURATION");
+
+            // then
+            assertThat(responseByDistance.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+            assertThat(responseByDuration.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
+
+        @DisplayName("출발역과 도착역이 노선에 없을 경우 조회 실패")
+        @Test
+        void findNotExistsEndStationAndStartStation() {
+            long 출발역 = 지하철역_생성_요청(관리자, "출발역").jsonPath().getLong("id");
+            long 도착역 = 지하철역_생성_요청(관리자, "도착역").jsonPath().getLong("id");
+
+            // when
+            ExtractableResponse<Response> responseByDistance = 두_역의_경로_조회를_요청(출발역, 도착역, "DISTANCE");
+            ExtractableResponse<Response> responseByDuration = 두_역의_경로_조회를_요청(출발역, 도착역, "DURATION");
+
+            // then
+            assertThat(responseByDiㅈstance.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+            assertThat(responseByDuration.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
     }
+
 
     private Long 지하철_노선_생성_요청(String name, String color, Long upStation, Long downStation, int distance) {
         Map<String, String> lineCreateParams;
