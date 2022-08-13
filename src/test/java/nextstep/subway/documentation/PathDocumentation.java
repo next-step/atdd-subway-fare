dc2f4488b93c272a562ed7c16725707c3d3c404f
MRSTK90 <dkgell13@gmail.com>
2022-08-14 01:01:28 +0900

    fix : path 테스트 문서도 최소 시간 경로 추가에 따른 업데이트 진행

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -10,8 +10,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
@@ -23,21 +28,35 @@ public class PathDocumentation extends Documentation {
     void path() {
         PathResponse pathResponse = new PathResponse(
                 Lists.newArrayList(
-                        new StationResponse(1L, "강남역", null, null),
-                        new StationResponse(2L, "역삼역", null, null)
-                ), 10
+                        new StationResponse(1L, "강남역"),
+                        new StationResponse(2L, "역삼역")
+                ), 10, 4
         );
 
-        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+        when(pathService.findPath(anyLong(), anyLong(), anyString())).thenReturn(pathResponse);
 
         RestAssured
                 .given(spec).log().all()
                 .filter(document("path",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint())))
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                requestParameters(
+                                        parameterWithName("source").description("Source Station Id"),
+                                        parameterWithName("target").description("Target Station Id"),
+                                        parameterWithName("type").description("Search Condition")
+                                ),
+                                responseFields(
+                                        fieldWithPath("stations[].id").description("Station Id"),
+                                        fieldWithPath("stations[].name").description("Station Name"),
+                                        fieldWithPath("distance").description("Total Distance"),
+                                        fieldWithPath("duration").description("Total Duration")
+                                )
+                        )
+                )
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
+                .queryParam("type", "DISTANCE")
                 .when().get("/paths")
                 .then().log().all().extract();
     }
