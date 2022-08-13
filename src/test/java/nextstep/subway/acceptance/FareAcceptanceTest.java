cf6ad2b3dba7748949e9321e73452fddb4931126
MangKyu <whalsrb1226@gmail.com>
2022-08-13 12:56:20 +0900

    요금 계산에 대한 인수테스트 분리 및 추가 요금 테스트 추가

--- src/test/java/nextstep/subway/acceptance/FareAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/FareAcceptanceTest.java
@@ -15,8 +15,8 @@ import static nextstep.subway.acceptance.LineSteps.지하철_노선에_지하철
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("지하철 경로 검색")
-class PathAcceptanceTest extends AcceptanceTest {
+@DisplayName("지하철 요금 계산")
+class FareAcceptanceTest extends AcceptanceTest {
     private Long 교대역;
     private Long 강남역;
     private Long 양재역;
@@ -41,14 +41,14 @@ class PathAcceptanceTest extends AcceptanceTest {
         양재역 = 지하철역_생성_요청(관리자, "양재역").jsonPath().getLong("id");
         남부터미널역 = 지하철역_생성_요청(관리자, "남부터미널역").jsonPath().getLong("id");
 
-        이호선 = 지하철_노선_생성_요청("2호선", "green", 교대역, 강남역, 10, 2);
-        신분당선 = 지하철_노선_생성_요청("신분당선", "red", 강남역, 양재역, 10, 1);
-        삼호선 = 지하철_노선_생성_요청("3호선", "orange", 교대역, 남부터미널역, 2, 3);
+        이호선 = 지하철_노선_생성_요청("2호선", "green", 교대역, 강남역, 10, 2, 200);
+        신분당선 = 지하철_노선_생성_요청("신분당선", "red", 강남역, 양재역, 10, 1, 1000);
+        삼호선 = 지하철_노선_생성_요청("3호선", "orange", 교대역, 남부터미널역, 2, 3, 300);
 
         지하철_노선에_지하철_구간_생성_요청(관리자, 삼호선, createSectionCreateParams(남부터미널역, 양재역, 3, 3));
     }
 
-    private Long 지하철_노선_생성_요청(String name, String color, Long upStation, Long downStation, int distance, final int duration) {
+    private Long 지하철_노선_생성_요청(String name, String color, Long upStation, Long downStation, int distance, final int duration, final int additionalFare) {
         Map<String, String> lineCreateParams;
         lineCreateParams = new HashMap<>();
         lineCreateParams.put("name", name);
@@ -57,6 +57,7 @@ class PathAcceptanceTest extends AcceptanceTest {
         lineCreateParams.put("downStationId", downStation + "");
         lineCreateParams.put("distance", distance + "");
         lineCreateParams.put("duration", duration + "");
+        lineCreateParams.put("additionalFare", additionalFare + "");
 
         return LineSteps.지하철_노선_생성_요청(관리자, lineCreateParams).jsonPath().getLong("id");
     }
@@ -70,24 +71,33 @@ class PathAcceptanceTest extends AcceptanceTest {
         return params;
     }
 
-    @DisplayName("두 역의 최단 거리 경로를 조회한다.")
+    @DisplayName("두 역의 최단 거리 경로에 대한 요금을 조회한다. 3호선을 거쳐가므로 추가 요금이 300원 있어야 한다.")
     @Test
-    void findPathByDistance() {
+    void getPriceOfPathByDistanceLine삼호선() {
+        // given
+        Long 잠실역 = 지하철역_생성_요청(관리자, "고속터미널역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(관리자, 이호선, createSectionCreateParams(강남역, 잠실역, 50, 3));
+
         // when
-        ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(교대역, 양재역, "DISTANCE");
+        ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(남부터미널역, 잠실역, "DISTANCE");
 
         // then
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
+        assertThat(response.jsonPath().getLong("distance")).isEqualTo(62L);
+        assertThat(response.jsonPath().getInt("fare")).isEqualTo(2550);
     }
 
-    @DisplayName("두 역의 최단 시간 경로를 조회한다.")
+    @DisplayName("두 역의 최단 거리 경로에 대한 요금을 조회한다. 신분당선을 거쳐가므로 추가 요금이 1000원 있어야 한다.")
     @Test
-    void findPathByDuration() {
+    void getPriceOfPathByDistanceLine신분당선() {
+        // given
+        Long 고속터미널역 = 지하철역_생성_요청(관리자, "고속터미널역").jsonPath().getLong("id");
+        지하철_노선에_지하철_구간_생성_요청(관리자, 이호선, createSectionCreateParams(강남역, 고속터미널역, 50, 3));
+
         // when
-        ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(남부터미널역, 강남역, "DURATION");
+        ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(고속터미널역, 양재역, "DISTANCE");
 
         // then
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(남부터미널역, 양재역, 강남역);
+        assertThat(response.jsonPath().getInt("fare")).isEqualTo(3250);
     }
 
     private ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target, final String type) {
