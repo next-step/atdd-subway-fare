5dac13768a7ba48be09a5bd9aa657bce6dd44a58
chkim <cndghks0213@gmail.com>
2022-08-14 03:15:08 +0900

    refactor: 경로 조회 시 타입을 받도록 수정

--- src/main/java/nextstep/subway/applicaion/PathService.java
+++ src/main/java/nextstep/subway/applicaion/PathService.java
@@ -4,27 +4,31 @@ import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Path;
 import nextstep.subway.domain.Station;
-import nextstep.subway.domain.SubwayMap;
+import nextstep.subway.util.SubwayMap;
+import nextstep.subway.util.SubwayMapFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PathService {
-    private LineService lineService;
-    private StationService stationService;
+    private final LineService lineService;
+    private final StationService stationService;
+    private final SubwayMapFactory subwayMapFactory;
 
-    public PathService(LineService lineService, StationService stationService) {
+    public PathService(LineService lineService, StationService stationService, SubwayMapFactory subwayMapFactory) {
         this.lineService = lineService;
         this.stationService = stationService;
+        this.subwayMapFactory = subwayMapFactory;
     }
 
-    public PathResponse findPath(Long source, Long target) {
+    public PathResponse findPath(Long source, Long target, String type) {
         Station upStation = stationService.findById(source);
         Station downStation = stationService.findById(target);
         List<Line> lines = lineService.findLines();
-        SubwayMap subwayMap = new SubwayMap(lines);
-        Path path = subwayMap.findPath(upStation, downStation);
+
+        SubwayMap subwayMap = subwayMapFactory.subwayMap(type);
+        Path path = subwayMap.findPath(lines, upStation, downStation);
 
         return PathResponse.of(path);
     }
