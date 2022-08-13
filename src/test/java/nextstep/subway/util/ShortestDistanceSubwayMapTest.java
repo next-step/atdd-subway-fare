65924d76731ab03d1d06c5caa5d04de102916868
chkim <cndghks0213@gmail.com>
2022-08-14 03:11:44 +0900

    test: 최단 거리 경로를 조회하는 ShortestDistanceSubwayMap 테스트 작성

--- src/test/java/nextstep/subway/util/ShortestDistanceSubwayMapTest.java
+++ src/test/java/nextstep/subway/util/ShortestDistanceSubwayMapTest.java
@@ -1,11 +1,11 @@
-package nextstep.subway.unit;
+package nextstep.subway.util;
 
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Path;
 import nextstep.subway.domain.Station;
-import nextstep.subway.domain.SubwayMap;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
@@ -13,8 +13,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SubwayMapTest {
-
+class ShortestDistanceSubwayMapTest {
     private Station 교대역;
     private Station 강남역;
     private Station 양재역;
@@ -34,33 +33,35 @@ public class SubwayMapTest {
         이호선 = new Line("2호선", "red");
         삼호선 = new Line("3호선", "red");
 
-        신분당선.addSection(강남역, 양재역, 3);
-        이호선.addSection(교대역, 강남역, 3);
-        삼호선.addSection(교대역, 남부터미널역, 5);
-        삼호선.addSection(남부터미널역, 양재역, 5);
+        신분당선.addSection(강남역, 양재역, 3, 10);
+        이호선.addSection(교대역, 강남역, 3, 10);
+        삼호선.addSection(교대역, 남부터미널역, 5, 1);
+        삼호선.addSection(남부터미널역, 양재역, 5, 1);
     }
 
+    @DisplayName("최단거리 경로를 조회한다.")
     @Test
     void findPath() {
         // given
         List<Line> lines = Lists.newArrayList(신분당선, 이호선, 삼호선);
-        SubwayMap subwayMap = new SubwayMap(lines);
+        ShortestDistanceSubwayMap subwayMap = new ShortestDistanceSubwayMap();
 
         // when
-        Path path = subwayMap.findPath(교대역, 양재역);
+        Path path = subwayMap.findPath(lines, 교대역, 양재역);
 
         // then
         assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(교대역, 강남역, 양재역));
     }
 
+    @DisplayName("최단거리 경로를 조회한다.(반대 방향)")
     @Test
     void findPathOppositely() {
         // given
         List<Line> lines = Lists.newArrayList(신분당선, 이호선, 삼호선);
-        SubwayMap subwayMap = new SubwayMap(lines);
+        ShortestDistanceSubwayMap subwayMap = new ShortestDistanceSubwayMap();
 
         // when
-        Path path = subwayMap.findPath(양재역, 교대역);
+        Path path = subwayMap.findPath(lines, 양재역, 교대역);
 
         // then
         assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(양재역, 강남역, 교대역));
