3fbe41390bc75cfc64dc211bb09e6e1e42a6ee77
chkim <cndghks0213@gmail.com>
2022-08-14 16:56:17 +0900

    feat: 경로 응답에 요금 정보 추가

--- src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
@@ -9,19 +9,21 @@ public class PathResponse {
     private List<StationResponse> stations;
     private int distance;
     private int duration;
+    private int fare;
 
-    public PathResponse(List<StationResponse> stations, int distance, int duration) {
+    public PathResponse(List<StationResponse> stations, int distance, int duration, int fare) {
         this.stations = stations;
         this.distance = distance;
         this.duration = duration;
+        this.fare = fare;
     }
 
-    public static PathResponse of(Path path) {
+    public static PathResponse of(Path path, int fare) {
         List<StationResponse> stations = path.getStations().stream()
                 .map(StationResponse::of)
                 .collect(Collectors.toList());
 
-        return new PathResponse(stations, path.extractDistance(), path.extractDuration());
+        return new PathResponse(stations, path.extractDistance(), path.extractDuration(), fare);
     }
 
     public List<StationResponse> getStations() {
@@ -34,5 +36,9 @@ public class PathResponse {
 
     public int getDuration() {
         return duration;
+    }
+
+    public int getFare() {
+        return fare;
     }
 }
