fb9b6c5cd27eb4036fcb099ab080966407bf559f
DahamLeee <dlek1011@gmail.com>
2022-08-14 17:00:43 +0900

    test: add LoginMemberAge Fixture

---
+++ src/test/java/nextstep/subway/fixture/LoginMemberAge.java
@@ -0,0 +1,9 @@
+package nextstep.subway.fixture;
+
+public final class LoginMemberAge {
+
+    public static final int ANONYMOUS_AGE = 0;
+    public static final int CHILDREN_AGE = 7;
+    public static final int TEENAGER_AGE = 14;
+    public static final int ADULT_AGE = 20;
+}
