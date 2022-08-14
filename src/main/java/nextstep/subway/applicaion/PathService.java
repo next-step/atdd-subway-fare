a8236e09a80482c7ad7640237765c90831132fb7
YoungHo Choi <0505zxc@gmail.com>
2022-08-14 10:49:32 +0900

    🚀 2단계 - 요금 조회 (#207)
    * test: "요금 조회" 테스트 케이스 스켈레톤 코드 작성
    
    * test: "요금 조회" 테스트 케이스 조건 수정
    
    * feat: "요금조회" 가장 최단 경로를 기준으로 요금 계산 기능 구현
    
    * refactor: enum에서 각 거리 정책별 값을 계산하도록 리펙터링함

--- src/main/java/nextstep/subway/applicaion/PathService.java
+++ src/main/java/nextstep/subway/applicaion/PathService.java
@@ -20,9 +20,23 @@ public class PathService {
         Station upStation = stationService.findById(source);
         Station downStation = stationService.findById(target);
         List<Line> lines = lineService.findLines();
-        SubwayMap subwayMap = new SubwayMap(lines, PathType.valueOf(type));
-        Path path = subwayMap.findPath(upStation, downStation);
 
-        return PathResponse.of(path);
+        return PathResponse.of(
+                getPathByType(upStation, downStation, lines, type),
+                getPathByShortestDistanceFare(upStation, downStation, lines)
+        );
+    }
+
+    private Path getPathByType(Station upStation, Station downStation, List<Line> lines, String type) {
+        SubwayMap subwayMap = new SubwayMap(lines, PathType.valueOf(type));
+        return subwayMap.findPath(upStation, downStation);
+    }
+
+
+    private int getPathByShortestDistanceFare(Station upStation, Station downStation, List<Line> lines){
+        SubwayMap subwayMap = new SubwayMap(lines, PathType.DISTANCE);
+        Path path = subwayMap.findPath(upStation, downStation);
+        int distance = path.extractDistance();
+        return new DistanceFare(distance).calculate();
     }
 }
---
+++ src/test/java/nextstep/subway/unit/FareTest.java
@@ -0,0 +1,55 @@
+package nextstep.subway.unit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import nextstep.subway.domain.DistanceFare;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class FareTest {
+    /**
+     * given 경로 길이가 주어지면
+     * when (10KM 부터 5km초과 될때마다 100원 추가, 50km부터 8km초과 마다 100원 추가)
+     *     when - 경로 거리가 n KM 일 경우 (n KM)
+     * then 경로 거리에 맞는 요금이 조회된다.
+     */
+    @ParameterizedTest
+    @CsvSource({
+            "9, 1250",
+            "10, 1250",
+            "11, 1350",
+            "14, 1350",
+            "15, 1350",
+            "16, 1450",
+            "20, 1450",
+            "21, 1550",
+            "25, 1550",
+            "26, 1650",
+            "30, 1650",
+            "31, 1750",
+            "35, 1750",
+            "36, 1850",
+            "40, 1850",
+            "41, 1950",
+            "45, 1950",
+            "46, 2050",
+            "50, 2050",
+            "57, 2150",
+            "58, 2150",
+            "59, 2250",
+            "59, 2250",
+            "65, 2250",
+            "66, 2250",
+            "67, 2350",
+    })
+    @DisplayName("요금 계산")
+    void calculateFare(int 경로_거리, int 요금 ) {
+        // when
+        DistanceFare distanceFare = new DistanceFare(경로_거리);
+        int fare = distanceFare.calculate();
+
+        // then
+        assertThat(fare).isEqualTo(요금);
+    }
+}
