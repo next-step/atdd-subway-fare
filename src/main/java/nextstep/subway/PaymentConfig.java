77aae3a921711e223d2acc9c44922d1a131f871f
DahamLeee <dlek1011@gmail.com>
2022-08-14 17:04:54 +0900

    feat: add Spring Bean [PaymentHandler]

--- src/main/java/nextstep/subway/PaymentConfig.java
+++ src/main/java/nextstep/subway/PaymentConfig.java
@@ -1,5 +1,9 @@
 package nextstep.subway;
 
+import nextstep.subway.payment.AgeDiscountPaymentPolicy;
+import nextstep.subway.payment.DistancePaymentPolicy;
+import nextstep.subway.payment.LinePaymentPolicy;
+import nextstep.subway.payment.PaymentHandler;
 import nextstep.subway.util.discount.AdultPaymentPolicy;
 import nextstep.subway.util.discount.AgeFactory;
 import nextstep.subway.util.discount.ChildrenPaymentPolicy;
@@ -22,5 +26,19 @@ public class PaymentConfig {
     @Bean
     public AgeFactory ageFactory() {
         return new AgeFactory();
+    }
+
+    @Bean
+    public PaymentHandler paymentHandler() {
+        return new PaymentHandler();
+    }
+
+    @Bean
+    public PaymentHandler addPaymentHandler() {
+        PaymentHandler paymentHandler = paymentHandler();
+        paymentHandler.addPaymentPolicy(new DistancePaymentPolicy());
+        paymentHandler.addPaymentPolicy(new LinePaymentPolicy());
+        paymentHandler.addPaymentPolicy(new AgeDiscountPaymentPolicy(ageFactory()));
+        return paymentHandler;
     }
 }
