4bb683d88627c14559d085a165e43d00641d1337
imcool2551 <imcool2551@gmail.com>
2022-08-15 21:46:41 +0900

    test: 나이 기반 할인 정책 테스트

---
+++ src/test/java/nextstep/path/domain/fare/MemberAgeFarePolicyTest.java
@@ -0,0 +1,27 @@
+package nextstep.path.domain.fare;
+
+import nextstep.member.domain.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberAgeFarePolicyTest {
+
+
+    @DisplayName("나이 기반 요금 할인")
+    @ParameterizedTest
+    @CsvSource({
+            "12,900",
+            "13,1440",
+            "19,1800",
+    })
+    void apply(int age, int expected) {
+        Member member = new Member("email@email.com", "password", age);
+
+        int result = new MemberAgeFarePolicy(member).apply(1800);
+
+        assertThat(result).isEqualTo(expected);
+    }
+}
