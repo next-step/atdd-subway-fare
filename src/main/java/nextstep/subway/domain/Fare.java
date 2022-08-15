b3564f399609708ffd08fb218b66572361c5e98a
iiaii <iiaii@woowahan.com>
2022-08-15 21:30:50 +0900

    refactor: 요금 계산 로직 리팩토링

--- src/main/java/nextstep/subway/domain/Fare.java
+++ src/main/java/nextstep/subway/domain/Fare.java
@@ -1,49 +1,44 @@
 package nextstep.subway.domain;
 
 import java.util.Arrays;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
 
 public enum Fare {
-    BASIC(0, 10, 0),
-    TEN_TO_FIFTY_KILO(11, 50, 5),
-    OVER_FIFTY_KILO(51, Integer.MAX_VALUE, 8);
-
-    private static final int BASIC_FARE = 1250;
+    BASIC(0, 10, distance -> 1250),
+    TEN_TO_FIFTY_KILO(11, 50, distance -> Math.min (800, (int) ((Math.ceil(distance - 11) / 5) + 1) * 100)),
+    OVER_FIFTY_KILO(51, Integer.MAX_VALUE, distance -> (int) ((Math.ceil(distance - 51) / 8) + 1) * 100);
 
     private final int minDistance;
     private final int maxDistance;
-    private final int divideNumber;
+    private final ToIntFunction<Integer> operation;
 
-    Fare(int minDistance, int maxDistance, int divideNumber) {
+    Fare(int minDistance, int maxDistance, ToIntFunction<Integer> operation) {
         this.minDistance = minDistance;
         this.maxDistance = maxDistance;
-        this.divideNumber = divideNumber;
+        this.operation = operation;
     }
 
     public static int getFare(int distance) {
-        if (distance <= BASIC.maxDistance) {
-            return BASIC_FARE;
+        validateDistance(distance);
+        return calculateFare(distance);
+    }
+
+    private static void validateDistance(int distance) {
+        if (distance < 0) {
+            throw new IllegalArgumentException();
         }
-        return BASIC_FARE + getAdditionalFare(distance);
     }
 
-    private static int getAdditionalFare(int distance) {
-        int additionalDistance = distance - BASIC.maxDistance;
-        int divideNumber = getDivideNumber(distance);
-
-        return (int) ((Math.ceil((additionalDistance - 1) / divideNumber) + 1) * 100);
-    }
-
-    private static int getDivideNumber(int distance) {
+    private static int calculateFare(int distance) {
         return Arrays.stream(values())
-                .filter(fare -> isBetweenDistance(distance, fare))
-                .findFirst()
-                .orElse(BASIC)
-                .divideNumber;
+                .filter(fare -> isUpperThanMinDistance(distance, fare))
+                .mapToInt(fare -> fare.operation.applyAsInt(distance))
+                .sum();
     }
 
-    private static boolean isBetweenDistance(int distance, Fare fare) {
-        return fare.minDistance <= distance
-                && distance <= fare.maxDistance;
+    private static boolean isUpperThanMinDistance(int distance, Fare fare) {
+        return fare.minDistance <= distance;
     }
 
 }
