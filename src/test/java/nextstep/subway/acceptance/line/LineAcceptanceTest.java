45489c4d7b5a38f34bab429f19db10dbdc2acfd7
임명섭 <lms@elonsoft.co.kr>
2022-08-15 17:46:03 +0900

    test 지하철 노선 시간 정보 인수 테스트 추가

--- src/test/java/nextstep/subway/acceptance/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/line/LineAcceptanceTest.java
@@ -7,7 +7,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
-import static nextstep.subway.acceptance.line.LineSteps.*;
+import java.time.LocalTime;
+
+import static nextstep.subway.acceptance.line.LineSteps.지하철_노선_검증;
+import static nextstep.subway.acceptance.line.LineSteps.지하철_노선_목록_조회_요청;
+import static nextstep.subway.acceptance.line.LineSteps.지하철_노선_삭제_요청;
+import static nextstep.subway.acceptance.line.LineSteps.지하철_노선_생성_요청;
+import static nextstep.subway.acceptance.line.LineSteps.지하철_노선_수정_요청;
+import static nextstep.subway.acceptance.line.LineSteps.지하철_노선_조회_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관리 기능")
@@ -104,5 +111,46 @@ class LineAcceptanceTest extends AcceptanceTest {
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+    /**
+     * When 지하철 노선 정보(첫차 시간, 막차 시간, 간격)을 추가하면
+     * Then 지하철 노선 정보가 추가된다.
+     * When 추가된 지하철 노선을 조회하면
+     * Then 추가된 지하철 노선 정보가 조회된다.
+     * When 추가된 지하철 노선의 첫차 시간, 막차 시간, 간격을 수정하면
+     * Then 지하철 노선의 정보가 수정된다.
+     * When 추가된 지하철 노선을 삭제하면
+     * Then 지하철 노선이 삭제된다.
+     */
+    @DisplayName("지하철 노선 첫차, 막차, 간격 관리")
+    @Test
+    void saveLineWithTimes() {
+        // when
+        ExtractableResponse<Response> createResponse = 지하철_노선_생성_요청(관리자, "2호선", "green", LocalTime.of(05, 00, 00), LocalTime.of(23, 00, 00), 10);
+
+        // then
+        assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        // when
+        ExtractableResponse<Response> findResponse = 지하철_노선_조회_요청(createResponse);
+
+        // then
+        지하철_노선_검증(findResponse, "2호선", "green", "05:00:00", "23:00:00", 10);
+
+        // when
+        ExtractableResponse<Response> updateResponse = 지하철_노선_수정_요청(관리자, createResponse, "red", LocalTime.of(06, 00, 00), LocalTime.of(23, 30, 00), 5);
+
+        // then
+        assertThat(updateResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        ExtractableResponse<Response> afterUpdateResponse = 지하철_노선_조회_요청(createResponse);
+        지하철_노선_검증(afterUpdateResponse, "2호선", "red", "06:00:00", "23:30:00", 5);
+
+        // when
+        ExtractableResponse<Response> deleteResponse = 지하철_노선_삭제_요청(관리자, createResponse);
+
+        // then
+        assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 }
