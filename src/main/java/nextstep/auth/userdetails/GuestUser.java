c68b77d16042134415846ff1b964e835dc5d739a
imcool2551 <imcool2551@gmail.com>
2022-08-16 13:32:39 +0900

    refactor: 비로그인 사용자를 위한 클래스에 싱글톤 패턴 적용

--- src/main/java/nextstep/auth/userdetails/GuestUser.java
+++ src/main/java/nextstep/auth/userdetails/GuestUser.java
@@ -1,7 +1,16 @@
 package nextstep.auth.userdetails;
 
 public class GuestUser extends User {
-    public GuestUser() {
+
+    private static class GuestUserHolder {
+        private static final GuestUser INSTANCE = new GuestUser();
+    }
+
+    private GuestUser() {
         super("GUEST", null, null);
+    }
+
+    public static GuestUser getInstance() {
+        return GuestUserHolder.INSTANCE;
     }
 }
--- src/main/java/nextstep/member/domain/GuestMember.java
+++ src/main/java/nextstep/member/domain/GuestMember.java
@@ -1,4 +1,12 @@
 package nextstep.member.domain;
 
 public class GuestMember extends Member {
+
+    private static class GuestMemberHolder {
+        private static final GuestMember INSTANCE = new GuestMember();
+    }
+
+    public static GuestMember getInstance() {
+        return GuestMemberHolder.INSTANCE;
+    }
 }
