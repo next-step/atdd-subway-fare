c45e58523e8dabcf0c5fc6ec4c5cd510c5ae34ec
uram <parkuram12@gmail.com>
2022-08-16 14:58:43 +0900

    test: 운임요금을 조회하는 인수 테스트 작성

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -1,19 +1,17 @@
 package nextstep.subway.acceptance;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import nextstep.subway.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
 
-import java.util.Arrays;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
 import static nextstep.subway.acceptance.LineSteps.지하철_노선에_지하철_구간_생성_요청;
+import static nextstep.subway.acceptance.PathSteps.*;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,6 +24,7 @@ class PathAcceptanceTest extends AcceptanceTest {
     private Long 이호선;
     private Long 신분당선;
     private Long 삼호선;
+    private static BigDecimal BASE_FARE = new BigDecimal(1250);
 
     /**
      * 교대역    --- *2호선* ---   강남역
@@ -61,7 +60,7 @@ class PathAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(교대역, 양재역);
 
         // then
-        Integer distance = 5;
+        final Integer distance = 5;
         최단_거리_경로_조회_검증(response, distance, 교대역, 남부터미널역, 양재역);
     }
 
@@ -77,8 +76,31 @@ class PathAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 두_역의_최단_소요시간_경로_조회를_요청(교대역, 양재역);
 
         //then
-        Integer duration = 4;
+        final Integer duration = 4;
         최단_소요시간_경로_조회_검증(response, duration, 교대역, 강남역, 양재역);
+    }
+
+    /*
+     * when 교대역과 양재역의 최단 거리 경로를 조회한다
+     * then 구간의 최단 거리의 합을 검증한다.
+     * and  경로가 "교대역" "남부터미널역" "양재역" 과 일치하는지 검증한다.
+     * and  거리가 10을 초과하지 않기 때문에 운임요금이 BASE_FARE 와 동일한지 검증한다.
+     */
+    @DisplayName("두 역의 최단 거리 경로 운임 요금을 조회한다.")
+    @Test
+    void FindFaresOnThePathByDistance() {
+        //when
+        ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(교대역, 양재역);
+
+        //then
+        final Integer distance = 5;
+        최단_거리_경로_운임요금_조회_검증(response, BASE_FARE, distance, 교대역, 남부터미널역, 양재역);
+    }
+
+    private void 최단_거리_경로_운임요금_조회_검증(ExtractableResponse<Response> response, BigDecimal fare, Integer distance, Long ... stationIds) {
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(stationIds);
+        assertThat(response.jsonPath().getInt("distance")).isEqualTo(distance);
+        assertThat(response.jsonPath().getInt("fare")).isEqualTo(fare.intValue());
     }
 
     private void 최단_소요시간_경로_조회_검증(ExtractableResponse<Response> response, Integer duration, Long ... stationIds) {
@@ -89,28 +111,6 @@ class PathAcceptanceTest extends AcceptanceTest {
     private void 최단_거리_경로_조회_검증(ExtractableResponse<Response> response, Integer distance, Long ... stationIds) {
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(stationIds);
         assertThat(response.jsonPath().getInt("distance")).isEqualTo(distance);
-    }
-
-    private ExtractableResponse<Response> 두_역의_최단_소요시간_경로_조회를_요청(Long source, Long target) {
-        return RestAssured
-                .given().log().all()
-                .param("source", source)
-                .param("target", target)
-                .param("type", "DURATION")
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/paths")
-                .then().log().all().extract();
-    }
-
-    private ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target) {
-        return RestAssured
-                .given().log().all()
-                .param("source", source)
-                .param("target", target)
-                .param("type", "DISTANCE")
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/paths")
-                .then().log().all().extract();
     }
 
     private Long 지하철_노선_생성_요청(String name, String color, Long upStation, Long downStation, int distance, int duration) {
