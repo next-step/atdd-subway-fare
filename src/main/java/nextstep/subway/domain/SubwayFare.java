2d8647dcc6fee28fee52f40ee78b32d100fd7189
kst <kst@osstem.com>
2022-08-17 13:12:10 +0900

    fix : 계산할 숫자값 상수화

--- src/main/java/nextstep/subway/domain/SubwayFare.java
+++ src/main/java/nextstep/subway/domain/SubwayFare.java
@@ -6,24 +6,27 @@ public class SubwayFare {
 
     private static final int LONG_FARE_DISTANCE = 50;
 
+    private static final int EXTRA_FARE_LESS_LONG_DISTANCE = 5;
+    private static final int EXTRA_FARE_OVER_LONG_DISTANCE = 8;
 
-    public int calculateFare(int distance) {
+
+    public static int calculateFare(int distance) {
         int overDistance = distance - BASE_FARE_DISTANCE;
         int totalFare = BASE_FARE;
 
         if (distance > LONG_FARE_DISTANCE) {
             overDistance = LONG_FARE_DISTANCE - BASE_FARE_DISTANCE;
-            totalFare += calculateOverFare(distance - LONG_FARE_DISTANCE, 8);
+            totalFare += calculateOverFare(distance - LONG_FARE_DISTANCE, EXTRA_FARE_OVER_LONG_DISTANCE);
         }
 
         if (overDistance > 0) {
-            totalFare += calculateOverFare(overDistance, 5);
+            totalFare += calculateOverFare(overDistance, EXTRA_FARE_LESS_LONG_DISTANCE);
         }
 
         return totalFare;
     }
 
-    private int calculateOverFare(int overDistance, int fareDistance) {
+    private static int calculateOverFare(int overDistance, int fareDistance) {
         return (int) ((Math.ceil((overDistance - 1) / fareDistance) + 1) * 100);
     }
 }
