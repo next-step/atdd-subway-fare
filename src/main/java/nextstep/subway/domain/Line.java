53c5e7dc3f29cdca31e05e1499bab9270a2b1600
chkim <cndghks0213@gmail.com>
2022-08-17 18:19:08 +0900

    feat: 노선 생성 시 추가요금을 설정

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -10,6 +10,7 @@ public class Line {
     private Long id;
     private String name;
     private String color;
+    private int surcharge;
 
     @Embedded
     private Sections sections = new Sections();
@@ -18,8 +19,17 @@ public class Line {
     }
 
     public Line(String name, String color) {
+        this(name, color, 0);
+    }
+
+    public Line(String name, String color, int surcharge) {
+        if (surcharge < 0) {
+            throw new IllegalArgumentException("지하철 노선의 추가요금은 0원 이상이어야 합니다.");
+        }
+
         this.name = name;
         this.color = color;
+        this.surcharge = surcharge;
     }
 
     public Long getId() {
@@ -34,17 +44,22 @@ public class Line {
         return color;
     }
 
+    public int getSurcharge() {
+        return surcharge;
+    }
+
     public List<Section> getSections() {
         return sections.getSections();
     }
 
-    public void update(String name, String color) {
+    public void update(String name, String color, int surcharge) {
         if (name != null) {
             this.name = name;
         }
         if (color != null) {
             this.color = color;
         }
+        this.surcharge = surcharge;
     }
 
     public void addSection(Station upStation, Station downStation, int distance, int duration) {
