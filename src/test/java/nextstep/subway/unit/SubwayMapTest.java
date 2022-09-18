4e7d2ce30488f13f4cd82e35cb478185c7400568
chosundeveloper <iddaumid@naver.com>
2022-09-18 20:07:03 +0900

    refactor : 경로 조회 테스트 로직 변경 반영

--- src/test/java/nextstep/subway/unit/SubwayMapTest.java
+++ src/test/java/nextstep/subway/unit/SubwayMapTest.java
@@ -1,18 +1,21 @@
 package nextstep.subway.unit;
 
-import nextstep.subway.domain.*;
+import nextstep.subway.domain.SubwayMap;
+import nextstep.subway.domain.line.Line;
+import nextstep.subway.domain.path.Path;
+import nextstep.subway.domain.path.PathType;
+import nextstep.subway.domain.station.Station;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
-import support.auth.userdetails.UserDetails;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 
+@DisplayName("경로 조회 테스트")
 public class SubwayMapTest {
 
     private Station 교대역;
@@ -25,12 +28,20 @@ public class SubwayMapTest {
     private Line 이호선;
     private Line 삼호선;
 
-    /**
-     * 교대역    --- *2호선* ---   강남역
-     * |                        |
-     * *3호선*                   *신분당선* 추가요금 900
-     * |                        |
-     * 남부터미널역  --- *3호선* ---   양재
+    /*
+     * 신사역
+     * |
+     * |  (46km, 5분)
+     * |
+     * 고속터미널역
+     * |
+     * |  (14km, 5분)
+     * |
+     * 교대역            --- *2호선*(3km, 3분) ---            강남역
+     * |                                                       |
+     * *3호선* + 500원 (5km, 5분)                   *신분당선* + 900원 (3km, 3분)
+     * |                                                       |
+     * 남부터미널역       --- *3호선* + 500원 (5km, 5분) ---   양재
      */
     @BeforeEach
     void setUp() {
@@ -42,11 +53,11 @@ public class SubwayMapTest {
         신사역 = createStation(6L, "신사역");
 
         신분당선 = new Line("신분당선", "red", 900);
-        이호선 = new Line("2호선", "red", 900);
+        이호선 = new Line("2호선", "red", 0);
         삼호선 = new Line("3호선", "red", 500);
 
-        신분당선.addSection(강남역, 양재역, 3, 5);
-        이호선.addSection(교대역, 강남역, 3, 5);
+        신분당선.addSection(강남역, 양재역, 3, 3);
+        이호선.addSection(교대역, 강남역, 3, 3);
         삼호선.addSection(교대역, 남부터미널역, 5, 5);
         삼호선.addSection(남부터미널역, 양재역, 5, 5);
         삼호선.addSection(고속터미널역, 교대역, 14, 5);
@@ -61,38 +72,14 @@ public class SubwayMapTest {
         SubwayMap subwayMap = SubwayMap.create(lines, PathType.DISTANCE);
 
         // when
-        Path path = subwayMap.findPath(교대역, 양재역, any(UserDetails.class));
-
-        // then
-        assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(교대역, 강남역, 양재역));
-    }
-
-    @DisplayName("최단 거리 구간 조회 구간 거리 10km이하")
-    @Test
-    void findFare_10km이하() {
-        // given
-        List<Line> lines = Lists.newArrayList(신분당선, 이호선, 삼호선);
-        SubwayMap subwayMap = SubwayMap.create(lines, PathType.DISTANCE);
-
-        // when
-        Path path = subwayMap.findPath(교대역, 양재역, any(UserDetails.class));
+        Path path = subwayMap.findPath(교대역, 양재역, 20);
 
         // then
         assertThat(path.extractDistance()).isEqualTo(6);
-        assertThat(path.extractFare()).isEqualTo(2150);
+        assertThat(path.extractDuration()).isEqualTo(6);
+        assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(교대역, 강남역, 양재역));
     }
 
-
-    /**
-     * 고속터미널역
-     * |
-     * 교대역    --- *2호선* ---   강남역
-     * |                        |
-     * *3호선*                   *신분당선* 추가요금 900
-     * |                        |
-     * 남부터미널역  --- *3호선* ---   양재
-     *                  추가요금 500
-     */
     @DisplayName("최단 거리 구간 조회 구간 거리 10 ~ 50")
     @Test
     void findFare_10km이상_50km이하() {
@@ -101,7 +88,7 @@ public class SubwayMapTest {
         SubwayMap subwayMap = SubwayMap.create(lines, PathType.DISTANCE);
 
         // when
-        Path path = subwayMap.findPath(양재역, 고속터미널역, any(UserDetails.class));
+        Path path = subwayMap.findPath(양재역, 고속터미널역, 20);
 
         // then
         assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(양재역, 강남역, 교대역, 고속터미널역));
@@ -117,10 +104,13 @@ public class SubwayMapTest {
         SubwayMap subwayMap = SubwayMap.create(lines, PathType.DISTANCE);
 
         // when
-        Path path = subwayMap.findPath(양재역, 신사역, any(UserDetails.class));
-        assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(양재역, 강남역, 교대역, 고속터미널역, 신사역));
+        Path path = subwayMap.findPath(양재역, 신사역, 20);
+
+        assertThat(path.extractDuration()).isEqualTo(16);
         assertThat(path.extractDistance()).isEqualTo(66);
         assertThat(path.extractFare()).isEqualTo(3150);
+        assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(양재역, 강남역, 교대역, 고속터미널역, 신사역));
+
     }
 
     @DisplayName("최단 시간 구간 조회")
@@ -131,9 +121,11 @@ public class SubwayMapTest {
         SubwayMap subwayMap = SubwayMap.create(lines, PathType.DURATION);
 
         // when
-        Path path = subwayMap.findPath(교대역, 양재역, any(UserDetails.class));
+        Path path = subwayMap.findPath(교대역, 양재역, 20);
 
         // then
+        assertThat(path.extractDistance()).isEqualTo(6);
+        assertThat(path.extractDuration()).isEqualTo(6);
         assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(교대역, 강남역, 양재역));
     }
 
@@ -144,7 +136,7 @@ public class SubwayMapTest {
         SubwayMap subwayMap = SubwayMap.create(lines, PathType.DISTANCE);
 
         // when
-        Path path = subwayMap.findPath(양재역, 교대역, any(UserDetails.class));
+        Path path = subwayMap.findPath(양재역, 교대역, 20);
 
         // then
         assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(양재역, 강남역, 교대역));
