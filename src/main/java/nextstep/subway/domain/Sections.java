0eedca8da3cde83afa68e39079fa903666a04eee
chkim <cndghks0213@gmail.com>
2022-10-06 17:40:50 +0900

    feat: 현재 시간, 노선의 정보, 도착 역, 상행/하행 여부를 통해 탑승 시간을 계산하는 기능 구현

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -3,11 +3,15 @@ package nextstep.subway.domain;
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Embeddable
 public class Sections {
@@ -30,6 +34,52 @@ public class Sections {
                 .map(Section::getLine)
                 .distinct()
                 .collect(Collectors.toList());
+    }
+
+    public LocalDateTime getStopTime(LocalDateTime current, LocalTime startTime, LocalTime endTime,
+                                     int intervalTime, Station target, boolean isGoingUp) {
+        List<Section> sections = getSectionsView(isGoingUp);
+        int totalDuration = sections.stream()
+                .takeWhile(section -> !isTarget(isGoingUp, target, section))
+                .mapToInt(Section::getDuration).sum();
+
+        LocalDateTime start = current.toLocalDate().atTime(startTime);
+        LocalDateTime end = current.toLocalDate().atTime(endTime);
+        LocalDateTime firstStopTime = start.plusMinutes(totalDuration);
+
+        return Stream.iterate(firstStopTime, stopTime -> stopTime.plusMinutes(intervalTime))
+                .takeWhile(stopTime -> between(stopTime, start, end))
+                .filter(it -> isAfter(current, it))
+                .findAny()
+                .orElseGet(getNextDayFirstStopTime(current, startTime));
+    }
+
+    private List<Section> getSectionsView(boolean isGoingUp) {
+        if (isGoingUp) {
+            List<Section> copy = sections.subList(0, sections.size());
+            Collections.reverse(copy);
+            return copy;
+        }
+        return Collections.unmodifiableList(sections);
+    }
+
+    private boolean isTarget(boolean isGoingUp, Station station, Section section) {
+        if (isGoingUp) {
+            return section.isSameDownStation(station);
+        }
+        return section.isSameUpStation(station);
+    }
+
+    private boolean between(LocalDateTime stopTime, LocalDateTime min, LocalDateTime max) {
+        return stopTime.compareTo(min) >= 0 && stopTime.compareTo(max) <= 0;
+    }
+
+    private boolean isAfter(LocalDateTime current, LocalDateTime stopTime) {
+        return current.compareTo(stopTime) <= 0;
+    }
+
+    private Supplier<LocalDateTime> getNextDayFirstStopTime(LocalDateTime current, LocalTime startTime) {
+        return () -> current.toLocalDate().plusDays(1).atTime(startTime);
     }
 
     public void add(Section section) {
