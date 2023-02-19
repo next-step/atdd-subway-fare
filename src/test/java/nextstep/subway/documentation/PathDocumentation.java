30282f98dd21f510cf2aad78c514e7c8d23d641b
michol.lee <lovethefeel@gmail.com>
2023-02-20 00:19:44 +0900

    refactor: 경로 Mock 추가 및 rest-docs 설정 추가

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,41 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 
-public class PathDocumentation extends Documentation {
+import static nextstep.subway.acceptance.PathSteps.*;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+class PathDocumentation extends Documentation {
+
+    @MockBean
+    private PathService pathService;
 
     @Test
-    void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+    void pathTest() {
+
+        final PathResponse pathResponse = new PathResponse(Lists.newArrayList(
+                new StationResponse(1L, "강남역"),
+                new StationResponse(2L, "역삼역")), 10);
+
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+        given(this.spec)
+                .filter(
+                        document("path",
+                                getPathRequestParametersSnippet(),
+                                getPathResponseFieldsSnippet()
+                        )
+                )
+                .when().get("/paths?source={source}&target={target}", 1L, 2L).then().log().all()
+        .assertThat().statusCode(is(HttpStatus.OK.value()));
     }
 }
