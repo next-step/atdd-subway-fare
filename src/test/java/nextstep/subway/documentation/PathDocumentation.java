4d90e0afb2d85fe9451b65598ddd70fd272afbc5
seondongpyo <seondongpyo@gmail.com>
2023-02-20 01:23:43 +0900

    feat: PathSteps 추가 및 적용

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,28 +1,30 @@
 package nextstep.subway.documentation;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.*;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 
-import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.acceptance.PathSteps;
 import nextstep.subway.applicaion.PathService;
 import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
 
-public class PathDocumentation extends Documentation {
+class PathDocumentation extends Documentation {
 
     @MockBean
     private PathService pathService;
 
     @Test
     void path() {
+        // given
         PathResponse pathResponse = new PathResponse(
             List.of(
                 new StationResponse(1L, "강남역"),
@@ -33,15 +35,10 @@ public class PathDocumentation extends Documentation {
 
         when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
 
-        RestAssured
-                .given(spec).log().all()
-                .filter(document("path",
-                    preprocessRequest(prettyPrint()),
-                    preprocessResponse(prettyPrint())))
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+        // when
+        ExtractableResponse<Response> response = PathSteps.지하철_경로_조회(spec, 1L, 2L);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
