a76c9c9cd7a4c4362a8dd6e7e5e049a20abcf152
seondongpyo <seondongpyo@gmail.com>
2023-02-20 01:07:29 +0900

    refactor: 문서화 설정 및 서비스 레이어 stubbing 추가

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,15 +1,43 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.*;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
     @Test
     void path() {
+        PathResponse pathResponse = new PathResponse(
+            List.of(
+                new StationResponse(1L, "강남역"),
+                new StationResponse(2L, "역삼역")
+            ),
+            10
+        );
+
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
         RestAssured
-                .given().log().all()
+                .given(spec).log().all()
+                .filter(document("path",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint())))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
