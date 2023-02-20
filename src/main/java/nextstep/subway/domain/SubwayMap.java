a9f734a1a9986a85d37ed0b2127276afb64613af
michol.lee <lovethefeel@gmail.com>
2023-02-21 01:14:12 +0900

    refactor: 타입의 따른 분기 로직

--- src/main/java/nextstep/subway/domain/SubwayMap.java
+++ src/main/java/nextstep/subway/domain/SubwayMap.java
@@ -14,7 +14,7 @@ public class SubwayMap {
         this.lines = lines;
     }
 
-    public Path findPath(Station source, Station target) {
+    public Path findPath(Station source, Station target, SearchType searchType) {
         SimpleDirectedWeightedGraph<Station, SectionEdge> graph = new SimpleDirectedWeightedGraph<>(SectionEdge.class);
 
         // 지하철 역(정점)을 등록
@@ -24,24 +24,45 @@ public class SubwayMap {
                 .collect(Collectors.toList())
                 .forEach(graph::addVertex);
 
-        // 지하철 역의 연결 정보(간선)을 등록
-        lines.stream()
-                .flatMap(it -> it.getSections().stream())
-                .forEach(it -> {
-                    SectionEdge sectionEdge = SectionEdge.of(it);
-                    graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
-                    graph.setEdgeWeight(sectionEdge, it.getDistance());
-                });
+        if (searchType == SearchType.DISTANCE) {
+            // 지하철 역의 연결 정보(간선)을 등록
+            lines.stream()
+                    .flatMap(it -> it.getSections().stream())
+                    .forEach(it -> {
+                        SectionEdge sectionEdge = SectionEdge.of(it);
+                        graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
+                        graph.setEdgeWeight(sectionEdge, it.getDistance());
+                    });
 
-        // 지하철 역의 연결 정보(간선)을 등록
-        lines.stream()
-                .flatMap(it -> it.getSections().stream())
-                .map(it -> new Section(it.getLine(), it.getDownStation(), it.getUpStation(), it.getDistance(), it.getDuration()))
-                .forEach(it -> {
-                    SectionEdge sectionEdge = SectionEdge.of(it);
-                    graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
-                    graph.setEdgeWeight(sectionEdge, it.getDistance());
-                });
+            // 지하철 역의 연결 정보(간선)을 등록
+            lines.stream()
+                    .flatMap(it -> it.getSections().stream())
+                    .map(it -> new Section(it.getLine(), it.getDownStation(), it.getUpStation(), it.getDistance(), it.getDuration()))
+                    .forEach(it -> {
+                        SectionEdge sectionEdge = SectionEdge.of(it);
+                        graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
+                        graph.setEdgeWeight(sectionEdge, it.getDistance());
+                    });
+        } else {
+            // 지하철 역의 연결 정보(간선)을 등록
+            lines.stream()
+                    .flatMap(it -> it.getSections().stream())
+                    .forEach(it -> {
+                        SectionEdge sectionEdge = SectionEdge.of(it);
+                        graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
+                        graph.setEdgeWeight(sectionEdge, it.getDuration());
+                    });
+
+            // 지하철 역의 연결 정보(간선)을 등록
+            lines.stream()
+                    .flatMap(it -> it.getSections().stream())
+                    .map(it -> new Section(it.getLine(), it.getDownStation(), it.getUpStation(), it.getDistance(), it.getDuration()))
+                    .forEach(it -> {
+                        SectionEdge sectionEdge = SectionEdge.of(it);
+                        graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
+                        graph.setEdgeWeight(sectionEdge, it.getDuration());
+                    });
+        }
 
         // 다익스트라 최단 경로 찾기
         DijkstraShortestPath<Station, SectionEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
