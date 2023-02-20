ec8643eaeaf90b25ec9a656f6b8873ab6b5b49bf
seondongpyo <seondongpyo@gmail.com>
2023-02-20 23:11:28 +0900

    feat: 경로 조회 타입인 PathSearchType 추가

--- src/main/java/nextstep/subway/ui/PathController.java
+++ src/main/java/nextstep/subway/ui/PathController.java
@@ -1,11 +1,13 @@
 package nextstep.subway.ui;
 
-import nextstep.subway.applicaion.PathService;
-import nextstep.subway.applicaion.dto.PathResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.domain.PathSearchType;
 
 @RestController
 public class PathController {
@@ -16,7 +18,11 @@ public class PathController {
     }
 
     @GetMapping("/paths")
-    public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
+    public ResponseEntity<PathResponse> findPath(
+        @RequestParam Long source,
+        @RequestParam Long target,
+        @RequestParam PathSearchType pathSearchType
+    ) {
         return ResponseEntity.ok(pathService.findPath(source, target));
     }
 }
