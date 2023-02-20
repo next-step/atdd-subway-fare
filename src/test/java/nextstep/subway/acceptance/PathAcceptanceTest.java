67bfcc76f073bc293d720743da06ad2f7ab3d714
michol.lee <lovethefeel@gmail.com>
2023-02-21 00:29:57 +0900

    refactor(test): 인수 조건 추가

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -17,6 +17,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 경로 검색")
 class PathAcceptanceTest extends AcceptanceTest {
+    private final String 최소시간 = "DURATION";
+    private final String 최단거리 = "DISTANCE";
     private Long 교대역;
     private Long 강남역;
     private Long 양재역;
@@ -58,7 +60,35 @@ class PathAcceptanceTest extends AcceptanceTest {
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
     }
 
+    /**
+     * Feature: 지하철 경로 검색
+     *
+     *   Scenario: 두 역의 최소 시간 경로를 조회
+     *     Given 지하철역이 등록되어있음
+     *     And 지하철 노선이 등록되어있음
+     *     And 지하철 노선에 지하철역이 등록되어있음
+     *     When 출발역에서 도착역까지의 최소 시간 기준으로 경로 조회를 요청
+     *     Then 최소 시간 기준 경로를 응답
+     *     And 총 거리와 소요 시간을 함께 응답함
+     */
+    @DisplayName("두 역의 최소 시간 경로를 조회한다.")
+    @Test
+    void findPathByDuration() {
+        ExtractableResponse<Response> response = 두_역의_최소_시간_경로_조회를_요청(교대역, 양재역, 최소시간);
+
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
+
+    }
+
     private ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target) {
+        return RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/paths?source={sourceId}&target={targetId}", source, target)
+                .then().log().all().extract();
+    }
+
+    private ExtractableResponse<Response> 두_역의_최소_시간_경로_조회를_요청(final Long source, final Long target, final String type) {
         return RestAssured
                 .given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
