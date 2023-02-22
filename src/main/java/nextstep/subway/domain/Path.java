a2adbddf7490be30078a952a91735a5590330c25
seondongpyo <seondongpyo@gmail.com>
2023-02-22 23:36:14 +0900

    refactor: 지하철 구간 요금을 계산하는 FareCalculator 추가 및 적용

--- src/main/java/nextstep/subway/domain/Path.java
+++ src/main/java/nextstep/subway/domain/Path.java
@@ -2,6 +2,8 @@ package nextstep.subway.domain;
 
 import java.util.List;
 
+import nextstep.subway.applicaion.FareCalculator;
+
 public class Path {
 
     private final Sections sections;
@@ -23,21 +25,7 @@ public class Path {
     }
 
     public int extractFare() {
-        int distance = extractDistance();
-
-        if (distance <= 10) {
-            return 1250;
-        }
-
-        if (distance <= 50) {
-            return 1250 + calculateOverFare(distance - 10, 5);
-        }
-
-        return 1250 + calculateOverFare(distance - 10, 8);
-    }
-
-    private int calculateOverFare(int distance, int kilometer) {
-        return (int) ((Math.ceil((distance - 1) / kilometer) + 1) * 100);
+        return FareCalculator.calculate(extractDistance());
     }
 
     public List<Station> getStations() {
---
+++ src/test/java/nextstep/subway/unit/FareCalculatorTest.java
@@ -0,0 +1,33 @@
+package nextstep.subway.unit;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import nextstep.subway.applicaion.FareCalculator;
+
+class FareCalculatorTest {
+
+    @DisplayName("지하철 구간 길이에 대한 요금을 계산한다.")
+    @MethodSource("calculateFareSource")
+    @ParameterizedTest
+    void calculate(int distance, int fare) {
+        assertThat(FareCalculator.calculate(distance)).isEqualTo(fare);
+    }
+
+    private static Stream<Arguments> calculateFareSource() {
+        return Stream.of(
+            Arguments.of(10, 1_250),
+            Arguments.of(12, 1_350),
+            Arguments.of(16, 1_450),
+            Arguments.of(50, 2_050),
+            Arguments.of(58, 1_850),
+            Arguments.of(64, 1_950)
+        );
+    }
+}
