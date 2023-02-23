e3f5afa463ff11ca8873d31e1772b311f3f95f57
hungrytech <74886812+hungrytech@users.noreply.github.com>
2023-02-24 07:35:47 +0900

    실습 - 문서화 (#241)
    feat: 지하철 경로 rest docs를 사용하여 문서화

---
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -0,0 +1,26 @@
+package nextstep.subway.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.http.MediaType;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+public class PathSteps {
+
+    public static ExtractableResponse<Response> 지하철_경로_조회(RequestSpecification spec, Long source, Long target) {
+        return RestAssured
+                .given(spec).log().all()
+                .filter(document("path",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())))
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("source", source)
+                .queryParam("target", target)
+                .when().get("/paths")
+                .then().log().all().extract();
+    }
+}
--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,47 @@
 package nextstep.subway.documentation;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.acceptance.PathSteps;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+@ExtendWith(MockitoExtension.class)
 public class PathDocumentation extends Documentation {
+
+    @MockBean
+    private PathService pathService;
 
     @Test
     void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+        PathResponse pathResponse = new PathResponse(
+                List.of(
+                        new StationResponse(1L, "강남역"),
+                        new StationResponse(2L, "역삼역")
+                ), 10
+        );
+
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+
+        ExtractableResponse<Response> response = PathSteps.지하철_경로_조회(spec, 1L, 2L);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
