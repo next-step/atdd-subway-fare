afe17e46bdd0070c1c89e89cca2e16a68b9086ca
michol.lee <lovethefeel@gmail.com>
2023-02-25 03:12:15 +0900

    refactor: 토큰 추출로 회원 정보 조회 메소드 추가 및 final 추가

--- src/main/java/nextstep/member/application/TokenService.java
+++ src/main/java/nextstep/member/application/TokenService.java
@@ -1,39 +1,58 @@
 package nextstep.member.application;
 
+import nextstep.common.exception.MissingTokenException;
 import nextstep.member.application.dto.GithubProfileResponse;
 import nextstep.member.application.dto.TokenResponse;
+import nextstep.member.domain.LoginMember;
 import nextstep.member.domain.Member;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+import static nextstep.common.error.MemberError.NOT_MISSING_TOKEN;
+import static nextstep.common.error.MemberError.NOT_VALID_TOKEN;
+
 @Service
 public class TokenService {
-    private MemberService memberService;
-    private JwtTokenProvider jwtTokenProvider;
-    private GithubClient githubClient;
+    private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final GithubClient githubClient;
 
-    public TokenService(MemberService memberService, JwtTokenProvider jwtTokenProvider, GithubClient githubClient) {
+    public TokenService(final MemberService memberService, final JwtTokenProvider jwtTokenProvider, final GithubClient githubClient) {
         this.memberService = memberService;
         this.jwtTokenProvider = jwtTokenProvider;
         this.githubClient = githubClient;
     }
 
-    public TokenResponse createToken(String email, String password) {
-        Member member = memberService.login(email, password);
-
-        String token = jwtTokenProvider.createToken(member.getId().toString(), member.getRoles());
+    public TokenResponse createToken(final String email, final String password) {
+        final Member member = memberService.login(email, password);
+        final String token = createToken(member.getEmail(), member.getRoles());
 
         return new TokenResponse(token);
     }
 
-    public TokenResponse createGithubToken(String code) {
-        String accessTokenFromGithub = githubClient.getAccessTokenFromGithub(code);
-
-        GithubProfileResponse githubProfile = githubClient.getGithubProfileFromGithub(accessTokenFromGithub);
-
-        Member member = memberService.createOrFindMember(githubProfile.getEmail());
-
-        String token = jwtTokenProvider.createToken(member.getId().toString(), member.getRoles());
+    public TokenResponse createGithubToken(final String code) {
+        final String accessTokenFromGithub = githubClient.getAccessTokenFromGithub(code);
+        final GithubProfileResponse githubProfile = githubClient.getGithubProfileFromGithub(accessTokenFromGithub);
+        final Member member = memberService.createOrFindMember(githubProfile.getEmail());
+        final String token = jwtTokenProvider.createToken(member.getId().toString(), member.getRoles());
 
         return new TokenResponse(token);
+    }
+
+    public LoginMember findMemberByToken(final String credentials) {
+        if (credentials.isEmpty()) {
+            throw new MissingTokenException(NOT_MISSING_TOKEN);
+        }
+        if (!jwtTokenProvider.validateToken(credentials)) {
+            throw new MissingTokenException(NOT_VALID_TOKEN);
+        }
+        final String email = jwtTokenProvider.getPrincipal(credentials);
+        final Member member = memberService.findByEmail(email);
+        return LoginMember.from(member);
+    }
+
+    private String createToken(final String email, final List<String> roles) {
+        return jwtTokenProvider.createToken(email, roles);
     }
 }
