7f2aac23954151e440438537f661d0f6922269a5
michol.lee <lovethefeel@gmail.com>
2023-02-25 02:55:48 +0900

    feat: 공통 에러 객체

---
+++ src/main/java/nextstep/common/ErrorResponse.java
@@ -0,0 +1,21 @@
+package nextstep.common;
+
+import java.util.List;
+
+public class ErrorResponse {
+
+    private List<String> messages;
+
+    public ErrorResponse(final List<String> messages) {
+        super();
+        this.messages = messages;
+    }
+
+    public static ErrorResponse from(final String message) {
+        return new ErrorResponse(List.of(message));
+    }
+
+    public List<String> getMessage() {
+        return messages;
+    }
+}
