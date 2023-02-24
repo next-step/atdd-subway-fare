32fe5b3eeffa14a30c27355107fb3f09787d8add
seondongpyo <seondongpyo@gmail.com>
2023-02-24 23:59:57 +0900

    test: 단위 테스트 작성 (추가 요금이 있는 여러 노선이 포함된 구간의 요금 계산)

--- src/test/java/nextstep/subway/unit/PathTest.java
+++ src/test/java/nextstep/subway/unit/PathTest.java
@@ -20,16 +20,24 @@ class PathTest {
     private Station 복정역;
     private Station 가천대역;
     private Station 오금역;
+    private Station 마천역;
     private Line 분당선;
     private Line 삼호선;
+    private Line 오호선;
     private Path 분당선_구간;
     private Path 삼호선_구간;
+    private Path 삼호선_오호선_구간;
 
     /**
-     *   수서역 ───── 복정역 ───── 가천대역
+     *   수서역 ─── *분당선* ─── 복정역 ─── *분당선* ─── 가천대역
      *     │
+     *  *3호선* (추가 요금: 900원)
      *     │
      *   오금역
+     *     │
+     *  *5호선* (추가 요금: 1,200원)
+     *     │
+     *   마천역
      */
     @BeforeEach
     void setUp() {
@@ -37,16 +45,21 @@ class PathTest {
         복정역 = new Station("복정역");
         가천대역 = new Station("가천대역");
         오금역 = new Station("오금역");
+        마천역 = new Station("마천역");
         분당선 = new Line("분당선", "yellow");
-        삼호선 = new Line("삼호선", "orange", 900);
+        삼호선 = new Line("3호선", "orange", 900);
+        오호선 = new Line("5호선", "purple", 1200);
 
         분당선_구간 = new Path(new Sections(List.of(
             new Section(분당선, 수서역, 복정역, 10, 5),
             new Section(분당선, 복정역, 가천대역, 8, 3)
         )));
 
-        삼호선_구간 = new Path(new Sections(List.of(
-            new Section(삼호선, 수서역, 오금역, 10, 4)
+        삼호선_구간 = new Path(new Sections(List.of(new Section(삼호선, 수서역, 오금역, 5, 4))));
+
+        삼호선_오호선_구간 = new Path(new Sections(List.of(
+            new Section(삼호선, 수서역, 오금역, 5, 4),
+            new Section(오호선, 오금역, 마천역, 4, 2)
         )));
     }
 
@@ -73,8 +86,15 @@ class PathTest {
 
     @DisplayName("추가 요금이 있는 노선이 포함된 지하철 구간의 요금을 구한다.")
     @Test
-    void extractFareWithExtraFare() {
+    void extractFareWithExtraFareLine() {
         // when & then
         assertThat(삼호선_구간.extractFare()).isEqualTo(2_150);
+    }
+
+    @DisplayName("추가 요금이 있는 여러 노선이 포함된 지하철 구간일 경우, 추가 요금은 추가 요금 중 가장 높은 금액으로 적용한다.")
+    @Test
+    void extraFareWithExtraFareLines() {
+        // when & then
+        assertThat(삼호선_오호선_구간.extractFare()).isEqualTo(2_450);
     }
 }
