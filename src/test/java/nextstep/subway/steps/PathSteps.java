a6f85ff73e9da54fae0b25c8fd006240afe5f100
정천우 <codehouse95@gmail.com>
2023-02-24 23:25:03 +0900

    test(step1): 코멘트 적용

--- src/test/java/nextstep/subway/steps/PathSteps.java
+++ src/test/java/nextstep/subway/steps/PathSteps.java
@@ -1,12 +1,12 @@
 package nextstep.subway.steps;
 
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -16,17 +16,24 @@ import io.restassured.specification.RequestSpecification;
 public class PathSteps {
 
 	public static ExtractableResponse<Response> searchPath(RequestSpecification spec, Long source, Long target) {
-		Map<String, Long> params = new HashMap<>();
-		params.put("source", source);
-		params.put("target", target);
-
 		ExtractableResponse<Response> searchResponse = RestAssured
 			.given(spec).log().all()
 			.filter(document("path",
 				preprocessRequest(prettyPrint()),
-				preprocessResponse(prettyPrint())))
+				preprocessResponse(prettyPrint()),
+				requestParameters(
+					parameterWithName("source").description("출발역 id"),
+					parameterWithName("target").description("도착역 id")
+				),
+				responseFields(
+					fieldWithPath("stations").type(JsonFieldType.ARRAY).description("경로 지하철역 목록"),
+					fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("지하철역 id"),
+					fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("지하철역 이름"),
+					fieldWithPath("distance").type(JsonFieldType.NUMBER).description("거리(km)")
+				)))
 			.accept(MediaType.APPLICATION_JSON_VALUE)
-			.params(params)
+			.queryParam("source", source)
+			.queryParam("target", target)
 			.when().get("/paths")
 			.then().log().all()
 			.extract();
