a376ffbbacfd8d1ce1959376e247b43a07fb61b9
정천우 <codehouse95@gmail.com>
2023-02-24 15:34:36 +0900

    test(step0): 테스트 문서 이미지 추가

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,44 @@
 package nextstep.subway.documentation;
 
+import static org.mockito.Mockito.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.*;
+
 import io.restassured.RestAssured;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 
 public class PathDocumentation extends Documentation {
 
-    @Test
-    void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
-    }
+	@MockBean
+	private PathService pathService;
+
+	@Test
+	void path() {
+		PathResponse pathResponse = new PathResponse(
+			Lists.newArrayList(
+				new StationResponse(1L, "강남역"),
+				new StationResponse(2L, "역삼역")
+			), 10
+		);
+
+		when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+		RestAssured
+			.given(spec).log().all()
+			.filter(document("path",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint())))
+			.accept(MediaType.APPLICATION_JSON_VALUE)
+			.queryParam("source", 1L)
+			.queryParam("target", 2L)
+			.when().get("/paths")
+			.then().log().all().extract();
+	}
 }
