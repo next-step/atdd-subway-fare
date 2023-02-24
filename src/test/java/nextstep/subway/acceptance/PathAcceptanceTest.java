5df251df97e9893631ff5aa066e0876c176a2214
정천우 <codehouse95@gmail.com>
2023-02-24 20:55:59 +0900

    test(step1): 인수 조건을 검증하는 인수 테스트 작성

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -24,11 +24,14 @@ class PathAcceptanceTest extends AcceptanceTest {
     private Long 삼호선;
 
     /**
+	 * 					(di:10, dr:)
      * 교대역		---	*2호선*	---	강남역
      * |								|
      * *3호선*						*신분당선*
+	 * (di:2, dr:)					(di:10, dr:)
      * |								|
      * 남부터미널역	---	*3호선*	---	양재역
+	 * 					(di:3, dr:)
      */
     @BeforeEach
     public void setUp() {
@@ -55,4 +58,22 @@ class PathAcceptanceTest extends AcceptanceTest {
         // then
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
     }
+
+	/**
+	 * Given 지하철역이 등록되어있음
+	 * And 지하철 노선이 등록되어있음
+	 * And 지하철 노선에 지하철역이 등록되어있음
+	 * When 출발역에서 도착역까지의 최소 시간 기준으로 경로 조회를 요청
+	 * Then 최소 시간 기준 경로를 응답
+	 * And 총 거리와 소요 시간을 함께 응답함
+	 * **/
+	@DisplayName("두 역의 최소 시간 경로를 조회")
+	@Test
+	void findPathByTime() {
+		// when
+		ExtractableResponse<Response> response = 두_역의_최소_시간_경로_조회를_요청(교대역, 양재역, "DURATION");
+
+		// then
+		assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 강남역, 양재역);
+	}
 }
--- src/test/java/nextstep/subway/steps/PathSteps.java
+++ src/test/java/nextstep/subway/steps/PathSteps.java
@@ -40,12 +40,19 @@ public class PathSteps {
 		return searchResponse;
 	}
 
-
 	public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target) {
 		return RestAssured
 			.given().log().all()
 			.accept(MediaType.APPLICATION_JSON_VALUE)
 			.when().get("/paths?source={sourceId}&target={targetId}", source, target)
+			.then().log().all().extract();
+	}
+
+	public static ExtractableResponse<Response> 두_역의_최소_시간_경로_조회를_요청(Long source, Long target, String type) {
+		return RestAssured
+			.given().log().all()
+			.accept(MediaType.APPLICATION_JSON_VALUE)
+			.when().get("/paths?source={sourceId}&target={targetId}&type={type}", source, target, type)
 			.then().log().all().extract();
 	}
 }
