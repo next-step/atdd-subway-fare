65b986195e2f2d6f7ace636bfdcaf52f3d7c8fda
michol.lee <lovethefeel@gmail.com>
2023-02-25 02:54:52 +0900

    refactor(test): 인증 관련 테스트 케이스 추가

--- src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/AuthAcceptanceTest.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.Test;
 
 import static nextstep.subway.acceptance.MemberSteps.깃허브_인증_로그인_요청;
 import static nextstep.subway.acceptance.MemberSteps.베어러_인증_로그인_요청;
+import static nextstep.subway.acceptance.MemberSteps.인증_로그인_응답_실패;
+import static nextstep.subway.acceptance.MemberSteps.회원_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("인증 관련 테스트")
 class AuthAcceptanceTest extends AcceptanceTest {
-
 
     @DisplayName("Bearer Auth")
     @Test
@@ -19,6 +21,17 @@ class AuthAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 베어러_인증_로그인_요청(EMAIL, PASSWORD);
 
         assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
+    }
+
+    @DisplayName("패스워드를 잘못 입력해서 Bearer Auth 인증에 실패한다.")
+    @Test
+    void error_bearerAuth() {
+
+        회원_생성_요청(EMAIL, PASSWORD, 10);
+
+        final ExtractableResponse<Response> response = 베어러_인증_로그인_요청(EMAIL, "different password");
+
+        인증_로그인_응답_실패(response);
     }
 
     @DisplayName("Github Auth")
