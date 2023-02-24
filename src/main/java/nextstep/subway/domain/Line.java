74cf93ca73ed1ef68a97b648aa68b6a01e968b60
michol.lee <lovethefeel@gmail.com>
2023-02-25 02:57:36 +0900

    refactor: 노선 객체 - 접근제한자 변경 - 부 생성자 추가 - 사용하지 않는 메소드 제거

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -10,16 +10,36 @@ public class Line {
     private Long id;
     private String name;
     private String color;
-
     @Embedded
     private Sections sections = new Sections();
 
-    public Line() {
-    }
+    protected Line() {}
 
-    public Line(String name, String color) {
+    public Line(final String name, final String color, final Sections sections) {
         this.name = name;
         this.color = color;
+        this.sections = sections;
+    }
+
+    public Line(final String name, final String color) {
+        this(name, color, new Sections());
+    }
+
+    public void addSection(Station upStation, Station downStation, int distance, int duration) {
+        sections.add(new Section(this, upStation, downStation, distance, duration));
+    }
+
+    public void deleteSection(Station station) {
+        sections.delete(station);
+    }
+
+    public void update(String name, String color) {
+        if (name != null) {
+            this.name = name;
+        }
+        if (color != null) {
+            this.color = color;
+        }
     }
 
     public Long getId() {
@@ -38,24 +58,7 @@ public class Line {
         return sections.getSections();
     }
 
-    public void update(String name, String color) {
-        if (name != null) {
-            this.name = name;
-        }
-        if (color != null) {
-            this.color = color;
-        }
-    }
-
-    public void addSection(Station upStation, Station downStation, int distance, int duration) {
-        sections.add(new Section(this, upStation, downStation, distance, duration));
-    }
-
     public List<Station> getStations() {
         return sections.getStations();
-    }
-
-    public void deleteSection(Station station) {
-        sections.delete(station);
     }
 }
