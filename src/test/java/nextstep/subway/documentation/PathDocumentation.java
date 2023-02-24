5100f091cb77778e8ebf62de8b8cee04b637efe3
정천우 <codehouse95@gmail.com>
2023-02-24 15:43:13 +0900

    test(step0): 코드 중복 제거

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,18 +1,19 @@
 package nextstep.subway.documentation;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.*;
 
-import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.applicaion.PathService;
 import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
+import nextstep.subway.steps.PathSteps;
 
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 
 public class PathDocumentation extends Documentation {
 
@@ -21,24 +22,19 @@ public class PathDocumentation extends Documentation {
 
 	@Test
 	void path() {
+		// given
 		PathResponse pathResponse = new PathResponse(
 			Lists.newArrayList(
 				new StationResponse(1L, "강남역"),
 				new StationResponse(2L, "역삼역")
 			), 10
 		);
-
 		when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
 
-		RestAssured
-			.given(spec).log().all()
-			.filter(document("path",
-				preprocessRequest(prettyPrint()),
-				preprocessResponse(prettyPrint())))
-			.accept(MediaType.APPLICATION_JSON_VALUE)
-			.queryParam("source", 1L)
-			.queryParam("target", 2L)
-			.when().get("/paths")
-			.then().log().all().extract();
+		// when
+		ExtractableResponse<Response> searchResponse = PathSteps.searchPath(spec, 1L, 2L);
+
+		// then
+		assertThat(searchResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
 	}
 }
---
+++ src/test/java/nextstep/subway/steps/PathSteps.java
@@ -0,0 +1,35 @@
+package nextstep.subway.steps;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class PathSteps {
+
+	public static ExtractableResponse<Response> searchPath(RequestSpecification spec, Long source, Long target) {
+		Map<String, Long> params = new HashMap<>();
+		params.put("source", source);
+		params.put("target", target);
+
+		ExtractableResponse<Response> searchResponse = RestAssured
+			.given(spec).log().all()
+			.filter(document("path",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint())))
+			.accept(MediaType.APPLICATION_JSON_VALUE)
+			.params(params)
+			.when().get("/paths")
+			.then().log().all()
+			.extract();
+		return searchResponse;
+	}
+}
