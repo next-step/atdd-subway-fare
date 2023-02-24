6daa1e9bf51d870aa6de634681aec9820c19927c
seondongpyo <seondongpyo@gmail.com>
2023-02-25 03:37:30 +0900

    test: 인수 테스트 작성 (경로를 조회한 로그인 사용자가 어린이인 경우)

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -251,7 +251,7 @@ class PathAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * Given 사용자가 로그인을 요청하고
+     * Given 성인 사용자가 로그인을 요청하고
      * When 지하철 경로 조회를 요청한 로그인 사용자가 성인이라면
      * Then 지하철 요금은 할인되지 않은 요금이다.
      */
@@ -269,7 +269,7 @@ class PathAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * Given 사용자가 회원 가입 및 로그인을 요청하고
+     * Given 청소년 사용자가 로그인을 요청하고
      * When 지하철 경로 조회를 요청한 로그인 사용자가 청소년이라면
      * Then 지하철 요금은 운임에서 350원을 공제한 금액의 20%가 할인된 요금이다.
      */
@@ -278,6 +278,24 @@ class PathAcceptanceTest extends AcceptanceTest {
     void findPathByTeenagerMember() {
         // given
         String accessToken = 베어러_인증_로그인_요청("teenager@email.com", "password").jsonPath().getString("accessToken");
+
+        // when
+        ExtractableResponse<Response> response = 로그인_사용자가_두_역의_최단_거리_경로_조회를_요청(교대역, 강남역, accessToken);
+
+        // then
+        assertThat(response.jsonPath().getInt("fare")).isEqualTo(800);
+    }
+
+    /**
+     * Given 어린이 사용자가 로그인을 요청하고
+     * When 지하철 경로 조회를 요청한 로그인 사용자가 어린이이라면
+     * Then 지하철 요금은 운임에서 350원을 공제한 금액의 50%가 할인된 요금이다.
+     */
+    @DisplayName("로그인 사용자가 어린이라면, 지하철 요금은 운임에서 350원을 공제한 금액의 50%가 할인된 요금이다.")
+    @Test
+    void findPathByChildMember() {
+        // given
+        String accessToken = 베어러_인증_로그인_요청("child@email.com", "password").jsonPath().getString("accessToken");
 
         // when
         ExtractableResponse<Response> response = 로그인_사용자가_두_역의_최단_거리_경로_조회를_요청(교대역, 강남역, accessToken);
