d2376e8123a6af8b4dda579a1ad0b4e79ca06d74
seondongpyo <seondongpyo@gmail.com>
2023-02-25 01:55:49 +0900

    test: 인수 테스트 작성 (50km 초과 + 추가 요금이 있는 노선)

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -195,7 +195,7 @@ class PathAcceptanceTest extends AcceptanceTest {
 
     /**
      * When 출발역에서 도착역까지의 구간 길이가 10km 초과 50km 이내이고, 추가 요금이 있는 노선을 이용하면
-     * Then 총 지하철 요금은 (기본 요금 + 추가 요금 + 노선 추가 요금) 이다.
+     * Then 총 지하철 요금은 (기본 요금 + 추가 요금(5km 당 100원) + 노선 추가 요금) 이다.
      */
     @DisplayName("구간 길이가 10km 초과 50km 이내이고 여러 추가 요금 노선이 포함되었다면, 총 지하철 요금은 (기본 요금 + 추가 요금(5km 당 100원) + 가장 큰 노선 추가 요금) 이다.")
     @Test
@@ -219,6 +219,20 @@ class PathAcceptanceTest extends AcceptanceTest {
 
         // then
         assertThat(response.jsonPath().getInt("fare")).isEqualTo(2_850);
+    }
+
+    /**
+     * When 출발역에서 도착역까지의 구간 길이가 50km 초과이고, 추가 요금이 있는 노선을 이용하면
+     * Then 총 지하철 요금은 (기본 요금 + 추가 요금(8km 당 100원) + 노선 추가 요금) 이다.
+     */
+    @DisplayName("구간 길이가 50km 초과이고 추가 요금 노선이 포함되었다면, 총 지하철 요금은 (기본 요금 + 추가 요금(8km 당 100원) + 노선 추가 요금) 이다.")
+    @Test
+    void longDistanceWithExtraFare() {
+        // when
+        ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(강남역, 공덕역);
+
+        // then
+        assertThat(response.jsonPath().getInt("fare")).isEqualTo(2_350);
     }
 
     private ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target) {
