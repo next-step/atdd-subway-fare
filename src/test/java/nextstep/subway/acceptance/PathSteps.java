80c71bed9abe314004f097eae09b6080292aa1a7
doorisopen <doorwasopen@gmail.com>
2023-02-25 10:07:51 +0900

    test: 최단 시간 경로 탐색 문서화 테스트 추가

--- src/test/java/nextstep/subway/acceptance/PathSteps.java
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -10,21 +10,24 @@ import org.springframework.http.MediaType;
 import java.util.Map;
 
 public class PathSteps {
+    public static final String DISTANCE = "DISTANCE";
+    public static final String DURATION = "DURATION";
+
     public static ExtractableResponse<Response> 두_역의_최단_시간_경로_조회를_요청(Long source, Long target) {
         RequestSpecification spec = new RequestSpecBuilder()
-                .addQueryParams(createPathParams(source, target, "DURATION"))
+                .addQueryParams(createPathParams(source, target, DURATION))
                 .build();
-        return 두_역의_최단_거리_경로_조회를_요청(spec);
+        return 두_역의_최단_경로_조회를_요청(spec);
     }
 
     public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target) {
         RequestSpecification spec = new RequestSpecBuilder()
-                .addQueryParams(createPathParams(source, target, "DISTANCE"))
+                .addQueryParams(createPathParams(source, target, DISTANCE))
                 .build();
-        return 두_역의_최단_거리_경로_조회를_요청(spec);
+        return 두_역의_최단_경로_조회를_요청(spec);
     }
 
-    public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(RequestSpecification specification) {
+    public static ExtractableResponse<Response> 두_역의_최단_경로_조회를_요청(RequestSpecification specification) {
         return RestAssured
                 .given(specification).log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -11,9 +11,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static nextstep.subway.acceptance.PathSteps.두_역의_최단_거리_경로_조회를_요청;
+import static nextstep.subway.acceptance.PathSteps.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.verify;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
@@ -26,20 +26,43 @@ public class PathDocumentation extends Documentation {
     private PathService pathService;
 
     @Test
-    void path() {
-        PathResponse pathResponse = new PathResponse(
-                Lists.newArrayList(
-                        new StationResponse(1L, "강남역"),
-                        new StationResponse(2L, "역삼역")
-                ), 10);
-        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+    void 최단_거리_경로를_탐색한다() {
+        경로_응답을_설정한다(new PathResponse(Lists.newArrayList(
+                new StationResponse(1L, "강남역"),
+                new StationResponse(2L, "역삼역")), 10));
 
-        this.spec
-                .queryParams("source", 1L, "target", 2L)
-                .filter(document("path", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())));
-        ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(this.spec);
+        문서화_요청_파라미터를_설정한다(1L, 2L, DISTANCE);
+        ExtractableResponse<Response> response = 두_역의_최단_경로_조회를_요청(this.spec);
 
-        verify(pathService, times(1)).findPath(anyLong(), anyLong());
+
+        경로_응답을_검증한다();
         assertThat(response.jsonPath().getList("stations.id")).containsExactly(1, 2);
+    }
+
+    @Test
+    void 최단_시간_경로를_탐색한다() {
+        경로_응답을_설정한다(new PathResponse(Lists.newArrayList(
+                        new StationResponse(1L, "강남역"),
+                        new StationResponse(2L, "역삼역")), 10, 5));
+
+        문서화_요청_파라미터를_설정한다(1L, 2L, DURATION);
+        ExtractableResponse<Response> response = 두_역의_최단_경로_조회를_요청(this.spec);
+
+        경로_응답을_검증한다();
+        assertThat(response.jsonPath().getList("stations.id")).containsExactly(1, 2);
+    }
+
+    private void 경로_응답을_설정한다(PathResponse pathResponse) {
+        when(pathService.findPath(any())).thenReturn(pathResponse);
+    }
+
+    private void 문서화_요청_파라미터를_설정한다(Long source, Long target, String type) {
+        this.spec
+                .queryParams("source", source, "target", target, "type", type)
+                .filter(document("path", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())));
+    }
+
+    private void 경로_응답을_검증한다() {
+        verify(pathService, times(1)).findPath(any());
     }
 }
