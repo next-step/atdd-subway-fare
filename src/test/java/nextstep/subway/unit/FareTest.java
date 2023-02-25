d2860f47d7a4ce36bc81ed5a0487b6e921b46596
doorisopen <doorwasopen@gmail.com>
2023-02-25 18:45:45 +0900

    feat: 기본 요금 정책 도메인 구현
    - test: 단위 테스트 수정

--- src/test/java/nextstep/subway/unit/FareTest.java
+++ src/test/java/nextstep/subway/unit/FareTest.java
@@ -1,7 +1,10 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.BaseFarePolicy;
 import nextstep.subway.domain.Fare;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static nextstep.subway.domain.FarePolicy.BASE_FARE;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,13 +12,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class FareTest {
 
     @Test
-    void 지하철_운임_요금을_생성한다(){
+    void 지하철_운임_요금을_생성한다() {
         Fare fare = new Fare(distance -> 0);
         assertThat(fare).isNotNull();
     }
 
     @Test
-    void 지하철_운임_요금을_계산한다(){
+    void 지하철_운임_요금을_계산한다() {
         Fare fare = new Fare(distance -> 0);
 
         int amount = fare.calculate(10);
@@ -24,7 +27,7 @@ class FareTest {
     }
 
     @Test
-    void 기본_요금_정책_중_거리_10km_이내는_기본_운임_요금이_발생한다(){
+    void 기본_요금_정책_중_거리_10km_이내는_기본_운임_요금이_발생한다() {
         Fare fare = new Fare(distance -> BASE_FARE);
 
         int amount = fare.calculate(10);
@@ -32,21 +35,23 @@ class FareTest {
         assertThat(amount).isEqualTo(BASE_FARE);
     }
 
-    @Test
-    void 기본_요금_정책_중_거리_10km_초과_50km_이내는_5km_당_100원_추가_운임_요금이_발생한다(){
-        Fare fare = new Fare(distance -> BASE_FARE);
+    @CsvSource(value = {"12,1350", "16,1450", "50,2050"}, delimiter = ',')
+    @ParameterizedTest
+    void 기본_요금_정책_중_거리_10km_초과_50km_이내는_5km_당_100원_추가_운임_요금이_발생한다(int distance, int fareAmount) {
+        Fare fare = new Fare(new BaseFarePolicy());
 
-        int amount = fare.calculate(50);
+        int amount = fare.calculate(distance);
 
-        assertThat(amount).isEqualTo(2050);
+        assertThat(amount).isEqualTo(fareAmount);
     }
 
-    @Test
-    void 기본_요금_정책_중_거리_50km_초과는_8km_당_100원_추가_운임_요금이_발생한다(){
-        Fare fare = new Fare(distance -> BASE_FARE);
+    @CsvSource(value = {"50,2050", "51,2150", "58,2250"}, delimiter = ',')
+    @ParameterizedTest
+    void 기본_요금_정책_중_거리_50km_초과는_8km_당_100원_추가_운임_요금이_발생한다(int distance, int fareAmount) {
+        Fare fare = new Fare(new BaseFarePolicy());
 
-        int amount = fare.calculate(58);
+        int amount = fare.calculate(distance);
 
-        assertThat(amount).isEqualTo(2150);
+        assertThat(amount).isEqualTo(fareAmount);
     }
 }
