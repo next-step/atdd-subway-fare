e5b79d9b173c8d109d85551e29dc8bdee8f54db0
doorisopen <doorwasopen@gmail.com>
2023-02-25 17:43:48 +0900

    refactor: 경로 탐색시 운임 금액 응답 필드 추가

--- src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
@@ -9,15 +9,21 @@ public class PathResponse {
     private List<StationResponse> stations;
     private int distance;
     private int duration;
+    private FareResponse fare;
 
     public PathResponse(List<StationResponse> stations, int distance) {
         this(stations, distance, 0);
     }
 
     public PathResponse(List<StationResponse> stations, int distance, int duration) {
+        this(stations, distance, duration, new FareResponse());
+    }
+
+    public PathResponse(List<StationResponse> stations, int distance, int duration, FareResponse fare) {
         this.stations = stations;
         this.distance = distance;
         this.duration = duration;
+        this.fare = fare;
     }
 
     public static PathResponse of(Path path) {
@@ -40,5 +46,9 @@ public class PathResponse {
 
     public int getDuration() {
         return duration;
+    }
+
+    public FareResponse getFare() {
+        return fare;
     }
 }
