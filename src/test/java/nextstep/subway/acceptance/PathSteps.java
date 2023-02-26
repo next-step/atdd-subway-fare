fe80f4131782dd79640aff2b61f0b3acff757c84
dhlee3994 <dhlee3994@gmail.com>
2023-02-26 16:16:33 +0900

    Test: PathSteps 중복되는 부분 리펙토링

--- src/test/java/nextstep/subway/acceptance/PathSteps.java
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -1,6 +1,7 @@
 package nextstep.subway.acceptance;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -10,16 +11,8 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class PathSteps {
 
     public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(final Long source, final Long target) {
-        return RestAssured
-                .given()
-                    .accept(APPLICATION_JSON_VALUE)
-                    .queryParam("source", source)
-                    .queryParam("target", target)
-                .when()
-                    .get("/paths")
-                .then()
-                    .log().all()
-                .extract();
+        final RequestSpecification spec = new RequestSpecBuilder().build();
+        return 두_역의_최단_거리_경로_조회를_요청(spec, source, target);
     }
 
     public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(final RequestSpecification spec, final Long source, final Long target) {
