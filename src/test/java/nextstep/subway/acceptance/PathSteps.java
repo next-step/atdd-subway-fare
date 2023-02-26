4da5c4d8b36d089ba55876eedfbc0514cc66a693
GiSeong Ji <89971435+jiggyjiggy@users.noreply.github.com>
2023-02-26 14:14:49 +0900

    문서화 미션 (#247)
    * fix: 깨지는 테스트 수정
    
    * style: wildcard import 명시적으로 정정
    
    * feat: 경로 조회 문서화
    
    * refactor: API 요청 PathSteps으로 분리

---
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -0,0 +1,35 @@
+package nextstep.subway.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+public class PathSteps {
+
+    public static ExtractableResponse<Response> 경로_조회_요청(RequestSpecification spec, Long source, Long target) {
+        Map<String, Long> params = new HashMap<>();
+        params.put("source", source);
+        params.put("target", target);
+
+        return RestAssured
+                .given(spec).log().all()
+                .filter(document("path",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())))
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .params(params)
+                .when().get("/paths")
+                .then().log().all()
+                .extract();
+    }
+}
--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,41 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+
+import static nextstep.subway.acceptance.PathSteps.경로_조회_요청;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 public class PathDocumentation extends Documentation {
+    public static final long SOURCE_ID = 1L;
+    public static final long TARGET_ID = 2L;
+    @MockBean
+    private PathService pathService;
 
     @Test
     void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+        // Given
+        PathResponse pathResponse = new PathResponse(
+                Lists.newArrayList(
+                        new StationResponse(SOURCE_ID, "강남역"),
+                        new StationResponse(TARGET_ID, "역삼역")
+                ), 10
+        );
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+        // When
+        ExtractableResponse<Response> response = 경로_조회_요청(spec, SOURCE_ID, TARGET_ID);
+
+        // Then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
