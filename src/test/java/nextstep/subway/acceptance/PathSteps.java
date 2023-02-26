524f75c9278373371474e1ff42cacdabe121e4c3
doorisopen <doorwasopen@gmail.com>
2023-02-26 23:22:07 +0900

    test: 회원 인증 요청 로직 리팩터링

--- src/test/java/nextstep/subway/acceptance/PathSteps.java
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -1,7 +1,6 @@
 package nextstep.subway.acceptance;
 
 import io.restassured.RestAssured;
-import io.restassured.authentication.OAuth2Scheme;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
@@ -15,36 +14,19 @@ public class PathSteps {
     public static final String DURATION = "DURATION";
 
     public static ExtractableResponse<Response> 두_역의_최단_시간_경로_조회를_요청(Long source, Long target) {
-        RequestSpecification spec = new RequestSpecBuilder()
-                .addQueryParams(createPathParams(source, target, DURATION))
-                .build();
-        return 두_역의_최단_경로_조회를_요청(spec);
+        return 두_역의_최단_경로_조회를_요청(createRequestSpecification(source, target, DURATION));
     }
 
     public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target) {
-        RequestSpecification spec = new RequestSpecBuilder()
-                .addQueryParams(createPathParams(source, target, DISTANCE))
-                .build();
-        return 두_역의_최단_경로_조회를_요청(spec);
+        return 두_역의_최단_경로_조회를_요청(createRequestSpecification(source, target, DISTANCE));
     }
 
     public static ExtractableResponse<Response> 인증_회원_두_역의_최단_시간_경로_조회를_요청(String token, Long source, Long target) {
-        OAuth2Scheme oAuth2Scheme = new OAuth2Scheme();
-        oAuth2Scheme.setAccessToken(token);
-        RequestSpecification spec = new RequestSpecBuilder()
-                .addQueryParams(createPathParams(source, target, DURATION))
-                .setAuth(oAuth2Scheme)
-                .build();
-        return 두_역의_최단_경로_조회를_요청(spec);
+        return 인증_회원_두_역의_최단_경로_조회를_요청(token, createRequestSpecification(source, target, DURATION));
     }
 
     public static ExtractableResponse<Response> 인증_회원_두_역의_최단_거리_경로_조회를_요청(String token, Long source, Long target) {
-        OAuth2Scheme oAuth2Scheme = new OAuth2Scheme();
-        oAuth2Scheme.setAccessToken(token);
-        RequestSpecification spec = new RequestSpecBuilder()
-                .addQueryParams(createPathParams(source, target, DISTANCE))
-                .build();
-        return 두_역의_최단_경로_조회를_요청(spec);
+        return 인증_회원_두_역의_최단_경로_조회를_요청(token, createRequestSpecification(source, target, DISTANCE));
     }
 
     public static ExtractableResponse<Response> 두_역의_최단_경로_조회를_요청(RequestSpecification specification) {
@@ -55,7 +37,22 @@ public class PathSteps {
                 .then().log().all().extract();
     }
 
+    public static ExtractableResponse<Response> 인증_회원_두_역의_최단_경로_조회를_요청(String token, RequestSpecification specification) {
+        return RestAssured
+                .given(specification).log().all()
+                .auth().oauth2(token)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/paths")
+                .then().log().all().extract();
+    }
+
     private static Map<String, String> createPathParams(Long source, Long target, String type) {
         return Map.of("source", source + "", "target", target + "", "type", type);
+    }
+
+    private static RequestSpecification createRequestSpecification(Long source, Long target, String type) {
+        return new RequestSpecBuilder()
+                .addQueryParams(createPathParams(source, target, type))
+                .build();
     }
 }
--- src/test/java/nextstep/subway/unit/FarePolicyTest.java
+++ src/test/java/nextstep/subway/unit/FarePolicyTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static nextstep.subway.domain.FarePolicy.BASE_FARE;
+import static nextstep.subway.domain.BaseFarePolicy.BASE_FARE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FarePolicyTest {
