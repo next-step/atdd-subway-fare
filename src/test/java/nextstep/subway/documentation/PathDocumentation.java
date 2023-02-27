ad035f3c0c95b9904e7d69a05bcd5b5fe7c92ca6
ess234 <ess234@naver.com>
2023-02-27 18:31:56 +0900

    feat: 실습 문서화
    - spring rest doc 적용

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,38 @@
 package nextstep.subway.documentation;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.acceptance.PathSteps;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    PathService pathService;
+
     @Test
     void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+        PathResponse pathResponse = new PathResponse(Lists.newArrayList(new StationResponse(1L, "강남역"),
+                new StationResponse(2L, "역삼역")),10);
+
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+        // when
+        ExtractableResponse<Response> searchResponse = PathSteps.searchPath(spec, 1L, 2L);
+        assertThat(searchResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
