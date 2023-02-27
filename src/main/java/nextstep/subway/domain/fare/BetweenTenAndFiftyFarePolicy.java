6bce59f9f6f1a8f235c89ee05f600484f9bda374
dhlee3994 <dhlee3994@gmail.com>
2023-02-28 00:17:26 +0900

    Refactor: 요금 계산 부분 중복 제거

--- src/main/java/nextstep/subway/domain/fare/BetweenTenAndFiftyFarePolicy.java
+++ src/main/java/nextstep/subway/domain/fare/BetweenTenAndFiftyFarePolicy.java
@@ -8,19 +8,6 @@ public class BetweenTenAndFiftyFarePolicy implements FarePolicy {
 
     @Override
     public int getFare(final int distance) {
-        if (isNotValidDistance(distance)) {
-            return 0;
-        }
-
-        int extraDistance = distance - MIN_DISTANCE;
-        if (MAX_DISTANCE < distance) {
-            extraDistance = MAX_DISTANCE - MIN_DISTANCE;
-        }
-
-        return calculateFare(extraDistance, EXTRA_FARE_RATE);
-    }
-
-    private boolean isNotValidDistance(final int distance) {
-        return distance <= MIN_DISTANCE;
+        return getFare(distance, MIN_DISTANCE, MAX_DISTANCE, EXTRA_FARE_RATE);
     }
 }
--- src/main/java/nextstep/subway/domain/fare/FarePolicy.java
+++ src/main/java/nextstep/subway/domain/fare/FarePolicy.java
@@ -6,6 +6,19 @@ public interface FarePolicy {
 
     int getFare(int distance);
 
+    default int getFare(int distance, int min, int start, int rate) {
+        if (distance <= min) {
+            return 0;
+        }
+
+        int extraDistance = distance - min;
+        if (start < distance) {
+            extraDistance = start - min;
+        }
+
+        return calculateFare(extraDistance, rate);
+    }
+
     default int calculateFare(int distance, int extraFareRate) {
         return (int) ((Math.ceil((distance - 1) / extraFareRate) + 1) * EXTRA_FARE);
     }
