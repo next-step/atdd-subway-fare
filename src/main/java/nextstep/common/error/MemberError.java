3d32d96e3ad7ca1d0a1ffc5c0bd23c652b2ea7e8
michol.lee <lovethefeel@gmail.com>
2023-02-27 23:19:22 +0900

    refactor: 로그인 익셉션 메세지 추가

--- src/main/java/nextstep/common/error/MemberError.java
+++ src/main/java/nextstep/common/error/MemberError.java
@@ -4,7 +4,7 @@ public enum MemberError {
     UNAUTHORIZED("인증에 실패하였습니다. 입력한 정보를 확인해주세요."),
     NOT_VALID_TOKEN("유효하지 않은 토큰입니다."),
     NOT_INPUT_EMAIL("입력한 Email을 확인해주세요."),
-    NOT_MISSING_TOKEN("토큰 정보가 없습니다."),
+    NOT_MISSING_TOKEN("로그인 이후 이용해주세요."),
     ;
 
     private final String message;
