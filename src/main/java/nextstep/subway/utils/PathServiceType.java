c7f0460beb075ef03bc0cabc3bebe7df066968cb
hiblue02 <hiblue02@gmail.com>
2023-02-27 23:15:36 +0900

    refactor(PathServiceType): 사용하지 않는 클래스 삭제

--- src/main/java/nextstep/subway/utils/PathServiceType.java
+++
@@ -1,9 +0,0 @@
-package nextstep.subway.utils;
-
-public enum PathServiceType {
-    DISTANCE;
-
-    public static boolean isDistance(String type){
-        return DISTANCE.name().equals(type);
-    }
-}
