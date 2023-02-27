4280f26f465688745dceb0fdd3ea73ee06a0d50d
seovalue <seominjeong.dev@gmail.com>
2023-02-27 22:53:44 +0900

    test: 환승역 테스트 보완

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -43,7 +43,7 @@ class PathAcceptanceTest extends AcceptanceTest {
         양재역 = 지하철역_생성_요청("양재역").jsonPath().getLong("id");
         남부터미널역 = 지하철역_생성_요청("남부터미널역").jsonPath().getLong("id");
 
-        이호선 = 지하철_노선_생성_요청("2호선", "green", 교대역, 강남역, 10, 1, 0);
+        이호선 = 지하철_노선_생성_요청("2호선", "green", 교대역, 강남역, 10, 1, 5);
         신분당선 = 지하철_노선_생성_요청("신분당선", "red", 강남역, 양재역, 10, 10, 10);
         삼호선 = 지하철_노선_생성_요청("3호선", "orange", 교대역, 남부터미널역, 2, 10, 0);
 
@@ -67,6 +67,20 @@ class PathAcceptanceTest extends AcceptanceTest {
     @DisplayName("두 역의 최소 시간 경로를 조회한다.")
     @Test
     void findPathByDuration() {
+        // when
+        ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(교대역, 양재역, PathLookUpType.DURATION);
+
+        // then
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 강남역, 양재역);
+        PathResponse pathResponse = response.as(PathResponse.class);
+        assertThat(pathResponse.getDistance()).isEqualTo(20);
+        assertThat(pathResponse.getDuration()).isEqualTo(11);
+        assertThat(pathResponse.getFare()).isEqualTo(1450 + 10);
+    }
+
+    @DisplayName("두 역의 최소 시간 경로를 조회한다. - 환승역에 추가요금이 부과된 경우 더 큰 요금만 적용된다.")
+    @Test
+    void findPathByDurationAndExtraFare() {
         // when
         ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(교대역, 양재역, PathLookUpType.DURATION);
 
