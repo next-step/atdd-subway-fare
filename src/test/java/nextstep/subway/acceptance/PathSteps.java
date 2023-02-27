78d5170d349177a539ba991204f523a6eb580e69
윤영택 <ytaek@gmarket.com>
2023-02-27 23:38:29 +0900

    [feat] 실습 - 문서화

---
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -0,0 +1,20 @@
+package nextstep.subway.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.http.MediaType;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+public class PathSteps {
+
+    public static RequestSpecification baseDocumentRequest(RequestSpecification spec) {
+        return RestAssured
+            .given(spec).log().all()
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .filter(document("path",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint())));
+    }
+}
--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,37 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static nextstep.subway.acceptance.PathSteps.baseDocumentRequest;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.when;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
+
     @Test
     void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+        PathResponse pathResponse = new PathResponse(
+            Lists.newArrayList(
+                new StationResponse(1L, "강남역"),
+                new StationResponse(2L, "역삼역")
+            ), 10
+        );
+
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+        baseDocumentRequest(spec)
+            .queryParam("source", 1L)
+            .queryParam("target", 2L)
+            .when().get("/paths")
+            .then().log().all().extract();
     }
 }
