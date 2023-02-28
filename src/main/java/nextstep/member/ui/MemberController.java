e383dbdc985eebce146e1c211fa3b729b96df40e
hiblue02 <hiblue02@gmail.com>
2023-02-28 21:30:26 +0900

    test(MemberController, FavoriteController): loginMember Optional 추가

--- src/main/java/nextstep/member/ui/MemberController.java
+++ src/main/java/nextstep/member/ui/MemberController.java
@@ -6,14 +6,16 @@ import nextstep.member.application.dto.MemberResponse;
 import nextstep.member.domain.AuthenticationPrincipal;
 import nextstep.member.domain.LoginMember;
 import nextstep.member.domain.Member;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 public class MemberController {
-    private MemberService memberService;
+    private final MemberService memberService;
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
@@ -44,8 +46,11 @@ public class MemberController {
     }
 
     @GetMapping("/members/me")
-    public ResponseEntity<MemberResponse> findMemberOfMine(@AuthenticationPrincipal LoginMember loginMember) {
-        Member member = memberService.findMember(loginMember.getId());
+    public ResponseEntity<MemberResponse> findMemberOfMine(@AuthenticationPrincipal Optional<LoginMember> loginMember) {
+        if(loginMember.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        Member member = memberService.findMember(loginMember.get().getId());
         return ResponseEntity.ok().body(MemberResponse.of(member));
     }
 }
--- src/main/java/nextstep/subway/ui/FavoriteController.java
+++ src/main/java/nextstep/subway/ui/FavoriteController.java
@@ -5,37 +5,48 @@ import nextstep.member.domain.LoginMember;
 import nextstep.subway.applicaion.FavoriteService;
 import nextstep.subway.applicaion.dto.FavoriteRequest;
 import nextstep.subway.applicaion.dto.FavoriteResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class FavoriteController {
-    private FavoriteService favoriteService;
+    private final FavoriteService favoriteService;
 
     public FavoriteController(FavoriteService favoriteService) {
         this.favoriteService = favoriteService;
     }
 
     @PostMapping("/favorites")
-    public ResponseEntity createFavorite(@AuthenticationPrincipal LoginMember loginMember, @RequestBody FavoriteRequest request) {
-        favoriteService.createFavorite(loginMember.getId(), request);
+    public ResponseEntity createFavorite(@AuthenticationPrincipal Optional<LoginMember> loginMember, @RequestBody FavoriteRequest request) {
+        if(loginMember.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        favoriteService.createFavorite(loginMember.get().getId(), request);
         return ResponseEntity
                 .created(URI.create("/favorites/" + 1L))
                 .build();
     }
 
     @GetMapping("/favorites")
-    public ResponseEntity<List<FavoriteResponse>> getFavorites(@AuthenticationPrincipal LoginMember loginMember) {
-        List<FavoriteResponse> favorites = favoriteService.findFavorites(loginMember.getId());
+    public ResponseEntity<List<FavoriteResponse>> getFavorites(@AuthenticationPrincipal Optional<LoginMember> loginMember) {
+        if(loginMember.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        List<FavoriteResponse> favorites = favoriteService.findFavorites(loginMember.get().getId());
         return ResponseEntity.ok().body(favorites);
     }
 
     @DeleteMapping("/favorites/{id}")
-    public ResponseEntity deleteFavorite(@AuthenticationPrincipal LoginMember loginMember, @PathVariable Long id) {
-        favoriteService.deleteFavorite(loginMember.getId(), id);
+    public ResponseEntity deleteFavorite(@AuthenticationPrincipal Optional<LoginMember> loginMember, @PathVariable Long id) {
+        if(loginMember.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        favoriteService.deleteFavorite(loginMember.get().getId(), id);
         return ResponseEntity.noContent().build();
     }
 }
