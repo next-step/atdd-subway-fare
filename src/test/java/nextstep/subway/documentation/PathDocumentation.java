fd2ccf0119f8cacb0ec052add2531b5b44b56e8b
hyunssooo <jhs695@gmail.com>
2023-03-01 04:06:21 +0900

    docs: 경로 문서화 코드 이동

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -2,14 +2,6 @@ package nextstep.subway.documentation;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 import io.restassured.RestAssured;
 import java.util.List;
@@ -19,7 +11,6 @@ import nextstep.subway.applicaion.dto.StationResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.payload.JsonFieldType;
 
 public class PathDocumentation extends Documentation {
 
@@ -41,27 +32,7 @@ public class PathDocumentation extends Documentation {
         when(pathService.findPath(any())).thenReturn(pathResponse);
 
         RestAssured
-            .given(spec).log().all()
-            .filter(
-                document(
-                    "path",
-                    preprocessRequest(prettyPrint()),
-                    preprocessResponse(prettyPrint()),
-                    requestParameters(
-                        parameterWithName("source").description("출발역"),
-                        parameterWithName("target").description("도착역"),
-                        parameterWithName("type").description("경로 조회 타입")
-                    ),
-                    responseFields(
-                        fieldWithPath("stations").type(JsonFieldType.ARRAY).description("경로의 역들"),
-                        fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("역 아이디"),
-                        fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("역 이름"),
-                        fieldWithPath("distance").type(JsonFieldType.NUMBER).description("거리"),
-                        fieldWithPath("duration").type(JsonFieldType.NUMBER).description("시간"),
-                        fieldWithPath("fare").type(JsonFieldType.NUMBER).description("요금(거리비례제로 책정)")
-                    )
-                )
-            )
+            .given().log().all()
             .accept(MediaType.APPLICATION_JSON_VALUE)
             .queryParam("source", 1L)
             .queryParam("target", 2L)
