8727221be1046ef96b9e2f3b206919886ad9bb1b
hyunssooo <jhs695@gmail.com>
2023-03-01 03:28:33 +0900

    test: 경로 검색 인수 테스트 수정

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -3,15 +3,18 @@ package nextstep.subway.acceptance;
 import static nextstep.subway.acceptance.LineSteps.지하철_노선에_지하철_구간_생성_요청;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 @DisplayName("지하철 경로 검색")
@@ -20,7 +23,6 @@ class PathAcceptanceTest extends AcceptanceTest {
     private static final String 소요시간 = "DURATION";
     private static final String 거리 = "DISTANCE";
 
-
     private Long 교대역;
     private Long 강남역;
     private Long 양재역;
@@ -28,6 +30,15 @@ class PathAcceptanceTest extends AcceptanceTest {
     private Long 이호선;
     private Long 신분당선;
     private Long 삼호선;
+
+    private final int 교대역_강남역_거리 = 10;
+    private final int 교대역_강남역_시간 = 10;
+    private final int 강남역_양재역_거리 = 10;
+    private final int 강남역_양재역_시간 = 3;
+    private final int 교대역_남부터미널역_거리 = 2;
+    private final int 교대역_남부터미널역_시간 = 10;
+    private final int 남부터미널역_양재역_거리 = 3;
+    private final int 남부터미널역_양재역_시간 = 10;
 
     /**
      * 교대역    --- *2호선*  ---   강남역
@@ -45,13 +56,18 @@ class PathAcceptanceTest extends AcceptanceTest {
         양재역 = 지하철역_생성_요청("양재역").jsonPath().getLong("id");
         남부터미널역 = 지하철역_생성_요청("남부터미널역").jsonPath().getLong("id");
 
-        이호선 = 지하철_노선_생성_요청("2호선", "green", 교대역, 강남역, 10, 2);
-        신분당선 = 지하철_노선_생성_요청("신분당선", "red", 강남역, 양재역, 10, 3);
-        삼호선 = 지하철_노선_생성_요청("3호선", "orange", 교대역, 남부터미널역, 2, 10);
+        이호선 = 지하철_노선_생성_요청("2호선", "green", 교대역, 강남역, 교대역_강남역_거리, 교대역_강남역_시간);
+        신분당선 = 지하철_노선_생성_요청("신분당선", "red", 강남역, 양재역, 강남역_양재역_거리, 강남역_양재역_시간);
+        삼호선 = 지하철_노선_생성_요청("3호선", "orange", 교대역, 남부터미널역, 교대역_남부터미널역_거리, 교대역_남부터미널역_시간);
 
-        지하철_노선에_지하철_구간_생성_요청(삼호선, createSectionCreateParams(남부터미널역, 양재역, 3, 10));
+        지하철_노선에_지하철_구간_생성_요청(삼호선, createSectionCreateParams(남부터미널역, 양재역, 남부터미널역_양재역_거리, 남부터미널역_양재역_시간));
     }
 
+
+    /**
+     *  총 거리 5 = 2 + 3
+     *  요금 1,250
+     */
     @DisplayName("두 역의 최단 거리 경로를 조회한다.")
     @Test
     void findPathByDuration() {
@@ -59,17 +75,59 @@ class PathAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 두_역의_최단_거리_경로_조회를_요청(교대역, 양재역);
 
         // then
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
+        두_역의_최단_거리_경로_조회에_성공한다(
+            response,
+            List.of(교대역, 남부터미널역, 양재역),
+            교대역_남부터미널역_거리 + 남부터미널역_양재역_거리,
+            1_250
+        );
     }
 
-    @DisplayName("두 역의 최소 시간 거리 경로를 조회한다.")
+    /**
+     *  총 거리 20 = 10 + 5 + 5
+     *  요금 1,250 + 100 * 2
+     */
+    @DisplayName("두 역의 최소 시간 경로를 조회한다.")
     @Test
     void findPathByDistance() {
         // when
         ExtractableResponse<Response> response = 두_역의_최소_시간_경로_조회를_요청(교대역, 양재역);
 
         // then
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 강남역, 양재역);
+        두_역의_최소_시간_경로_조회에_성공한다(
+            response,
+            List.of(교대역, 강남역, 양재역),
+            교대역_강남역_시간 + 강남역_양재역_시간,
+            1_450
+        );
+    }
+
+    private void 두_역의_최단_거리_경로_조회에_성공한다(
+        ExtractableResponse<Response> response,
+        List<Long> 역들,
+        int distance,
+        int fare
+    ) {
+        assertAll(
+            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+            () -> assertThat(response.jsonPath().getList("stations.id", Long.class)).containsAll(역들),
+            () -> assertThat(response.jsonPath().getInt("distance")).isEqualTo(distance),
+            () -> assertThat(response.jsonPath().getInt("fare")).isEqualTo(fare)
+        );
+    }
+
+    private void 두_역의_최소_시간_경로_조회에_성공한다(
+        ExtractableResponse<Response> response,
+        List<Long> 역들,
+        int duration,
+        int fare
+    ) {
+        assertAll(
+            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+            () -> assertThat(response.jsonPath().getList("stations.id", Long.class)).containsAll(역들),
+            () -> assertThat(response.jsonPath().getInt("duration")).isEqualTo(duration),
+            () -> assertThat(response.jsonPath().getInt("fare")).isEqualTo(fare)
+        );
     }
 
     private ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target) {
