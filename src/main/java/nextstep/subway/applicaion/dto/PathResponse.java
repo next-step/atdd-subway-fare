4519e2b46e6a442a7cca2ec6fb7ff85a051ea05b
dongho.geum <dongho.geum@myrealtrip.com>
2023-03-01 03:58:02 +0900

    test: 요금 문서 장석

--- src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
@@ -10,16 +10,19 @@ public class PathResponse {
     private List<StationResponse> stations;
     private int distance;
     private int duration;
-
-    public PathResponse(List<StationResponse> stations, int distance) {
-        this.stations = stations;
-        this.distance = distance;
-    }
+    private int fare;
 
     public PathResponse(List<StationResponse> stations, int distance, int duration) {
         this.stations = stations;
         this.distance = distance;
         this.duration = duration;
+    }
+
+    public PathResponse(List<StationResponse> stations, int distance, int duration, int fare) {
+        this.stations = stations;
+        this.distance = distance;
+        this.duration = duration;
+        this.fare = fare;
     }
 
     public static PathResponse of(Path path) {
@@ -42,6 +45,10 @@ public class PathResponse {
 
     public int getDistance() {
         return distance;
+    }
+
+    public int getFare() {
+        return fare;
     }
 
 }
