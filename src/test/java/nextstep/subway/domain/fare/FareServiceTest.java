9fd76d2162433d3b3d82cb9476ef32cb1c9e4df6
seovalue <seominjeong.dev@gmail.com>
2023-02-28 22:56:35 +0900

    test: fareServiceTest 테스트 분리

--- src/test/java/nextstep/subway/domain/fare/FareServiceTest.java
+++ src/test/java/nextstep/subway/domain/fare/FareServiceTest.java
@@ -1,8 +1,10 @@
 package nextstep.subway.domain.fare;
 
 import nextstep.subway.domain.Line;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,26 +13,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class FareServiceTest {
 
+    private FareService fareService;
+
+    @BeforeEach
+    void setUp() {
+        fareService = new FareService();
+    }
+
     @Test
-    @DisplayName("거리에 따라 적절한 요금을 반환한다.")
-    /**
-     * 기본운임(10㎞ 이내) : 기본운임 1,250원
-     * 이용 거리초과 시 추가운임 부과
-     * 10km초과∼50km까지(5km마다 100원)
-     * 50km초과 시 (8km마다 100원)
-     */
-    void calculateFare() {
-        FareService fareService = new FareService();
+    void 십km_이내_거리는_기본_요금() {
         assertThat(fareService.calculateFare(10)).isEqualTo(1250);
+    }
+
+    @Test
+    void 십km_초과_시_추가운임() {
         assertThat(fareService.calculateFare(12)).isEqualTo(1350);
-        assertThat(fareService.calculateFare(16)).isEqualTo(1450);
-        assertThat(fareService.calculateFare(75)).isEqualTo(2450);
+    }
+
+    @Test
+    void 오십km_초과_시_추가운임() {
+        assertThat(fareService.calculateFare(51)).isEqualTo(2150);
     }
 
     @Test
     @DisplayName("추가 요금이 있는 노선의 경우 추가요금을 포함한 요금을 반환한다.")
     void calculateExtraFare() {
-        FareService fareService = new FareService();
         final int defaultFare = fareService.calculateFare(10);
         List<Line> lines = Arrays.asList(new Line("이호선", "green", 100),
                 new Line("삼호선", "yellow", 0));
