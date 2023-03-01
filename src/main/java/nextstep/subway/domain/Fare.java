3f522c35caf8cb7ff6c10b087c957761326fc630
bactoria <bactoria@gmail.com>
2023-03-01 11:36:47 +0900

    test:: 이용요금(Fare) 단위테스트 추가

---
+++ src/main/java/nextstep/subway/domain/Fare.java
@@ -0,0 +1,11 @@
+package nextstep.subway.domain;
+
+public class Fare {
+    public static Fare of(int value) {
+        return null;
+    }
+
+    public Fare plus(Fare that) {
+        return null;
+    }
+}
