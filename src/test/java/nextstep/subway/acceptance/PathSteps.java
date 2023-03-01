41c06e284e8ae0ea76a41b96e9997f22ab64f608
dhlee3994 <dhlee3994@gmail.com>
2023-03-01 14:40:10 +0900

    Test: 문서화 테스트의 Step과 분리되면서 최적 경로 요청 Step 수정

--- src/test/java/nextstep/subway/acceptance/PathSteps.java
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -1,7 +1,6 @@
 package nextstep.subway.acceptance;
 
 import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -16,18 +15,16 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class PathSteps {
 
     public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(final Long source, final Long target) {
-        final RequestSpecification spec = new RequestSpecBuilder().build();
-        return 두_역의_최단_거리_경로_조회를_요청(spec, source, target, PathType.DISTANCE);
+        return 두_역의_최적_경로_조회를_요청(source, target, PathType.DISTANCE);
     }
 
     public static ExtractableResponse<Response> 두_역의_최소_시간_경로_조회를_요청(final Long source, final Long target) {
-        final RequestSpecification spec = new RequestSpecBuilder().build();
-        return 두_역의_최단_거리_경로_조회를_요청(spec, source, target, PathType.DURATION);
+        return 두_역의_최적_경로_조회를_요청(source, target, PathType.DURATION);
     }
 
-    public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(final RequestSpecification spec, final Long source, final Long target, PathType pathType) {
+    public static ExtractableResponse<Response> 두_역의_최적_경로_조회를_요청(final Long source, final Long target, PathType pathType) {
         return RestAssured
-                .given(spec)
+                .given()
                     .accept(APPLICATION_JSON_VALUE)
                     .queryParam("source", source)
                     .queryParam("target", target)
