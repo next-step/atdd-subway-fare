0711f2856e7d7194a1c43388c1d92eaf7cf17c0e
윤영택 <ytaek@gmarket.com>
2023-03-01 20:58:15 +0900

    [test] 인수테스트 작성

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -58,6 +58,7 @@ class PathAcceptanceTest extends AcceptanceTest {
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
         assertThat(response.jsonPath().getInt("distance")).isEqualTo(5);
         assertThat(response.jsonPath().getInt("duration")).isEqualTo(15);
+        assertThat(response.jsonPath().getInt("fare")).isEqualTo(1250);
     }
 
     /**
@@ -67,6 +68,7 @@ class PathAcceptanceTest extends AcceptanceTest {
      * When 출발역에서 도착역까지의 최소 시간 기준으로 경로 조회를 요청
      * Then 최소 시간 기준 경로를 응답
      *  And 총 거리와 소요 시간을 함께 응답함
+     *  And 지하철 이용 요금도 함께 응답함
      */
     @DisplayName("두 역의 최소 시간 경로를 조회한다.")
     @Test
@@ -78,6 +80,7 @@ class PathAcceptanceTest extends AcceptanceTest {
         assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 강남역, 양재역);
         assertThat(response.jsonPath().getInt("distance")).isEqualTo(20);
         assertThat(response.jsonPath().getInt("duration")).isEqualTo(10);
+        assertThat(response.jsonPath().getInt("fare")).isEqualTo(1450);
     }
 
     private Long 지하철_노선_생성_요청(String name, String color, Long upStation, Long downStation, int distance, int duration) {
