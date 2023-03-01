8ae10531d7836c88a810fe00c84e221239ead080
bactoria <bactoria@gmail.com>
2023-03-01 12:23:55 +0900

    refactor:: 소요시간(Duration) 불변식을 팩터리 메서드에서 검증하도록 변경

--- src/main/java/nextstep/subway/domain/Duration.java
+++ src/main/java/nextstep/subway/domain/Duration.java
@@ -13,25 +13,25 @@ public class Duration {
     private int value;
 
     public static Duration of(int value) {
+        if (value <= 0) {
+            throw new NotPositiveNumberException("소요시간은 0보다 커야 합니다");
+        }
         return new Duration(value);
     }
 
     public Duration() {
     }
 
-    public Duration(int value) {
-        if (value <= 0) {
-            throw new NotPositiveNumberException("소요시간은 0보다 커야 합니다");
-        }
+    private Duration(int value) {
         this.value = value;
     }
 
     public Duration minus(Duration that) {
-        return new Duration(this.value - that.value);
+        return Duration.of(this.value - that.value);
     }
 
     public Duration plus(Duration that) {
-        return new Duration(this.value + that.value);
+        return Duration.of(this.value + that.value);
     }
 
     public int getValue() {
