87d7af19a0004a0c77e75111b12821079d4b5f29
dongho.geum <dongho.geum@myrealtrip.com>
2023-03-02 00:41:24 +0900

    feat: 나이 정책에 따른 요금 계산이 된다

--- src/main/java/nextstep/subway/domain/AgeFarePolicy.java
+++ src/main/java/nextstep/subway/domain/AgeFarePolicy.java
@@ -2,6 +2,8 @@ package nextstep.subway.domain;
 
 public class AgeFarePolicy implements FarePolicy {
 
+    private static final int DEFAULT_DISCOUNT_FARE = 350;
+
     private final int age;
 
     public AgeFarePolicy(int age) {
@@ -11,10 +13,18 @@ public class AgeFarePolicy implements FarePolicy {
     @Override
     public int calcFare(int currentFare) {
         if (isChild(age)) {
-            return (currentFare - 350) / 2;
+            return (currentFare - DEFAULT_DISCOUNT_FARE) / 2;
+        }
+
+        if (isYouth(age)) {
+            return (int) Math.floor((currentFare - DEFAULT_DISCOUNT_FARE) * 0.8);
         }
 
         return currentFare;
+    }
+
+    private boolean isYouth(int age) {
+        return 13 <= age && age < 19;
     }
 
     private boolean isChild(int age) {
