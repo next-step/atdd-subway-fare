efbcef47fbe536c6baed4f7c1b89e8252b555478
bactoria <bactoria@gmail.com>
2023-03-01 11:40:23 +0900

    feat:: 이용요금(Fare) 구현

--- src/main/java/nextstep/subway/domain/Fare.java
+++ src/main/java/nextstep/subway/domain/Fare.java
@@ -1,11 +1,41 @@
 package nextstep.subway.domain;
 
+import nextstep.subway.domain.exceptions.NegativeNumberException;
+
+import java.util.Objects;
+
 public class Fare {
+    private int value;
+
+    private Fare(int value) {
+        this.value = value;
+    }
+
     public static Fare of(int value) {
-        return null;
+        if (value < 0) {
+            throw new NegativeNumberException("요금은 0보다 적을 수 없습니다");
+        }
+        return new Fare(value);
     }
 
     public Fare plus(Fare that) {
-        return null;
+        return Fare.of(this.value + that.value);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fare fare = (Fare) o;
+        return value == fare.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
