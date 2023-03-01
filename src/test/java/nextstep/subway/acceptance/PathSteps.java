a2426240bc0a2f15e89d40da9682731e96492a8f
ess234 <ess234@naver.com>
2023-03-01 15:32:10 +0900

    refactor: 피드백 적용

--- src/test/java/nextstep/subway/acceptance/PathSteps.java
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -9,9 +9,6 @@ import org.springframework.restdocs.payload.JsonFieldType;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
