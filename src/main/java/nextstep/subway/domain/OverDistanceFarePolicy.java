e500d32afbac672fa863c87b85073c10be87de69
bactoria <bactoria@gmail.com>
2023-03-02 13:11:47 +0900

    refactor:: 추과요금 정책 리팩토링 (review)
    관련 코멘트: https://github.com/next-step/atdd-subway-fare/pull/277#discussion_r1121924887

--- src/main/java/nextstep/subway/domain/OverDistanceFarePolicy.java
+++ src/main/java/nextstep/subway/domain/OverDistanceFarePolicy.java
@@ -1,36 +1,34 @@
 package nextstep.subway.domain;
 
-import java.util.Arrays;
+import static nextstep.subway.domain.Fare.ZERO_FARE;
 
-public enum OverDistanceFarePolicy {
-    OVER_TEN_FARE(10, 50, 100, 5),
-    OVER_FIFTY_FARE(50, Integer.MAX_VALUE, 100, 8);
+public class OverDistanceFarePolicy {
 
-    private int minKm;
-    private int maxKm;
-    private int farePer;
-    private int perKm;
+    private static int FIRST_RANGE_DISTANCE = 10;
+    private static int FIRST_RANGE_PER_KM = 5;
 
-    OverDistanceFarePolicy(int minKm, int maxKm, int farePer, int perKm) {
-        this.minKm = minKm;
-        this.maxKm = maxKm;
-        this.farePer = farePer;
-        this.perKm = perKm;
-    }
+    private static int SECOND_RANGE_DISTANCE = 50;
+    private static int SECOND_RANGE_PER_KM = 8;
+    private static int PER_FARE = 100;
 
     public static Fare calculateOverDistanceFare(int distance) {
-        return Arrays.stream(values())
-                .map(it -> calculateOverDistanceFare(it, distance))
-                .reduce(Fare.of(0), Fare::plus);
+        Fare fare = ZERO_FARE;
+
+        if (distance > FIRST_RANGE_DISTANCE) {
+            int overDistance = Math.min(SECOND_RANGE_DISTANCE, distance) - FIRST_RANGE_DISTANCE;
+            fare = fare.plus(calculate(overDistance, FIRST_RANGE_PER_KM));
+        }
+
+        if (distance > SECOND_RANGE_DISTANCE) {
+            int overDistance = distance - SECOND_RANGE_DISTANCE;
+            fare = fare.plus(calculate(overDistance, SECOND_RANGE_PER_KM));
+        }
+
+        return fare;
     }
 
-    private static Fare calculateOverDistanceFare(OverDistanceFarePolicy policy, int totalDistance) {
-        int overDistance = Math.max(Math.min(totalDistance, policy.maxKm) - policy.minKm, 0);
-        return calculateOverDistanceFare(overDistance, policy.perKm, policy.farePer);
-    }
-
-    private static Fare calculateOverDistanceFare(int overDistance, int perKm, int farePer) {
-        int overFare = (int) (Math.ceil((overDistance + perKm - 1) / perKm) * farePer);
+    private static Fare calculate(int overDistance, int perKm) {
+        int overFare = (int) (Math.ceil((overDistance + perKm - 1) / perKm) * PER_FARE);
         return Fare.of(overFare);
     }
 }
