e74822bad444b5b6c01fdbcfbc70d0513993b08c
ess234 <ess234@naver.com>
2023-03-02 22:41:37 +0900

    refactor: 헤더 문서화 추가
    - 경로조회 로그인 헤더 추가

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -2,6 +2,8 @@ package nextstep.subway.documentation;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.member.domain.LoginMember;
+import nextstep.member.ui.AuthenticationPrincipalArgumentResolver;
 import nextstep.subway.acceptance.PathSteps;
 import nextstep.subway.applicaion.PathService;
 import nextstep.subway.domain.PathType;
@@ -13,7 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 
-import static nextstep.subway.acceptance.MemberSteps.베어러_인증_로그인_요청;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -24,6 +27,8 @@ public class PathDocumentation extends Documentation {
 
     @MockBean
     PathService pathService;
+    @MockBean
+    AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver;
 
     @Override
     @BeforeEach
@@ -32,14 +37,15 @@ public class PathDocumentation extends Documentation {
     }
 
     @Test
-    void path() {
+    void path() throws Exception{
         PathResponse pathResponse = new PathResponse(Lists.newArrayList(new StationResponse(1L, "강남역"),
                 new StationResponse(2L, "역삼역")),10, 20, 1250L);
 
         when(pathService.findPath(any(),anyLong(), anyLong(), eq(PathType.시간.getType()))).thenReturn(pathResponse);
+        when(authenticationPrincipalArgumentResolver.resolveArgument(any(), any(), any(), any())).thenReturn(new LoginMember(1L, List.of("ROLE_MEMBER")));
 
         // when
-        ExtractableResponse<Response> searchResponse = PathSteps.두_역의_경로_조회를_요청(spec, 1L, 2L, PathType.시간.getType());
+        ExtractableResponse<Response> searchResponse = PathSteps.회원계정으로_두_역의_경로_조회를_요청(spec, "TestAccessToken",1L, 2L, PathType.시간.getType());
         assertThat(searchResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
