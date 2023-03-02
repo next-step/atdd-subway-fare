157d5bd14874301debc639e960ed16976663b845
wu2ee <gulb1602@gmail.com>
2023-03-02 20:44:13 +0900

    feat : 최단 시간 타입으로 경로 찾기 로직 추가

--- src/main/java/nextstep/subway/domain/PathType.java
+++ src/main/java/nextstep/subway/domain/PathType.java
@@ -1,6 +1,18 @@
 package nextstep.subway.domain;
 
+import java.util.function.Function;
+
 public enum PathType {
-    DISTANCE,
-    DURATION
+    DISTANCE(Section::getDistance),
+    DURATION(Section::getDuration);
+
+    private Function<Section, Integer> weightFunction;
+
+    PathType(Function<Section, Integer> weightFunction) {
+        this.weightFunction = weightFunction;
+    }
+
+    public int weight(Section section) {
+        return weightFunction.apply(section);
+    }
 }
