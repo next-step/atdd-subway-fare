bfabd7af3565f424dc2b2c475d388cce0b8bc166
Jaepil Yu <jaepilyu1324@gmail.com>
2023-03-02 22:15:02 +0900

    refactor: 불필요한 어노테이션 제거

--- src/test/java/nextstep/subway/documentation/Documentation.java
+++ src/test/java/nextstep/subway/documentation/Documentation.java
@@ -5,12 +5,10 @@ import static org.springframework.restdocs.restassured3.RestAssuredRestDocumenta
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.ActiveProfiles;
 
-import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 
@@ -18,14 +16,10 @@ import io.restassured.specification.RequestSpecification;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(RestDocumentationExtension.class)
 public class Documentation {
-    @LocalServerPort
-    private int port;
-
     protected RequestSpecification spec;
 
     @BeforeEach
     public void setUp(RestDocumentationContextProvider restDocumentation) {
-        RestAssured.port = port;
         this.spec = new RequestSpecBuilder()
             .addFilter(documentationConfiguration(restDocumentation))
             .build();
