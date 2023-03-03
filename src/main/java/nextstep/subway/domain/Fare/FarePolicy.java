0f972ba6b74a8d57eadd90593766373e1c306389
Sangmin <sm9171@nate.com>
2023-03-03 23:44:38 +0900

    refactor(fare): FarePolicy 요금 정책 구간 상수화

--- src/main/java/nextstep/subway/domain/Fare/FarePolicy.java
+++ src/main/java/nextstep/subway/domain/Fare/FarePolicy.java
@@ -3,13 +3,16 @@ package nextstep.subway.domain.Fare;
 public class FarePolicy {
     private final static int DEFAULT_FARE = 1250;
     private final static int EXTRA_FARE = 100;
+    private final static int FIRST_FARE_SECTION = 10;
+    private final static int SECOND_FARE_SECTION = 50;
+
     public static int getFare(int distance) {
         int fare = DEFAULT_FARE;
-        if (distance > 10 && distance <= 50) {
-            fare += (int) ((Math.ceil((distance - 11) / 5) + 1) * EXTRA_FARE);
+        if (distance > FIRST_FARE_SECTION && distance <= SECOND_FARE_SECTION) {
+            fare += (int) ((Math.ceil((distance - FIRST_FARE_SECTION -1) / 5) + 1) * EXTRA_FARE);
         }
-        if (distance > 50) {
-            fare += (int) (800 + (Math.ceil((distance - 51) / 8) + 1) * EXTRA_FARE);
+        if (distance > SECOND_FARE_SECTION) {
+            fare += (int) (800 + (Math.ceil((distance - SECOND_FARE_SECTION - 1) / 8) + 1) * EXTRA_FARE);
         }
         return fare;
     }
