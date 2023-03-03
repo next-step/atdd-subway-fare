0c5cdd3b190f3cfb35a19c0db8fd7b4ae2583698
ji-one <rlawldnjs8@gmail.com>
2023-03-04 05:04:54 +0900

    config: active profile 설정

--- src/test/java/nextstep/subway/utils/DataLoader.java
+++ src/test/java/nextstep/subway/utils/DataLoader.java
@@ -1,16 +1,16 @@
 package nextstep.subway.utils;
 
+import java.util.Arrays;
 import nextstep.member.domain.Member;
 import nextstep.member.domain.MemberRepository;
 import nextstep.member.domain.RoleType;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.test.context.ActiveProfiles;
 
-import java.util.Arrays;
-
-@Profile("test")
+@ActiveProfiles("test")
 @Component
 public class DataLoader {
+
     private MemberRepository memberRepository;
 
     public DataLoader(MemberRepository memberRepository) {
@@ -18,11 +18,17 @@ public class DataLoader {
     }
 
     public void loadData() {
-        memberRepository.save(new Member("admin@email.com", "password", 20, Arrays.asList(RoleType.ROLE_ADMIN.name())));
-        memberRepository.save(new Member("member@email.com", "password", 20, Arrays.asList(RoleType.ROLE_MEMBER.name())));
-        memberRepository.save(new Member(GithubResponses.사용자1.getEmail(), "password", 20, Arrays.asList(RoleType.ROLE_MEMBER.name())));
-        memberRepository.save(new Member(GithubResponses.사용자2.getEmail(), "password", 20, Arrays.asList(RoleType.ROLE_MEMBER.name())));
-        memberRepository.save(new Member(GithubResponses.사용자3.getEmail(), "password", 20, Arrays.asList(RoleType.ROLE_MEMBER.name())));
-        memberRepository.save(new Member(GithubResponses.사용자4.getEmail(), "password", 20, Arrays.asList(RoleType.ROLE_MEMBER.name())));
+        memberRepository.save(new Member("admin@email.com", "password", 20,
+            Arrays.asList(RoleType.ROLE_ADMIN.name())));
+        memberRepository.save(new Member("member@email.com", "password", 20,
+            Arrays.asList(RoleType.ROLE_MEMBER.name())));
+        memberRepository.save(new Member(GithubResponses.사용자1.getEmail(), "password", 20,
+            Arrays.asList(RoleType.ROLE_MEMBER.name())));
+        memberRepository.save(new Member(GithubResponses.사용자2.getEmail(), "password", 20,
+            Arrays.asList(RoleType.ROLE_MEMBER.name())));
+        memberRepository.save(new Member(GithubResponses.사용자3.getEmail(), "password", 20,
+            Arrays.asList(RoleType.ROLE_MEMBER.name())));
+        memberRepository.save(new Member(GithubResponses.사용자4.getEmail(), "password", 20,
+            Arrays.asList(RoleType.ROLE_MEMBER.name())));
     }
 }
