336913d8be3e16b8bcd2a975eea7bfbf1ee49dbd
ChanYung Park <50781066+cyPark95@users.noreply.github.com>
2023-03-03 14:54:15 +0900

    🚀 실습 - 문서화 (#291)
    * fix: DataLoader Bean 등록을 위한 Profile 활성화
    
    * docs: 실습 요구사항 정리
    
    * docs: 경로 찾기 문서화
    
    * refactor: 공통 설정 분리
    
    * fix: 테스트 포트 중복

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,16 +1,36 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static nextstep.subway.acceptance.PathSteps.basicDocumentRequest;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
     @Test
     void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
+        // given
+        PathResponse pathResponse = new PathResponse(
+                Lists.newArrayList(
+                        new StationResponse(1L, "강남역"),
+                        new StationResponse(2L, "역삼역")
+                ), 10
+        );
+
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+        // when
+        // then
+        basicDocumentRequest(spec, "path")
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
                 .when().get("/paths")
