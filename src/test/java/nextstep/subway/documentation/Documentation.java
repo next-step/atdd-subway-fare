84d8135e219cd99dc1876fca09ecedabf32fee39
heoseungyeon <dia0312@naver.com>
2023-03-04 21:10:21 +0900

    fix: 톰캣 서버의 포트 번호를 RestAssured port 번호로 설정

--- src/test/java/nextstep/subway/documentation/Documentation.java
+++ src/test/java/nextstep/subway/documentation/Documentation.java
@@ -5,10 +5,12 @@ import static org.springframework.restdocs.restassured3.RestAssuredRestDocumenta
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.ActiveProfiles;
 
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 
@@ -17,9 +19,12 @@ import io.restassured.specification.RequestSpecification;
 @ExtendWith(RestDocumentationExtension.class)
 class Documentation {
 	protected RequestSpecification spec;
+	@LocalServerPort
+	int port;
 
 	@BeforeEach
 	public void setUp(RestDocumentationContextProvider restDocumentation) {
+		RestAssured.port = port;
 		this.spec = new RequestSpecBuilder()
 			.addFilter(documentationConfiguration(restDocumentation))
 			.build();
