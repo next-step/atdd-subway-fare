5af04e9b305ad31dfc2c0305777c1c76dd36762d
Jaepil Yu <jaepilyu1324@gmail.com>
2023-03-05 01:13:45 +0900

    test: 나이 관련 계산 별도의 테스트들로 분리

--- src/test/java/nextstep/subway/domain/AgeTest.java
+++ src/test/java/nextstep/subway/domain/AgeTest.java
@@ -3,6 +3,7 @@ package nextstep.subway.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -21,14 +22,21 @@ class AgeTest {
         assertThat(Age.of(intAge)).isEqualTo(age);
     }
 
-    @DisplayName("입력으로 요금이 주어지면 나이대에 맞는 할인된 값을 리턴한다.")
-    @ParameterizedTest
-    @CsvSource({
-        "CHILDREN,1350,500",
-        "TEENAGER,1350,800",
-        "ADULT,1350,1350"
-    })
-    void getDiscountFare(Age age, int fare, int expected) {
-        assertThat(age.discountFare(fare)).isEqualTo(expected);
+    @DisplayName("어린이 요금은 요금에서 350원을 공제하고 50% 할인을 적용한다.")
+    @Test
+    void getChildrenFare() {
+        assertThat(Age.CHILDREN.discountFare(1350)).isEqualTo(500);
+    }
+
+    @DisplayName("청소년 요금은 요금에서 350원을 공제하고 20% 할인을 적용한다.")
+    @Test
+    void getTeenagerFare() {
+        assertThat(Age.TEENAGER.discountFare(1350)).isEqualTo(800);
+    }
+
+    @DisplayName("성인 요금은 별도의 할인은 존재하지 않는다.")
+    @Test
+    void getAdultFare() {
+        assertThat(Age.ADULT.discountFare(1350)).isEqualTo(1350);
     }
 }
