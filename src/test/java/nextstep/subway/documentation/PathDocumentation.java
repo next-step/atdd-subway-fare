19c457eba6ca3e0f17c3c5061890a73e2ffd25e8
heoseungyeon <dia0312@naver.com>
2023-03-04 16:38:40 +0900

    test: path() 문서화 테스트 작성

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,44 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.*;
+
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 
-public class PathDocumentation extends Documentation {
+import io.restassured.RestAssured;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
 
-    @Test
-    void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
-    }
+public class PathDocumentation extends Documentation {
+	@MockBean
+	private PathService pathService;
+
+	@Test
+	void path() {
+		PathResponse pathResponse = new PathResponse(
+			Lists.newArrayList(
+				new StationResponse(1L, "강남역"),
+				new StationResponse(2L, "역삼역")
+			), 10
+		);
+
+		when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+		RestAssured
+			.given(spec).log().all()
+			.filter(document("path",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint())))
+			.accept(MediaType.APPLICATION_JSON_VALUE)
+			.queryParam("source", 1L)
+			.queryParam("target", 2L)
+			.when().get("/paths")
+			.then().log().all().extract();
+	}
 }
