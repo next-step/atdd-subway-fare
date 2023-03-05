821adf6b1737abf5724563be0e72d6bb65c9ed35
Yunsik-Choi <choiys0311@gmail.com>
2023-03-05 10:48:41 +0900

    ⛑️ test : 상행역끼리 연결하는 테스트 코드 작성

---
+++ src/test/java/nextstep/subway/domain/SectionTest.java
@@ -0,0 +1,39 @@
+package nextstep.subway.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("구간 관련 기능")
+class SectionTest {
+
+    private Line 이호선;
+    private Station 강남역;
+    private Station 역삼역;
+    private Station 선릉역;
+
+    @BeforeEach
+    void setUp() {
+        이호선 = new Line("2호선", "green");
+        강남역 = new Station("강남역");
+        역삼역 = new Station("역삼역");
+        선릉역 = new Station("선릉역");
+    }
+
+    @DisplayName("상행역 끼리 연결한다.")
+    @Test
+    void connectBetweenUpStation() {
+        Section existSection = new Section(이호선, 강남역, 역삼역, 10, 10);
+        Section newSection = new Section(이호선, 선릉역, 역삼역, 10, 10);
+
+        Section section = existSection.replaceDownStationWithUpStation(newSection);
+
+        Assertions.assertAll(
+                () -> assertThat(section.getUpStation()).isEqualTo(강남역),
+                () -> assertThat(section.getDownStation()).isEqualTo(선릉역)
+        );
+    }
+}
