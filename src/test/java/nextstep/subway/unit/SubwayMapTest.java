89974d9d56b9748fe9cffbdee819b45f20ee24f3
heoseungyeon <dia0312@naver.com>
2023-03-05 17:24:27 +0900

    test: 경로 조회 도메인 테스트 작성

--- src/test/java/nextstep/subway/unit/SubwayMapTest.java
+++ src/test/java/nextstep/subway/unit/SubwayMapTest.java
@@ -36,10 +36,10 @@ public class SubwayMapTest {
 		이호선 = new Line("2호선", "red");
 		삼호선 = new Line("3호선", "red");
 
-		신분당선.addSection(강남역, 양재역, 3, 3);
-		이호선.addSection(교대역, 강남역, 3, 3);
-		삼호선.addSection(교대역, 남부터미널역, 5, 5);
-		삼호선.addSection(남부터미널역, 양재역, 5, 5);
+		신분당선.addSection(강남역, 양재역, 3, 5);
+		이호선.addSection(교대역, 강남역, 3, 5);
+		삼호선.addSection(교대역, 남부터미널역, 5, 3);
+		삼호선.addSection(남부터미널역, 양재역, 5, 3);
 	}
 
 	@Test
@@ -66,6 +66,32 @@ public class SubwayMapTest {
 
 		// then
 		assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(양재역, 강남역, 교대역));
+	}
+
+	@Test
+	void findPath_by_distance() {
+		// given
+		List<Line> lines = Lists.newArrayList(신분당선, 이호선, 삼호선);
+		SubwayMap subwayMap = new SubwayMap(lines);
+
+		// when
+		Path path = subwayMap.findPath(교대역, 양재역, PathType.DISTANCE);
+
+		// then
+		assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(교대역, 강남역, 양재역));
+	}
+
+	@Test
+	void findPath_by_duration() {
+		// given
+		List<Line> lines = Lists.newArrayList(신분당선, 이호선, 삼호선);
+		SubwayMap subwayMap = new SubwayMap(lines);
+
+		// when
+		Path path = subwayMap.findPath(교대역, 양재역, PathType.DURATION);
+
+		// then
+		assertThat(path.getStations()).containsExactlyElementsOf(Lists.newArrayList(교대역, 남부터미널역, 양재역));
 	}
 
 	private Station createStation(long id, String name) {
