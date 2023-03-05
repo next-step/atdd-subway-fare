c2e27f9eac75aeb465ab701fe29ecc9b21a06d82
wu2ee <gulb1602@gmail.com>
2023-03-06 02:24:24 +0900

    feat : 요금 타입 클래스 추가

---
+++ src/main/java/nextstep/subway/domain/FareType.java
@@ -0,0 +1,55 @@
+package nextstep.subway.domain;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+public enum FareType {
+    BASIC(0, 0, distance -> distance > 0 && distance <= 10),
+    OVER_10KM(5, 10, distance -> distance > 10 && distance <= 50),
+    OVER_50KM(8, 50, distance -> distance > 50);
+
+    private static final int BASIC_FARE = 1250;
+    private static final int OVER_10KM_MAX_FARE = 2050;
+    private static final int EXTRA_UNIT_FARE = 100;
+
+    private final int extraUnitDistance;
+    private final int extraBoundaryDistance;
+    private final Predicate<Integer> fareTypeCheck;
+
+    FareType(int extraUnitDistance, int extraBoundaryDistance, Predicate<Integer> fareTypeCheck) {
+        this.extraUnitDistance = extraUnitDistance;
+        this.extraBoundaryDistance = extraBoundaryDistance;
+        this.fareTypeCheck = fareTypeCheck;
+    }
+
+    public static FareType findByDistance(int distance) {
+        return Arrays.stream(values())
+                .filter(fareType -> fareType.match(distance))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("거리가 0 이거나 음수일 경우 요금 계산할 수 없습니다."));
+    }
+
+    public boolean match(int distance) {
+        return this.fareTypeCheck.test(distance);
+    }
+
+    public int calculateOverFare(int distance) {
+        if (this == BASIC) {
+            return BASIC_FARE;
+        }
+
+        if (this == OVER_10KM) {
+            return BASIC_FARE + calculateExtraFare(distance);
+        }
+
+        return OVER_10KM_MAX_FARE + calculateExtraFare(distance);
+    }
+
+    private int calculateExtraFare(int distance) {
+        return (int) ((Math.ceil((distance - 1 - extraBoundaryDistance) / extraUnitDistance) + 1) * EXTRA_UNIT_FARE);
+    }
+
+    public static int calculateFare(int distance) {
+        return findByDistance(distance).calculateOverFare(distance);
+    }
+}
