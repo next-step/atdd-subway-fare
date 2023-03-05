bc9494d29a6fb0df5f13d81218660042c0a4972d
Yunsik-Choi <choiys0311@gmail.com>
2023-03-05 12:28:18 +0900

    ♻️ refactor : 지하철 지도 클래스 메서드 분리

--- src/main/java/nextstep/subway/domain/SubwayMap.java
+++ src/main/java/nextstep/subway/domain/SubwayMap.java
@@ -1,6 +1,7 @@
 package nextstep.subway.domain;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 import nextstep.subway.domain.exception.PathNotFoundException;
 import org.jgrapht.GraphPath;
 import org.jgrapht.WeightedGraph;
@@ -17,7 +18,7 @@ public class SubwayMap {
         this.lines = lines;
     }
 
-    public Path findPath(Station source, Station target, PathType pathType) {
+    public Path findPath(final Station source, final Station target, final PathType pathType) {
         WeightedGraph<Station, SectionEdge> graph = new SimpleDirectedWeightedGraph<>(SectionEdge.class);
         addVertex(graph);
         addNode(graph, pathType);
@@ -32,23 +33,34 @@ public class SubwayMap {
                 .forEach(graph::addVertex);
     }
 
-    private void addNode(final WeightedGraph<Station, SectionEdge> graph, final PathType pathType) {
-        lines.stream()
-                .flatMap(it -> it.getSections().stream())
-                .forEach(it -> {
-                    SectionEdge sectionEdge = SectionEdge.of(it);
-                    graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
-                    graph.setEdgeWeight(sectionEdge, pathType.getStrategy().apply(it));
-                });
+    private void addNode(final WeightedGraph graph, final PathType pathType) {
+        Stream<Section> sectionStream = lines.stream().flatMap(it -> it.getSections().stream());
+        addEdge(sectionStream, graph, pathType);
+        addEdge(sectionStream.map(SubwayMap::createReverseSectionBy), graph, pathType);
+    }
 
-        lines.stream()
-                .flatMap(it -> it.getSections().stream())
-                .map(it -> new Section(it.getLine(), it.getDownStation(), it.getUpStation(), it.getDistance(), it.getDuration()))
-                .forEach(it -> {
-                    SectionEdge sectionEdge = SectionEdge.of(it);
-                    graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
-                    graph.setEdgeWeight(sectionEdge, pathType.getStrategy().apply(it));
-                });
+    private void addEdge(final Stream<Section> lineStream, final WeightedGraph graph, final PathType pathType) {
+        lineStream.forEach(it -> setEdge(graph, pathType, it));
+    }
+
+    private static void setEdge(
+            final WeightedGraph<Station, SectionEdge> graph,
+            final PathType pathType,
+            final Section it
+    ) {
+        SectionEdge sectionEdge = SectionEdge.of(it);
+        graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
+        graph.setEdgeWeight(sectionEdge, pathType.getStrategy().apply(it));
+    }
+
+    private static Section createReverseSectionBy(final Section section) {
+        return new Section(
+                section.getLine(),
+                section.getDownStation(),
+                section.getUpStation(),
+                section.getDistance(),
+                section.getDuration()
+        );
     }
 
     private static Path findShortestPath(
