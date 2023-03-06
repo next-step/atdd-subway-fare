c2915ad17f2eaba9cb94cc6b001cec5329a867fb
cyPark95 <cas15987@gmail.com>
2023-03-06 09:37:33 +0900

    refactor: (피드백) List.of로 변경

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -3,9 +3,10 @@ package nextstep.subway.documentation;
 import nextstep.subway.applicaion.PathService;
 import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
 
 import static nextstep.subway.acceptance.PathSteps.basicDocumentRequest;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -20,7 +21,7 @@ public class PathDocumentation extends Documentation {
     void path() {
         // given
         PathResponse pathResponse = new PathResponse(
-                Lists.newArrayList(
+                List.of(
                         new StationResponse(1L, "강남역"),
                         new StationResponse(2L, "역삼역")
                 ), 10
