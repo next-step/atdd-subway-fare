036b7754e11b5aa41396265601b1f8d4e75101e2
Yunsik-Choi <choiys0311@gmail.com>
2023-03-06 11:14:57 +0900

    ♻️ refactor : 역이 존재하는지 ID로 체크하는 기능 구현

--- src/main/java/nextstep/subway/applicaion/StationService.java
+++ src/main/java/nextstep/subway/applicaion/StationService.java
@@ -1,15 +1,16 @@
 package nextstep.subway.applicaion;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import nextstep.subway.applicaion.dto.StationRequest;
 import nextstep.subway.applicaion.dto.StationResponse;
 import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -37,8 +38,15 @@ public class StationService {
         stationRepository.deleteById(id);
     }
 
+    public StationResponse createStationResponse(Station station) {
+        return new StationResponse(
+                station.getId(),
+                station.getName()
+        );
+    }
+
     public boolean checkExistsId(Long... ids) {
-        return stationRepository.countByIdIn(Arrays.asList(ids)) == ids.length;
+        return stationRepository.findAllById(Arrays.asList(ids)).size() == ids.length;
     }
 
     public Station findById(Long id) {
