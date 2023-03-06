463147b68039760f8330d6022f6fb006790ff581
YunHwan Park <47476276+Parkyunhwan@users.noreply.github.com>
2023-03-07 08:55:47 +0900

    Practice 실습 (#313)
    * documentation 실행 random port로 변경
    
    * 테스트 실행환경 "test"로 설정
    
    * Spring Rest Docs 사용을 위한 restAssured 사용 및 pathSteps로 공통화

--- src/test/java/nextstep/subway/documentation/Documentation.java
+++ src/test/java/nextstep/subway/documentation/Documentation.java
@@ -5,6 +5,7 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.ActiveProfiles;
@@ -12,15 +13,18 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
 
 @ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(RestDocumentationExtension.class)
 public class Documentation {
     protected RequestSpecification spec;
+
+    @LocalServerPort
+    private int port;
 
     @BeforeEach
     public void setUp(RestDocumentationContextProvider restDocumentation) {
         this.spec = new RequestSpecBuilder()
                 .addFilter(documentationConfiguration(restDocumentation))
-                .build();
+                .build().port(port);
     }
 }
