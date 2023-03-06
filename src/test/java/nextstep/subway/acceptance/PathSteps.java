3c71ca8cf621e9368c5044fda203b27ef5da1e3f
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-03-07 03:16:22 +0900

    test(나이-요금정책): 인수조건 추가로 인한 테스트 코드추가

--- src/test/java/nextstep/subway/acceptance/PathSteps.java
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -20,6 +20,19 @@ public class PathSteps {
                 .extract();
     }
 
+    public static ExtractableResponse<Response> 경로_조회_요청(String accessToken, Long source, Long target, String type) {
+        return RestAssured
+                .given().log().all()
+                .auth().oauth2(accessToken)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("source", source)
+                .queryParam("target", target)
+                .queryParam("type", type)
+                .when().get("/paths")
+                .then().log().all()
+                .extract();
+    }
+
     public static ExtractableResponse<Response> 경로_조회_요청(Long source, Long target, String type, RequestSpecification requestSpecification) {
         return requestSpecification
                 .accept(MediaType.APPLICATION_JSON_VALUE)
--- src/test/java/nextstep/subway/utils/DataLoader.java
+++ src/test/java/nextstep/subway/utils/DataLoader.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+import static nextstep.subway.utils.Users.성인;
+import static nextstep.subway.utils.Users.어린이;
+import static nextstep.subway.utils.Users.청소년;
+
 @Profile("test")
 @Component
 public class DataLoader {
@@ -24,5 +28,8 @@ public class DataLoader {
         memberRepository.save(new Member(GithubResponses.사용자2.getEmail(), "password", 20, Arrays.asList(RoleType.ROLE_MEMBER.name())));
         memberRepository.save(new Member(GithubResponses.사용자3.getEmail(), "password", 20, Arrays.asList(RoleType.ROLE_MEMBER.name())));
         memberRepository.save(new Member(GithubResponses.사용자4.getEmail(), "password", 20, Arrays.asList(RoleType.ROLE_MEMBER.name())));
+        memberRepository.save(new Member(성인.getEmail(), 성인.getPassword(), 성인.getAge(), Arrays.asList(RoleType.ROLE_MEMBER.name())));
+        memberRepository.save(new Member(청소년.getEmail(), 청소년.getPassword(), 청소년.getAge(), Arrays.asList(RoleType.ROLE_MEMBER.name())));
+        memberRepository.save(new Member(어린이.getEmail(), 어린이.getPassword(), 어린이.getAge(), Arrays.asList(RoleType.ROLE_MEMBER.name())));
     }
 }
