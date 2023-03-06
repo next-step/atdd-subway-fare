5f5b762cec905f1864930a99f32061ba2d257ecb
Yunsik-Choi <choiys0311@gmail.com>
2023-03-06 11:46:18 +0900

    ⚡feat : 노선에 구간 추가 로직 수정

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -1,10 +1,19 @@
 package nextstep.subway.domain;
 
-import javax.persistence.*;
 import java.util.List;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import nextstep.subway.domain.exception.SectionCreateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Entity
 public class Line {
+    private static final Logger logger = LoggerFactory.getLogger(Line.class);
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -48,18 +57,11 @@ public class Line {
     }
 
     public void addSection(Station upStation, Station downStation, int distance, int duration) {
-        if (canAddSection(upStation, downStation, distance, duration)) {
+        try {
             sections.add(new Section(this, upStation, downStation, distance, duration));
+        } catch (SectionCreateException exception) {
+            logger.info(SectionCreateException.MESSAGE);
         }
-    }
-
-    private boolean canAddSection(
-            final Station upStation,
-            final Station downStation,
-            final int distance,
-            final int duration
-    ) {
-        return upStation != null && downStation != null && distance != 0 && duration != 0;
     }
 
     public List<Station> getStations() {
