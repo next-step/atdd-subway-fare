9b7abc7c02b30b0232e71c12fc5bc45601befdb1
jiggyjiggy <jiggyjiggy0323@gmail.com>
2023-03-06 17:10:09 +0900

    refactor: document 공통 설정 추출

--- src/test/java/nextstep/subway/documentation/Documentation.java
+++ src/test/java/nextstep/subway/documentation/Documentation.java
@@ -9,9 +9,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.request.ParameterDescriptor;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
 
 @ActiveProfiles("test")
@@ -34,5 +43,19 @@ public class Documentation {
                         .withRequestDefaults(prettyPrint())
                         .withResponseDefaults(prettyPrint()))
                 .build();
+    }
+
+    protected RequestSpecification documentConfig(String documentIdentifier, List<ParameterDescriptor> parameterDescriptors, List<FieldDescriptor> fieldDescriptors) {
+        return RestAssured
+                .given(spec).log().all()
+                .filter(
+                        document(
+                                documentIdentifier,
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                requestParameters(parameterDescriptors),
+                                responseFields(fieldDescriptors)
+                        )
+                );
     }
 }
--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,6 +1,5 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -14,19 +13,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.restdocs.payload.JsonFieldType;
 
+import java.util.List;
+
 import static nextstep.subway.acceptance.PathSteps.경로_조회_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
     public static final long SOURCE_ID = 1L;
@@ -44,30 +39,23 @@ public class PathDocumentation extends Documentation {
                 ), 10, 10, 1_250);
         when(pathService.findPath(anyLong(), anyLong(), any(SearchType.class))).thenReturn(pathResponse);
 
+        RequestSpecification requestSpecification = documentConfig(
+                "path",
+                List.of(parameterWithName("source").description("출발역 id"),
+                        parameterWithName("target").description("도착역 id"),
+                        parameterWithName("type").description("경로 조회 조건")),
+                List.of(fieldWithPath("stations").type(JsonFieldType.ARRAY).description("경로의 지하철 역 목록"),
+                        fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("지하철 역 id"),
+                        fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("지하철 역 이름"),
+                        fieldWithPath("distance").type(JsonFieldType.NUMBER).description("경로의 이동거리"),
+                        fieldWithPath("duration").type(JsonFieldType.NUMBER).description("경로의 이동시간"),
+                        fieldWithPath("fare").type(JsonFieldType.NUMBER).description("지하철 이용요금"))
+        );
+
         // When
-        ExtractableResponse<Response> response = 경로_조회_요청(SOURCE_ID, TARGET_ID, SearchType.DURATION.name(), documentConfig("path"));
+        ExtractableResponse<Response> response = 경로_조회_요청(SOURCE_ID, TARGET_ID, SearchType.DURATION.name(), requestSpecification);
 
         // Then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-    }
-
-    private RequestSpecification documentConfig(String documentIdentifier) {
-        return RestAssured
-                .given(spec).log().all()
-                .filter(document(documentIdentifier, preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
-                        requestParameters(
-                                parameterWithName("source").description("출발역 id"),
-                                parameterWithName("target").description("도착역 id"),
-                                parameterWithName("type").description("경로 조회 조건")
-                        ),
-                        responseFields(
-                                fieldWithPath("stations").type(JsonFieldType.ARRAY).description("경로의 지하철 역 목록"),
-                                fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("지하철 역 id"),
-                                fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("지하철 역 이름"),
-                                fieldWithPath("distance").type(JsonFieldType.NUMBER).description("경로의 이동거리"),
-                                fieldWithPath("duration").type(JsonFieldType.NUMBER).description("경로의 이동시간"),
-                                fieldWithPath("fare").type(JsonFieldType.NUMBER).description("지하철 이용요금")
-                        )
-                ));
     }
 }
