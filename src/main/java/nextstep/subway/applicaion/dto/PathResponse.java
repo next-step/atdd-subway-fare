06f2d7059294b3df6f121c7509db5ad9f2fc8065
joel-you <ruew12@gmail.com>
2023-03-06 11:49:16 +0900

    feature: 요금 조회 문서화
    * API 에 요금 추가 (임시코드)
    * PathResponse 리팩터링

--- src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
@@ -9,21 +9,29 @@ public class PathResponse {
     private final List<StationResponse> stations;
     private final int distance;
     private final int duration;
+    private final int fare;
 
-    public PathResponse(List<StationResponse> stations, int distance, int duration) {
+    private PathResponse(List<StationResponse> stations, int distance, int duration, int fare) {
         this.stations = stations;
         this.distance = distance;
         this.duration = duration;
+        this.fare = fare;
     }
 
-    public static PathResponse of(Path path) {
+    public static PathResponse of(List<StationResponse> stations, int distance, int duration, int fare) {
+        return new PathResponse(stations, distance, duration, fare);
+    }
+
+    public static PathResponse from(Path path) {
         List<StationResponse> stations = path.getStations().stream()
                 .map(StationResponse::of)
                 .collect(Collectors.toList());
         int distance = path.extractDistance();
         int duration = path.extractDuration();
+        // TODO: 요금정보 기능 구현 후 수정.
+        int fare = 1250;
 
-        return new PathResponse(stations, distance, duration);
+        return new PathResponse(stations, distance, duration, fare);
     }
 
     public List<StationResponse> getStations() {
