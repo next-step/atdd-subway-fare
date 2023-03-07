cc7b7a8d12eee23f2abed8f3745d56c989a149af
swsong <ssw890720@gmail.com>
2023-03-08 00:46:34 +0900

    feat : 요금정책 전략 factory 구현

--- src/main/java/nextstep/subway/domain/fare/FareStrategyFactory.java
+++ src/main/java/nextstep/subway/domain/fare/FareStrategyFactory.java
@@ -1,4 +1,16 @@
 package nextstep.subway.domain.fare;
 
+import java.util.List;
+
+
 public class FareStrategyFactory {
+
+    private final List<AgeFareStrategy> strategies = List.of(new FreeStrategy(), new AdultStrategy(), new TeenagerStrategy(), new ChildrenStrategy());
+
+    public AgeFareStrategy of(int age) {
+        return strategies.stream()
+                .filter(s -> s.match(age))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 }
