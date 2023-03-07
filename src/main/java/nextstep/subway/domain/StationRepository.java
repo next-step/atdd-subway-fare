a0f960eb5207bfa43571fb4a7bfd35dd5ea8c30d
Yunsik-Choi <choiys0311@gmail.com>
2023-03-07 13:33:32 +0900

    ♻️ refactor : 지하철 역 아이디 체크 로직 수정

--- src/main/java/nextstep/subway/domain/StationRepository.java
+++ src/main/java/nextstep/subway/domain/StationRepository.java
@@ -1,6 +1,9 @@
 package nextstep.subway.domain;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
+
+    long countByIdIn(List<Long> ids);
 }
