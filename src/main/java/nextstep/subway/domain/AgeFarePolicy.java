bfb9c1896e9aff053114c22b98167c9a836ba38f
swsong <ssw890720@gmail.com>
2023-03-07 23:46:08 +0900

    refactor: 나이정책명 변경, 무료정책 추가

--- src/main/java/nextstep/subway/domain/AgeFarePolicy.java
+++ src/main/java/nextstep/subway/domain/AgeFarePolicy.java
@@ -8,30 +8,27 @@ import java.util.function.IntPredicate;
 
 @RequiredArgsConstructor
 public enum AgeFarePolicy {
-    쳥소년(age -> age >= 13 && age < 19, 0.8),
-    어린이(age -> age >= 6 && age < 13,0.5),
-    NONE(age -> false, 0);
+    무료(age -> age < 6, fare -> 0),
+    쳥소년(age -> age >= 13 && age < 19, fare -> calculateFareByWeight(fare, 0.8)),
+    어린이(age -> age >= 6 && age < 13, fare -> calculateFareByWeight(fare, 0.5)),
+    성인(age -> false, fare -> fare);
 
     private static final int FIXED_DISCOUNT = 350;
     private final IntPredicate matchPredicate;
-    private final double weight;
+    private final IntFunction<Integer> calculateFareFunc;
 
     public static AgeFarePolicy of(int age) {
         return Arrays.stream(values())
                 .filter(e -> e.matchPredicate.test(age))
                 .findAny()
-                .orElse(NONE);
+                .orElse(성인);
     }
 
     public int calculateFare(int fare) {
-        return calculateFare(fare, weight);
+        return calculateFareFunc.apply(fare);
     }
 
-    public int calculateFare(int fare, double weight) {
-        if (weight == 0) {
-            return fare;
-        }
-
-        return  (int) ((fare - FIXED_DISCOUNT) * weight);
+    private static int calculateFareByWeight(int fare, double weight) {
+        return (int) ((fare - FIXED_DISCOUNT) * weight);
     }
 }
