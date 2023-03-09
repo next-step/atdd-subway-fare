e6726cbc898473a8d6d9a17987882e7fa4111e03
heoseungyeon <dia0312@naver.com>
2023-03-10 08:43:42 +0900

    feat: 요금 조회 기능 구현

--- src/main/java/nextstep/subway/domain/FarePolicy.java
+++ src/main/java/nextstep/subway/domain/FarePolicy.java
@@ -1,8 +1,37 @@
 package nextstep.subway.domain;
 
+import java.util.function.IntFunction;
+import java.util.function.IntPredicate;
+import java.util.stream.Stream;
+
 public enum FarePolicy {
-	;
+	BASIC(distance -> distance > 0,
+		distance -> 1_250),
+	OVER_10KM_AND_UNDER_50KM(distance -> distance > 10,
+		distance -> (distance - 10 / 5) * 100),
+	OVER_50KM(distance -> distance > 50,
+		distance -> (distance - 50 / 8) * 100);
+
+	private final IntPredicate condition;
+	private final IntFunction<Integer> calculate;
+
+	FarePolicy(IntPredicate condition, IntFunction<Integer> calculate) {
+		this.condition = condition;
+		this.calculate = calculate;
+	}
 
 	public static int calculateByDistance(int distance) {
+		return Stream.of(FarePolicy.values())
+			.filter(farePolicy -> farePolicy.findPolicy(distance))
+			.mapToInt(farePolicy -> farePolicy.getFare(distance))
+			.sum();
+	}
+
+	private boolean findPolicy(int distance) {
+		return condition.test(distance);
+	}
+
+	private int getFare(int distance) {
+		return calculate.apply(distance);
 	}
 }
--- src/main/java/nextstep/subway/exception/ErrorMessage.java
+++ src/main/java/nextstep/subway/exception/ErrorMessage.java
@@ -2,7 +2,8 @@ package nextstep.subway.exception;
 
 public enum ErrorMessage {
 	SHOULD_BE_PROVIDED_EXISTED_STATION("존재하는 지하철역이 제공되어야 합니다."),
-	SHOULD_BE_DIFFERENT_SOURCE_AND_TARGET("출발역과 도찰역은 달라야 합니다.");
+	SHOULD_BE_DIFFERENT_SOURCE_AND_TARGET("출발역과 도찰역은 달라야 합니다."),
+	SHOULD_BE_POSITIVE("양수이어야 합니다.");
 
 	private final String message;
 
