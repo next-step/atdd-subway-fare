140c3902a411fe4fdf2934821a3383bf6db6a47b
wu2ee <gulb1602@gmail.com>
2023-03-10 07:57:02 +0900

    refactor : 거리요금타입 fare 패키지로 이동

--- src/main/java/nextstep/subway/domain/Path.java
+++ src/main/java/nextstep/subway/domain/Path.java
@@ -1,5 +1,7 @@
 package nextstep.subway.domain;
 
+import nextstep.subway.domain.fare.DistanceFareType;
+
 import java.util.List;
 
 public class Path {
--- src/test/java/nextstep/subway/domain/fare/DistanceFareTypeTest.java
+++ src/test/java/nextstep/subway/domain/fare/DistanceFareTypeTest.java
@@ -1,4 +1,4 @@
-package nextstep.subway.domain;
+package nextstep.subway.domain.fare;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
