dbe519d473bfdf184a74b7643d8698282871855b
최현웅 <121544753+hyunwoongchoi-29cm@users.noreply.github.com>
2023-03-11 20:12:01 +0900

    Revert "feat : 문서화 실습"
    This reverts commit 10323414f1c7f6d851076639bb58b51f11259437.

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,55 +1,19 @@
 package nextstep.subway.documentation;
 
-import static nextstep.subway.acceptance.PathSteps.경로_조회_요청;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
-
 import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import nextstep.subway.applicaion.PathService;
-import nextstep.subway.applicaion.dto.PathResponse;
-import nextstep.subway.applicaion.dto.StationResponse;
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.restdocs.request.RequestParametersSnippet;
+import org.springframework.http.MediaType;
 
 public class PathDocumentation extends Documentation {
 
-    @MockBean
-    private PathService pathService;
-
     @Test
     void path() {
-        Long source = 2L;
-        Long target = 3L;
-        PathResponse pathResponse = new PathResponse(
-                Lists.newArrayList(new StationResponse(source, "강남역"),
-                        new StationResponse(target, "역삼역")), 10);
-
-        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
-
-        final RequestParametersSnippet requestParametersSnippet = requestParameters(
-                parameterWithName("source").description("출발역"),
-                parameterWithName("target").description("도착역")
-        );
-
-        final RequestSpecification requestSpecification = RestAssured
-                .given(spec).log().all()
-                .filter(
-                        document("path", requestParametersSnippet)
-                );
-
-        final ExtractableResponse<Response> response = 경로_조회_요청(requestSpecification, source,
-                target);
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("source", 1L)
+                .queryParam("target", 2L)
+                .when().get("/paths")
+                .then().log().all().extract();
     }
 }
--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -9,14 +9,12 @@ import nextstep.subway.domain.StationRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
-@ActiveProfiles("test")
 class LineServiceTest {
     @Autowired
     private StationRepository stationRepository;
