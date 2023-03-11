caba362d7851b8276e73fe0b6ce7330a07fdfa30
bada.kang <kangbada0728@gmail.com>
2023-03-11 20:44:27 +0900

    add: 지하철 경로 검색 인수 시나리오 작성

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -85,4 +85,21 @@ class PathAcceptanceTest extends AcceptanceTest {
         params.put("distance", distance + "");
         return params;
     }
+
+    /**
+     * Feature: 지하철 경로 검색
+     *
+     * Scenario: 두 역의 최소 시간 경로를 조회
+     *
+     * Given 지하철 역이 등록되어 있음
+     * And 지하철 노선이 등록되어 있음
+     * And 지하철 노선에 지하철역이 등록되어 있음
+     * When 출발역에서 도착역까지의 최소 시간 기준으로 경로 조회를 요청
+     * Then 최소 시간 기준 경로를 응답
+     * And 총 거리와 소요 시간을 함께 응답함
+     */
+
+
+
+
 }
