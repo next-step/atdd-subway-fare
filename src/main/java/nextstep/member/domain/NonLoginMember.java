f92e3159e3195126b08eb996fd9380f27fe8f84d
wu2ee <gulb1602@gmail.com>
2023-03-13 22:26:20 +0900

    refactor : 비로그인 멤버 역할 추가 & id 값 null 로 변경

--- src/main/java/nextstep/member/domain/NonLoginMember.java
+++ src/main/java/nextstep/member/domain/NonLoginMember.java
@@ -3,10 +3,9 @@ package nextstep.member.domain;
 import java.util.List;
 
 public class NonLoginMember extends LoginMember {
-    private static final Long nonLoginId = -1L;
     private static final int nonLoginAge = 25;
 
     public NonLoginMember() {
-        super(nonLoginId, nonLoginAge, List.of(RoleType.ROLE_MEMBER.name()));
+        super(null, nonLoginAge, List.of(RoleType.NON_LOGIN_MEMBER.name()));
     }
 }
