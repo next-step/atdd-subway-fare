d464d8d4f010c4f421a3185c20282f94cdbc3468
cafitac <cafitac99@gmail.com>
2023-03-14 01:10:28 +0900

    docs: 경로 조회 응답에 요금 정보가 포함된 API 문서를 작성합니다

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -10,6 +10,7 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.restdocs.payload.JsonFieldType;
 
 import static nextstep.subway.acceptance.PathSteps.두_역의_경로_조회를_요청;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,6 +18,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
@@ -44,11 +47,15 @@ public class PathDocumentation extends Documentation {
                                 parameterWithName("source").description("출발역"),
                                 parameterWithName("target").description("도착역"),
                                 parameterWithName("type").description("경로 조회 타입 (distance | duration)")
+                        ),
+                        responseFields(
+                                fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("역 ID"),
+                                fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("역 이름"),
+                                fieldWithPath("distance").type(JsonFieldType.NUMBER).description("경로 거리"),
+                                fieldWithPath("fare").type(JsonFieldType.STRING).description("최단 거리 경로 기준 요금 (원)")
                         )));
         final ExtractableResponse<Response> response = 두_역의_경로_조회를_요청(requestSpec, 1L, 2L, "distance");
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsOnly(1L, 2L);
-        assertThat(response.jsonPath().getInt("distance")).isEqualTo(10);
     }
 }
