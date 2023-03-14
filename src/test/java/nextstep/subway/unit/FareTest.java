59f98ca3c95de1205a9cf7291d5da82ab3b8f471
cafitac <cafitac99@gmail.com>
2023-03-15 01:46:58 +0900

    feat: 요금 조회 기능에 대한 코드를 작성합니다

--- src/test/java/nextstep/subway/unit/FareTest.java
+++ src/test/java/nextstep/subway/unit/FareTest.java
@@ -12,49 +12,49 @@ public class FareTest {
     @Test
     void calculateFareWithUnder10km() {
         // when
-        final Fare fare = new Fare(10);
+        final int fare = Fare.calculateAmount(10);
 
         // then
-        assertThat(fare.getAmount()).isEqualTo(1250);
+        assertThat(fare).isEqualTo(1250);
     }
 
     @DisplayName("10km 거리에 대한 요금 계산")
     @Test
     void calculateFareWithOver10kmAndUpTo50km() {
         // when
-        final Fare fare = new Fare(11);
+        final int fare = Fare.calculateAmount(11);
 
         // then
-        assertThat(fare.getAmount()).isEqualTo(1350);
+        assertThat(fare).isEqualTo(1350);
     }
 
     @DisplayName("50km 이하 거리에 대한 요금 계산")
     @Test
     void calculateFareWithUpTo50km() {
         // when
-        final Fare fare = new Fare(50);
+        final int fare = Fare.calculateAmount(50);
 
         // then
-        assertThat(fare.getAmount()).isEqualTo(2050);
+        assertThat(fare).isEqualTo(2050);
     }
 
     @DisplayName("50km 초과 거리에 대한 요금 계산")
     @Test
     void calculateFareWithOver50km() {
         // when
-        final Fare fare = new Fare(51);
+        final int fare = Fare.calculateAmount(51);
 
         // then
-        assertThat(fare.getAmount()).isEqualTo(2150);
+        assertThat(fare).isEqualTo(2150);
     }
 
     @DisplayName("50km 초과 거리에 대한 요금 계산")
     @Test
     void calculateFareWith59Distance() {
         // when
-        final Fare fare = new Fare(59);
+        final int fare = Fare.calculateAmount(59);
 
         // then
-        assertThat(fare.getAmount()).isEqualTo(2250);
+        assertThat(fare).isEqualTo(2250);
     }
 }
