e2ca53816d55b10ab5b55e2cb7f282ceb539e1c6
cafitac <cafitac99@gmail.com>
2023-03-15 13:19:41 +0900

    refactor: 요금 계산 로직의 정수 값을 enum 상수로 분리합니다

--- src/main/java/nextstep/subway/domain/Fare.java
+++ src/main/java/nextstep/subway/domain/Fare.java
@@ -2,21 +2,22 @@ package nextstep.subway.domain;
 
 public class Fare {
 
-    private static final int DEFAULT_FARE = 1250;
+    private static final int BASIC_FARE = 1250;
+    private static final int DEFAULT_DISTANCE = 10;
 
     public static int calculateAmount(final int distance) {
-        if (distance <= 10) {
-            return DEFAULT_FARE;
+        if (distance <= DEFAULT_DISTANCE) {
+            return BASIC_FARE;
         }
-        else if (distance > 10 && distance <= 50) {
-            return DEFAULT_FARE + calculateOverFare(distance - 10, 5);
+        else if (distance > DEFAULT_DISTANCE && distance <= 50) {
+            return BASIC_FARE + calculateOverFare(distance - DEFAULT_DISTANCE, DistanceRange.OVER_10_AND_UNDER_50);
         }
         else {
-            return DEFAULT_FARE + calculateOverFare(40, 5) + calculateOverFare(distance - 50, 8);
+            return BASIC_FARE + calculateOverFare(40, DistanceRange.OVER_10_AND_UNDER_50) + calculateOverFare(distance - 50, DistanceRange.OVER_50);
         }
     }
 
-    private static int calculateOverFare(final int distance, final int per_distance) {
-        return (int) ((Math.ceil((distance - 1) / per_distance) + 1) * 100);
+    private static int calculateOverFare(final int distance, final DistanceRange distanceRange) {
+        return (int) ((Math.ceil((distance - 1) / distanceRange.getIntervalDistance()) + 1) * distanceRange.getAmountPerDistance());
     }
 }
