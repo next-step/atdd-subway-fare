f9c66ddb5d8ad35966ce7b25310570545fa7d110
bada.kang <kangbada0728@gmail.com>
2023-03-26 11:59:53 +0900

    refactor: 템플릿 메서드 패턴을 이용한 중복 제거
    https://github.com/next-step/atdd-subway-fare/pull/328#discussion_r1138666771

--- src/main/java/nextstep/subway/domain/PathType.java
+++ src/main/java/nextstep/subway/domain/PathType.java
@@ -4,37 +4,39 @@ import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public enum PathType {
     DISTANCE {
         @Override
-        public void registerEdge(List<Line> lines, SimpleDirectedWeightedGraph<Station, SectionEdge> graph) {
-            lines.stream()
-                    .flatMap(it -> it.getSections().stream())
-                    .map(it -> new Section(it.getLine(), it.getDownStation(), it.getUpStation(), it.getDistance(), it.getDuration()))
-                    .forEach(it -> {
-                        SectionEdge sectionEdge = SectionEdge.of(it);
-                        graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
-                        graph.setEdgeWeight(sectionEdge, it.getDistance());
-                    });
+        protected void setEdgeWeight(SimpleDirectedWeightedGraph<Station, SectionEdge> graph, Section section) {
+            SectionEdge sectionEdge = SectionEdge.of(section);
+            graph.setEdgeWeight(sectionEdge, section.getDistance());
         }
     },
     DURATION {
         @Override
-        public void registerEdge(List<Line> lines, SimpleDirectedWeightedGraph<Station, SectionEdge> graph) {
-            lines.stream()
-                    .flatMap(it -> it.getSections().stream())
-                    .map(it -> new Section(it.getLine(), it.getDownStation(), it.getUpStation(), it.getDistance(), it.getDuration()))
-                    .forEach(it -> {
-                        SectionEdge sectionEdge = SectionEdge.of(it);
-                        graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
-                        graph.setEdgeWeight(sectionEdge, it.getDuration());
-                    });
+        protected void setEdgeWeight(SimpleDirectedWeightedGraph<Station, SectionEdge> graph, Section section) {
+            SectionEdge sectionEdge = SectionEdge.of(section);
+            graph.setEdgeWeight(sectionEdge, section.getDuration());
         }
     },
     ;
 
-    public abstract void registerEdge(List<Line> lines, SimpleDirectedWeightedGraph<Station, SectionEdge> graph);
+    public void registerEdge(List<Line> lines, SimpleDirectedWeightedGraph<Station, SectionEdge> graph) {
+        lines.stream()
+                .flatMap(it -> it.getSections().stream())
+                .map(it -> new Section(it.getLine(), it.getDownStation(), it.getUpStation(), it.getDistance(), it.getDuration()))
+                .forEach(it -> {
+                    SectionEdge sectionEdge = SectionEdge.of(it);
+                    graph.addEdge(it.getUpStation(), it.getDownStation(), sectionEdge);
+                    setEdgeWeight(graph, it);
+                });
+    }
+
+    protected abstract void setEdgeWeight(SimpleDirectedWeightedGraph<Station, SectionEdge> graph, Section section);
 
     public static PathType of(String codeStr) {
         if (codeStr == null) {
