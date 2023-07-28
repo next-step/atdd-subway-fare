029d1c375b1e4f8b2cd188806236c84a20d6efd0
boorownie <probitanima11@gmail.com>
2023-07-28 09:29:02 +0900

    7th default sample

---
+++ src/test/java/nextstep/subway/acceptance/AcceptanceTest.java
@@ -0,0 +1,32 @@
+package nextstep.subway.acceptance;
+
+import nextstep.subway.utils.DataLoader;
+import nextstep.subway.utils.DatabaseCleanup;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static nextstep.subway.acceptance.MemberSteps.베어러_인증_로그인_요청;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class AcceptanceTest {
+    public static final String EMAIL = "admin@email.com";
+    public static final String PASSWORD = "password";
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+    @Autowired
+    private DataLoader dataLoader;
+
+    String 관리자;
+
+    @BeforeEach
+    public void setUp() {
+        databaseCleanup.execute();
+        dataLoader.loadData();
+
+        관리자 = 베어러_인증_로그인_요청(EMAIL, PASSWORD).jsonPath().getString("accessToken");
+    }
+}
