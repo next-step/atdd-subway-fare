a6acf0db5482c3776b41d145a2f004f8a348a9f1
boorownie <probitanima11@gmail.com>
2023-07-28 09:29:02 +0900

    7th default sample

---
+++ src/test/java/nextstep/subway/utils/DataLoader.java
@@ -0,0 +1,26 @@
+package nextstep.subway.utils;
+
+import nextstep.member.domain.Member;
+import nextstep.member.domain.MemberRepository;
+import nextstep.member.domain.RoleType;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile("test")
+@Component
+public class DataLoader {
+    private MemberRepository memberRepository;
+
+    public DataLoader(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void loadData() {
+        memberRepository.save(new Member("admin@email.com", "password", 20, RoleType.ROLE_ADMIN.name()));
+        memberRepository.save(new Member("member@email.com", "password", 20, RoleType.ROLE_MEMBER.name()));
+        memberRepository.save(new Member(GithubResponses.사용자1.getEmail(), "password", 20, RoleType.ROLE_MEMBER.name()));
+        memberRepository.save(new Member(GithubResponses.사용자2.getEmail(), "password", 20, RoleType.ROLE_MEMBER.name()));
+        memberRepository.save(new Member(GithubResponses.사용자3.getEmail(), "password", 20, RoleType.ROLE_MEMBER.name()));
+        memberRepository.save(new Member(GithubResponses.사용자4.getEmail(), "password", 20, RoleType.ROLE_MEMBER.name()));
+    }
+}
