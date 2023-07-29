74cf796a083c0395a4164f05171d244d57f82c8e
ktae23 <pktpkt8917@gmail.com>
2023-07-29 23:20:35 +0900

    refactor : add abstract method for common config docs filter

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,22 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
 import nextstep.subway.controller.resonse.PathResponse;
 import nextstep.subway.controller.resonse.StationResponse;
 import nextstep.subway.service.PathFindService;
+import nextstep.subway.steps.PathSteps;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.restdocs.restassured3.RestDocumentationFilter;
 
 import java.util.List;
 
+import static nextstep.utils.AcceptanceTestUtils.verifyResponseStatus;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
@@ -24,28 +27,29 @@ class PathDocumentation extends Document {
     @MockBean
     private PathFindService pathFindService;
 
-    private Long 강남역Id = 1L;
-    private Long 역삼역Id = 2L;
+    private Long 강남역_id = 1L;
+    private Long 역삼역_id = 2L;
+
+    private String 강남역_이름 = "강남역";
+    private String 역삼역_이름 = "역삼역";
 
     @Test
     void path() {
-        RestAssured
-                .given(spec).log().all()
-                .filter(getDocumentationFilter())
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 강남역Id)
-                .queryParam("target", 역삼역Id)
-                .when().get("/paths")
-                .then().log().all().extract();
+        ValidatableResponse pathFoundResponse = PathSteps.getPath(강남역_id, 역삼역_id);
+
+        verifyResponseStatus(pathFoundResponse, HttpStatus.OK);
+
+        PathSteps.verifyFoundPath(pathFoundResponse, 10L, 강남역_이름, 역삼역_이름);
     }
 
-    private RestDocumentationFilter getDocumentationFilter() {
-        final StationResponse 강남역 = new StationResponse(강남역Id, "강남역");
-        final StationResponse 역삼역 = new StationResponse(역삼역Id, "역삼역");
+    @Override
+    protected RestDocumentationFilter getDocumentationFilter() {
+        final StationResponse 강남역 = new StationResponse(강남역_id, 강남역_이름);
+        final StationResponse 역삼역 = new StationResponse(역삼역_id, 역삼역_이름);
 
         final PathResponse pathResponse = new PathResponse(List.of(강남역, 역삼역), 10L);
 
-        given(pathFindService.getShortestPath(강남역Id, 역삼역Id)).willReturn(pathResponse);
+        given(pathFindService.getShortestPath(강남역_id, 역삼역_id)).willReturn(pathResponse);
 
         return document("path",
                 preprocessRequest(prettyPrint()),
