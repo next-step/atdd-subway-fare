69fea9e72b793e0aabc3a72d369d84ef5878b19c
psy3720 <qkrtmddyd12@naver.com>
2023-07-30 16:25:55 +0900

    test: 경로찾기 문서화 테스트 추가..

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,15 +1,56 @@
 package nextstep.subway.documentation;
 
 import io.restassured.RestAssured;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.restassured3.RestDocumentationFilter;
+
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
+    @MockBean
+    private PathService pathService;
 
     @Test
     void path() {
+        PathResponse pathResponse = new PathResponse(
+                Lists.newArrayList(
+                        new StationResponse(1L, "강남역"),
+                        new StationResponse(2L, "역삼역")
+                ), 10
+        );
+
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+        RestDocumentationFilter restDocumentationFilter = document("path"
+                , preprocessRequest(prettyPrint())
+                , preprocessResponse(prettyPrint())
+                , requestParameters(
+                        parameterWithName("source").description("출발역 id")
+                        , parameterWithName("target").description("도착역 id"))
+                , responseFields(
+                        subsectionWithPath("stations").description("출발역으로부터 도착역까지의 경로에 있는 역 목록")
+                        , subsectionWithPath("distance").description("조회한 경로 구간의 거리")
+                )
+        );
+
         RestAssured
-                .given().log().all()
+                .given(spec).log().all()
+                .filter(restDocumentationFilter)
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
