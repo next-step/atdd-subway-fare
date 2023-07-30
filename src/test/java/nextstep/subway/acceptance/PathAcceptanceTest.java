f4e05d719cd6a32bc61e6b9c14b3e98a44634f18
ktae23 <pktpkt8917@gmail.com>
2023-07-30 18:10:51 +0900

    test : modify acceptance test (add path type and duration property)

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -49,34 +49,87 @@ public class PathAcceptanceTest {
         createSection(삼호선, 남부터미널역, 양재역, 3);
     }
 
+
+    /**
+     * Feature: 지하철 경로 검색
+     * Scenario: 두 역의 최소 시간 경로를 조회
+     * Given 지하철역이 등록되어있음
+     * And 지하철 노선이 등록되어있음
+     * And 지하철 노선에 지하철역이 등록되어있음
+     * When 출발역에서 도착역까지의 최소 거리 기준으로 경로 조회를 요청
+     * Then 최소 거리 기준 경로를 응답
+     * And 총 거리와 소요 시간을 함께 응답함
+     */
     @Nested
-    class Success {
+    class Success_Ditance {
 
         @Test
         void 교대역에서_양재역을_가는_최단_경로는_교대_남부터미널_양재_5미터이다() {
             // when
-            ValidatableResponse pathResponse = getPath(교대역, 양재역);
+            ValidatableResponse pathResponse = getPath(교대역, 양재역, PathType.DISTANCE);
 
             // then
-            verifyFoundPath(pathResponse, 5L, "교대역", "남부터미널역", "양재역");
+            verifyFoundPath(pathResponse, 5L, 20, "교대역", "남부터미널역", "양재역");
         }
 
         @Test
         void 강남역에서_남부터미널역을_가는_최단_경로는_강남_교대_남부터미널_12미터이다() {
             // when
-            ValidatableResponse pathResponse = getPath(강남역, 남부터미널역);
+            ValidatableResponse pathResponse = getPath(강남역, 남부터미널역, PathType.DISTANCE);
 
             // then
-            verifyFoundPath(pathResponse, 12L, "강남역", "교대역", "남부터미널역");
+            verifyFoundPath(pathResponse, 12L, 48L, "강남역", "교대역", "남부터미널역");
         }
 
         @Test
         void 강남역에서_양재역을_가는_최단_경로는_강남_양재_10미터이다() {
             // when
-            ValidatableResponse pathResponse = getPath(강남역, 양재역);
+            ValidatableResponse pathResponse = getPath(강남역, 양재역, PathType.DISTANCE);
 
             // then
-            verifyFoundPath(pathResponse, 10L, "강남역", "양재역");
+            verifyFoundPath(pathResponse, 10L, 40L, "강남역", "양재역");
+        }
+
+    }
+
+    /**
+     * Feature: 지하철 경로 검색
+     * Scenario: 두 역의 최소 시간 경로를 조회
+     * Given 지하철역이 등록되어있음
+     * And 지하철 노선이 등록되어있음
+     * And 지하철 노선에 지하철역이 등록되어있음
+     * When 출발역에서 도착역까지의 최소 시간 기준으로 경로 조회를 요청
+     * Then 최소 시간 기준 경로를 응답
+     * And 총 거리와 소요 시간을 함께 응답함
+     */
+    @Nested
+    class Success_Duration {
+
+        @Test
+        void 교대역에서_양재역을_가는_최단_경로는_교대_남부터미널_양재_5미터이다() {
+            // when
+            ValidatableResponse pathResponse = getPath(교대역, 양재역, PathType.DURATION);
+
+            // then
+            verifyFoundPath(pathResponse, 5L, 20, "교대역", "남부터미널역", "양재역");
+        }
+
+        @Test
+        void 강남역에서_남부터미널역을_가는_최단_경로는_강남_교대_남부터미널_12미터이다() {
+            // when
+            ValidatableResponse pathResponse = getPath(강남역, 남부터미널역, PathType.DURATION);
+
+            // then
+            verifyFoundPath(pathResponse, 12L, 48, "강남역", "교대역", "남부터미널역");
+        }
+
+        @Test
+        void 강남역에서_양재역을_가는_최단_경로는_강남_양재_10미터이다() {
+            // when
+            ValidatableResponse pathResponse = getPath(강남역, 양재역, PathType.DURATION);
+
+            // then
+            verifyFoundPath(pathResponse, 10L, 40,"강남역", "양재역");
         }
 
     }
--- src/test/java/nextstep/subway/steps/PathSteps.java
+++ src/test/java/nextstep/subway/steps/PathSteps.java
@@ -13,17 +13,19 @@ public class PathSteps {
 
     public static final String PATH_RESOURCE_URL = "/paths";
 
-    public static ValidatableResponse getPath(Long sourceStationId, Long targetStationId) {
+    public static ValidatableResponse getPath(Long sourceStationId, Long targetStationId, PathType type) {
         Map<String, Object> params = new HashMap<>();
         params.put("source", sourceStationId);
         params.put("target", targetStationId);
+        params.put("type", type);
 
         return getResource(PATH_RESOURCE_URL, params);
     }
 
-    public static void verifyFoundPath(ValidatableResponse foundPathResponse, long distance, String... stationNames) {
+    public static void verifyFoundPath(ValidatableResponse foundPathResponse, long distance, int duration, String... stationNames) {
         JsonPath jsonPath = foundPathResponse.extract().jsonPath();
         assertThat(jsonPath.getList("stationResponses.name", String.class)).containsExactly(stationNames);
         assertThat(jsonPath.getLong("distance")).isEqualTo(distance);
+        assertThat(jsonPath.getLong("duration")).isEqualTo(duration);
     }
 }
