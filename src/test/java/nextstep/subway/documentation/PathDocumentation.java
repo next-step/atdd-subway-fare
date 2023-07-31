0130e5a0ebeeb2ea3bd93a8d97e44f6a6ac0ebea
wade.song <wade.song@kakaocorp.com>
2023-08-01 05:53:44 +0900

    [step0] rest docs에 request/response 설정

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -13,6 +13,10 @@ import static nextstep.subway.acceptance.PathSteps.경로_조회_요청;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
@@ -20,11 +24,24 @@ public class PathDocumentation extends Documentation {
     @MockBean
     private PathService pathService;
 
-    private final RequestSpecification requestSpecification = RestAssured.given(spec).log().all()
-            .filter(document("path", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())));
-
     @Test
     void path() {
+
+        // given
+        RequestSpecification requestSpecification = RestAssured.given(spec).log().all()
+                .filter(document("path",
+                                 preprocessRequest(prettyPrint()),
+                                 preprocessResponse(prettyPrint()),
+                                 requestParameters(
+                                        parameterWithName("source").description("출발역 ID"),
+                                        parameterWithName("target").description("도착역 ID")),
+                                responseFields(
+                                        fieldWithPath("stations[].id").description("지하철역 ID"),
+                                        fieldWithPath("stations[].name").description("지하철역 이름"),
+                                        fieldWithPath("distance").description("조회된 경로 거리")))
+                );
+
+
         PathResponse pathResponse = new PathResponse(
                 Lists.newArrayList(
                         new StationResponse(1L, "강남역"),
