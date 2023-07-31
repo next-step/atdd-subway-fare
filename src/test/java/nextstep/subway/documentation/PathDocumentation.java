a023b5cdf03dcec168366b947878f87becc1e093
yhm <yhm@wemakeprice.com>
2023-07-31 17:58:05 +0900

    실습 - 뼈대 코드 준비 & 문서화 실습
    - default-sample cherry-pick
    - PathDocumentation Stub 추가
    - PathSteps 추가하여 RestAssured 중복 코드 분리

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,39 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import nextstep.util.PathSteps;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
     @Test
-    void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+    void 경로를_조회한다() {
+        PathResponse pathResponse = new PathResponse(
+                Lists.newArrayList(
+                        new StationResponse(1L, "강남역"),
+                        new StationResponse(2L, "역삼역")
+                ), 10
+        );
+
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("source", 1L);
+        parameters.put("target", 2L);
+
+        PathSteps.getRestAssured(spec, "path", "/paths", parameters);
     }
 }
