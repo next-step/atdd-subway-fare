436faa4a31673fa81a2993fd038c7e4e18c2b543
유기훈 <98267772+62hoon99@users.noreply.github.com>
2023-07-31 18:03:15 +0900

    Step0 리뷰 요청 드립니다. (#344)
    * init: 뼈대 코드 사용
    
    * test: 경로 찾기 기능 문서용 테스트 구현
    
    * refactor: Path Step 메서드로 중복 코드 해결

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,35 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import nextstep.subway.acceptance.PathSteps;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
     @Test
     void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+        // given
+        PathResponse pathResponse = new PathResponse(
+                Lists.newArrayList(
+                        new StationResponse(1L, "강남역"),
+                        new StationResponse(2L, "역삼역")
+                ), 10
+        );
+
+        // when
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+        // then
+        PathSteps.문서용_두_역의_최단_거리_경로_조회를_요청(getRequestSpecification("path"), 1L, 2L);
     }
 }
