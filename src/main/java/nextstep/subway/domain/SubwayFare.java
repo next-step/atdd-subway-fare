e7cf7a7c56e7a2c610ffef51762513eb56c41c3f
psy3720 <qkrtmddyd12@naver.com>
2023-08-01 21:53:20 +0900

    refactor: 요금정보 클래스 리팩토링

--- src/main/java/nextstep/subway/domain/SubwayFare.java
+++ src/main/java/nextstep/subway/domain/SubwayFare.java
@@ -1,28 +1,36 @@
 package nextstep.subway.domain;
 
 public class SubwayFare {
-    public int calculateOverFare(int distance, int chargeDistance) {
-        return (int) ((Math.ceil((distance - 1) / chargeDistance) + 1) * 100);
-    }
+    private final int BASIC_FARE = 1250;
+    private final int BASIC_DISTANCE = 10;
+    private final int CHARGE_DISTANCE_FIVE = 5;
+    private final int CHARGE_DISTANCE_EIGHT = 8;
+    private final int ADDITIONAL_CHARGE_RATE = 100;
 
-    public int calculateBasicFare() {
-        return 1250;
+    public int calculateOverFare(int distance, int chargeDistance) {
+        return (int) ((Math.ceil((distance - 1) / chargeDistance) + 1) * ADDITIONAL_CHARGE_RATE);
     }
 
     public int calculateFare(int distance) {
-        if (distance < 0) {
-            throw new RuntimeException("거리가 올바르지 않습니다.");
-        }
+        validateDistance(distance);
 
-        if (distance > 10 && distance <= 50) {
-            return calculateBasicFare() + calculateOverFare(distance - 10, 5);
+        int fare = BASIC_FARE;
+
+        if (distance > BASIC_DISTANCE && distance <= 50) {
+            fare += calculateOverFare(distance - BASIC_DISTANCE, CHARGE_DISTANCE_FIVE);
         }
 
         if (distance > 50) {
-            return calculateBasicFare() + calculateOverFare(distance - 10, 5)
-                    + calculateOverFare(distance - 50, 8);
+            fare += (calculateOverFare(distance - BASIC_DISTANCE, CHARGE_DISTANCE_FIVE) +
+                    calculateOverFare(distance - 50, CHARGE_DISTANCE_EIGHT));
         }
 
-        return calculateBasicFare();
+        return fare;
+    }
+
+    private void validateDistance(int distance) {
+        if (distance < 0) {
+            throw new IllegalArgumentException("거리가 올바르지 않습니다.");
+        }
     }
 }
