e9eee96bb2c51a5c9009e8adf9aac83fa8226bfc
SongTeaheon <39703207+SongTeaheon@users.noreply.github.com>
2023-08-01 20:53:22 +0900

    실습 - 뼈대 코드 준비 & 문서화 실습 (#349)
    * 7th default sample
    
    * [step0] 자동화 docs를 위한 path test 수정
    
    * [step0] spec을 설정하는 부분과 filter 설정하는 부분을 RequestSpecification 객체로 주입 받도록 변경( 나머지 RestAssured 부분은 인수 테스트의 Step과 함께 사용할 수 있음)
    
    * [step0] rest docs에 request/response 설정
    
    * [step0] copyDocument
    
    * [step0] readme 및 주석 추가
    
    ---------
    
    Co-authored-by: boorownie <probitanima11@gmail.com>
    Co-authored-by: wade.song <wade.song@kakaocorp.com>

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # 지하철 노선도 미션
 [ATDD 강의](https://edu.nextstep.camp/c/R89PYi5H) 실습을 위한 지하철 노선도 애플리케이션
+
+
+
+# merge 이후 미션 수행 명령어
+```
+git checkout songteaheon  
+git fetch upstream songteaheon  
+git rebase upstream/songteaheon
+```  
+
+# Study
+- https://docs.spring.io/spring-restdocs/docs/current/reference/htmlsingle/#getting-started
+- Documentation test에서 RANDOM_PORT를 사용한 이유: 다른 테스트와 병렬로 수행하면서 PortInUseException이 발생할 수 있기 때문.
+
+
+# 할 일
+## 0단계 - 뼈대 코드 준비 & 문서화 실습
+- [X] 경로 찾기 기능을 문서화하기 위한 테스트를 작성하세요.
+  - [X] PathDocumentation의 path 메서드를 완성시키세요. 
+  - [X] PathDocumentation의 테스트를 수행시켜 snippet을 생성하세요. 
+  - [X] gradle로 asciidoctor task를 수행시켜 문서 파일을 생성하세요 
+  - [X] build > asciidoc > html5 > index.html을 브라우저로 열고 캡쳐한 이미지를 PR에 포함시켜 주세요. 
+  - [X] PathSteps 클래스를 활용하여 PathDocumentation 내 코드 중복을 제거해주세요 
+- [X] 테스트 작성 후 Spring Rest Docs 적용을 통해 문서에 기재할 정보를 설정하세요.
--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,56 @@
 package nextstep.subway.documentation;
 
 import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static nextstep.subway.acceptance.PathSteps.경로_조회_요청;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    private PathService pathService;
+
     @Test
     void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+
+        // given
+        PathResponse pathResponse = new PathResponse(
+                Lists.newArrayList(
+                        new StationResponse(1L, "강남역"),
+                        new StationResponse(2L, "역삼역")
+                ), 10);
+
+        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+
+        // doc
+        RequestSpecification requestSpecification = RestAssured.given(spec).log().all()
+                .filter(document("path",
+                                 preprocessRequest(prettyPrint()),
+                                 preprocessResponse(prettyPrint()),
+                                 requestParameters(
+                                         parameterWithName("source").description("출발역 ID"),
+                                         parameterWithName("target").description("도착역 ID")),
+                                 responseFields(
+                                         fieldWithPath("stations[].id").description("지하철역 ID"),
+                                         fieldWithPath("stations[].name").description("지하철역 이름"),
+                                         fieldWithPath("distance").description("조회된 경로 거리")))
+                );
+
+        // when
+        경로_조회_요청(requestSpecification);
     }
 }
