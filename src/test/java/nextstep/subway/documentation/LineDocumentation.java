fbf030175336533a45f7d52e368bb6af3c1d2683
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-08-02 21:57:38 +0900

    refactor: 노선 생성 문서 테스트에 노선 생성 시 필요한 값들 모두 포함되도록 수정

--- src/test/java/nextstep/subway/documentation/LineDocumentation.java
+++ src/test/java/nextstep/subway/documentation/LineDocumentation.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -33,7 +34,8 @@ public class LineDocumentation extends Documentation {
         when(lineService.saveLine(any())).thenReturn(response);
 
         //then
-        LineSteps.지하철_노선_생성_요청(getRequestSpecification("line"), "2호선", "green");
+        Map<String, String> params = LineSteps.getLineCreateParams("2호선", "green", 1L, 2L, 10, 90);
+        LineSteps.지하철_노선_생성_요청(getRequestSpecification("line"), params);
     }
 
     @Test
