141fdb40d0d4b6c05f423c9607e7d15dd873aff0
wade.song <wade.song@kakaocorp.com>
2023-08-02 19:01:00 +0900

    [step1] createLine document를 위한 테스트 생성 (확인 필요....)

--- src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
+++ src/main/java/nextstep/subway/applicaion/dto/LineRequest.java
@@ -6,6 +6,7 @@ public class LineRequest {
     private Long upStationId;
     private Long downStationId;
     private int distance;
+    private int duration;
 
     public String getName() {
         return name;
