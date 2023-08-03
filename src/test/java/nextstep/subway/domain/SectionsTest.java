a97f0cfa1f9dca7193286af7f22e941ffe3678da
Hyeongmin Park <thalsal@naver.com>
2023-08-03 17:18:54 +0900

    9. test : Sections 구간 삭제 기존 비지니스 로직 성공하는 케이스

--- src/test/java/nextstep/subway/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/domain/SectionsTest.java
@@ -70,4 +70,26 @@ class SectionsTest {
         assertThat(sections.getSections()).extracting("duration").containsOnly(200, 250, 350);
 
     }
+
+    @Test
+    @DisplayName("구간 삭제 - 삭제하는 구간의 상행, 하행 구간이 존재할 경우 두 구간을 연결")
+    void deleteMiddleSection() {
+
+        List<Section> sectionList = new ArrayList<>();
+        sectionList.add(new Section(line, 부평역, 부천역, 10, 200));
+        sectionList.add(new Section(line, 부천역, 개봉역, 10, 300));
+        sectionList.add(new Section(line, 개봉역, 구로역, 16, 600));
+
+        //given
+        Sections sections = new Sections(sectionList);
+
+        //----부평 부천 (개봉) 구로
+        //when
+        sections.delete(개봉역);
+
+        //then
+        assertThat(sections.getStations()).containsExactly(부평역, 부천역, 구로역);
+        assertThat(sections.getSections()).extracting("distance").containsOnly(10, 26);
+
+    }
 }
