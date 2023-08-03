23860fb54f48e29fb1f5f2412cac026c1443ce5f
ktae23 <pktpkt8917@gmail.com>
2023-08-03 23:51:32 +0900

    feat : add fare

--- src/main/java/nextstep/subway/domain/SubwayFare.java
+++ src/main/java/nextstep/subway/domain/SubwayFare.java
@@ -4,26 +4,36 @@ import nextstep.subway.domain.vo.Path;
 
 public final class SubwayFare {
 
-    public static final int BASIC_FARE = 1250;
+    private static final int BASIC_FARE = 1250;
     private static final int OVER_FARE = 100;
-    private static final int OVER_FARE_PER_KM = 5;
+    private static final int OVER_FARE_PER_FIVE_KM = 5;
+    private static final int OVER_FARE_PER_EIGHT_KM = 8;
     private static final int OVER_FARE_STANDARD_PER_FIVE_KM = 10;
     private static final int OVER_FARE_STANDARD_PER_EIGHT_KM = 50;
+
 
     private SubwayFare() {
     }
 
     public static int calculateFare(Path shortestPath) {
         int shortestPathDistance = (int) shortestPath.getDistance();
-        if (shortestPathDistance <= OVER_FARE_STANDARD_PER_FIVE_KM) {
+        if (shortestPathDistance < OVER_FARE_STANDARD_PER_FIVE_KM) {
             return BASIC_FARE;
-        } else if (shortestPathDistance <= OVER_FARE_STANDARD_PER_EIGHT_KM) {
-            return BASIC_FARE + calculateOverFare(shortestPathDistance - OVER_FARE_STANDARD_PER_FIVE_KM);
         }
-        return BASIC_FARE + calculateOverFare(shortestPathDistance - OVER_FARE_STANDARD_PER_FIVE_KM);
+
+        if (shortestPathDistance < OVER_FARE_STANDARD_PER_EIGHT_KM) {
+            return BASIC_FARE
+                    + calculateOverFare(shortestPathDistance - OVER_FARE_STANDARD_PER_FIVE_KM, OVER_FARE_PER_FIVE_KM);
+        }
+
+        int 초과_요금 = calculateOverFare(shortestPathDistance - OVER_FARE_STANDARD_PER_EIGHT_KM, OVER_FARE_PER_EIGHT_KM);
+        System.out.println(shortestPathDistance - OVER_FARE_STANDARD_PER_EIGHT_KM + "미터 초과_요금 = " + 초과_요금);
+        return BASIC_FARE
+                + calculateOverFare(OVER_FARE_STANDARD_PER_EIGHT_KM - OVER_FARE_STANDARD_PER_FIVE_KM, OVER_FARE_PER_FIVE_KM)
+                + 초과_요금;
     }
 
-    private static int calculateOverFare(int overDistance) {
-        return (int) ((Math.ceil((double) (overDistance - 1) / OVER_FARE_PER_KM) + 1) * OVER_FARE);
+    private static int calculateOverFare(int overDistance, int overFarePerKm) {
+        return (int) ((Math.ceil((double) overDistance / overFarePerKm)) * OVER_FARE);
     }
 }
--- src/test/java/nextstep/subway/unit/SubwayFareTest.java
+++ src/test/java/nextstep/subway/unit/SubwayFareTest.java
@@ -1,11 +1,15 @@
 package nextstep.subway.unit;
 
 import nextstep.marker.ClassicUnitTest;
-import nextstep.subway.domain.*;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.PathFinder;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.SubwayFare;
 import nextstep.subway.domain.enums.PathType;
 import nextstep.subway.domain.vo.Path;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static nextstep.subway.unit.utils.ClassicUnitTestUtils.getLine;
 import static nextstep.subway.unit.utils.ClassicUnitTestUtils.getStation;
@@ -22,12 +26,13 @@ class SubwayFareTest {
     void setUp() {
         gangnameStation = getStation("강남역", 1L);
         eonjuStation = getStation("언주역", 2L);
-        secondaryLine = getLine("2호선", "bg-red-600", 10L, 40, gangnameStation, eonjuStation, 1L);
     }
 
-    @Test
-    void 경로를_받으면_요금을_반환한다() {
+    @ParameterizedTest
+    @CsvSource({"1, 1250", "9, 1250", "10, 1250", "11, 1350"})
+    void 경로가_10km_이하이면_기본_요금을_반환한다(long distance, int expectedFare) {
         // given
+        secondaryLine = getLine("2호선", "bg-red-600", distance, 40, gangnameStation, eonjuStation, 1L);
         PathFinder pathFinder = new PathFinder(secondaryLine.getSections());
 
         // when
@@ -35,6 +40,36 @@ class SubwayFareTest {
         int fare = SubwayFare.calculateFare(shortestPath);
 
         // then
-        assertThat(fare).isEqualTo(SubwayFare.BASIC_FARE);
+        assertThat(fare).isEqualTo(expectedFare);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"10, 1250", "11, 1350", "14, 1350", "15, 1350", "49, 2050", "50, 2050", "51, 2150"})
+    void 경로가_10km_초과_50km_이하면_5km당_100원씩_추가_요금이_붙는다(long distance, int expectedFare) {
+        // given
+        secondaryLine = getLine("2호선", "bg-red-600", distance, 40, gangnameStation, eonjuStation, 1L);
+        PathFinder pathFinder = new PathFinder(secondaryLine.getSections());
+
+        // when
+        Path shortestPath = pathFinder.getShortestPath(gangnameStation, eonjuStation, PathType.DISTANCE);
+        int fare = SubwayFare.calculateFare(shortestPath);
+
+        // then
+        assertThat(fare).isEqualTo(expectedFare);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"49, 2050", "50, 2050", "51, 2150", "58, 2150", "59, 2250", "66, 2250", "67, 2350"})
+    void 경로가_50km_초과면_8km당_100원씩_추가_요금이_붙는다(long distance, int expectedFare) {
+        // given
+        secondaryLine = getLine("2호선", "bg-red-600", distance, 220, gangnameStation, eonjuStation, 1L);
+        PathFinder pathFinder = new PathFinder(secondaryLine.getSections());
+
+        // when
+        Path shortestPath = pathFinder.getShortestPath(gangnameStation, eonjuStation, PathType.DISTANCE);
+        int fare = SubwayFare.calculateFare(shortestPath);
+
+        // then
+        assertThat(fare).isEqualTo(expectedFare);
     }
 }
