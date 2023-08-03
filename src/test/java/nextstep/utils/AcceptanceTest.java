c746f879d5390f15b3bbe3d7b4af9df63f315468
Jivebreaddev <jivebreaddev@gmail.com>
2023-08-04 07:10:37 +0900

    test(Documentation): 0단계 asciidoc 생성 완료했습니다!

--- src/test/java/nextstep/utils/AcceptanceTest.java
+++ src/test/java/nextstep/utils/AcceptanceTest.java
@@ -1,21 +1,27 @@
 package nextstep.utils;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class AcceptanceTest {
 
     @Autowired
     private DatabaseCleanup databaseCleanup;
     @Autowired
     private DataLoader dataLoader;
+    @LocalServerPort
+    private int port;
 
     @BeforeEach
     public void setUp() {
+        RestAssured.port = port;
         databaseCleanup.execute();
         dataLoader.loadData();
     }
