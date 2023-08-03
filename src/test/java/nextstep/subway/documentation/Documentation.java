62742af41fe99dbb9793bd763006dc8090868a6d
Hyeongmin Park <thalsal@naver.com>
2023-08-03 18:00:55 +0900

    14. pending : path 경로 문서화

--- src/test/java/nextstep/subway/documentation/Documentation.java
+++ src/test/java/nextstep/subway/documentation/Documentation.java
@@ -1,5 +1,11 @@
 package nextstep.subway.documentation;
 
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
+
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
@@ -9,13 +15,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.RequestPartFieldsSnippet;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 import org.springframework.test.context.ActiveProfiles;
-
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -11,6 +11,8 @@ import nextstep.subway.domain.PathType;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.payload.RequestPartFieldsSnippet;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 
 public class PathDocumentation extends Documentation {
 
@@ -25,11 +27,21 @@ public class PathDocumentation extends Documentation {
                 new StationResponse(1L, "강남역"),
                 new StationResponse(2L, "역삼역")
             ),
-            10
+            10,
+            180
         );
 
         when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
 
         PathSteps.두_역의_최단_거리_경로_조회를_요청_docs(1L, 2L, PathType.DISTANCE.name(), getSpec("path"));
+    }
+
+    private ResponseFieldsSnippet getResponseFieldsSnippet() {
+        return responseFields(
+            fieldWithPath("eventTime").type(JsonFieldType.STRING).description("이벤트 시간"),
+            fieldWithPath("status").type(JsonFieldType.STRING).description("HTTP 상태값"),
+            fieldWithPath("code").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
+            fieldWithPath("data").type(JsonFieldType.NUMBER).description("유저 id값")
+        );
     }
 }
