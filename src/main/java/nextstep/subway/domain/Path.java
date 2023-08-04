6aadb9d490a6d60b243bc82ba88248aca20c9778
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-08-05 00:33:51 +0900

    test: 거리에 따른 요금 계산 단위 테스트 구현

--- src/main/java/nextstep/subway/domain/Path.java
+++ src/main/java/nextstep/subway/domain/Path.java
@@ -3,6 +3,9 @@ package nextstep.subway.domain;
 import java.util.List;
 
 public class Path {
+
+    private static final int FIXED_AMOUNT = 1250;
+    private static final int BASE_DISTANCE = 10;
     private Sections sections;
 
     public Path(Sections sections) {
@@ -23,5 +26,29 @@ public class Path {
 
     public List<Station> getStations() {
         return sections.getStations();
+    }
+
+    public int getFee() {
+        return FIXED_AMOUNT + calculateOverFee(sections.totalDistance() - BASE_DISTANCE);
+    }
+
+    private int calculateOverFee(int distance) {
+        if (distance <= 0) {
+            return 0;
+        }
+        if (distance > 40) {
+            return calculateOverFeeAbove40(distance);
+        }
+        return calculateOverFeeBelow40(distance);
+    }
+
+    private int calculateOverFeeAbove40(int distance) {
+        int over40Distance = distance - 41;
+        return 800 + (int) ((Math.ceil(over40Distance / 8) + 1) * 100);
+    }
+
+    private int calculateOverFeeBelow40(int distance) {
+        int below40Distance = distance - 1;
+        return (int) ((Math.ceil(below40Distance / 5) + 1) * 100);
     }
 }
