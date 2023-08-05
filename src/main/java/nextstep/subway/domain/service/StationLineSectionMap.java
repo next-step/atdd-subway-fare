2241cab2e492612f460226c578005a27844deebf
yuseongan <dhaorktm125@naver.com>
2023-08-05 16:54:45 +0900

    Apply review

---
+++ src/main/java/nextstep/subway/domain/service/StationLineSectionMap.java
@@ -0,0 +1,27 @@
+package nextstep.subway.domain.service;
+
+import lombok.AllArgsConstructor;
+import nextstep.subway.domain.StationLineSection;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public class StationLineSectionMap {
+    final Map<Long, StationLineSection> sectionMap;
+
+    public StationLineSection getSectionBy(Long stationId) {
+        return sectionMap.get(stationId);
+    }
+
+    public static StationLineSectionMap of(List<StationLineSection> sections, Function<StationLineSection, Long> keySelector) {
+
+        final Map<Long, StationLineSection> sectionMap = sections.stream()
+                .collect(Collectors.toMap(keySelector, Function.identity()));
+
+        return new StationLineSectionMap(sectionMap);
+
+    }
+}
