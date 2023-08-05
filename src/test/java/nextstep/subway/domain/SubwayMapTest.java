91eeea4c45685352b5f47e601f7ec90003b2ac93
wade.song <wade.song@kakaocorp.com>
2023-08-05 22:13:04 +0900

    [step1] 경로 조회 테스트를 nested class로 묶기

--- src/test/java/nextstep/subway/domain/SubwayMapTest.java
+++ src/test/java/nextstep/subway/domain/SubwayMapTest.java
@@ -2,6 +2,7 @@ package nextstep.subway.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -59,146 +60,152 @@ class SubwayMapTest {
 
     }
 
-    /**
-     *
-     * line1: station1 - (9) - station2
-     * line2: station2 - (2) - station3
-     * line3: station3 - (10) - station4 - (11) - station1
-     *
-     * GIVEN 정점 4개, 간선 4개
-     * WHEN station1에서 station3으로 가는 경로 조회하면
-     * THEN station1-station2-station3으로 경로가 나온다.
-     */
-    @Test
-    @DisplayName("경로 조회 테스트 1 (환승 있는 경우1) - 정점 4개, 간선 4개")
-    void searchPath1() {
+    @Nested
+    @DisplayName("거리 기준 경로 조회 테스트")
+    public class SearchPathByDistanceTest {
 
-        // given
-        line1.addSection(station1, station2, 9);
-        line2.addSection(station2, station3, 2);
-        line3.addSection(station3, station4, 10);
-        line3.addSection(station4, station1, 11);
+        /**
+         *
+         * line1: station1 - (9) - station2
+         * line2: station2 - (2) - station3
+         * line3: station3 - (10) - station4 - (11) - station1
+         *
+         * GIVEN 정점 4개, 간선 4개
+         * WHEN station1에서 station3으로 가는 경로 조회하면
+         * THEN station1-station2-station3으로 경로가 나온다.
+         */
+        @Test
+        @DisplayName("경로 조회 테스트 1 (환승 있는 경우1) - 정점 4개, 간선 4개")
+        void searchPath1() {
 
-        // when
-        SubwayMap subwayMap = new SubwayMap(List.of(line1, line2, line3));
-        Path path = subwayMap.findPath(station1, station3);
+            // given
+            line1.addSection(station1, station2, 9);
+            line2.addSection(station2, station3, 2);
+            line3.addSection(station3, station4, 10);
+            line3.addSection(station4, station1, 11);
+
+            // when
+            SubwayMap subwayMap = new SubwayMap(List.of(line1, line2, line3));
+            Path path = subwayMap.findPath(station1, station3);
 
 
-        // then
-        assertThat(path.getStations().stream().map(Station::getId).collect(Collectors.toList())).containsExactly(station1Id, station2Id, station3Id);
+            // then
+            assertThat(path.getStations().stream().map(Station::getId).collect(Collectors.toList())).containsExactly(station1Id, station2Id, station3Id);
+        }
+
+        /**
+         *
+         * line1: station1 - (22) - station2
+         * line2: station2 - (15) - station3
+         * line3: station3 - (10) - station4 - (11) - station1
+         *
+         * GIVEN 정점 4개, 간선 4개
+         * WHEN station1에서 station3으로 가는 경로 조회하면
+         * THEN: station1-station2-station3으로 경로가 나온다.
+         */
+        @Test
+        @DisplayName("경로 조회 테스트 2 (환승 있는 경우2) - 정점 4개, 간선 4개")
+        void searchPath2() {
+
+            // given
+            line1.addSection(station1, station2, 22);
+            line2.addSection(station2, station3, 15);
+            line3.addSection(station3, station4, 10);
+            line3.addSection(station4, station1, 11);
+
+            // when
+            SubwayMap subwayMap = new SubwayMap(List.of(line1, line2, line3));
+            Path path = subwayMap.findPath(station1, station3);
+
+            // then
+            assertThat(path.getStations().stream().map(Station::getId).collect(Collectors.toList())).containsExactly(station1Id, station4Id, station3Id);
+        }
+
+        /**
+         *
+         * line1: station1 - (10) - station2 - (9) - station3
+         *
+         * GIVEN 정점 3개, 간선 2개
+         * WHEN station1에서 station3으로 가는 경로 조회하면
+         * THEN: station1-station2-station3으로 경로가 나온다.
+         */
+        @Test
+        @DisplayName("경로 조회 테스트 3 (환승 없는 경우) - 정점 3개, 간선 2개")
+        void searchPath3() {
+
+            // given
+            line1.addSection(station1, station2, 10);
+            line1.addSection(station2, station3, 9);
+
+            // when
+            SubwayMap subwayMap = new SubwayMap(List.of(line1, line2, line3));
+            Path path = subwayMap.findPath(station1, station3);
+
+            // then
+            assertThat(path.getStations().stream().map(Station::getId).collect(Collectors.toList())).containsExactly(station1Id, station2Id, station3Id);
+        }
+
+        /**
+         *
+         * line1: station1 - (2) - station2 - (3) - station3 - (3) - station4
+         * line2: station5 - (7) - station2 - (2)  - station6 - (2) - station7 - (4) - station4
+         *
+         * GIVEN 정점 7개, 간선 7개
+         * WHEN station1에서 station4으로 가는 경로 조회하면
+         * THEN: station1-station2-station3-station4으로 경로가 나온다.
+         */
+        @Test
+        @DisplayName("경로 조회 테스트 4 (환승 없는게 빠른 경우) - 정점 7개, 간선 7개")
+        void searchPath4() {
+
+            // given
+            line1.addSection(station1, station2, 2);
+            line1.addSection(station2, station3, 3);
+            line1.addSection(station3, station4, 3);
+
+            line2.addSection(station5, station2, 7);
+            line2.addSection(station2, station6, 2);
+            line2.addSection(station6, station7, 2);
+            line2.addSection(station7, station4, 4);
+
+            // when
+            SubwayMap subwayMap = new SubwayMap(List.of(line1, line2));
+            Path path = subwayMap.findPath(station1, station4);
+
+            // then
+            assertThat(path.getStations().stream().map(Station::getId).collect(Collectors.toList())).containsExactly(station1Id, station2Id, station3Id, station4Id);
+        }
+
+        /**
+         *
+         * line1: station1 - (12) - station2 - (10) - station3 - (20) - station4
+         * line2: station5 - (7) - station2 - (2)  - station6 - (2) - station7 - (4) - station4
+         *
+         * GIVEN 정점 7개, 간선 7개
+         * WHEN station1에서 station4으로 가는 경로 조회하면
+         * THEN: station1-station2-station6-station7-station4으로 경로가 나온다.
+         */
+        @Test
+        @DisplayName("경로 조회 테스트 5 (환승 하는게 빠른 경우) - 정점 7개, 간선 7개")
+        void searchPath5() {
+
+            // given
+            line1.addSection(station1, station2, 12);
+            line1.addSection(station2, station3, 10);
+            line1.addSection(station3, station4, 20);
+
+            line2.addSection(station5, station2, 7);
+            line2.addSection(station2, station6, 2);
+            line2.addSection(station6, station7, 2);
+            line2.addSection(station7, station4, 4);
+
+            // when
+            SubwayMap subwayMap = new SubwayMap(List.of(line1, line2));
+            Path path = subwayMap.findPath(station1, station4);
+
+            // then
+            assertThat(path.getStations().stream().map(Station::getId).collect(Collectors.toList())).containsExactly(station1Id, station2Id, station6Id, station7Id, station4Id);
+        }
     }
 
-    /**
-     *
-     * line1: station1 - (22) - station2
-     * line2: station2 - (15) - station3
-     * line3: station3 - (10) - station4 - (11) - station1
-     *
-     * GIVEN 정점 4개, 간선 4개
-     * WHEN station1에서 station3으로 가는 경로 조회하면
-     * THEN: station1-station2-station3으로 경로가 나온다.
-     */
-    @Test
-    @DisplayName("경로 조회 테스트 2 (환승 있는 경우2) - 정점 4개, 간선 4개")
-    void searchPath2() {
-
-        // given
-        line1.addSection(station1, station2, 22);
-        line2.addSection(station2, station3, 15);
-        line3.addSection(station3, station4, 10);
-        line3.addSection(station4, station1, 11);
-
-        // when
-        SubwayMap subwayMap = new SubwayMap(List.of(line1, line2, line3));
-        Path path = subwayMap.findPath(station1, station3);
-
-        // then
-        assertThat(path.getStations().stream().map(Station::getId).collect(Collectors.toList())).containsExactly(station1Id, station4Id, station3Id);
-    }
-
-    /**
-     *
-     * line1: station1 - (10) - station2 - (9) - station3
-     *
-     * GIVEN 정점 3개, 간선 2개
-     * WHEN station1에서 station3으로 가는 경로 조회하면
-     * THEN: station1-station2-station3으로 경로가 나온다.
-     */
-    @Test
-    @DisplayName("경로 조회 테스트 3 (환승 없는 경우) - 정점 3개, 간선 2개")
-    void searchPath3() {
-
-        // given
-        line1.addSection(station1, station2, 10);
-        line1.addSection(station2, station3, 9);
-
-        // when
-        SubwayMap subwayMap = new SubwayMap(List.of(line1, line2, line3));
-        Path path = subwayMap.findPath(station1, station3);
-
-        // then
-        assertThat(path.getStations().stream().map(Station::getId).collect(Collectors.toList())).containsExactly(station1Id, station2Id, station3Id);
-    }
-
-    /**
-     *
-     * line1: station1 - (2) - station2 - (3) - station3 - (3) - station4
-     * line2: station5 - (7) - station2 - (2)  - station6 - (2) - station7 - (4) - station4
-     *
-     * GIVEN 정점 7개, 간선 7개
-     * WHEN station1에서 station4으로 가는 경로 조회하면
-     * THEN: station1-station2-station3-station4으로 경로가 나온다.
-     */
-    @Test
-    @DisplayName("경로 조회 테스트 4 (환승 없는게 빠른 경우) - 정점 7개, 간선 7개")
-    void searchPath4() {
-
-        // given
-        line1.addSection(station1, station2, 2);
-        line1.addSection(station2, station3, 3);
-        line1.addSection(station3, station4, 3);
-
-        line2.addSection(station5, station2, 7);
-        line2.addSection(station2, station6, 2);
-        line2.addSection(station6, station7, 2);
-        line2.addSection(station7, station4, 4);
-
-        // when
-        SubwayMap subwayMap = new SubwayMap(List.of(line1, line2));
-        Path path = subwayMap.findPath(station1, station4);
-
-        // then
-       assertThat(path.getStations().stream().map(Station::getId).collect(Collectors.toList())).containsExactly(station1Id, station2Id, station3Id, station4Id);
-    }
-
-    /**
-     *
-     * line1: station1 - (12) - station2 - (10) - station3 - (20) - station4
-     * line2: station5 - (7) - station2 - (2)  - station6 - (2) - station7 - (4) - station4
-     *
-     * GIVEN 정점 7개, 간선 7개
-     * WHEN station1에서 station4으로 가는 경로 조회하면
-     * THEN: station1-station2-station6-station7-station4으로 경로가 나온다.
-     */
-    @Test
-    @DisplayName("경로 조회 테스트 5 (환승 하는게 빠른 경우) - 정점 7개, 간선 7개")
-    void searchPath5() {
-
-        // given
-        line1.addSection(station1, station2, 12);
-        line1.addSection(station2, station3, 10);
-        line1.addSection(station3, station4, 20);
-
-        line2.addSection(station5, station2, 7);
-        line2.addSection(station2, station6, 2);
-        line2.addSection(station6, station7, 2);
-        line2.addSection(station7, station4, 4);
-
-        // when
-        SubwayMap subwayMap = new SubwayMap(List.of(line1, line2));
-        Path path = subwayMap.findPath(station1, station4);
-
-        // then
-       assertThat(path.getStations().stream().map(Station::getId).collect(Collectors.toList())).containsExactly(station1Id, station2Id, station6Id, station7Id, station4Id);
-    }
 }
