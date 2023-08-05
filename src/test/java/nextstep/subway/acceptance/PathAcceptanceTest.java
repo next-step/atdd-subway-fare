86e54912a015558a6df5499dae379ab44e52150e
psy3720 <qkrtmddyd12@naver.com>
2023-08-05 10:19:56 +0900

    test: 경로조회 연령별 요금정책 인수테스트 추가

--- src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PathAcceptanceTest.java
@@ -11,6 +11,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static nextstep.subway.acceptance.LineSteps.지하철_노선에_지하철_구간_생성_요청;
+import static nextstep.subway.acceptance.MemberSteps.베어러_인증_로그인_요청;
+import static nextstep.subway.acceptance.MemberSteps.회원_생성_요청;
 import static nextstep.subway.acceptance.PathSteps.두_역의_경로_조회를_요청;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -93,6 +95,39 @@ class PathAcceptanceTest extends AcceptanceTest {
         assertThat(response.jsonPath().getInt("distance"))
                 .isEqualTo(교대역_남부터미널역_거리 + 남부터미널역_양재역_거리);
         assertThat(response.jsonPath().getInt("fare")).isEqualTo(기본요금 + 삼호선_추가요금);
+    }
+
+    /**
+     * Given 지하철역과 지하철노선에 역을 등록하고
+     * When 출발역에서 도착역까지의 최단 거리 기준으로 경로 조회를 요청하면
+     * Then 최단 거리 기준 경로를 응답한다
+     * And 총 거리, 소요 시간, 지하철 이용 요금을 함께 응답한다
+     * And 요금 계산시 최단거리 기준 및 노선별 추가요금 정책이 적용된다
+     * And 로그인 사용자의 경우 연령별 요금 정책이 적용된다.
+     */
+    @DisplayName("두 역의 최단 거리 경로를 조회한다.")
+    @Test
+    void findPathBy_Distance_Fare_Age() {
+        // given
+        회원_생성_요청("email", "password", 15);
+        String accessToken = 베어러_인증_로그인_요청("email", "password").jsonPath()
+                .getString("accessToken");
+
+        // when
+        var response = 두_역의_경로_조회를_요청(requestSpecification, 교대역, 양재역, PathType.DISTANCE.toString()
+                , accessToken);
+
+        // then
+        assertThat(response.jsonPath().getList("stations.id", Long.class))
+                .containsExactly(교대역, 남부터미널역, 양재역);
+
+        // and
+        assertThat(response.jsonPath().getInt("distance"))
+                .isEqualTo(교대역_남부터미널역_거리 + 남부터미널역_양재역_거리);
+        int deductionFee = 350;
+        int fare = (int) ((기본요금 + 삼호선_추가요금 - deductionFee) * 0.8); // 1280
+
+        assertThat(response.jsonPath().getInt("fare")).isEqualTo(fare);
     }
 
     /**
