6d0f9734a9502e757bc8a2dada0f32cbae6ad1f8
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-08-06 10:55:11 +0900

    test: path 실제 객체사용하는 단위 테스트 구현

--- src/test/java/nextstep/subway/unit/PathTest.java
+++ src/test/java/nextstep/subway/unit/PathTest.java
@@ -1,33 +1,30 @@
 package nextstep.subway.unit;
 
-import nextstep.subway.domain.Path;
-import nextstep.subway.domain.Sections;
+import nextstep.subway.domain.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.when;
+import java.util.List;
 
-@ExtendWith(MockitoExtension.class)
 public class PathTest {
 
-    @Mock
-    private Sections sections;
-
-    private Path path;
+    private Section 중계_노원;
+    private Section 노원_마들;
+    private Section 마들_수락산;
 
     @BeforeEach
     void setUp() {
-        path = new Path(sections);
+        Line 칠호선 = new Line();
+        중계_노원 = new Section(칠호선, new Station("중계역"), new Station("노원역"), 10, 10);
+        노원_마들 = new Section(칠호선, new Station("노원역"), new Station("마들역"), 6, 6);
+        마들_수락산 = new Section(칠호선, new Station("마들역"), new Station("수락산역"), 42, 42);
     }
 
     @Test
     void 거리_10km_이하_요금_계산() {
         //when
-        when(sections.totalDistance()).thenReturn(10);
+        Path path = new Path(new Sections(List.of(중계_노원)));
 
         //then
         Assertions.assertThat(path.getFee()).isEqualTo(1250);
@@ -36,7 +33,7 @@ public class PathTest {
     @Test
     void 거리_10km_초과_50km_까지_요금_계산() {
         //when
-        when(sections.totalDistance()).thenReturn(16);
+        Path path = new Path(new Sections(List.of(중계_노원, 노원_마들)));
 
         //then
         Assertions.assertThat(path.getFee()).isEqualTo(1450);
@@ -45,7 +42,7 @@ public class PathTest {
     @Test
     void 거리_50km_초과_요금_계산() {
         //when
-        when(sections.totalDistance()).thenReturn(58);
+        Path path = new Path(new Sections(List.of(중계_노원, 노원_마들, 마들_수락산)));
 
         //then
         Assertions.assertThat(path.getFee()).isEqualTo(2150);
