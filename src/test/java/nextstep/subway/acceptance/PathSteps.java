4cc756acd6d21e22b82592db4c2c3ecae32c016d
kim jisu <jisu3316@naver.com>
2023-08-06 20:18:00 +0900

    구간 시간 단위 테스트 작성 및 도메인 변경에 따른 코드 수정

--- src/test/java/nextstep/subway/acceptance/PathSteps.java
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -16,6 +16,14 @@ public class PathSteps {
                 .then().log().all().extract();
     }
 
+    public static ExtractableResponse<Response> 두_역의_최단_거리_경로_소요시간_조회를_요청(Long source, Long target, String type) {
+        return RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/paths?source={sourceId}&target={targetId}&type={type}", source, target, type)
+                .then().log().all().extract();
+    }
+
     public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(Long source, Long target, RequestSpecification requestSpecification) {
         return requestSpecification
                 .when().get("/paths?source={sourceId}&target={targetId}", source, target)
