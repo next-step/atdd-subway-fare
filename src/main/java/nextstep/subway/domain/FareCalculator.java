c039c13e4039ac272e9943b885ae6efd2d78cc81
wade.song <wade.song@kakaocorp.com>
2023-08-06 23:50:29 +0900

    [step2] 상수 추출

--- src/main/java/nextstep/subway/domain/FareCalculator.java
+++ src/main/java/nextstep/subway/domain/FareCalculator.java
@@ -9,15 +9,20 @@ public class FareCalculator {
     private static final int OVER_FARE_INTERVAL_UNTIL_50KM = 5;
     private static final int OVER_FARE_INTERVAL_AFTER_50KM = 8;
 
+    private static final int FIRST_OVER_FARE_SECTION_START = 10;
+    private static final int FIRST_OVER_FARE_SECTION_MAX_DISTANCE = 40;
+    private static final int SECOND_OVER_FARE_SECTION_START = 50;
+
+
 
     private int distanceBasic;
     private int distanceOver10km;
     private int distanceOver50km;
 
     public FareCalculator(int distance) {
-        this.distanceBasic = Math.min(distance, 10);
-        this.distanceOver10km = Math.min(Math.max(distance - 10, 0), 40);
-        this.distanceOver50km = Math.max(distance - 50, 0);
+        this.distanceBasic = Math.min(distance, FIRST_OVER_FARE_SECTION_START);
+        this.distanceOver10km = Math.min(Math.max(distance - FIRST_OVER_FARE_SECTION_START, 0), FIRST_OVER_FARE_SECTION_MAX_DISTANCE);
+        this.distanceOver50km = Math.max(distance - SECOND_OVER_FARE_SECTION_START, 0);
     }
 
     public int fare() {
