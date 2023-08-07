abf135e5456ba89670bb6385bf61c88cfea7476b
SeokyungKim <kskyung0624@gmail.com>
2023-08-07 21:50:11 +0900

    refactor : 인수테스트의 api 호출을 재사용해서 문서 자동화 테스트에서 활용할 수 있도록 한다

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,5 +1,6 @@
 package nextstep.subway.documentation;
 
+import static nextstep.subway.acceptance.PathSteps.두_역의_최단단_거리_경로_조회를_요청;
 import static nextstep.subway.utils.ApiDocumentUtils.getDocumentRequest;
 import static nextstep.subway.utils.ApiDocumentUtils.getDocumentResponse;
 import static org.mockito.Mockito.when;
@@ -7,14 +8,12 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWit
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
-import io.restassured.RestAssured;
 import nextstep.subway.applicaion.PathService;
 import nextstep.subway.applicaion.dto.PathResponse;
 import nextstep.subway.applicaion.dto.StationResponse;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
 public class PathDocumentation extends Documentation {
@@ -33,21 +32,16 @@ public class PathDocumentation extends Documentation {
 
         when(pathService.findPath(1L, 2L)).thenReturn(pathResponse);
 
-        RestAssured
-                .given().spec(this.spec).log().all()
-                .filter(document("path",
-                    getDocumentRequest(),
-                    getDocumentResponse(),
-                    responseFields(
-                        fieldWithPath("stations").type(JsonFieldType.ARRAY).description("경로에 포함된 역 목록"),
-                        fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("역 ID"),
-                        fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("역 이름"),
-                        fieldWithPath("distance").type(JsonFieldType.NUMBER).description("총 거리")
-                    )))
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+        var document = document("path",
+            getDocumentRequest(),
+            getDocumentResponse(),
+            responseFields(
+                fieldWithPath("stations").type(JsonFieldType.ARRAY).description("경로에 포함된 역 목록"),
+                fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("역 ID"),
+                fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("역 이름"),
+                fieldWithPath("distance").type(JsonFieldType.NUMBER).description("총 거리")
+            ));
+
+        두_역의_최단단_거리_경로_조회를_요청(1L, 2L, this.spec, document);
     }
 }
