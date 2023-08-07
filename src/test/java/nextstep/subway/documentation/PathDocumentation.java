becc7ad99f08b62bee0f38628365274436107a79
Jivebreaddev <jivebreaddev@gmail.com>
2023-08-07 19:12:38 +0900

    test(PathAcceptanceTest): 경로 조회 타입 인수테스트에 대한 문서화 진행했습니다.

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,10 +1,16 @@
 package nextstep.subway.documentation;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 import io.restassured.RestAssured;
@@ -27,20 +33,32 @@ public class PathDocumentation extends Documentation {
             Lists.newArrayList(
                 new StationResponse(1L, "강남역"),
                 new StationResponse(2L, "역삼역")
-            ), 10
+            ), 10, 12
         );
 
-        when(pathService.findPath(anyLong(), anyLong())).thenReturn(pathResponse);
+        when(pathService.findPath(anyLong(), anyLong(), any())).thenReturn(pathResponse);
 
         RestAssured
             .given(spec).log().all()
             .filter(document("path",
                 preprocessRequest(prettyPrint()),
-                preprocessResponse(prettyPrint())))
+                preprocessResponse(prettyPrint()),
+                requestParameters(
+                    parameterWithName("source").description("출발역 id"),
+                    parameterWithName("target").description("도착역 id"),
+                    parameterWithName("type").description("경로 조회 타입 (DURATION, DISTANCE)")
+                ),
+                responseFields(
+                    fieldWithPath("stations[].id").description("조회 경로 역들의 id"),
+                    fieldWithPath("stations[].name").description("조회 경로 역들의 이름"),
+                    fieldWithPath("distance").description("조회 경로 거리"),
+                    fieldWithPath("duration").description("조회 경로 시간")
+                )
+            ))
             .accept(MediaType.APPLICATION_JSON_VALUE)
             .queryParam("source", 1L)
             .queryParam("target", 2L)
-            .queryParam("type", "DISTANCE")
+            .queryParam("type", "DURATION")
             .when().get("/paths")
             .then().log().all().extract();
     }
