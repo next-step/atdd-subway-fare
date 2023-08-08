196aa5427793adf21dc7ce5e05c194fb61cb2b40
Jivebreaddev <jivebreaddev@gmail.com>
2023-08-08 20:11:34 +0900

    feature(PathResponse, SectionRequest): 경로 조회 요금을 PathResponse에 추가하고 SectionRequest에 추가하였습니다.

--- src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
+++ src/main/java/nextstep/subway/applicaion/dto/PathResponse.java
@@ -1,17 +1,20 @@
 package nextstep.subway.applicaion.dto;
 
-import nextstep.subway.domain.Path;
-
 import java.util.List;
 import java.util.stream.Collectors;
+import nextstep.subway.domain.Path;
 
 public class PathResponse {
     private List<StationResponse> stations;
     private int distance;
+    private int duration;
+    private int fare;
 
-    public PathResponse(List<StationResponse> stations, int distance, int duration) {
+    public PathResponse(List<StationResponse> stations, int distance, int duration, int fare) {
         this.stations = stations;
         this.distance = distance;
+        this.duration = duration;
+        this.fare = fare;
     }
 
     public static PathResponse of(Path path) {
@@ -20,8 +23,8 @@ public class PathResponse {
                 .collect(Collectors.toList());
         int distance = path.extractDistance();
         int duration = path.extractDuration();
-
-        return new PathResponse(stations, distance, duration);
+        int fare = path.extractFare();
+        return new PathResponse(stations, distance, duration, fare);
     }
 
     public List<StationResponse> getStations() {
@@ -31,4 +34,6 @@ public class PathResponse {
     public int getDistance() {
         return distance;
     }
+    public int getDuration() { return duration; }
+    public int getFare() { return fare; }
 }
