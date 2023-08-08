000f687a99a95bdfca5a535af318a64106874f36
SeokyungKim <kskyung0624@gmail.com>
2023-08-08 19:31:54 +0900

    🚀 실습 - 뼈대 코드 준비 & 문서화 실습 (#374)
    * 7th default sample
    
    * fit: injection port number for at and dt
    
    * docs : 지하철 경로 API에 대해 문서화하기 위한 테스트를 작성한다
    
    * refactor : 인수테스트의 api 호출을 재사용해서 문서 자동화 테스트에서 활용할 수 있도록 한다
    
    ---------
    
    Co-authored-by: boorownie <probitanima11@gmail.com>

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,47 @@
 package nextstep.subway.documentation;
 
-import io.restassured.RestAssured;
+import static nextstep.subway.acceptance.PathSteps.두_역의_최단단_거리_경로_조회를_요청;
+import static nextstep.subway.utils.ApiDocumentUtils.getDocumentRequest;
+import static nextstep.subway.utils.ApiDocumentUtils.getDocumentResponse;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+import nextstep.subway.applicaion.PathService;
+import nextstep.subway.applicaion.dto.PathResponse;
+import nextstep.subway.applicaion.dto.StationResponse;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.payload.JsonFieldType;
 
 public class PathDocumentation extends Documentation {
 
+    @MockBean
+    PathService pathService;
+
     @Test
     void path() {
-        RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", 1L)
-                .queryParam("target", 2L)
-                .when().get("/paths")
-                .then().log().all().extract();
+        PathResponse pathResponse = new PathResponse(
+            Lists.newArrayList(
+                new StationResponse(1L, "강남역"),
+                new StationResponse(2L, "역삼역")
+            ), 10
+        );
+
+        when(pathService.findPath(1L, 2L)).thenReturn(pathResponse);
+
+        var document = document("path",
+            getDocumentRequest(),
+            getDocumentResponse(),
+            responseFields(
+                fieldWithPath("stations").type(JsonFieldType.ARRAY).description("경로에 포함된 역 목록"),
+                fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("역 ID"),
+                fieldWithPath("stations[].name").type(JsonFieldType.STRING).description("역 이름"),
+                fieldWithPath("distance").type(JsonFieldType.NUMBER).description("총 거리")
+            ));
+
+        두_역의_최단단_거리_경로_조회를_요청(1L, 2L, this.spec, document);
     }
 }
