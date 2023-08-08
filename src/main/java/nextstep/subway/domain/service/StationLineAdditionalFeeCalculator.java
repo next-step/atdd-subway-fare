fe6721e5999671255571d168b1769baa8064533d
yuseongan <dhaorktm125@naver.com>
2023-08-08 22:15:06 +0900

    Implement line additional fee calculator service feature

--- src/main/java/nextstep/subway/domain/service/StationLineAdditionalFeeCalculator.java
+++ src/main/java/nextstep/subway/domain/service/StationLineAdditionalFeeCalculator.java
@@ -1,16 +1,25 @@
 package nextstep.subway.domain.service;
 
 import lombok.Builder;
+import nextstep.subway.domain.StationLine;
 
 import java.math.BigDecimal;
+import java.util.List;
 
-public class StationLineAdditionalFeeCalculator extends AbstractStationPathFeeCalculator{
+public class StationLineAdditionalFeeCalculator extends AbstractStationPathFeeCalculator {
     @Builder
     public StationLineAdditionalFeeCalculator() {
     }
 
     @Override
     public BigDecimal calculate(StationPathFeeContext context) {
-        return BigDecimal.valueOf(900);
+        return getMaximumFeeOfLineAdditionalFee(context.getLines());
+    }
+
+    private BigDecimal getMaximumFeeOfLineAdditionalFee(List<StationLine> lines) {
+        return lines.stream()
+                .map(StationLine::getAdditionalFee)
+                .max(BigDecimal::compareTo)
+                .orElse(BigDecimal.ZERO);
     }
 }
