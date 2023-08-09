f15c069ea70a3b41944f1fe157cf1e79ca93cabd
ALbertIM0427 <guang7226961@naver.com>
2023-08-10 08:26:30 +0900

    feat: 노선 추가요금 등록 단위테스트 구현

--- src/test/java/nextstep/subway/domain/LineTest.java
+++ src/test/java/nextstep/subway/domain/LineTest.java
@@ -23,4 +23,27 @@ class LineTest {
         Section exceptedSection = new Section(line, sinsaStation, gwanggyoStation, 15, 42 * 60);
         assertThat(section).usingRecursiveComparison().isEqualTo(exceptedSection);
     }
+
+    @DisplayName("노선에 추가요금을 등록하면 추가요금 조회시 등록된 추가요금이 조회된다")
+    @Test
+    void addExtraFareToLine() {
+        // given
+        Line line = new Line();
+
+        // when
+        line.updateExtraFare(10);
+
+        // then
+        assertThat(line.getExtraFare()).isEqualTo(10);
+    }
+
+    @DisplayName("노선에 추가요금 기본값은 0이다")
+    @Test
+    void getDefaultExtraFare() {
+        // given
+        Line line = new Line();
+
+        // when,then
+        assertThat(line.getExtraFare()).isZero();
+    }
 }
