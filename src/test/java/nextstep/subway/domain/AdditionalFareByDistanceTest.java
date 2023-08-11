22f680385b82f6b3f60c9de7446a5eee11a9dd16
wade.song <wade.song@kakaocorp.com>
2023-08-11 19:10:39 +0900

    [step3] ParameterizedTest의 네이밍 작성

--- src/test/java/nextstep/subway/domain/AdditionalFareByDistanceTest.java
+++ src/test/java/nextstep/subway/domain/AdditionalFareByDistanceTest.java
@@ -1,5 +1,6 @@
 package nextstep.subway.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -8,7 +9,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AdditionalFareByDistanceTest {
 
-    @ParameterizedTest
+    @DisplayName("거리에 따른 추가 요금을 확인")
+    @ParameterizedTest(name = "[{index}] 거리가 {0}km인 경우, 요금은 {1}원이다.")
     @CsvSource(value = {"8,1250", "10,1250", "11,1350", "16,1450", "50,2050", "51,2150", "58,2150", "59,2250", "80,2450", "100,2750"})
     void fare(int distance, int expected) {
         AdditionalFareByDistance fareCalculator = new AdditionalFareByDistance(distance);
--- src/test/java/nextstep/subway/domain/DiscountFareByAgeTest.java
+++ src/test/java/nextstep/subway/domain/DiscountFareByAgeTest.java
@@ -1,5 +1,6 @@
 package nextstep.subway.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -8,7 +9,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class DiscountFareByAgeTest {
 
 
-    @ParameterizedTest
+    @DisplayName("나이에 따른 할인 요금을 확인")
+    @ParameterizedTest(name = "[{index}] 나이가 {0}세이고 요금이 {1}원인 경우, 할인 된 요금은 {2}원이다.")
     @CsvSource(value = {"0,1250,0", "1,1250,0", "5,1250,0", "6,1250,800", "12,1250,800", "13,1250,1070", "18,1250,1070", "19,1250,1250"})
     void get(int age, int fare, int expectedDiscountFare) {
 
