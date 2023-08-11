1272be774b1d48122886ecf56016afdf6981bfc1
wade.song <wade.song@kakaocorp.com>
2023-08-11 19:10:57 +0900

    [step3] 불필요 import 제거

--- src/test/java/nextstep/subway/domain/AdditionalFareByDistanceTest.java
+++ src/test/java/nextstep/subway/domain/AdditionalFareByDistanceTest.java
@@ -1,11 +1,10 @@
 package nextstep.subway.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AdditionalFareByDistanceTest {
 
--- src/test/java/nextstep/subway/domain/OverFareCalculatorTest.java
+++ src/test/java/nextstep/subway/domain/OverFareCalculatorTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OverFareCalculatorTest {
 
