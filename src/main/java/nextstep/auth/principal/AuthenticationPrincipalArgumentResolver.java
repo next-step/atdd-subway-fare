47e6c32e0baa80b71c33f0e07e5326e67c8c1a9a
Jivebreaddev <jivebreaddev@gmail.com>
2023-08-12 11:09:44 +0900

    feature(NonLoginUser): 비회원에 대해서도 경로조회 기능을 지원합니다.

--- src/main/java/nextstep/auth/principal/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/nextstep/auth/principal/AuthenticationPrincipalArgumentResolver.java
@@ -24,22 +24,23 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        AuthenticationPrincipal principal = parameter.getParameterAnnotation(AuthenticationPrincipal.class);
+        boolean required = principal.required();
         String authorization = webRequest.getHeader("Authorization");
-        emptyValidate(authorization);
-        if (!"bearer".equalsIgnoreCase(authorization.split(" ")[0])) {
-            throw new AuthenticationException("오류가 있는 토큰 값입니다.");
+
+        if(!StringUtils.hasText(authorization) && !required){
+            return NonLoginUser.of();
         }
+
+        if("bearer".equalsIgnoreCase(authorization) || (!"bearer".equalsIgnoreCase(authorization.split(" ")[0]))){
+            throw new AuthenticationException("로그인 정보를 확인해주세요!");
+        }
+
         String token = authorization.split(" ")[1];
 
         String username = jwtTokenProvider.getPrincipal(token);
         String role = jwtTokenProvider.getRoles(token);
 
         return new UserPrincipal(username, role);
-    }
-
-    private void emptyValidate(String authorization){
-        if(!StringUtils.hasText(authorization) || "bearer".equalsIgnoreCase(authorization)){
-            throw new MalformedJwtException("오류가 있는 토큰 값입니다.");
-        }
     }
 }
