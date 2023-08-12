597b942c9a583daa886ccb63cf54e9b937a06de3
charlotte.chk <charlotte.chk@kakaocorp.com>
2023-08-12 10:04:31 +0900

    feat: 테스트 실패 수정

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -10,12 +10,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static nextstep.subway.acceptance.PathSteps.두_역의_최단_거리_경로_조회를_요청;
+import static nextstep.subway.acceptance.PathSteps.두_역의_최단_시간_경로_조회를_요청;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 @ExtendWith(MockitoExtension.class)
 public class PathDocumentation extends Documentation {
@@ -31,8 +28,8 @@ public class PathDocumentation extends Documentation {
                 ), 10, 10
         );
 
-        when(pathService.findPath(anyLong(), anyLong(), PathType.DURATION)).thenReturn(pathResponse);
+        when(pathService.findPath(1L, 2L, PathType.DURATION)).thenReturn(pathResponse);
 
-        두_역의_최단_거리_경로_조회를_요청(spec, 1L, 2L);
+        두_역의_최단_시간_경로_조회를_요청(spec, 1L, 2L);
     }
 }
