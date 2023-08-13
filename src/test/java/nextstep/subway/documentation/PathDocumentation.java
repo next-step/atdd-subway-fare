96ab375bc34a6de3a59d1d74b76c1e5089a40c37
SeokyungKim <kskyung0624@gmail.com>
2023-08-13 14:22:08 +0900

    docs : 경로 조회 docs에 request parameters를 추가한다

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -6,6 +6,8 @@ import static nextstep.subway.utils.ApiDocumentUtils.getDocumentResponse;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 import nextstep.subway.applicaion.PathService;
@@ -36,6 +38,11 @@ public class PathDocumentation extends Documentation {
         var document = document("path",
             getDocumentRequest(),
             getDocumentResponse(),
+            requestParameters(
+                parameterWithName("source").description("출발하는 역 id"),
+                parameterWithName("target").description("도착하는 역 id"),
+                parameterWithName("type").description("경로 조회 방법: DURATION: 최단시간, DISTANCE: 최단거리")
+            ),
             responseFields(
                 fieldWithPath("stations").type(JsonFieldType.ARRAY).description("경로에 포함된 역 목록"),
                 fieldWithPath("stations[].id").type(JsonFieldType.NUMBER).description("역 ID"),
