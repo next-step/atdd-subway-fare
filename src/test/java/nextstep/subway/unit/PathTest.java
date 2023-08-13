70ddd6929c8d4b70b4a1847c296823cae12e6763
SeolYoungKim <nasur4da@gmail.com>
2023-08-13 17:32:01 +0900

    test: 단위 테스트에 경계값 케이스 추가

--- src/test/java/nextstep/subway/unit/PathTest.java
+++ src/test/java/nextstep/subway/unit/PathTest.java
@@ -1,5 +1,8 @@
 package nextstep.subway.unit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import nextstep.subway.path.domain.Path;
 import nextstep.subway.section.domain.Section;
 import nextstep.subway.section.domain.Sections;
@@ -9,10 +12,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Path 단위 테스트")
 class PathTest {
@@ -42,7 +41,7 @@ class PathTest {
 
     @DisplayName("10km 초과 ~ 50km 이하 거리에서는 기본요금에 5km마다 추가 100원의 요금이 계산된다.")
     @ParameterizedTest(name = "거리={0}")
-    @CsvSource({"15,1350", "20,1450", "50,2050"})
+    @CsvSource({"14,1350", "16,1450", "19,1450", "21,1550", "50,2050"})
     void feeOver10Under50(int distance, int expectedFee) {
         // given
         Sections sections = new Sections(List.of(new Section(강남역, 양재역, distance, 5)));
@@ -57,7 +56,7 @@ class PathTest {
 
     @DisplayName("50km 초과 거리에서는 기본요금에 8km마다 추가 100원의 요금이 계산된다.")
     @ParameterizedTest(name = "거리={0}")
-    @CsvSource({"51,2150", "58,2150", "66,2250"})
+    @CsvSource({"51,2150", "58,2150", "59,2250", "66,2250"})
     void feeOver50(int distance, int expectedFee) {
         // given
         Sections sections = new Sections(List.of(new Section(강남역, 양재역, distance, 5)));
