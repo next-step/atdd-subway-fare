3319e0a189a8f1a82db0954b88ffd6e9f17c9c8e
charlotte.chk <charlotte.chk@kakaocorp.com>
2023-08-14 19:12:56 +0900

    refactor: 요금정책 추상화 및 매직 넘버 상수화

--- src/main/java/nextstep/subway/domain/Price.java
+++ src/main/java/nextstep/subway/domain/Price.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class Price {
+    public static final int DEFAULT_PRICE = 1250;
     private final int price;
 
     private Price(int price) {
@@ -11,22 +12,16 @@ public class Price {
     }
 
     public static Price calculate(int distance) {
-        int price = 1250;
-        if (distance <= 10) {
-            return new Price(price);
+        int price = DEFAULT_PRICE;
+        int total = distance;
+
+        for (PathPolicy policy : PathPolicy.values()) {
+            if(policy.inRange(total)) {
+                price += policy.calculateOverFare(total);
+                total = policy.remainDistance(total);
+            }
         }
-        if (distance <= 50) {
-            return new Price(price + getOverFareFirst(distance));
-        }
-        return new Price(price + getOverFareFirst(50) + getOverFareSecond(distance));
-    }
 
-
-    private static int getOverFareFirst(int distance) {
-        return (int) Math.ceil((double) (distance - 10) / 5) * 100;
-    }
-
-    private static int getOverFareSecond(double distance) {
-        return (int) Math.ceil((double) (distance - 50) / 8) * 100;
+        return new Price(price);
     }
 }
