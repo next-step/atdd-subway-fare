cf48790653e732864c9093434074aff0a5e4b1c3
deserve82 <inth8204@gmail.com>
2023-08-15 12:07:48 +0900

    refactor : final 추가

--- src/main/java/nextstep/subway/domain/Fare.java
+++ src/main/java/nextstep/subway/domain/Fare.java
@@ -2,13 +2,13 @@ package nextstep.subway.domain;
 
 public class Fare {
 
-    private static int DEFAULT_FARE = 1250;
-    private static int BASIC_DISTANCE = 10;
-    private static int MID_RANGE = 10;
-    private static int MID_RANGE_PER_DISTANCE = 5;
-    private static int BIG_RANGE = 50;
-    private static int BIG_RANGE_PER_DISTANCE = 8;
-    private static int ADDITONAL_FEE_PER_DISTANCE = 100;
+    private static final int DEFAULT_FARE = 1250;
+    private static final int BASIC_DISTANCE = 10;
+    private static final int MID_RANGE = 10;
+    private static final int MID_RANGE_PER_DISTANCE = 5;
+    private static final int BIG_RANGE = 50;
+    private static final int BIG_RANGE_PER_DISTANCE = 8;
+    private static final int ADDITIONAL_FEE_PER_DISTANCE = 100;
 
     public int getFareByDistance(int distance) {
 
@@ -24,6 +24,6 @@ public class Fare {
     }
 
     private int calculateOverFare(int distance, int perDistance) {
-        return (((distance - BASIC_DISTANCE - 1) / perDistance) + 1) * ADDITONAL_FEE_PER_DISTANCE;
+        return (((distance - BASIC_DISTANCE - 1) / perDistance) + 1) * ADDITIONAL_FEE_PER_DISTANCE;
     }
 }
