8f27998be98af30f87e484ad88a62044852607a5
deserve82 <inth8204@gmail.com>
2023-08-15 12:06:33 +0900

    feat : 요금 객체 추가

--- src/main/java/nextstep/subway/domain/Path.java
+++ src/main/java/nextstep/subway/domain/Path.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 public class Path {
     private Sections sections;
+    private Fare fare;
 
     public Path(Sections sections) {
         this.sections = sections;
+        this.fare = new Fare();
     }
 
     public Sections getSections() {
@@ -23,5 +25,9 @@ public class Path {
 
     public List<Station> getStations() {
         return sections.getStations();
+    }
+
+    public int getFare() {
+        return fare.getFareByDistance(extractDistance());
     }
 }
