1e07a322f0c4fdfb5be43fc3855a933ca14cde2d
sinb57 <sinb57@naver.com>
2023-08-17 00:52:58 +0900

    test: 운임거리에 따른 추가요금 인수테스트 추가

--- src/test/java/nextstep/api/acceptance/subway/path/FareAcceptanceTest.java
+++ src/test/java/nextstep/api/acceptance/subway/path/FareAcceptanceTest.java
@@ -7,8 +7,10 @@ import static nextstep.api.acceptance.member.MemberSteps.회원_생성_성공;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import nextstep.api.acceptance.AcceptanceTest;
@@ -19,7 +21,7 @@ import nextstep.api.subway.domain.path.PathSelection;
 @DisplayName("지하철 경로 관리 기능")
 class FareAcceptanceTest extends AcceptanceTest {
 
-    private Long 교대역, 강남역, 양재역, 남부터미널역, 광교역;
+    private Long 교대역, 강남역, 양재역;
 
     @Override
     @BeforeEach
@@ -28,8 +30,49 @@ class FareAcceptanceTest extends AcceptanceTest {
         교대역 = StationSteps.지하철역_생성_성공("교대역").getId();
         강남역 = StationSteps.지하철역_생성_성공("강남역").getId();
         양재역 = StationSteps.지하철역_생성_성공("양재역").getId();
-        남부터미널역 = StationSteps.지하철역_생성_성공("남부터미널역").getId();
-        광교역 = StationSteps.지하철역_생성_성공("광교역").getId();
+    }
+
+    @Nested
+    class 운임거리에_따른_추가요금을_적용한다 {
+
+        @ParameterizedTest
+        @CsvSource(value = {"0,0", "1,1250", "10,1250"})
+        void 거리가_10km_이하인_경우(final int distance, final long expectedFare) {
+            // given
+            LineSteps.지하철노선_생성_성공(교대역, 강남역, distance, 10, 0);
+
+            // when
+            final var response = PathSteps.최단경로조회_성공(교대역, 강남역, PathSelection.DISTANCE.name());
+
+            // then
+            assertThat(response.getFare()).isEqualTo(expectedFare);
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {"11,1350", "15,1350", "16,1450", "50,2050"})
+        void 거리가_10km_초과_50km_이하인_경우(final int distance, final long expectedFare) {
+            // given
+            LineSteps.지하철노선_생성_성공(교대역, 강남역, distance, 10, 0);
+
+            // when
+            final var response = PathSteps.최단경로조회_성공(교대역, 강남역, PathSelection.DISTANCE.name());
+
+            // then
+            assertThat(response.getFare()).isEqualTo(expectedFare);
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {"51,2150", "58,2150", "59,2250"})
+        void 거리가_50km_초과인_경우(final int distance, final long expectedFare) {
+            // given
+            LineSteps.지하철노선_생성_성공(교대역, 강남역, distance, 10, 0);
+
+            // when
+            final var response = PathSteps.최단경로조회_성공(교대역, 강남역, PathSelection.DISTANCE.name());
+
+            // then
+            assertThat(response.getFare()).isEqualTo(expectedFare);
+        }
     }
 
     @Test
