ccc4d8b16607a2f932efc543a018697f86f3e5ef
deserve82 <inth8204@gmail.com>
2023-08-19 14:37:10 +0900

    test : 50km 이상 일 때에 단위 테스트 추가

--- src/test/java/nextstep/subway/unit/FareTest.java
+++ src/test/java/nextstep/subway/unit/FareTest.java
@@ -10,7 +10,7 @@ class FareTest {
     private final Fare fare = new Fare();
 
     @Test
-    void getFareByDistance() {
+    void getFareByDistanceUnderMidRange() {
         int expectedFare = fare.getFareByDistance(10);
         int expectedFare1 = fare.getFareByDistance(12);
         int expectedFare2 = fare.getFareByDistance(16);
@@ -19,5 +19,14 @@ class FareTest {
         assertThat(expectedFare).isEqualTo(1250);
         assertThat(expectedFare1).isEqualTo(1350);
         assertThat(expectedFare2).isEqualTo(1450);
+    }
+
+    @Test
+    void getFareByDistanceOverMidRange() {
+        int expectedFare = fare.getFareByDistance(51);
+        int expectedFare1 = fare.getFareByDistance(50);
+
+        assertThat(expectedFare).isEqualTo(2150);
+        assertThat(expectedFare1).isEqualTo(2050);
     }
 }
