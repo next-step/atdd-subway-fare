f0a825e8f8622f7263df5c2c69cc13e8e9e9440d
김설영 <kimsy8979@mrblue.com>
2023-08-21 16:41:44 +0900

    feat: 요금 할인 정책 단위 테스트가 통과하도록 기능 구현

--- src/main/java/nextstep/subway/path/domain/Path.java
+++ src/main/java/nextstep/subway/path/domain/Path.java
@@ -2,6 +2,7 @@ package nextstep.subway.path.domain;
 
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Lines;
+import nextstep.subway.path.domain.discount.DiscountPolicy;
 import nextstep.subway.path.domain.fare.distance.DistanceFarePolicies;
 import nextstep.subway.path.domain.fare.line.LineFarePolicy;
 import nextstep.subway.section.domain.Sections;
@@ -35,10 +36,11 @@ public class Path {
         return sections.calculateTotalDuration();
     }
 
-    public int calculateFare(DistanceFarePolicies distanceFarePolicies, LineFarePolicy lineFarePolicy) {
+    public int calculateFare(DistanceFarePolicies distanceFarePolicies, LineFarePolicy lineFarePolicy, DiscountPolicy discountPolicy) {
         int fare = distanceFarePolicies.calculateFare(getTotalDistance());
-        int maxAdditionalFee = lineFarePolicy.calculateAdditionalFee(lines);
+        int maxAdditionalFare = lineFarePolicy.calculateAdditionalFare(lines);
 
-        return fare + maxAdditionalFee;
+        int totalFare = fare + maxAdditionalFare;
+        return discountPolicy.discount(totalFare);
     }
 }
