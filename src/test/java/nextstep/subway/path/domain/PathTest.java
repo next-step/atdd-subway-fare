8caa56830ea5c5b437bb726a1524d621b5be471b
김설영 <kimsy8979@mrblue.com>
2023-08-21 16:41:05 +0900

    test: 요금 할인 정책에 대한 단위 테스트 작성

--- src/test/java/nextstep/subway/path/domain/PathTest.java
+++ src/test/java/nextstep/subway/path/domain/PathTest.java
@@ -2,6 +2,7 @@ package nextstep.subway.path.domain;
 
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Lines;
+import nextstep.subway.path.domain.discount.DiscountPolicy;
 import nextstep.subway.path.domain.fare.distance.DistanceFarePolicies;
 import nextstep.subway.path.domain.fare.distance.LongDistanceFarePolicy;
 import nextstep.subway.path.domain.fare.distance.MiddleDistanceFarePolicy;
@@ -22,11 +23,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PathTest {
     private static final int BASIC_FARE = 1250;
-    private static final int ADDITIONAL_FEE = 900;
+    private static final int ADDITIONAL_FARE = 900;
 
     private Path path;
     private DistanceFarePolicies distanceFarePolicies;
     private LineFarePolicy lineFarePolicy;
+    private DiscountPolicy discountPolicy;
 
     @BeforeEach
     void setUp() {
@@ -34,7 +36,7 @@ class PathTest {
         Section 강남_양재_구간 = new Section(new Station(2L, "강남역"), new Station(3L, "양재역"), 1, 1);
 
         Line 이호선 = new Line("2호선", "green", 0, 교대_강남_구간);
-        Line 신분당선 = new Line("신분당선", "red", ADDITIONAL_FEE, 강남_양재_구간);
+        Line 신분당선 = new Line("신분당선", "red", ADDITIONAL_FARE, 강남_양재_구간);
 
         path = new Path(new Lines(Set.of(이호선, 신분당선)), new Sections(List.of(교대_강남_구간, 강남_양재_구간)));
 
@@ -51,11 +53,37 @@ class PathTest {
 
     @DisplayName("거리가 10km 미만이고, 추가 요금이 900원인 노선이 있을 때 기본요금에 900원이 더해진 값이 반환된다.")
     @Test
-    void additionalFee() {
+    void additionalFare() {
         // when
-        int fare = path.calculateFare(distanceFarePolicies, lineFarePolicy);
+        int fare = path.calculateFare(distanceFarePolicies, lineFarePolicy, discountPolicy);
 
         // then
-        assertThat(fare).isEqualTo(BASIC_FARE + ADDITIONAL_FEE);
+        assertThat(fare).isEqualTo(BASIC_FARE + ADDITIONAL_FARE);
+    }
+
+    @DisplayName("요금 2150원에 청소년 할인 정책이 적용된 금액이 반환된다.")
+    @Test
+    void discountFareTeenager() {
+        // given
+        discountPolicy = totalFare -> (int) ((totalFare - 350) * 0.8);
+
+        // when
+        int fare = path.calculateFare(distanceFarePolicies, lineFarePolicy, discountPolicy);
+
+        // then
+        assertThat(fare).isEqualTo((int) ((BASIC_FARE + ADDITIONAL_FARE - 350) * 0.8));
+    }
+
+    @DisplayName("요금 2150원에 어린이 할인 정책이 적용된 금액이 반환된다.")
+    @Test
+    void discountFareChildren() {
+        // given
+        discountPolicy = totalFare -> (int) ((totalFare - 350) * 0.5);
+
+        // when
+        int fare = path.calculateFare(distanceFarePolicies, lineFarePolicy, discountPolicy);
+
+        // then
+        assertThat(fare).isEqualTo((int) ((BASIC_FARE + ADDITIONAL_FARE - 350) * 0.5));
     }
 }
