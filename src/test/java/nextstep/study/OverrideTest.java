2c139fa55661daebc5bde946855d796100418ee2
SeolYoungKim <nasur4da@gmail.com>
2023-08-29 21:22:16 +0900

    test: Override Test를 통해 구현과 상속 학습

--- src/test/java/nextstep/study/OverrideTest.java
+++ src/test/java/nextstep/study/OverrideTest.java
@@ -1,8 +1,10 @@
 package nextstep.study;
 
+import org.junit.jupiter.api.Test;
+
 public class OverrideTest {
     interface TestPolicy {
-        int calculateFare(TestPath testPath);
+        void calculateFare(TestPath testPath);
     }
 
     static class TestPath {}
@@ -15,11 +17,57 @@ public class OverrideTest {
         }
 
         @Override
-        public int calculateFare(TestPath testPath) {
-            int fare = next.calculateFare(testPath);
-            return afterCalculated(fare);
+        public void calculateFare(TestPath testPath) {
+            next.calculateFare(testPath);
+            afterCalculated();
         }
 
-        abstract protected int afterCalculated(int fare);
+        abstract protected void afterCalculated();
+    }
+
+    static class BasicTestPolicy implements TestPolicy {
+        @Override
+        public void calculateFare(TestPath testPath) {
+            System.out.println("기본 정책의 calculateFare() 메서드 호출");
+        }
+    }
+
+    static class DistanceTestPolicy extends AdditionalPolicy {
+        DistanceTestPolicy(TestPolicy next) {
+            super(next);
+        }
+
+        @Override
+        protected void afterCalculated() {
+            System.out.println("거리 정책의 afterCalculated() 메서드 호출");
+        }
+    }
+
+    static class LineTestPolicy extends AdditionalPolicy {
+        LineTestPolicy(TestPolicy next) {
+            super(next);
+        }
+
+        @Override
+        protected void afterCalculated() {
+            System.out.println("노선 정책의 afterCalculated() 메서드 호출");
+        }
+    }
+
+    static class DiscountTestPolicy extends AdditionalPolicy {
+        DiscountTestPolicy(TestPolicy next) {
+            super(next);
+        }
+
+        @Override
+        protected void afterCalculated() {
+            System.out.println("할인 정책의 afterCalculated() 메서드 호출");
+        }
+    }
+
+    @Test
+    void test() {
+        TestPolicy testPolicy = new DiscountTestPolicy(new LineTestPolicy(new BasicTestPolicy()));
+        testPolicy.calculateFare(new TestPath());
     }
 }
