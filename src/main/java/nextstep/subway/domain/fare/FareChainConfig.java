6c62593ba0c551a269f1d731e5a60a3ffb46e20c
deserve82 <inth8204@gmail.com>
2023-09-02 20:34:38 +0900

    fix : chain config 이름 변경

--- src/main/java/nextstep/subway/domain/fare/FareChainConfig.java
+++ src/main/java/nextstep/subway/domain/fare/FareChainConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class FareChainCalculator {
+public class FareChainConfig {
 
     @Bean
     public FareChain defaultFareChain() {
