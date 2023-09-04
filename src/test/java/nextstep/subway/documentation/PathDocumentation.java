e1981cdd74f0bb4897aa213978542966cf87ce09
hellonayeon <hellonykwon@gmail.com>
2023-09-04 22:25:39 +0900

    Test: RestDocs 를 생성하기 위한 경로 조회 요청 PathDocumentationSteps 구현

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -1,19 +1,36 @@
 package nextstep.subway.documentation;
 
+import static nextstep.subway.documentation.PathDocumentationSteps.PathInformation;
+import static nextstep.subway.documentation.PathDocumentationSteps.경로_조회_요청_문서_데이터_생성;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
 public class PathDocumentation extends Documentation {
 
     @Test
-    void path() {
+    @DisplayName("[성공] 경로를 조회 요청 문서")
+    void 경로_조회_요청_문서() {
+        PathInformation 경로_정보 = 경로_조회_요청_문서_데이터_생성();
+        Long 교대역 = 경로_정보.출발역;
+        Long 양재역 = 경로_정보.도착역;
+
         RestAssured
-            .given().log().all()
+            .given(spec).log().all()
+            .filter(document("path",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint())))
             .accept(MediaType.APPLICATION_JSON_VALUE)
-            .queryParam("source", 1L)
-            .queryParam("target", 2L)
+            .queryParam("source", 교대역)
+            .queryParam("target", 양재역)
             .when().get("/paths")
             .then().log().all().extract();
     }
+
 }
