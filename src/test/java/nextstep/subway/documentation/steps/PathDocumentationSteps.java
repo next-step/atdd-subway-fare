bc5f87af2a963f4d0764a2c27376fa0f3e52b7b5
hellonayeon <hellonykwon@gmail.com>
2023-09-12 00:03:06 +0900

    Fix: 경로 조회 요청 API 문서 Fixture 거리(distance) 값 수정 - 새로 추가되는 구간의 거리는 기존 거리보다 클 수 없다는 요구사항 반영

--- src/test/java/nextstep/subway/documentation/steps/PathDocumentationSteps.java
+++ src/test/java/nextstep/subway/documentation/steps/PathDocumentationSteps.java
@@ -21,11 +21,11 @@ public class PathDocumentationSteps {
         Long 양재역 = 지하철역_생성_요청(new StationRequest("양재역")).as(StationResponse.class).getId();
         Long 남부터미널역 = 지하철역_생성_요청(new StationRequest("남부터미널역")).as(StationResponse.class).getId();
 
-        Long 삼호선 = 노선_생성_요청(new LineRequest("삼호선", "bg-orange-600", 교대역, 양재역, 5L, 5))
+        Long 삼호선 = 노선_생성_요청(new LineRequest("삼호선", "bg-orange-600", 교대역, 양재역, 30L, 5))
             .as(LineResponse.class).getId();
-        구간_생성_요청(삼호선, new SectionRequest(남부터미널역, 양재역, 2L, 10));
+        구간_생성_요청(삼호선, new SectionRequest(남부터미널역, 양재역, 10L, 10));
 
-        return new PathInformation(교대역, 양재역);
+        return new PathInformation(남부터미널역, 양재역);
     }
 
     public static class PathInformation {
