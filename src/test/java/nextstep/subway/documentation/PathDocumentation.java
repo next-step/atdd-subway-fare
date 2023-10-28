6ba798bada91c7d2a5d02621a0c77bc03b3a1ea4
hoon25 <cchoon95@hanmail.net>
2023-10-28 15:55:32 +0900

    feat(adoc): Spring Rest Doc 뼈대 생성

--- src/test/java/nextstep/subway/documentation/PathDocumentation.java
+++ src/test/java/nextstep/subway/documentation/PathDocumentation.java
@@ -4,12 +4,19 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+
 public class PathDocumentation extends Documentation {
 
     @Test
     void path() {
         RestAssured
-                .given().log().all()
+                .given(spec).log().all()
+                .filter(document("path",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())))
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("source", 1L)
                 .queryParam("target", 2L)
