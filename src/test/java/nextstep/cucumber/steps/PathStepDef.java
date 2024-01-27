087f62512572e793a9ed7ef1aac29e73ba427656
bactoria <bactoria@gmail.com>
2024-01-27 17:55:58 +0900

    Given 절을 추가하기

--- src/test/java/nextstep/cucumber/steps/PathStepDef.java
+++ src/test/java/nextstep/cucumber/steps/PathStepDef.java
@@ -1,10 +1,15 @@
 package nextstep.cucumber.steps;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java8.En;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static nextstep.subway.acceptance.LineSteps.*;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
@@ -12,38 +17,54 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PathStepDef implements En {
 
-    private Long 교대역;
-    private Long 강남역;
-    private Long 양재역;
-    private Long 남부터미널역;
-    private Long 이호선;
-    private Long 신분당선;
-    private Long 삼호선;
-
+    private Map<String, Long> stations = new HashMap<>();
+    private Map<String, Long> lines = new HashMap<>();
     ExtractableResponse<Response> response;
 
     public PathStepDef() {
-        교대역 = 지하철역_생성_요청("교대역").jsonPath().getLong("id");
-        강남역 = 지하철역_생성_요청("강남역").jsonPath().getLong("id");
-        양재역 = 지하철역_생성_요청("양재역").jsonPath().getLong("id");
-        남부터미널역 = 지하철역_생성_요청("남부터미널역").jsonPath().getLong("id");
+        Given("지하철 역들을 생성 요청하고", (DataTable table) -> {
+            List<Map<String, String>> maps = table.asMaps();
+            for (Map<String, String> map : maps) {
+                String name = map.get("name");
+                Long stationId = 지하철역_생성_요청(name).jsonPath().getLong("id");
+                stations.put(name, stationId);
+            }
+        });
 
-        이호선 = 지하철_노선_생성_요청("2호선", "green", 교대역, 강남역, 10);
-        신분당선 = 지하철_노선_생성_요청("신분당선", "red", 강남역, 양재역, 10);
-        삼호선 = 지하철_노선_생성_요청("3호선", "orange", 교대역, 남부터미널역, 2);
+        Given("지하철 노선들을 생성 요청하고", (DataTable table) -> {
+            for (Map<String, String> map : table.asMaps()) {
+                String name = map.get("name");
+                String color = map.get("color");
+                String upStationName = map.get("upStationName");
+                String downStationName = map.get("downStationName");
+                int distance = Integer.parseInt(map.get("distance"));
 
-        지하철_노선에_지하철_구간_생성_요청(삼호선, createSectionCreateParams(남부터미널역, 양재역, 3));
+                Long lineId = 지하철_노선_생성_요청(name, color, stations.get(upStationName), stations.get(downStationName), distance);
+                lines.put(name, lineId);
+            }
+        });
+
+        Given("지하철 노선에 지하철 구간 생성 요청하고", (DataTable table) -> {
+            for (Map<String, String> map : table.asMaps()) {
+                String name = map.get("name");
+                String upStationName = map.get("upStationName");
+                String downStationName = map.get("downStationName");
+                int distance = Integer.parseInt(map.get("distance"));
+
+                지하철_노선에_지하철_구간_생성_요청(lines.get(name), createSectionCreateParams(stations.get(upStationName), stations.get(downStationName), distance));
+            }
+        });
 
         When("두 역의 최단 거리 경로를 조회하면", () -> {
             response = RestAssured
                     .given().log().all()
                     .accept(MediaType.APPLICATION_JSON_VALUE)
-                    .when().get("/paths?source={sourceId}&target={targetId}", 교대역, 양재역)
+                    .when().get("/paths?source={sourceId}&target={targetId}", stations.get("교대역"), stations.get("양재역"))
                     .then().log().all().extract();
         });
 
         Then("최단 거리 경로를 응답 받는다", () -> {
-            assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(교대역, 남부터미널역, 양재역);
+            assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(stations.get("교대역"), stations.get("남부터미널역"), stations.get("양재역"));
         });
     }
 
