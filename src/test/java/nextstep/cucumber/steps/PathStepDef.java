d9664cd64033e9ff4d122e774200ff2fc48753e7
Jongmin Yoon <jongmin4943@gmail.com>
2024-02-23 01:11:38 +0900

    fix: Cucumber 최단 시간 경로 조회 테스트 추가

--- src/test/java/nextstep/cucumber/steps/PathStepDef.java
+++ src/test/java/nextstep/cucumber/steps/PathStepDef.java
@@ -16,16 +16,22 @@ public class PathStepDef implements En {
     private AcceptanceContext context;
 
     public PathStepDef() {
-        Given("{string}과 {string}의 경로를 조회하면", (final String source, final String target) -> {
+        Given("{string}과 {string}의 최단 거리 경로를 조회하면", (final String source, final String target) -> {
             final Long sourceId = context.store.get(source, StationResponse.class).getId();
             final Long targetId = context.store.get(target, StationResponse.class).getId();
             context.response = PathApiHelper.findPath(sourceId, targetId, PathType.DISTANCE);
         });
-        Then("{string} 경로와 거리 {int}이 조회된다", (final String pathString, final Integer distance) -> {
+        Given("{string}과 {string}의 최단 시간 경로를 조회하면", (final String source, final String target) -> {
+            final Long sourceId = context.store.get(source, StationResponse.class).getId();
+            final Long targetId = context.store.get(target, StationResponse.class).getId();
+            context.response = PathApiHelper.findPath(sourceId, targetId, PathType.DURATION);
+        });
+        Then("{string} 경로와 거리 {int}, 소요시간 {int}이 조회된다", (final String pathString, final Integer distance, final Integer duration) -> {
             final List<String> split = List.of(pathString.split(","));
             assertSoftly(softly -> {
                 softly.assertThat(context.response.jsonPath().getList("stations.name", String.class)).containsExactly(split.toArray(new String[0]));
                 softly.assertThat(context.response.jsonPath().getInt("distance")).isEqualTo(distance);
+                softly.assertThat(context.response.jsonPath().getInt("duration")).isEqualTo(duration);
             });
         });
     }
--- src/test/resources/features/path.feature
+++ src/test/resources/features/path.feature
@@ -13,14 +13,19 @@ Feature: 지하철역 경로 찾기 기능
       | 양재역    |
       | 남부터미널역 |
     And 지하철 노선들을 생성 요청하고
-      | name | color  | upStation | downStation | distance |
-      | 2호선  | green  | 교대역       | 강남역         | 5       |
-      | 신분당선 | red    | 강남역       | 양재역         | 10       |
-      | 3호선  | orange | 교대역       | 남부터미널역      | 2        |
+      | name | color  | upStation | downStation | distance | duration |
+      | 2호선  | green  | 교대역       | 강남역         | 5        | 4        |
+      | 신분당선 | red    | 강남역       | 양재역         | 10       | 4        |
+      | 3호선  | orange | 교대역       | 남부터미널역      | 2        | 3        |
     And 지하철 구간을 등록 요청하고
-      | lineName | upStation | downStation | distance |
-      | 3호선      | 남부터미널역    | 양재역         | 3        |
+      | lineName | upStation | downStation | distance | duration |
+      | 3호선      | 남부터미널역    | 양재역         | 3        | 2        |
 
   Scenario: 두 역의 최단 거리 경로를 조회한다
-    When "강남역"과 "남부터미널역"의 경로를 조회하면
-    Then "강남역,교대역,남부터미널역" 경로와 거리 7이 조회된다
+    When "강남역"과 "남부터미널역"의 최단 거리 경로를 조회하면
+    Then "강남역,교대역,남부터미널역" 경로와 거리 7, 소요시간 7이 조회된다
+
+  Scenario: 두 역의 최단 시간 경로를 조회한다
+    When "강남역"과 "남부터미널역"의 최단 시간 경로를 조회하면
+    Then "강남역,양재역,남부터미널역" 경로와 거리 13, 소요시간 6이 조회된다
+
