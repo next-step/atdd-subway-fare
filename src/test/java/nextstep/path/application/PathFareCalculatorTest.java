866bb8c7fc38b8fd3e28cbbf4189f474d1236bdb
Jongmin Yoon <jongmin4943@gmail.com>
2024-02-24 20:47:54 +0900

    fix: CsvSource 를 통해 증가되는 경계 구간 테스트 추가

--- src/test/java/nextstep/path/application/PathFareCalculatorTest.java
+++ src/test/java/nextstep/path/application/PathFareCalculatorTest.java
@@ -1,18 +1,29 @@
 package nextstep.path.application;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PathFareCalculatorTest {
 
-    @Test
-    @DisplayName("거리로 요금을 계산할 수 있다.")
-    void calculateFareTest() {
+    @ParameterizedTest
+    @CsvSource(value =
+            {
+                    "10,1250",
+                    "15,1350",
+                    "20,1450",
+                    "45,1950",
+                    "50,2050",
+                    "58,2150",
+                    "66,2250"
+            }, delimiterString = ",")
+    @DisplayName("거리가 대비 요금을 계산할 수 있다.")
+    void calculateFareTest(final int distance, final long expected) {
         final PathFareCalculator pathFareCalculator = new PathFareCalculator();
-        final long fare = pathFareCalculator.calculate(9);
+        final long fare = pathFareCalculator.calculate(distance);
 
-        assertThat(fare).isEqualTo(1250L);
+        assertThat(fare).isEqualTo(expected);
     }
 }
