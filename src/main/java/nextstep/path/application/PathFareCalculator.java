f08b87a783c34953c96663ef1cac4e6fc1db419f
Jongmin Yoon <jongmin4943@gmail.com>
2024-02-24 20:51:56 +0900

    fix: `PathFareCalculator` 구간별 조건 추가

--- src/main/java/nextstep/path/application/PathFareCalculator.java
+++ src/main/java/nextstep/path/application/PathFareCalculator.java
@@ -1,11 +1,33 @@
 package nextstep.path.application;
 
 public class PathFareCalculator {
-    public long calculate(final int distance) {
-        // do something;
 
-        return 1250L;
+    public static final long DEFAULT_FARE = 1250L;
+    public static final long EXTRA_FARE_BASE = 100L;
+    public static final int FIRST_STANDARD_DISTANCE = 10;
+    public static final int FIRST_FARE_INTERVAL = 5;
+    public static final int SECOND_STANDARD_DISTANCE = 50;
+    public static final int SECOND_FARE_INTERVAL = 8;
+
+    public long calculate(final int distance) {
+        if (distance <= FIRST_STANDARD_DISTANCE) {
+            return DEFAULT_FARE;
+        }
+
+        int firsDistanceExtraFare = 0;
+        if (distance <= SECOND_STANDARD_DISTANCE) {
+            firsDistanceExtraFare += calculateOverFare(distance - FIRST_STANDARD_DISTANCE, FIRST_FARE_INTERVAL);
+        }
+
+        if (SECOND_STANDARD_DISTANCE < distance) {
+            firsDistanceExtraFare += calculateOverFare(SECOND_STANDARD_DISTANCE - FIRST_STANDARD_DISTANCE, FIRST_FARE_INTERVAL);
+            firsDistanceExtraFare += calculateOverFare(distance - SECOND_STANDARD_DISTANCE, SECOND_FARE_INTERVAL);
+        }
+
+        return DEFAULT_FARE + firsDistanceExtraFare;
     }
 
-
+    private int calculateOverFare(final int distance, final int intervalDistance) {
+        return (int) ((Math.floor((double) (distance - 1) / intervalDistance) + 1) * EXTRA_FARE_BASE);
+    }
 }
