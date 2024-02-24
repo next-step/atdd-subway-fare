d026edef7e7751a1271db9cb5ade42a367bd5867
Jongmin Yoon <jongmin4943@gmail.com>
2024-02-24 22:23:28 +0900

    fix: 요금 계산 구간별 책임연쇄패턴 적용

---
+++ src/main/java/nextstep/path/application/FirstExtraFareHandler.java
@@ -0,0 +1,29 @@
+package nextstep.path.application;
+
+public class FirstExtraFareHandler extends PathFareHandler {
+
+    @Override
+    public long calculate(final int distance) {
+        final int targetDistance = getTargetDistance(distance);
+
+        int extraFare = 0;
+        if (targetDistance > getStandardDistance()) {
+            extraFare += calculateOverFare(targetDistance - getStandardDistance());
+        }
+
+        return extraFare + calculateNext(distance);
+    }
+
+    @Override
+    protected int getStandardDistance() {
+        return 10;
+    }
+
+    @Override
+    protected int getStandardInterval() {
+        return 5;
+    }
+
+}
+
+
--- src/main/java/nextstep/path/application/PathFareCalculator.java
+++ src/main/java/nextstep/path/application/PathFareCalculator.java
@@ -2,32 +2,15 @@ package nextstep.path.application;
 
 public class PathFareCalculator {
 
-    public static final long DEFAULT_FARE = 1250L;
-    public static final long EXTRA_FARE_BASE = 100L;
-    public static final int FIRST_STANDARD_DISTANCE = 10;
-    public static final int FIRST_FARE_INTERVAL = 5;
-    public static final int SECOND_STANDARD_DISTANCE = 50;
-    public static final int SECOND_FARE_INTERVAL = 8;
+    private final PathFareHandler pathFareCalculator;
 
-    public long calculate(final int distance) {
-        if (distance <= FIRST_STANDARD_DISTANCE) {
-            return DEFAULT_FARE;
-        }
-
-        int firsDistanceExtraFare = 0;
-        if (distance <= SECOND_STANDARD_DISTANCE) {
-            firsDistanceExtraFare += calculateOverFare(distance - FIRST_STANDARD_DISTANCE, FIRST_FARE_INTERVAL);
-        }
-
-        if (SECOND_STANDARD_DISTANCE < distance) {
-            firsDistanceExtraFare += calculateOverFare(SECOND_STANDARD_DISTANCE - FIRST_STANDARD_DISTANCE, FIRST_FARE_INTERVAL);
-            firsDistanceExtraFare += calculateOverFare(distance - SECOND_STANDARD_DISTANCE, SECOND_FARE_INTERVAL);
-        }
-
-        return DEFAULT_FARE + firsDistanceExtraFare;
+    public PathFareCalculator() {
+        this.pathFareCalculator = new BaseFareHandler()
+                .next(new FirstExtraFareHandler())
+                .next(new SecondExtraFareHandler());
     }
 
-    private int calculateOverFare(final int distance, final int intervalDistance) {
-        return (int) ((Math.floor((double) (distance - 1) / intervalDistance) + 1) * EXTRA_FARE_BASE);
+    public long calculate(final int distance) {
+        return pathFareCalculator.calculate(distance);
     }
 }
