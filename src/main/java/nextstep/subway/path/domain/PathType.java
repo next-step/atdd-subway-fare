6edd4a3f70be9a3d2c6e1cc2ccb58b3617fb1178
g0rani <pjw0405@naver.com>
2024-02-24 16:58:51 +0900

    refactor: type Lambda식으로 수정

--- src/main/java/nextstep/subway/path/domain/PathType.java
+++ src/main/java/nextstep/subway/path/domain/PathType.java
@@ -2,14 +2,19 @@ package nextstep.subway.path.domain;
 
 import nextstep.subway.line.section.domain.Section;
 
+import java.util.function.Function;
+
 public enum PathType {
-    DISTANCE,
-    DURATION;
+    DISTANCE(Section::distance),
+    DURATION(Section::duration);
+
+    private final Function<Section, Long> valueExtractor;
+
+    PathType(Function<Section, Long> valueExtractor) {
+        this.valueExtractor = valueExtractor;
+    }
 
     public Long findBy(Section section) {
-        if(this == DISTANCE) {
-            return section.distance();
-        }
-        return section.duration();
+        return valueExtractor.apply(section);
     }
 }
