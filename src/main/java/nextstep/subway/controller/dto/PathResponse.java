0e0826ec16cdfa0a7edd8eeef27208fcf1b5ec2e
jikimee64 <jikimee64@gmail.com>
2024-02-25 00:53:13 +0900

    fix: PathResponse 총 거리와 소요시간 함께 응답할 수 있도록 수정

--- src/main/java/nextstep/subway/controller/dto/PathResponse.java
+++ src/main/java/nextstep/subway/controller/dto/PathResponse.java
@@ -1,18 +1,12 @@
 package nextstep.subway.controller.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
-
 @Getter
 @Builder
-@JsonInclude(NON_NULL)
-@AllArgsConstructor
 public class PathResponse {
     private List<StationResponse> stations;
     private Integer distance;
@@ -21,25 +15,10 @@ public class PathResponse {
     public PathResponse() {
     }
 
-    public static PathResponse distanceOf(List<StationResponse> stations, int distance) {
-        return PathResponse.builder()
-                .stations(stations)
-                .distance(distance)
-                .build();
-    }
-
-    public static PathResponse durationOf(List<StationResponse> stations, int duration) {
-        return PathResponse.builder()
-                .stations(stations)
-                .duration(duration)
-                .build();
-    }
-
-    public static PathResponse otherOf(List<StationResponse> stations) {
-        return PathResponse.builder()
-                .stations(stations)
-                .duration(0)
-                .build();
+    public PathResponse(List<StationResponse> stations, Integer distance, Integer duration) {
+        this.stations = stations;
+        this.distance = distance;
+        this.duration = duration;
     }
 
 }
