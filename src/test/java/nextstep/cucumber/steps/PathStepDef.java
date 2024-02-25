357569ba96144d0ef19b291476099f84596816df
renechoi <115696395+renechoi@users.noreply.github.com>
2024-02-25 13:39:23 +0900

    feat: implement shortest path case tests

--- src/test/java/nextstep/cucumber/steps/PathStepDef.java
+++ src/test/java/nextstep/cucumber/steps/PathStepDef.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 
 import io.cucumber.datatable.DataTable;
+import io.cucumber.java.Scenario;
 import io.cucumber.java8.En;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
@@ -32,10 +33,14 @@ public class PathStepDef implements En {
 		And("지하철 노선에 지하철역이 등록되어있음", this::createSectionsWithDuration);
 
 		When("역ID {long}에서 역ID {long}까지의 최소 시간 경로를 조회하면", (Long sourceId, Long targetId) -> response = executeFindPathRequest(sourceId, targetId, "DURATION"));
+		When("역ID {long}에서 역ID {long}까지의 최단 거리 경로를 조회하면", (Long sourceId, Long targetId) -> response = executeFindPathRequest(sourceId, targetId, "DISTANCE"));
 
 		Then("최소 시간 기준 경로를 응답", this::verifyMinimumTimePath);
+		Then("최단 거리 기준 경로를 응답", this::verifyMinimumDistancePath);
 		And("총 거리와 소요 시간을 함께 응답함", this::verifyTotalDistanceWithTotalDuration);
+
 	}
+
 
 	private void verifyTotalDistanceWithTotalDuration(DataTable expectedPathTable) {
 		List<Map<String, String>> expectedPath = expectedPathTable.asMaps(String.class, String.class);
@@ -48,6 +53,14 @@ public class PathStepDef implements En {
 	}
 
 	private void verifyMinimumTimePath(DataTable expectedPathTable) {
+		List<Map<String, String>> expectedPath = expectedPathTable.asMaps(String.class, String.class);
+		List<String> expectedStationNames = Arrays.asList(expectedPath.get(0).get("stationNames").split(", "));
+
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+		assertThat(parseStations(response)).extracting("name").containsExactlyElementsOf(expectedStationNames);
+	}
+
+	private void verifyMinimumDistancePath(DataTable expectedPathTable) {
 		List<Map<String, String>> expectedPath = expectedPathTable.asMaps(String.class, String.class);
 		List<String> expectedStationNames = Arrays.asList(expectedPath.get(0).get("stationNames").split(", "));
 
