b22fd09cf706649a1d3cf36159a471bd51cfcafb
g0rani <pjw0405@naver.com>
2024-02-25 23:16:09 +0900

    test: 10 ~ 50 구간 운임 요금 테스트 코드 작성

--- src/test/java/nextstep/subway/path/domain/PathTest.java
+++ src/test/java/nextstep/subway/path/domain/PathTest.java
@@ -9,9 +9,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PathTest {
 
-    @CsvSource({"1, 1250", "9, 1250"})
+    @CsvSource({"1, 1250", "10, 1250"})
     @ParameterizedTest(name = "10㎞ 이내면 1Km 마다 기본운임(1,250원) 이 부과된다. (거리: {0}, 요금: {1} )")
     void calculateDefaultFare(Long distance, Long fare) {
+        Path path = new Path(Collections.emptyList(), distance, 1L);
+
+        Long actual = path.fare();
+        assertThat(actual).isEqualTo(fare);
+    }
+
+    @CsvSource({"11, 1350", "50, 2150"})
+    @ParameterizedTest(name = "10km 초과 ∼ 50km 까지 5km 마다 추가운임(100원) 부과된다. (거리: {0}, 요금: {1} )")
+    void calculate10To50tFare(Long distance, Long fare) {
         Path path = new Path(Collections.emptyList(), distance, 1L);
 
         Long actual = path.fare();
