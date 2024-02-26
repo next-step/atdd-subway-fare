83e8ffd1c09d1b546909b0906b7008fc70518af2
jikimee64 <jikimee64@gmail.com>
2024-02-26 23:04:53 +0900

    feat: Over10kmFareHandler 핸들러 구현

--- src/main/java/nextstep/subway/domain/chain/Over10kmFareHandler.java
+++ src/main/java/nextstep/subway/domain/chain/Over10kmFareHandler.java
@@ -3,9 +3,13 @@ package nextstep.subway.domain.chain;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import static nextstep.subway.domain.chain.BasicFareHandler.BASIC_DISTANCE;
+
 @Order(1)
 @Component
 public class Over10kmFareHandler implements FareHandler {
+    private static final long START_DISTANCE = 10;
+    private static final long END_DISTANCE = 50;
 
     private FareHandler nextHandler;
 
@@ -15,8 +19,27 @@ public class Over10kmFareHandler implements FareHandler {
     }
 
     @Override
-    public long calculate(long duration) {
-        return 0;
+    public long calculate(long distance) {
+        long addedFare = addedFare(distance);
+        return addedFare + nextCalculate(nextHandler, distance);
+    }
+
+    private long addedFare(long distance) {
+        if (distance > START_DISTANCE) {
+            return calculateFareForDistance(distance);
+        }
+        return 0L;
+    }
+
+    private long calculateFareForDistance(long distance) {
+        if (distance <= END_DISTANCE) {
+            return calculateOverFare(distance - BASIC_DISTANCE);
+        }
+        return calculateOverFare(END_DISTANCE - BASIC_DISTANCE);
+    }
+
+    private long calculateOverFare(long distance) {
+        return (long) ((Math.ceil((distance - 1) / 5) + 1) * 100);
     }
 
 }
