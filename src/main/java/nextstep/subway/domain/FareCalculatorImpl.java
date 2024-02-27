037a56474b32558a62931fd6ca95e71b8bff4c33
Lee seung chan <dltmdcks702@gmail.com>
2024-02-28 05:32:20 +0900

    fix: calculate correctly

--- src/main/java/nextstep/subway/domain/FareCalculatorImpl.java
+++ src/main/java/nextstep/subway/domain/FareCalculatorImpl.java
@@ -9,14 +9,17 @@ public class FareCalculatorImpl implements FareCalculator {
     private static final String DISTANCE_OVER = "distanceOver";
     private static final String DISTANCE_UNDER = "distanceUnder";
     private static final String FARE = "fare";
+    private static final String STEP = "step";
+
+    private static final int DEFAULT_FARE = 1250;
 
     private final List<Map<String, Integer>> fareTable;
 
     public FareCalculatorImpl() {
         this(List.of(
-            Map.of(DISTANCE_OVER, 0, DISTANCE_UNDER, 10, FARE, 1250),
-            Map.of(DISTANCE_OVER, 10, DISTANCE_UNDER, 50, FARE, 100),
-            Map.of( DISTANCE_OVER, 50, DISTANCE_UNDER, Integer.MAX_VALUE , FARE, 100)
+            Map.of(DISTANCE_OVER, 0, DISTANCE_UNDER, 10, STEP, 10, FARE, 0),
+            Map.of(DISTANCE_OVER, 10, DISTANCE_UNDER, 50, STEP, 5, FARE, 100),
+            Map.of( DISTANCE_OVER, 50, DISTANCE_UNDER, Integer.MAX_VALUE, STEP, 8, FARE, 100)
         ));
     }
 
@@ -25,22 +28,33 @@ public class FareCalculatorImpl implements FareCalculator {
     }
 
     public int calculateFare(int distance) {
+        int fare = DEFAULT_FARE;
         for (Map<String, Integer> fareMap : fareTable) {
-            Integer distanceOver = fareMap.get(DISTANCE_OVER);
-            Integer distanceUnder = fareMap.get(DISTANCE_UNDER);
-            Integer fare = fareMap.get(FARE);
+            int distanceToCalculate = distance;
+            int distanceOver = fareMap.get(DISTANCE_OVER);
+            int distanceUnder = fareMap.get(DISTANCE_UNDER);
+            int fareValue = fareMap.get(FARE);
+            int step = fareMap.get(STEP);
 
-            if (distance < distanceUnder || distance > distanceOver) {
+            if (distanceToCalculate < distanceOver) {
                 continue;
             }
 
-            return calculateOverFare(distance, distanceUnder, fare);
+            if (distanceToCalculate > distanceUnder) {
+                distanceToCalculate = distanceUnder;
+            }
+
+            fare += calculateOverFare(distanceToCalculate - distanceOver, step, fareValue);
         }
 
-        throw new IllegalArgumentException("요금을 계산할 수 없습니다.");
+        return fare;
     }
 
     private static int calculateOverFare(int distance, int step, int fare) {
-        return (int) (Math.ceil((double) (distance - 1) / step) + 1) * fare;
+        if(distance < 1) {
+            return 0;
+        }
+
+        return (int) Math.ceil((double) distance / step) * fare;
     }
 }
