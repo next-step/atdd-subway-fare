31ee915ad4e1ce1e0cc61379d979e433b3906160
seonje1 <re552@naver.com>
2024-03-02 21:49:32 +0900

    테스트 격리 설정

--- src/test/java/nextstep/utils/DatabaseCleanup.java
+++ src/test/java/nextstep/utils/DatabaseCleanup.java
@@ -11,7 +11,6 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Profile("test")
 @Service
 public class DatabaseCleanup implements InitializingBean {
     @PersistenceContext
