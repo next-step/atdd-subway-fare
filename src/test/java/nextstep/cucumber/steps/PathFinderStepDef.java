3e9c0acbfa9730f5520cecf391a90e4e163b87ef
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-03-02 23:07:30 +0900

    refactor: cucumber step 간 정보를 공유할 객체에 중복되는 메서드 추가
    - refactor: 상기 변경사항 관련 경로 조회 step def 메서드 리팩토링

--- src/test/java/nextstep/cucumber/steps/PathFinderStepDef.java
+++ src/test/java/nextstep/cucumber/steps/PathFinderStepDef.java
@@ -17,73 +17,73 @@ import static nextstep.core.subway.station.step.StationSteps.지하철_역_생�
 public class PathFinderStepDef implements En {
 
     @Autowired
-    AcceptanceContext context;
+    AcceptanceContext 컨텍스트;
 
     public PathFinderStepDef() {
         Given("지하철 노선을 생성하고, 구간을 추가한다.", () -> {
-            context.store.put("교대역", 지하철_역_생성(StationFixture.교대역));
-            context.store.put("강남역", 지하철_역_생성(StationFixture.강남역));
-            context.store.put("양재역", 지하철_역_생성(StationFixture.양재역));
-            context.store.put("남부터미널역", 지하철_역_생성(StationFixture.남부터미널역));
-            context.store.put("정왕역", 지하철_역_생성(StationFixture.정왕역));
-            context.store.put("오이도역", 지하철_역_생성(StationFixture.오이도역));
-            context.store.put("존재하지_않는_역", 999L);
+            컨텍스트.저장소_숫자_추가하기("교대역", 지하철_역_생성(StationFixture.교대역));
+            컨텍스트.저장소_숫자_추가하기("강남역", 지하철_역_생성(StationFixture.강남역));
+            컨텍스트.저장소_숫자_추가하기("양재역", 지하철_역_생성(StationFixture.양재역));
+            컨텍스트.저장소_숫자_추가하기("남부터미널역", 지하철_역_생성(StationFixture.남부터미널역));
+            컨텍스트.저장소_숫자_추가하기("정왕역", 지하철_역_생성(StationFixture.정왕역));
+            컨텍스트.저장소_숫자_추가하기("오이도역", 지하철_역_생성(StationFixture.오이도역));
+            컨텍스트.저장소_숫자_추가하기("존재하지_않는_역", 999L);
 
-            context.store.put("이호선", 지하철_노선_생성(이호선(
-                    (Long) context.store.get("교대역"), (Long) context.store.get("강남역"), 10)));
-            context.store.put("신분당선", 지하철_노선_생성(신분당선(
-                    (Long) context.store.get("강남역"), (Long) context.store.get("양재역"), 10)));
-            context.store.put("삼호선", 지하철_노선_생성(삼호선(
-                    (Long) context.store.get("교대역"), (Long) context.store.get("남부터미널역"), 2)));
-            context.store.put("사호선", 지하철_노선_생성(사호선(
-                    (Long) context.store.get("정왕역"), (Long) context.store.get("오이도역"), 10)));
+            컨텍스트.저장소_숫자_추가하기("이호선", 지하철_노선_생성(이호선(
+                    컨텍스트.저장소_숫자_가져오기("교대역"), 컨텍스트.저장소_숫자_가져오기("강남역"), 10)));
+            컨텍스트.저장소_숫자_추가하기("신분당선", 지하철_노선_생성(신분당선(
+                    컨텍스트.저장소_숫자_가져오기("강남역"), 컨텍스트.저장소_숫자_가져오기("양재역"), 10)));
+            컨텍스트.저장소_숫자_추가하기("삼호선", 지하철_노선_생성(삼호선(
+                    컨텍스트.저장소_숫자_가져오기("교대역"), 컨텍스트.저장소_숫자_가져오기("남부터미널역"), 2)));
+            컨텍스트.저장소_숫자_추가하기("사호선", 지하철_노선_생성(사호선(
+                    컨텍스트.저장소_숫자_가져오기("정왕역"), 컨텍스트.저장소_숫자_가져오기("오이도역"), 10)));
 
             성공하는_지하철_구간_추가요청(
-                    (Long) context.store.get("삼호선"),
-                    지하철_구간((Long) context.store.get("남부터미널역"), (Long) context.store.get("양재역"), 3));
+                    컨텍스트.저장소_숫자_가져오기("삼호선"),
+                    지하철_구간(컨텍스트.저장소_숫자_가져오기("남부터미널역"), 컨텍스트.저장소_숫자_가져오기("양재역"), 3));
         });
 
-        When("출발역과 도착역을 통해 경로를 조회할 경우",  () -> {
-            context.response = 성공하는_지하철_경로_조회_요청(지하철_경로(
-                    (Long) context.store.get("강남역"),
-                    (Long) context.store.get("남부터미널역")));
+        When("출발역과 도착역을 통해 경로를 조회할 경우", () -> {
+            컨텍스트.저장된_응답 = 성공하는_지하철_경로_조회_요청(지하철_경로(
+                    컨텍스트.저장소_숫자_가져오기("강남역"),
+                    컨텍스트.저장소_숫자_가져오기("남부터미널역")));
         });
 
-        When("출발역과 도착역이 동일한 경로를 조회할 경우",  () -> {
-            context.store.put("조회할_구간", new PathFinderRequest(
-                    (Long) context.store.get("강남역"),
-                    (Long) context.store.get("강남역")));
+        When("출발역과 도착역이 동일한 경로를 조회할 경우", () -> {
+            컨텍스트.저장소_정보_추가하기("조회할_구간", 지하철_경로(
+                    컨텍스트.저장소_숫자_가져오기("강남역"),
+                    컨텍스트.저장소_숫자_가져오기("강남역")));
         });
 
-        When("출발역과 도착역이 연결되어 있지 않은 경로를 조회할 경우",  () -> {
-            context.store.put("조회할_구간", new PathFinderRequest(
-                    (Long) context.store.get("강남역"),
-                    (Long) context.store.get("오이도역")));
+        When("출발역과 도착역이 연결되어 있지 않은 경로를 조회할 경우", () -> {
+            컨텍스트.저장소_정보_추가하기("조회할_구간", 지하철_경로(
+                    컨텍스트.저장소_숫자_가져오기("강남역"),
+                    컨텍스트.저장소_숫자_가져오기("오이도역")));
         });
 
-        When("출발역이 존재하지 않는 출발역으로 경로를 조회할 경우",  () -> {
-            context.store.put("조회할_구간", new PathFinderRequest(
-                    (Long) context.store.get("존재하지_않는_역"),
-                    (Long) context.store.get("오이도역")));
+        When("출발역이 존재하지 않는 출발역으로 경로를 조회할 경우", () -> {
+            컨텍스트.저장소_정보_추가하기("조회할_구간", 지하철_경로(
+                    컨텍스트.저장소_숫자_가져오기("존재하지_않는_역"),
+                    컨텍스트.저장소_숫자_가져오기("오이도역")));
         });
 
-        When("도착역이 존재하지 않는 도착역으로 경로를 조회할 경우",  () -> {
-            context.store.put("조회할_구간", new PathFinderRequest(
-                    (Long) context.store.get("오이도역"),
-                    (Long) context.store.get("존재하지_않는_역")));
+        When("도착역이 존재하지 않는 도착역으로 경로를 조회할 경우", () -> {
+            컨텍스트.저장소_정보_추가하기("조회할_구간", 지하철_경로(
+                    컨텍스트.저장소_숫자_가져오기("오이도역"),
+                    컨텍스트.저장소_숫자_가져오기("존재하지_않는_역")));
         });
 
         Then("최단거리의 존재하는 역 목록과 최단거리 값을 확인할 수 있다.", () -> {
-            경로에_포함된_역_목록_검증(context.response,
-                    (Long) context.store.get("강남역"),
-                    (Long) context.store.get("교대역"),
-                    (Long) context.store.get("남부터미널역"));
+            경로에_포함된_역_목록_검증(컨텍스트.저장된_응답,
+                    컨텍스트.저장소_숫자_가져오기("강남역"),
+                    컨텍스트.저장소_숫자_가져오기("교대역"),
+                    컨텍스트.저장소_숫자_가져오기("남부터미널역"));
 
-            경로에_포함된_최단거리_검증(context.response, 12);
+            경로에_포함된_최단거리_검증(컨텍스트.저장된_응답, 12);
         });
 
         Then("최단거리의 존재하는 역 목록과 최단거리 값을 확인할 수 없다.", () -> {
-            실패하는_지하철_경로_조회_요청((PathFinderRequest) context.store.get("조회할_구간"));
+            실패하는_지하철_경로_조회_요청((PathFinderRequest) 컨텍스트.저장소_정보_가져오기("조회할_구간"));
         });
     }
 }
--- src/test/java/nextstep/cucumber/util/AcceptanceContext.java
+++ src/test/java/nextstep/cucumber/util/AcceptanceContext.java
@@ -11,6 +11,22 @@ import java.util.Map;
 @Profile("test")
 @Component
 public class AcceptanceContext {
-    public Map<String, Object> store = new HashMap<>();
-    public ExtractableResponse<Response> response;
+    public Map<String, Object> 저장소 = new HashMap<>();
+    public ExtractableResponse<Response> 저장된_응답;
+
+    public void 저장소_숫자_추가하기(String 키, Long 값) {
+        저장소.put(키, 값);
+    }
+
+    public void 저장소_정보_추가하기(String 키, Object 정보) {
+        저장소.put(키, 정보);
+    }
+
+    public Long 저장소_숫자_가져오기(String 키) {
+        return (Long) 저장소.get(키);
+    }
+
+    public Object 저장소_정보_가져오기(String 키) {
+        return (Object) 저장소.get(키);
+    }
 }
