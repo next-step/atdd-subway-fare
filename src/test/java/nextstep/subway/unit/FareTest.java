a1d3cc692e498fabbbdf6fcde21ce5191502df93
Lee seung chan <dltmdcks702@gmail.com>
2024-03-03 22:01:55 +0900

    test: add calculate option

--- src/test/java/nextstep/subway/unit/FareTest.java
+++ src/test/java/nextstep/subway/unit/FareTest.java
@@ -3,17 +3,17 @@ package nextstep.subway.unit;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import nextstep.subway.domain.fareOption.Fare10KmTo50KmOption;
-import nextstep.subway.domain.fareOption.Fare50KmOverOption;
+import nextstep.subway.domain.fareOption.Fare10KmTo50KmDistanceOption;
+import nextstep.subway.domain.fareOption.Fare50KmOverDistanceOption;
 import nextstep.subway.domain.FareCalculator;
 import nextstep.subway.domain.FareCalculatorImpl;
 import org.junit.jupiter.api.Test;
 
-public class FareDistanceTest {
-    private static FareCalculator fareCalculator = new FareCalculatorImpl(
+public class FareTest {
+    private static FareCalculator fareDistanceCalculator = new FareCalculatorImpl(
         List.of(
-            new Fare10KmTo50KmOption(),
-            new Fare50KmOverOption()
+            new Fare10KmTo50KmDistanceOption(),
+            new Fare50KmOverDistanceOption()
         )
     );
 
@@ -21,7 +21,7 @@ public class FareDistanceTest {
     @Test
     void 요금_계산__10km이내_기본요금() {
         int distance = 10;
-        int fare = fareCalculator.calculateFare(distance);
+        int fare = fareDistanceCalculator.calculateFare(distance, null);
 
         assertThat(fare).isEqualTo(1_250);
     }
@@ -29,7 +29,7 @@ public class FareDistanceTest {
     @Test
     void 요금_계산__10km초과_50km_이내_5km마다_100원_추가_요금() {
         int distance = 50;
-        int fare = fareCalculator.calculateFare(distance);
+        int fare = fareDistanceCalculator.calculateFare(distance, null);
 
         assertThat(fare).isEqualTo(2_050);
     }
@@ -37,7 +37,7 @@ public class FareDistanceTest {
     @Test
     void 요금_계산__50km초과_8km마다_100원_추가_요금() {
         int distance = 100;
-        int fare = fareCalculator.calculateFare(distance);
+        int fare = fareDistanceCalculator.calculateFare(distance, null);
 
         assertThat(fare).isEqualTo(2_750);
     }
