679fc4e162d5b305f8cc73dd9fe702e46a6548fd
LEE-DAYOUNG-SARAH <ldy8536@gmail.com>
2024-03-03 17:02:52 +0900

    path cucumber 수정

--- src/test/java/nextstep/cucumber/steps/PathStepDef.java
+++ src/test/java/nextstep/cucumber/steps/PathStepDef.java
@@ -4,11 +4,13 @@ import io.cucumber.java8.En;
 import io.restassured.RestAssured;
 import nextstep.cucumber.AcceptanceContext;
 import nextstep.subway.dto.path.PathResponse;
+import nextstep.subway.dto.path.PathType;
 import nextstep.subway.dto.station.StationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,36 +19,31 @@ public class PathStepDef implements En {
     private AcceptanceContext context;
 
     public PathStepDef() {
-        Given("{string}과 {string}의 {string}경로를 조회하면", (String source, String target, String requestType) -> {
+        Given("{string}과 {string}의 {string} 경로를 조회하면", (String source, String target, String requestType) -> {
             Long sourceId = ((StationResponse) context.store.get(source)).getId();
             Long targetId = ((StationResponse) context.store.get(target)).getId();
-            String type = requestType.equals("최단") ? "DISTANCE" : "DURATION";
+            PathType type = requestType.equals("최단거리") ? PathType.DISTANCE : PathType.DURATION;
 
             context.response = RestAssured.given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
                 .when().get("/paths?source={sourceId}&target={targetId}&type={type}", sourceId, targetId, type)
                 .then().log().all()
                 .extract();
         });
 
-        Then("최단 거리 기준 경로를 응답", () -> {
-            assertThat(context.response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        });
-
-        Then("최소 시간 기준 경로를 응답", () -> {
-            assertThat(context.response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        });
-
         Then("{string} 경로가 조회된다", (String pathString) -> {
-            List<String> split = List.of(pathString.split(","));
-            assertThat(context.response.jsonPath().getList("stations.name", String.class)).containsExactly(split.toArray(new String[0]));
+            List<String> stations = context.response.as(PathResponse.class).getStations().stream()
+                .map(StationResponse::getName)
+                .collect(Collectors.toList());
+
+            assertThat(stations).containsExactly(pathString.split(","));
         });
 
-        Then("최단거리 {string}가 조회된다", (String distance) -> {
-            assertThat(context.response.as(PathResponse.class).getDistance()).isEqualTo(Integer.parseInt(distance));
-        });
+        Then("총 거리 {int}km와 소요시간 {int}분을 함께 응답한다", (Integer distance, Integer duration) -> {
+            PathResponse response = context.response.as(PathResponse.class);
 
-        Then("최소시간 {string}가 조회된다", (String duration) -> {
-            assertThat(context.response.as(PathResponse.class).getDuration()).isEqualTo(Integer.parseInt(duration));
+            assertThat(response.getDistance()).isEqualTo(distance);
+            assertThat(response.getDuration()).isEqualTo(duration);
         });
     }
 }
--- src/test/resources/features/path.feature
+++ src/test/resources/features/path.feature
@@ -15,21 +15,19 @@ Feature: 지하철 경로 검색
       | 남부터미널역 |
     And 지하철 노선들을 생성 요청하고
       | name | color  | upStation | downStation | distance | duration |
-      | 2호선  | green  | 교대역       | 강남역         | 10       | 4        |
-      | 신분당선 | red    | 강남역       | 양재역         | 10       | 3         |
-      | 3호선  | orange | 교대역       | 남부터미널역      | 2        | 2         |
+      | 2호선  | green  | 교대역       | 강남역         | 10       | 1        |
+      | 신분당선 | red    | 강남역       | 양재역         | 10       | 2         |
+      | 3호선  | orange | 교대역       | 남부터미널역      | 2        | 3         |
     And 지하철 구간을 등록 요청하고
       | lineName | upStation | downStation | distance | duration |
-      | 3호선      | 남부터미널역    | 양재역         | 3        | 1        |
+      | 3호선      | 남부터미널역    | 양재역         | 3        | 4        |
 
-  Scenario: 두 역의 최단 거리 경로를 조회한다
-    When "교대역"과 "양재역"의 "최단"경로를 조회하면
-    Then 최단 거리 기준 경로를 응답
-    And "교대역,남부터미널역,양재역" 경로가 조회된다
-    And 최단거리 "5"가 조회된다
+  Scenario Outline: 두 역의 <type> 경로를 조회한다
+    When <source>과 <target>의 <type> 경로를 조회하면
+    Then <stations> 경로가 조회된다
+    And 총 거리 <distance>km와 소요시간 <duration>분을 함께 응답한다
 
-  Scenario: 두 역의 최소 시간 경로를 조회한다
-    When "교대역"과 "양재역"의 "최소"경로를 조회하면
-    Then 최소 시간 기준 경로를 응답
-    And "교대역,남부터미널역,양재역" 경로가 조회된다
-    And 최소시간 "3"가 조회된다
+    Examples:
+      | type | source | target | stations       | distance | duration |
+      | "최단거리" | "교대역"    | "양재역"    | "교대역,남부터미널역,양재역" | 5 | 7  |
+      | "최소시간" | "교대역"    | "양재역"    | "교대역,강남역,양재역"    | 20  | 3  |
