b3aa8477804708fe05697f659ec9b585909e9da6
gutenLee <3bnm1128@naver.com>
2024-03-04 22:51:23 +0900

    refactor : path 인수테스트 수정

--- src/test/java/nextstep/cucumber/steps/PathStepDef.java
+++ src/test/java/nextstep/cucumber/steps/PathStepDef.java
@@ -3,6 +3,7 @@ package nextstep.cucumber.steps;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.ParameterType;
 import io.cucumber.java8.En;
+import io.cucumber.java8.StepDefinitionBody;
 import nextstep.path.domain.dto.StationDto;
 import nextstep.path.ui.PathType;
 import nextstep.path.ui.PathsResponse;
@@ -43,23 +44,24 @@ public class PathStepDef implements En {
         When("{string}에서 {string}까지의 {string} 기준으로 경로 조회를 요청", (String start, String end, String pathType) -> {
             response = PathSteps.getPath(cxt.stationStore.get(start), cxt.stationStore.get(end), convertPathType(pathType));
         });
-        Then("최소 시간 기준 경로를 응답", () -> {
+        Then("^최소 시간 기준 경로인 \"([^\"]*)\"를 응답$", (String stringPathList) -> {
             List<StationDto> stationsOnPath = response.getStationDtoList();
             List<String> names = stationsOnPath.stream().map(StationDto::getName).collect(Collectors.toList());
 
             Assertions.assertThat(names).containsExactly(
-                    "을지로4가", "동대문역사문화공원", "신당", "상왕십리", "왕십리");
+                    stringPathList.split(","));
         });
-        And("총 거리와 소요 시간을 함께 응답함", () -> {
+        And("총 거리 {int}km와 소요 시간 {int}을 함께 응답함", (Integer expectDistance, Integer expectDuration) -> {
             int duration = response.getDuration();
             int distance = response.getDistance();
-            Assertions.assertThat(duration).isEqualTo(4);
-            Assertions.assertThat(distance).isEqualTo(20);
+            Assertions.assertThat(duration).isEqualTo(expectDuration);
+            Assertions.assertThat(distance).isEqualTo(expectDistance);
         });
-        And("지하철 이용 요금도 함께 응답함", () -> {
-            Assertions.assertThat(response.getFare()).isEqualTo(1450);
+        And("지하철 이용 요금 {int}원도 함께 응답함", (Integer fare) -> {
+            Assertions.assertThat(response.getFare()).isEqualTo(fare);
         });
     }
+
 
     private static PathType convertPathType(String pathType) {
         if ("DURATION".equals(pathType)) {
