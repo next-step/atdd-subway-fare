6f757cbb758c51c8bf85b107cdd23a98826be415
라우라(이유희) <bnm1128@jaranda.kr>
2024-03-05 23:17:40 +0900

    Revert "refactor : path 인수테스트 코드 정리"
    This reverts commit bac0da422d2edbab15f7c0cc09db786d2f096c40.

--- src/test/java/nextstep/cucumber/steps/PathStepDef.java
+++ src/test/java/nextstep/cucumber/steps/PathStepDef.java
@@ -1,18 +1,12 @@
 package nextstep.cucumber.steps;
 
 import io.cucumber.datatable.DataTable;
-import io.cucumber.java.DataTableType;
 import io.cucumber.java.ParameterType;
 import io.cucumber.java8.En;
 import io.cucumber.java8.StepDefinitionBody;
-import nextstep.line.domain.Color;
-import nextstep.line.domain.Line;
-import nextstep.line.ui.LineRequest;
-import nextstep.line.ui.SectionRequest;
 import nextstep.path.domain.dto.StationDto;
 import nextstep.path.ui.PathType;
 import nextstep.path.ui.PathsResponse;
-import nextstep.subway.fixture.LineSteps;
 import nextstep.subway.fixture.PathSteps;
 import org.apache.groovy.util.Maps;
 import org.assertj.core.api.Assertions;
@@ -36,18 +30,16 @@ public class PathStepDef implements En {
             List<Map<String, String>> maps = table.asMaps();
             cxt.setUpStation(maps);
         });
-        And("지하철 노선이 등록되어있음", (DataTable table) -> {
-            List<LineRequest> lineRequests = table.asMaps()
-                    .stream().map(this::lineRequestTransform)
-                    .collect(Collectors.toList());
-            cxt.setUpLine(lineRequests);
+        And("지하철 노선이 등록되어있음", () -> {
+            cxt.setUpLine();
         });
         And("지하철 노선에 지하철역이 등록되어있음", (DataTable table) -> {
-            Map<String, SectionInfo> sectionInfoMap = table.asMaps().stream()
-                    .map(this::sectionRequestTransform)
-                    .collect(Collectors.toMap(s -> s.stationName, s -> s));
-
-            cxt.setUpSection(sectionInfoMap);
+            String 이호선_구간별_duration = table.cell(1, 2);
+            String 오호선_구간별_duration = table.cell(2, 2);
+            Map<String, Integer> durationByLine = Maps.of(
+                    "이호선", Integer.parseInt(이호선_구간별_duration),
+                    "오호선", Integer.parseInt(오호선_구간별_duration));
+            cxt.setUpSection(durationByLine);
         });
         When("{string}에서 {string}까지의 {string} 기준으로 경로 조회를 요청", (String start, String end, String pathType) -> {
             response = PathSteps.getPath(cxt.stationStore.get(start), cxt.stationStore.get(end), convertPathType(pathType));
@@ -68,7 +60,6 @@ public class PathStepDef implements En {
         And("지하철 이용 요금 {int}원도 함께 응답함", (Integer fare) -> {
             Assertions.assertThat(response.getFare()).isEqualTo(fare);
         });
-
     }
 
 
@@ -81,36 +72,4 @@ public class PathStepDef implements En {
         throw new IllegalStateException("PathType enum doesn't have : " + pathType);
     }
 
-    @DataTableType
-    public LineRequest lineRequestTransform(Map<String, String> entry) {
-        return new LineRequest(
-                entry.get("name"),
-                Color.GREEN,
-                cxt.stationStore.get(entry.get("upStation")),
-                cxt.stationStore.get(entry.get("downStation")),
-                Integer.parseInt(entry.get("distance")),
-                Integer.parseInt(entry.get("duration"))
-        );
-    }
-
-    @DataTableType
-    public SectionInfo sectionRequestTransform(Map<String, String> entry) {
-        return new SectionInfo(
-                entry.get("name"),
-                Integer.parseInt(entry.get("distance")),
-                Integer.parseInt(entry.get("duration"))
-        );
-    }
-
-    static class SectionInfo {
-        String stationName;
-        int distance;
-        int duration;
-
-        public SectionInfo(String stationName, int distance, int duration) {
-            this.stationName = stationName;
-            this.distance = distance;
-            this.duration = duration;
-        }
-    }
 }
