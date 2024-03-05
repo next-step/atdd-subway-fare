94aa14725c9bcb9c98fd029afd81578d46fa9572
Lee seung chan <dltmdcks702@gmail.com>
2024-03-05 21:55:16 +0900

    test: add test for login user fare

--- src/test/java/nextstep/cucumber/steps/PathStepDef.java
+++ src/test/java/nextstep/cucumber/steps/PathStepDef.java
@@ -1,8 +1,11 @@
 package nextstep.cucumber.steps;
 
+import static nextstep.member.acceptance.AuthStep.로그인;
+import static nextstep.member.acceptance.MemberSteps.회원_생성_요청;
 import static nextstep.subway.acceptance.LineSteps.지하철_노선_생성_요청;
 import static nextstep.subway.acceptance.LineSteps.지하철_노선에_지하철_구간_생성_요청;
 import static nextstep.subway.acceptance.PathStep.지하철_경로_조회;
+import static nextstep.subway.acceptance.PathStep.지하철_경로_조회__로그인_유저;
 import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
 import static nextstep.utils.ResponseUtils.응답에서_id_조회;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,57 +22,83 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 public class PathStepDef implements En {
+
     @Autowired
     private AcceptanceContext context;
 
     public PathStepDef() {
-         Given("지하철역 생성을 요청하고", (DataTable table) -> {
-             List<Map<String, String>> maps = table.asMaps();
-             maps.forEach(it ->
-                 context.store.put(
-                     it.get("name"),
-                     응답에서_id_조회(지하철역_생성_요청(it.get("name")))
-                 )
-             );
-         });
+        Given("지하철역 생성을 요청하고", (DataTable table) -> {
+            List<Map<String, String>> maps = table.asMaps();
+            maps.forEach(it ->
+                context.store.put(
+                    it.get("name"),
+                    응답에서_id_조회(지하철역_생성_요청(it.get("name")))
+                )
+            );
+        });
 
-         And("지하철 노선 생성을 요청하고", (DataTable table) -> {
-             List<Map<String, String>> maps = table.asMaps();
-             maps.forEach(it -> context.store.put(
-                 it.get("name"),
-                 응답에서_id_조회(
-                     지하철_노선_생성_요청(
-                         it.get("name"),
-                         it.get("color"),
-                         Long.parseLong(context.store.get(it.get("upStation")).toString()),
-                         Long.parseLong(context.store.get(it.get("downStation")).toString()),
-                         Integer.parseInt(it.get("distance")),
-                         Integer.parseInt(it.get("duration")),
-                         Integer.parseInt(it.get("extraFare"))
-                     )
-                 )
-             ));
-         });
+        And("지하철 노선 생성을 요청하고", (DataTable table) -> {
+            List<Map<String, String>> maps = table.asMaps();
+            maps.forEach(it -> context.store.put(
+                it.get("name"),
+                응답에서_id_조회(
+                    지하철_노선_생성_요청(
+                        it.get("name"),
+                        it.get("color"),
+                        Long.parseLong(context.store.get(it.get("upStation")).toString()),
+                        Long.parseLong(context.store.get(it.get("downStation")).toString()),
+                        Integer.parseInt(it.get("distance")),
+                        Integer.parseInt(it.get("duration")),
+                        Integer.parseInt(it.get("extraFare"))
+                    )
+                )
+            ));
+        });
 
-         And("지하철 노선에 구간 생성을 요청한다", (DataTable table) -> {
-             List<Map<String, String>> maps = table.asMaps();
-             maps.forEach(it -> {
-                 지하철_노선에_지하철_구간_생성_요청(
-                     Long.parseLong(context.store.get(it.get("line")).toString()),
-                     createSectionCreateParams(
-                         context.store.get(it.get("upStation")).toString(),
-                         context.store.get(it.get("downStation")).toString(),
-                         it.get("distance"),
-                         it.get("duration")
-                     ));
-             });
-         });
+        And("지하철 노선에 구간 생성을 요청한다", (DataTable table) -> {
+            List<Map<String, String>> maps = table.asMaps();
+            maps.forEach(it -> {
+                지하철_노선에_지하철_구간_생성_요청(
+                    Long.parseLong(context.store.get(it.get("line")).toString()),
+                    createSectionCreateParams(
+                        context.store.get(it.get("upStation")).toString(),
+                        context.store.get(it.get("downStation")).toString(),
+                        it.get("distance"),
+                        it.get("duration")
+                    ));
+            });
+        });
+
+        And("로그인 유저가 존재한다", (DataTable table) -> {
+            List<Map<String, String>> maps = table.asMaps();
+            maps.forEach(it -> {
+                회원_생성_요청(
+                    it.get("email"),
+                    it.get("password"),
+                    Integer.parseInt(it.get("age"))
+                );
+
+                context.store.put(it.get("age"),
+                    로그인(it.get("email"),
+                        it.get("password")).jsonPath().getString("accessToken")
+                );
+            });
+        });
 
         When("{string} 과 {string} 경로를 조회하면,", (String from, String to) -> {
             context.response = 지하철_경로_조회(
                 Long.parseLong(context.store.get(from).toString()),
                 Long.parseLong(context.store.get(to).toString()),
                 PathSearchType.DISTANCE
+            );
+        });
+
+        When("{string} 과 {string} 경로를 {string}살이 조회하면,", (String from, String to, String age) -> {
+            context.response = 지하철_경로_조회__로그인_유저(
+                Long.parseLong(context.store.get(from).toString()),
+                Long.parseLong(context.store.get(to).toString()),
+                PathSearchType.DISTANCE,
+                context.store.get(age).toString()
             );
         });
 
@@ -82,7 +111,8 @@ public class PathStepDef implements En {
         });
 
         Then("{string} 과 {string} 간 경로가 조회 된다.", (String from, String to) -> {
-            final List<Station> stations = context.response.jsonPath().getList("stations", Station.class);
+            final List<Station> stations = context.response.jsonPath()
+                .getList("stations", Station.class);
 
             assertThat(stations.stream().mapToLong(Station::getId)).containsAll(List.of(
                 Long.parseLong(context.store.get(from).toString()),
@@ -91,14 +121,14 @@ public class PathStepDef implements En {
         });
 
         Then("거리는 {int}이다.", (Integer distance) -> {
-           assertThat(context.response.jsonPath().getInt("distance")).isEqualTo(distance);
+            assertThat(context.response.jsonPath().getInt("distance")).isEqualTo(distance);
         });
 
         Then("시간은 {int}이다.", (Integer duration) -> {
             assertThat(context.response.jsonPath().getInt("duration")).isEqualTo(duration);
         });
 
-        Then("요금은 {int}원이다.",(Integer fare) -> {
+        Then("요금은 {int}원이다.", (Integer fare) -> {
             assertThat(context.response.jsonPath().getInt("fare")).isEqualTo(fare);
         });
 
@@ -108,7 +138,8 @@ public class PathStepDef implements En {
     }
 
 
-    private Map<String, String> createSectionCreateParams(String upStationId, String downStationId, String distance, String duration) {
+    private Map<String, String> createSectionCreateParams(String upStationId, String downStationId,
+        String distance, String duration) {
         final Map<String, String> params = new HashMap<>();
 
         params.put("upStationId", upStationId);
--- src/test/java/nextstep/subway/acceptance/PathStep.java
+++ src/test/java/nextstep/subway/acceptance/PathStep.java
@@ -3,12 +3,21 @@ package nextstep.subway.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.Optional;
 import nextstep.subway.domain.PathSearchType;
 
 public class PathStep {
     public static ExtractableResponse<Response> 지하철_경로_조회(Long source, Long target, PathSearchType type) {
         return RestAssured
             .given().log().all()
+            .when().get("/paths?source={source}&target={target}&type={type}", source, target, type)
+            .then().log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 지하철_경로_조회__로그인_유저(Long source, Long target, PathSearchType type, String token) {
+        return RestAssured
+            .given().log().all()
+            .header("Authorization", "Bearer " + token)
             .when().get("/paths?source={source}&target={target}&type={type}", source, target, type)
             .then().log().all().extract();
     }
