bac0da422d2edbab15f7c0cc09db786d2f096c40
라우라(이유희) <bnm1128@jaranda.kr>
2024-03-05 23:16:20 +0900

    refactor : path 인수테스트 코드 정리

--- src/test/java/nextstep/cucumber/steps/PathAcceptanceContext.java
+++ src/test/java/nextstep/cucumber/steps/PathAcceptanceContext.java
@@ -1,9 +1,7 @@
 package nextstep.cucumber.steps;
 
-import nextstep.line.domain.Color;
 import nextstep.line.ui.LineRequest;
 import nextstep.line.ui.SectionRequest;
-import nextstep.line.ui.SectionResponse;
 import nextstep.subway.fixture.LineSteps;
 import nextstep.subway.fixture.SectionSteps;
 import nextstep.subway.fixture.StationSteps;
@@ -18,21 +16,16 @@ public class PathAcceptanceContext {
     public Map<String, Long> stationStore = new HashMap<>();
     public Map<String, Long> lineStore = new HashMap<>();
     List<String> 이호선_역;
-    String 이호선 = "이호선";
-
     List<String> 오호선_역;
+    List<String> 분당선_역;
+    String 이호선 = "이호선";
     String 오호선 = "오호선";
 
-    public void setUpLine() {
-        String start = 이호선_역.get(0);
-        String end = 이호선_역.get(이호선_역.size() - 1);
-        long lineId = LineSteps.createLine(new LineRequest(이호선, Color.GREEN, stationStore.get(start), stationStore.get(end), 50, 10)).getId();
-        lineStore.put(이호선, lineId);
-
-        start = 오호선_역.get(0);
-        end = 오호선_역.get(오호선_역.size() - 1);
-        lineId = LineSteps.createLine(new LineRequest(오호선, Color.GREEN, stationStore.get(start), stationStore.get(end), 90, 100)).getId();
-        lineStore.put(오호선, lineId);
+    public void setUpLine(List<LineRequest> lineRequests){
+        for (LineRequest lineRequest : lineRequests) {
+            Long id = LineSteps.createLine(lineRequest).getId();
+            lineStore.put(lineRequest.getName(), id);
+        }
     }
 
     public void setUpStation(List<Map<String, String>> rows) {
@@ -41,43 +34,43 @@ public class PathAcceptanceContext {
                 .filter(entry -> Objects.nonNull(entry.getValue()))
                 .collect(groupingBy(Map.Entry::getKey,
                         mapping(Map.Entry::getValue, toList())));
-        이호선_역 = staionMap.get("이호선");
-        오호선_역 = staionMap.get("오호선");
 
-        for (String station : 이호선_역) {
-            if(!stationStore.containsKey(station)){
-                Long id = StationSteps.createStation(station).getId();
-                stationStore.put(station, id);
-            }
+        이호선_역.addAll(staionMap.get("이호선"));
+        오호선_역.addAll(staionMap.get("오호선"));
+        분당선_역.addAll(staionMap.get("분당선"));
+        addStationStore(이호선_역);
+        addStationStore(오호선_역);
+        addStationStore(분당선_역);
+    }
+
+    public void setUpSection(Map<String, PathStepDef.SectionInfo> sectionInfos) {
+        createSection(오호선_역, sectionInfos, "오호선");
+        createSection(분당선_역, sectionInfos, "분당선");
+        createSection(이호선_역, sectionInfos, "이호선");
+    }
+
+    private void createSection(List<String> stationNames,
+                               Map<String, PathStepDef.SectionInfo> sectionInfos,
+                               String lineName) {
+        for (int i = 0; i < stationNames.size() - 2; i++) {
+            int next = i + 1;
+            PathStepDef.SectionInfo info = sectionInfos.get(lineName);
+            SectionSteps.라인에_구간을_추가한다(lineStore.get(lineName), new SectionRequest(
+                    stationStore.get(stationNames.get(i)),
+                    stationStore.get(stationNames.get(next)),
+                    info.distance,
+                    info.duration
+            ));
         }
+    }
 
-        for (String station : 오호선_역) {
+
+    private void addStationStore(List<String> stationNames) {
+        for (String station : stationNames) {
             if (!stationStore.containsKey(station)) {
                 Long id = StationSteps.createStation(station).getId();
                 stationStore.put(station, id);
             }
-        }
-    }
-
-    public void setUpSection(Map<String, Integer> durationByLine) {
-        for (int i = 0; i < 이호선_역.size() - 2; i++) {
-            int next = i + 1;
-            SectionSteps.라인에_구간을_추가한다(lineStore.get(이호선), new SectionRequest(
-                    stationStore.get(이호선_역.get(i)),
-                    stationStore.get(이호선_역.get(next)),
-                    5,
-                    durationByLine.get("이호선")
-            ));
-        }
-
-        for (int i = 0; i < 오호선_역.size() - 2; i++) {
-            int next = i + 1;
-            SectionSteps.라인에_구간을_추가한다(lineStore.get(오호선), new SectionRequest(
-                    stationStore.get(오호선_역.get(i)),
-                    stationStore.get(오호선_역.get(next)),
-                    (5 + 2),
-                    durationByLine.get("오호선")
-            ));
         }
     }
 
@@ -86,5 +79,7 @@ public class PathAcceptanceContext {
         lineStore = new HashMap<>();
         이호선_역 = new ArrayList<>();
         오호선_역 = new ArrayList<>();
+        분당선_역 = new ArrayList<>();
     }
+
 }
