2863a9375af7b9a9837a1298b19c2b5f36879c09
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-03-06 14:43:35 +0900

    fix: 기존 추가 요금이 부가되는 거리를 관리하는 상수명을 간결한 상수명으로 수정

--- src/main/java/nextstep/core/subway/pathFinder/application/FareCalculator.java
+++ src/main/java/nextstep/core/subway/pathFinder/application/FareCalculator.java
@@ -8,25 +8,27 @@ public class FareCalculator {
     public static final int BASE_FARE = 1250;
 
     public static final int STANDARD_DISTANCE = 10;
-    public static final int STANDARD_RATE = 5;
+    public static final int STANDARD_ADDITIONAL_DISTANCE = 5;
 
     public static final int LONG_DISTANCE = 50;
-    public static final int LONG_RATE = 8;
+    public static final int LONG_ADDITIONAL_DISTANCE = 8;
+
+    public static final int ADDITIONAL_FARE_PER_KM = 100;
 
     public int calculateFare(int distance) {
         int fare = BASE_FARE;
 
         if (distance > STANDARD_DISTANCE && distance <= LONG_DISTANCE) {
-            fare += calculateOverFare(distance - STANDARD_DISTANCE, STANDARD_RATE);
+            fare += calculateOverFare(distance - STANDARD_DISTANCE, STANDARD_ADDITIONAL_DISTANCE);
         }
         if (distance > LONG_DISTANCE) {
-            fare += calculateOverFare(LONG_DISTANCE - STANDARD_DISTANCE, STANDARD_RATE);
-            fare += calculateOverFare(distance - LONG_DISTANCE, LONG_RATE);
+            fare += calculateOverFare(LONG_DISTANCE - STANDARD_DISTANCE, STANDARD_ADDITIONAL_DISTANCE);
+            fare += calculateOverFare(distance - LONG_DISTANCE, LONG_ADDITIONAL_DISTANCE);
         }
         return fare;
     }
 
-    private int calculateOverFare(int distance, int rate) {
-        return (int) (((double) ((distance - 1) / rate) + 1) * 100);
+    private int calculateOverFare(int distance, int additionalDistance) {
+        return (int) (((double) ((distance - 1) / additionalDistance) + 1) * ADDITIONAL_FARE_PER_KM);
     }
 }
