b58d1844a7a9403ac8fccf83c20a88cf8c34c871
domodazzi <domodazzi0@gmail.com>
2024-03-07 18:18:31 +0900

    feat : 노선 별 추가요금 로직 구현

--- src/main/java/nextstep/subway/applicaion/FareCalculator.java
+++ src/main/java/nextstep/subway/applicaion/FareCalculator.java
@@ -1,15 +1,18 @@
 package nextstep.subway.applicaion;
 
+import java.util.Collection;
 import nextstep.member.domain.Member;
 import nextstep.subway.domain.AgeBasedDiscountPolicy;
 import nextstep.subway.domain.DistanceBasedSurchargePolicy;
 import nextstep.subway.domain.Fare;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.vo.Path;
 
 public class FareCalculator {
 
   public Fare calculate(final Path path, final Member member) {
     final var fare = Fare.baseFare();
+    fare.addSurcharge(getLineSurcharge(path.getLines()));
     fare.addSurcharge(getDistanceSurcharge(path.getDistance()));
     fare.applyDiscount(getDiscountAmount(fare.getTotalFare(), member.getAge()));
 
@@ -20,6 +23,13 @@ public class FareCalculator {
     return DistanceBasedSurchargePolicy.getApplicablePolicies(distance).stream()
         .mapToInt(it -> it.calculate(distance))
         .sum();
+  }
+
+  private int getLineSurcharge(final Collection<Line> lines) {
+    return lines.stream()
+        .mapToInt(Line::getExtraFare)
+        .max()
+        .orElse(0);
   }
 
   private int getDiscountAmount(final int totalFare, final int age) {
