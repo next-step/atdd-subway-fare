a5018b557c3ae7f13aedf56c34b6dfc9ff4165d1
domodazzi <domodazzi0@gmail.com>
2024-03-07 17:26:51 +0900

    test : 나이 기반 할인 정책 테스트 코드 추가

--- src/test/java/nextstep/subway/domain/AgeBasedDiscountPolicyTest.java
+++ src/test/java/nextstep/subway/domain/AgeBasedDiscountPolicyTest.java
@@ -1,16 +1,100 @@
 package nextstep.subway.domain;
 
+import static nextstep.subway.domain.AgeBasedDiscountPolicy.CHILD_DISCOUNT_POLICY;
+import static nextstep.subway.domain.AgeBasedDiscountPolicy.JUVENILE_DISCOUNT_POLICY;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AgeBasedDiscountPolicyTest {
 
+  @DisplayName("추가요금 적용 대상이 아닌 경우 에러 발생")
   @Test
-  void calculate() {
+  void calculate_fail_추가요금_적용_대상_아님() {
+    // given
+    var fare = 3000;
+    var age = 30;
+
+    // when
+    var tenKilometerSurchargeResult = catchThrowable(() -> CHILD_DISCOUNT_POLICY.calculate(fare, age));
+    var fiftyKilometerSurchargeResult = catchThrowable(() -> JUVENILE_DISCOUNT_POLICY.calculate(fare, age));
+
+    // then
+    assertThat(tenKilometerSurchargeResult).isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("추가요금 적용 대상이 아닙니다.");
+    assertThat(fiftyKilometerSurchargeResult).isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("추가요금 적용 대상이 아닙니다.");
   }
 
+  @DisplayName("어린이 할인금액은 총 요금에서 350원을 제한 값의 50%이다.")
   @Test
-  void getApplicablePolicy() {
+  void calculate_어린이_할인금액() {
+    // given
+    var fare = 1250;
+    var age = 8;
+
+    // when
+    var discountAmount = CHILD_DISCOUNT_POLICY.calculate(fare, age);
+
+    // then
+    assertThat(discountAmount).isEqualTo(450);
+  }
+
+  @DisplayName("청소년 할인금액은 총 요금에서 350원을 제한 값의 20%이다.")
+  @Test
+  void calculate_청소년_할인금액() {
+    // given
+    var fare = 1650;
+    var age = 15;
+
+    // when
+    var discountAmount = JUVENILE_DISCOUNT_POLICY.calculate(fare, age);
+
+    // then
+    assertThat(discountAmount).isEqualTo(260);
+  }
+
+  @DisplayName("어린이에게 적용 가능한 정책")
+  @Test
+  void getApplicablePolicy_어린이() {
+    // given
+    int age = 7;
+
+    // when
+    var policy = AgeBasedDiscountPolicy.getApplicablePolicy(7);
+
+    // then
+    assertTrue(policy.isPresent());
+    assertThat(policy.get()).isEqualTo(CHILD_DISCOUNT_POLICY);
+  }
+
+  @DisplayName("청소년에게 적용 가능한 정책")
+  @Test
+  void getApplicablePolicy_청소년() {
+    // given
+    int age = 17;
+
+    // when
+    var policy = AgeBasedDiscountPolicy.getApplicablePolicy(age);
+
+    // then
+    assertTrue(policy.isPresent());
+    assertThat(policy.get()).isEqualTo(JUVENILE_DISCOUNT_POLICY);
+  }
+
+  @DisplayName("성인에게 적용 가능한 정책")
+  @Test
+  void getApplicablePolicy_성인() {
+    // given
+    int age = 27;
+
+    // when
+    var policy = AgeBasedDiscountPolicy.getApplicablePolicy(age);
+
+    // then
+    assertTrue(policy.isEmpty());
   }
 }
