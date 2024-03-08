c649d02daa79a10f7c9d8fec77dc477930e29ce2
ksyk1205 <ksyk1205@naver.com>
2024-03-08 20:00:29 +0900

    구간 등록시 duration 등록할 수 있도록 추가(테스트)

--- src/test/java/nextstep/cucumber/steps/LineStepDef.java
+++ src/test/java/nextstep/cucumber/steps/LineStepDef.java
@@ -31,6 +31,7 @@ public class LineStepDef implements En {
                         params.put("upStationId", ((StationResponse) context.store.get(param.get("upStation"))).getId().toString());
                         params.put("downStationId", ((StationResponse) context.store.get(param.get("downStation"))).getId().toString());
                         params.put("distance", param.get("distance"));
+                        params.put("duration", param.get("duration"));
                         ExtractableResponse<Response> response = RestAssured.given().log().all()
                                 .body(params)
                                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -51,6 +52,7 @@ public class LineStepDef implements En {
                         params.put("upStationId", ((StationResponse) context.store.get(param.get("upStation"))).getId().toString());
                         params.put("downStationId", ((StationResponse) context.store.get(param.get("downStation"))).getId().toString());
                         params.put("distance", param.get("distance"));
+                        params.put("duration", param.get("duration"));
                         LineResponse line = (LineResponse) context.store.get(lineName);
                         RestAssured.given().log().all()
                                 .body(params)
