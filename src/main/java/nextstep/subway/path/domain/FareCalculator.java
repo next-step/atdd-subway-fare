cc2cde0d5bc1a66ce76ce8e6caaacc24fc17d210
perhona <perhona@kakao.com>
2024-03-13 12:59:55 +0900

    refactor(step2): 요금 계산 로직 개선

--- src/main/java/nextstep/subway/path/domain/FareCalculator.java
+++ src/main/java/nextstep/subway/path/domain/FareCalculator.java
@@ -3,23 +3,34 @@ package nextstep.subway.path.domain;
 public class FareCalculator {
     private static final int BASIC_FARE = 1_250;
     private static final int ADDITIONAL_FARE = 100;
+    private static final int DISTANCE_UNIT_1 = 10;
+    private static final int DISTANCE_UNIT_2 = 50;
+    private static final int ADDITIONAL_FARE_UNIT_2 = 5;
+    private static final int ADDITIONAL_FARE_OVER_UNIT = 8;
+
 
     public static int calculateFare(int totalDistance) {
-        if (totalDistance < 11) {
+        if (totalDistance <= DISTANCE_UNIT_1) {
             return BASIC_FARE;
         }
 
-        int totalFare = BASIC_FARE;
-        if (totalDistance < 51) {
-            int distance = totalDistance - 10;
-            totalFare += calculateAdditionalFare(distance, 5);
-            return totalFare;
+        if (totalDistance <= DISTANCE_UNIT_2) {
+            return calculateDistanceUnit2(totalDistance);
         }
 
-        totalFare += 800;
-        int distance = totalDistance - 50;
-        totalFare += calculateAdditionalFare(distance, 8);
-        return totalFare;
+        return calculateOverDistanceUnit(totalDistance);
+    }
+
+    private static int calculateDistanceUnit2(int totalDistance) {
+        int additionalDistance = totalDistance - DISTANCE_UNIT_1;
+        int additionalFare = calculateAdditionalFare(additionalDistance, ADDITIONAL_FARE_UNIT_2);
+        return BASIC_FARE + additionalFare;
+    }
+
+    private static int calculateOverDistanceUnit(int totalDistance) {
+        int additionalDistance = totalDistance - DISTANCE_UNIT_2;
+        int additionalFare1 = calculateAdditionalFare((DISTANCE_UNIT_2 - DISTANCE_UNIT_1), DISTANCE_UNIT_1);
+        return BASIC_FARE + additionalFare1 + calculateAdditionalFare(additionalDistance, ADDITIONAL_FARE_OVER_UNIT);
     }
 
     private static int calculateAdditionalFare(int distance, int unit) {
