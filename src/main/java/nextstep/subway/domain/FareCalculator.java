faa7fee34a84949d4078e2cf9742fb886c6fd87a
hhhhhwi <aab555586@gmail.com>
2024-03-22 20:36:35 +0900

    refactor: 기본 요금 상수화

--- src/main/java/nextstep/subway/domain/FareCalculator.java
+++ src/main/java/nextstep/subway/domain/FareCalculator.java
@@ -5,18 +5,19 @@ import nextstep.member.domain.entity.Member;
 import java.util.List;
 
 public class FareCalculator {
+    private static final int BASIC_FARE = 1250;
     private final List<FarePolicy> policies;
     private int fare;
 
     public FareCalculator(Path path) {
         this.policies = List.of(new DistancePolicy(path.getDistance()), new LinePolicy(path.getSections()));
-        this.fare = 1250;
+        this.fare = BASIC_FARE;
         calculateFare();
     }
 
     public FareCalculator(Path path, Member member) {
         this.policies = List.of(new DistancePolicy(path.getDistance()), new LinePolicy(path.getSections()), new AgePolicy(member.getAge()));
-        this.fare = 1250;
+        this.fare = BASIC_FARE;
         calculateFare();
     }
 
