98e4f30c29cadd293310af51689bedc4d59abf88
Giwan Kim <giwan.kim@gmail.com>
2024-08-04 09:16:54 +0900

    refactor(station): cucumber 지하철역 인수 테스트를 리팩토링했다.

--- src/test/java/nextstep/cucumber/steps/StationStepDefinitions.java
+++ src/test/java/nextstep/cucumber/steps/StationStepDefinitions.java
@@ -1,12 +1,20 @@
 package nextstep.cucumber.steps;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import nextstep.cucumber.support.AcceptanceContext;
 import nextstep.subway.station.application.dto.StationRequest;
 import nextstep.subway.station.application.dto.StationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 @SuppressWarnings("NonAsciiCharacters")
@@ -31,5 +39,45 @@ public class StationStepDefinitions {
                   .extract();
           context.store.put(request.getName(), response.as(StationResponse.class));
         });
+  }
+
+  @When("지하철역을 생성하면")
+  public void 지하철역을_생성하면() {
+    Map<String, String> params = new HashMap<>();
+    params.put("name", "강남역");
+    context.response =
+        RestAssured.given()
+            .log()
+            .all()
+            .body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/stations")
+            .then()
+            .log()
+            .all()
+            .extract();
+  }
+
+  @Then("지하철역이 생성된다")
+  public void 지하철역이_생성된다() {
+    assertThat(context.response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+  }
+
+  @Then("지하철역 목록 조회 시 생성한 역을 찾을 수 있다")
+  public void 지하철역_목록_조회_시_생성한_역을_찾을_수_있다() {
+    List<String> stationNames =
+        RestAssured.given()
+            .log()
+            .all()
+            .when()
+            .get("/stations")
+            .then()
+            .log()
+            .all()
+            .extract()
+            .jsonPath()
+            .getList("name", String.class);
+    assertThat(stationNames).containsAnyOf("강남역");
   }
 }
