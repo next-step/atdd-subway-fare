348cfee28736de4496a2f42fa6a496152e54c3f1
손정운 <jungwoon571@gmail.com>
2024-09-20 09:49:50 +0900

    refactor: 요금 단위 테스트 개선

--- src/test/java/nextstep/path/unit/FareTest.java
+++ src/test/java/nextstep/path/unit/FareTest.java
@@ -17,7 +17,7 @@ public class FareTest {
 
     private static final long BASE_FARE = 1250L;
 
-    @DisplayName("요금 생성 함수는, 1미만의 거리가 입력되면 예외를 발생한다.")
+    @DisplayName("요금 생성 함수는, 1미만의 거리가 입력되면 예외가 발생한다.")
     @Test
     void createFareExceptionTest() {
         // when
@@ -27,36 +27,44 @@ public class FareTest {
         assertThatThrownBy(actual).isInstanceOf(LessThanMinimumDistanceException.class);
     }
 
-    @DisplayName("요금 생성 함수는, 거리 10이하 가 주어지면 기본운임을 가진다.")
-    @ValueSource(ints = {1, 10})
+    @DisplayName("요금 생성 함수는, 거리 1에서 10까지는 기본 요금을 생성한다.")
+    @ValueSource(ints = {1, 5, 10})
     @ParameterizedTest
     void calculateFareTest(int distance) {
-        // when
-        Fare fare = Fare.from(distance);
-
-        // then
-        assertThat(fare).isEqualTo(new Fare(BASE_FARE));
+        assertFareForDistance(distance, BASE_FARE);
     }
 
-    @DisplayName("요금 조회 함수는, 거리 10초과 50이하는 기본운임 + 초과된 거리 5마다 100이 추가된 요금을 반환한다.")
-    @CsvSource({"11,1350", "15,1350", "16,1450", "50,2050"})
+    @DisplayName("요금 생성 함수는, 거리 11에서 50까지는 초과된 거리 5마다 100원이 추가된 요금을 생성한다.")
+    @CsvSource({
+            "11, 1350",
+            "15, 1350",
+            "16, 1450",
+            "50, 2050"
+    })
     @ParameterizedTest
     void calculateAdditionalTest(int distance, Long expected) {
-        // when
-        Fare fare = Fare.from(distance);
-
-        // then
-        assertThat(fare.getFare()).isEqualTo(new Fare(expected).getFare());
+        assertFareForDistance(distance, expected);
     }
 
-    @DisplayName("요금 조회 함수는, 거리 50초과는 50이하 요금 + 초과된 거리 8마다 100이 추가된 요금을 반환한다.")
-    @CsvSource({"51,2150", "58,2150", "59,2250", "66,2250", "67,2350"})
+    @DisplayName("요금 생성 함수는, 거리 50초과는 50까지 계산된 요금에 초과된 거리 8마다 100원이 추가된 요금을 생성한다.")
+    @CsvSource({
+            "51, 2150",
+            "58, 2150",
+            "59, 2250",
+            "66, 2250",
+            "67, 2350"
+    })
     @ParameterizedTest
     void calculateReducedAdditionalFare(int distance, Long expected) {
+        assertFareForDistance(distance, expected);
+    }
+
+    // 중복된 요금 검증 로직 분리
+    private void assertFareForDistance(int distance, Long expectedFare) {
         // when
         Fare fare = Fare.from(distance);
 
         // then
-        assertThat(fare).isEqualTo(new Fare(expected));
+        assertThat(fare).isEqualTo(new Fare(expectedFare));
     }
 }
